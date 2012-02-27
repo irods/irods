e056a8eba9cd7a2a118027d53b68dc9dfb4bbeac
Terrell Russell <terrellrussell@gmail.com>
2012-02-27 16:25:17 -0500

    spelling and spacing tweaks

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -3,39 +3,39 @@
 # =-=-=-=-=-=-=-
 # clean up any stray irods files in /tmp which will cause problems
 if [ -f /tmp/irodsServer.* ]; then
-	rm /tmp/irodsServer.*
+  rm /tmp/irodsServer.*
 fi
 
 # =-=-=-=-=-=-=-
-# determine if the service account already exits
+# determine if the service account already exists
 USER=$( grep $5 /etc/passwd )
 if [ -n "$USER" ]; then 
-	echo "WARNING :: Service Account $5 Already Exists."
+  echo "WARNING :: Service Account $5 Already Exists."
 else
-	# =-=-=-=-=-=-=-
- 	# create the service account
-	echo "Creating Service Account: $5 At $9"
-	useradd -m -d $9 $5
-	chown $5:$5 $9
+  # =-=-=-=-=-=-=-
+  # create the service account
+  echo "Creating Service Account: $5 At $9"
+  useradd -m -d $9 $5
+  chown $5:$5 $9
 fi
 
 # =-=-=-=-=-=-=-
-# determine if the database role already exits
+# determine if the database role already exists
 ROLE=$(sudo -u $7 psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$5'")
 if [ $ROLE ]; then
-	echo "WARNING :: Role $5 Already Exists in Database."
+  echo "WARNING :: Role $5 Already Exists in Database."
 else
-	# =-=-=-=-=-=-=-
-	# create the database role
-	echo "Creating Data Base Role: $5 As $7"
-	sudo -u $7 createuser -s $5
+  # =-=-=-=-=-=-=-
+  # create the database role
+  echo "Creating Database Role: $5 As $7"
+  sudo -u $7 createuser -s $5
 fi
 
 # =-=-=-=-=-=-=-
-# determine if the database already exits
+# determine if the database already exists
 DB=$(sudo -u $7 psql --list  | grep $8 )
 if [ -n "$DB" ]; then
-	echo "WARNING :: Database $8 Already Exits"
+  echo "WARNING :: Database $8 Already Exists"
 fi
 
 # =-=-=-=-=-=-=-
@@ -55,6 +55,4 @@ sudo -H -u $5 perl ./scripts/perl/eirods_setup.pl $2 $3 $4 $5 $6
 # =-=-=-=-=-=-=-
 # exit with success
 exit 0
-
-
 
--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -7,27 +7,27 @@ echo "Stopping iRODS :: $1/irodsctl stop"
 sudo -u $2 $1/irodsctl istop
 
 # =-=-=-=-=-=-=-
-# determine if the database already exits
+# determine if the database already exists
 DB=$(sudo -u $3 psql --list  | grep $4 )
 if [ -n "$DB" ]; then
-	echo "Removing Database $4"
-	sudo -u $3 dropdb $4
+  echo "Removing Database $4"
+  sudo -u $3 dropdb $4
 fi
 
 # =-=-=-=-=-=-=-
-# determine if the database role already exits
+# determine if the database role already exists
 ROLE=$(sudo -u $3 psql postgres -tAc "SELECT 1 FROM pg_roles WHERE rolname='$2'")
 if [ $ROLE ]; then
-	echo "Removing Database Role $2"
-	sudo -u $3 dropuser $2
+  echo "Removing Database Role $2"
+  sudo -u $3 dropuser $2
 fi
 
 # =-=-=-=-=-=-=-
-# determine if the service account already exits
+# determine if the service account already exists
 USER=$( grep $2 /etc/passwd )
 if [ -n "$USER" ]; then 
-	echo "Removing Service Account $2"
-	deluser $2
+  echo "Removing Service Account $2"
+  deluser $2
 fi
 
 # =-=-=-=-=-=-=-
