068c216e9f00e6175207fbdc169ca7ca1527a6ad
Terrell Russell <terrellrussell@gmail.com>
2012-02-28 14:45:37 -0500

    install and then remove irodsctl and icommands symlinks in /usr/bin

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -76,6 +76,59 @@ cd $1
 sudo -H -u $5 perl ./scripts/perl/eirods_setup.pl $2 $3 $4 $5 $6
 
 # =-=-=-=-=-=-=-
+# symlink irodsctl and the icommands
+cd $1
+ln -s ./irodsctl                             /usr/bin/irodsctl
+ln -s ./clients/icommands/bin/genOSAuth      /usr/bin/genOSAuth
+ln -s ./clients/icommands/bin/iadmin         /usr/bin/iadmin
+ln -s ./clients/icommands/bin/ibun           /usr/bin/ibun
+ln -s ./clients/icommands/bin/icd            /usr/bin/icd
+ln -s ./clients/icommands/bin/ichksum        /usr/bin/ichksum
+ln -s ./clients/icommands/bin/ichmod         /usr/bin/ichmod
+ln -s ./clients/icommands/bin/icp            /usr/bin/icp
+ln -s ./clients/icommands/bin/idbo           /usr/bin/idbo
+ln -s ./clients/icommands/bin/idbug          /usr/bin/idbug
+ln -s ./clients/icommands/bin/ienv           /usr/bin/ienv
+ln -s ./clients/icommands/bin/ierror         /usr/bin/ierror
+ln -s ./clients/icommands/bin/iexecmd        /usr/bin/iexecmd
+ln -s ./clients/icommands/bin/iexit          /usr/bin/iexit
+ln -s ./clients/icommands/bin/ifsck          /usr/bin/ifsck
+ln -s ./clients/icommands/bin/iget           /usr/bin/iget
+ln -s ./clients/icommands/bin/igetwild.sh    /usr/bin/igetwild.sh
+ln -s ./clients/icommands/bin/ihelp          /usr/bin/ihelp
+ln -s ./clients/icommands/bin/iinit          /usr/bin/iinit
+ln -s ./clients/icommands/bin/ilocate        /usr/bin/ilocate
+ln -s ./clients/icommands/bin/ils            /usr/bin/ils
+ln -s ./clients/icommands/bin/ilsresc        /usr/bin/ilsresc
+ln -s ./clients/icommands/bin/imcoll         /usr/bin/imcoll
+ln -s ./clients/icommands/bin/imeta          /usr/bin/imeta
+ln -s ./clients/icommands/bin/imiscsvrinfo   /usr/bin/imiscsvrinfo
+ln -s ./clients/icommands/bin/imkdir         /usr/bin/imkdir
+ln -s ./clients/icommands/bin/imv            /usr/bin/imv
+ln -s ./clients/icommands/bin/ipasswd        /usr/bin/ipasswd
+ln -s ./clients/icommands/bin/iphybun        /usr/bin/iphybun
+ln -s ./clients/icommands/bin/iphymv         /usr/bin/iphymv
+ln -s ./clients/icommands/bin/ips            /usr/bin/ips
+ln -s ./clients/icommands/bin/iput           /usr/bin/iput
+ln -s ./clients/icommands/bin/ipwd           /usr/bin/ipwd
+ln -s ./clients/icommands/bin/iqdel          /usr/bin/iqdel
+ln -s ./clients/icommands/bin/iqmod          /usr/bin/iqmod
+ln -s ./clients/icommands/bin/iqstat         /usr/bin/iqstat
+ln -s ./clients/icommands/bin/iquest         /usr/bin/iquest
+ln -s ./clients/icommands/bin/iquota         /usr/bin/iquota
+ln -s ./clients/icommands/bin/ireg           /usr/bin/ireg
+ln -s ./clients/icommands/bin/irepl          /usr/bin/irepl
+ln -s ./clients/icommands/bin/irm            /usr/bin/irm
+ln -s ./clients/icommands/bin/irmtrash       /usr/bin/irmtrash
+ln -s ./clients/icommands/bin/irsync         /usr/bin/irsync
+ln -s ./clients/icommands/bin/irule          /usr/bin/irule
+ln -s ./clients/icommands/bin/iscan          /usr/bin/iscan
+ln -s ./clients/icommands/bin/isysmeta       /usr/bin/isysmeta
+ln -s ./clients/icommands/bin/itrim          /usr/bin/itrim
+ln -s ./clients/icommands/bin/iuserinfo      /usr/bin/iuserinfo
+ln -s ./clients/icommands/bin/ixmsg          /usr/bin/ixmsg
+
+# =-=-=-=-=-=-=-
 # exit with success
 exit 0
 
--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -30,6 +30,8 @@ if [ -n "$USER" ]; then
   deluser $2
 fi
 
+# =-=-=-=-=-=-=-
+# remove runlevel symlinks
 rm /etc/init.d/e-irods
 rm /etc/rc0.d/K15e-irods
 rm /etc/rc2.d/S95e-irods
@@ -37,6 +39,58 @@ rm /etc/rc3.d/S95e-irods
 rm /etc/rc4.d/S95e-irods
 rm /etc/rc5.d/S95e-irods
 rm /etc/rc6.d/K15e-irods
+
+# =-=-=-=-=-=-=-
+# remove irodsctl and icommands symlinks
+rm /usr/bin/irodsctl
+rm /usr/bin/genOSAuth
+rm /usr/bin/iadmin
+rm /usr/bin/ibun
+rm /usr/bin/icd
+rm /usr/bin/ichksum
+rm /usr/bin/ichmod
+rm /usr/bin/icp
+rm /usr/bin/idbo
+rm /usr/bin/idbug
+rm /usr/bin/ienv
+rm /usr/bin/ierror
+rm /usr/bin/iexecmd
+rm /usr/bin/iexit
+rm /usr/bin/ifsck
+rm /usr/bin/iget
+rm /usr/bin/igetwild.sh
+rm /usr/bin/ihelp
+rm /usr/bin/iinit
+rm /usr/bin/ilocate
+rm /usr/bin/ils
+rm /usr/bin/ilsresc
+rm /usr/bin/imcoll
+rm /usr/bin/imeta
+rm /usr/bin/imiscsvrinfo
+rm /usr/bin/imkdir
+rm /usr/bin/imv
+rm /usr/bin/ipasswd
+rm /usr/bin/iphybun
+rm /usr/bin/iphymv
+rm /usr/bin/ips
+rm /usr/bin/iput
+rm /usr/bin/ipwd
+rm /usr/bin/iqdel
+rm /usr/bin/iqmod
+rm /usr/bin/iqstat
+rm /usr/bin/iquest
+rm /usr/bin/iquota
+rm /usr/bin/ireg
+rm /usr/bin/irepl
+rm /usr/bin/irm
+rm /usr/bin/irmtrash
+rm /usr/bin/irsync
+rm /usr/bin/irule
+rm /usr/bin/iscan
+rm /usr/bin/isysmeta
+rm /usr/bin/itrim
+rm /usr/bin/iuserinfo
+rm /usr/bin/ixmsg
 
 # =-=-=-=-=-=-=-
 # exit with success
