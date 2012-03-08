fdcab81fa773ffb413c1e4da9a414897bc80ba6b
Terrell Russell <terrellrussell@gmail.com>
2012-03-08 11:36:18 -0500

    build.sh knows where it lives now

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-pushd iRODS
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DIR/../iRODS
 export EIRODSPOSTGRESVERSION=`\`which psql\` --version | head -n1 | awk '{print $3}' | cut -d'.' -f1,2`
 echo "Detecting PostgreSQL Version: [$EIRODSPOSTGRESVERSION]"
 sed -e s/EIRODSPOSTGRESVERSION/$EIRODSPOSTGRESVERSION/ ../packaging/irods.config.epm > /tmp/irods.config.epm
@@ -9,5 +10,4 @@ cp /tmp/irods.config.epm ./config/irods.config
 ./scripts/configure
 cp /tmp/irods.config.epm ./config/irods.config
 make
-popd
 
