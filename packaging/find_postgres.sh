6d23d137dd832daccb6e443e09961bad7334c5a0
Terrell Russell <terrellrussell@gmail.com>
2012-03-28 12:35:14 -0400

    don't display intermediate results

--- packaging/find_postgres.sh
+++ packaging/find_postgres.sh
@@ -7,7 +7,7 @@ cd $newdir
 
 while [[ -L $result ]]; do
     link=$(ls -l $result | awk '/->/ {print $NF}');
-    echo "$result pointing to $link"
+#    echo "$result pointing to $link"
     newdir=$(dirname $link)
     filename=$(basename $link)
     cd $newdir
