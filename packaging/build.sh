4910581847e5dba68d158c27c2a495d5f7353c01
Terrell Russell <terrellrussell@gmail.com>
2012-03-28 12:05:17 -0400

    build.sh needs to be icat or resource aware, building a resource server requires the ICAT IP address as second parameter

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,18 +1,62 @@
 #!/bin/bash
 
+SCRIPTNAME=`basename $0`
+
+# check arguments
+if [ $# -ne 1 -a $# -ne 2 ] ; then
+  echo "Usage: $SCRIPTNAME icat   OR   $SCRIPTNAME resource {icatip}"
+  exit 1
+fi
+
+if [ $1 != "icat" -a $1 != "resource" ] ; then
+  echo "Usage: $SCRIPTNAME icat   OR   $SCRIPTNAME resource {icatip}"
+  exit 1
+fi
+
+if [ $1 == "icat" -a $# -eq 2 ] ; then
+  echo "Usage: $SCRIPTNAME icat   OR   $SCRIPTNAME resource {icatip}"
+  exit 1
+fi
+
+if [ $1 == "resource" -a $# -eq 1 ] ; then
+  echo "Usage: $SCRIPTNAME icat   OR   $SCRIPTNAME resource {icatip}"
+  exit 1
+fi
+
+# get into the correct directory
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../iRODS
 
 
-export EIRODSPOSTGRESPATH=`../packaging/find_postgres.sh | sed -e s,\/[^\/]*$,, -e s,\/[^\/]*$,,`
-export EIRODSPOSTGRESPATH="$EIRODSPOSTGRESPATH/"
-echo "Detecting PostgreSQL Path: [$EIRODSPOSTGRESPATH]"
+# set up variables for icat configuration
+if [ $1 == "icat" ] ; then
+  EPMFILE=../packaging/irods.config.icat.epm
+  EIRODSPOSTGRESPATH=`../packaging/find_postgres.sh | sed -e s,\/[^\/]*$,, -e s,\/[^\/]*$,,`
+  EIRODSPOSTGRESPATH="$EIRODSPOSTGRESPATH/"
+  echo "Detecting PostgreSQL Path: [$EIRODSPOSTGRESPATH]"
 
-sed -e s,EIRODSPOSTGRESPATH,$EIRODSPOSTGRESPATH, ../packaging/irods.config.epm > /tmp/irods.config.epm
+  sed -e s,EIRODSPOSTGRESPATH,$EIRODSPOSTGRESPATH, $EPMFILE > /tmp/irods.config.epm
 
+
+# set up variables for resource configuration
+else
+
+  EPMFILE=../packaging/irods.config.resource.epm
+  ICATIP=$2
+
+  sed -e s,REMOTEICATIPADDRESS,$ICATIP, $EPMFILE > /tmp/irods.config.epm
+
+fi
+
+
+
+# run configure to create Makefile, config.mk, platform.mk, etc.
 ./scripts/configure
+# overwrite with our values
 cp /tmp/irods.config.epm ./config/irods.config
+# run with our updated irods.config
 ./scripts/configure
+# again to reset IRODS_HOME
 cp /tmp/irods.config.epm ./config/irods.config
-make
-
+# go!
+make -j
