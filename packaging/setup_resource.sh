9ac56d03a0b2390601b04437d22c417a44798b78
Terrell Russell <terrellrussell@gmail.com>
2012-03-29 10:41:31 -0400

    allow for multiple rounds of input

--- packaging/setup_resource.sh
+++ packaging/setup_resource.sh
@@ -1,25 +1,76 @@
 #!/bin/bash
 
+STATUS="firstpass"
 echo "==================================================================="
 echo ""
 echo "You have installed an E-iRODS resource server.  Resource servers"
 echo "cannot be started until they have been properly configured to"
 echo "communicate with a live iCAT server."
 echo ""
-echo -n "iCAT server's hostname or IP address: "
-read ICATHOSTORIP
-echo ""
-echo -n "iCAT server's port [1247]: "
-read ICATPORT
-if [ "$ICATPORT" == "" ]; then
-  ICATPORT=1247
-fi
-echo ""
-echo -n "iCAT server's ZoneName: "
-read ICATZONE
-echo ""
-echo "-------------------"
-echo "Hostname or IP: [$ICATHOSTORIP]"
-echo "iCAT Port:      [$ICATPORT]"
-echo "iCAT Zone:      [$ICATZONE]"
+while [ "$STATUS" != "complete" ] ; do
+
+  # set default values from an earlier loop
+  if [ "$STATUS" != "firstpass" ] ; then
+    LASTICATHOSTORIP=$ICATHOSTORIP
+    LASTICATPORT=$ICATPORT
+    LASTICATZONE=$ICATZONE
+  fi
+
+  # get host
+  echo -n "iCAT server's hostname or IP address"
+  if [ "$LASTICATHOSTORIP" ] ; then echo -n " [$LASTICATHOSTORIP]"; fi
+  echo -n ": "
+  read ICATHOSTORIP
+  if [ "$ICATHOSTORIP" == "" ] ; then
+    if [ "$LASTICATHOSTORIP" ] ; then ICATHOSTORIP=$LASTICATHOSTORIP; fi
+  fi
+  echo ""
+
+  # get port
+  echo -n "iCAT server's port"
+  if [ "$LASTICATPORT" ] ; then
+    echo -n " [$LASTICATPORT]"
+  else
+    echo -n " [1247]"
+  fi
+  echo -n ": "
+  read ICATPORT
+  if [ "$ICATPORT" == "" ] ; then
+    if [ "$LASTICATPORT" ] ; then
+      ICATPORT=$LASTICATPORT
+    else
+      ICATPORT=1247
+    fi
+  fi
+  echo ""
+
+  # get zone
+  echo -n "iCAT server's ZoneName"
+  if [ "$LASTICATZONE" ] ; then echo -n " [$LASTICATZONE]"; fi
+  echo -n ": "
+  read ICATZONE
+  if [ "$ICATZONE" == "" ] ; then
+    if [ "$LASTICATZONE" ] ; then ICATZONE=$LASTICATZONE; fi
+  fi
+  echo ""
+
+  # confirm
+  echo "-------------------------------------------"
+  echo "Hostname or IP:   $ICATHOSTORIP"
+  echo "iCAT Port:        $ICATPORT"
+  echo "iCAT Zone:        $ICATZONE"
+  echo "-------------------------------------------"
+  echo -n "Please confirm these settings [yes]: "
+  read CONFIRM
+  if [ "$CONFIRM" == "" -o "$CONFIRM" == "y" -o "$CONFIRM" == "Y" -o "$CONFIRM" == "yes" ]; then
+    STATUS="complete"
+  else
+    STATUS="loop"
+  fi
+  echo ""
+  echo ""
+
+done
 echo "==================================================================="
+
+echo "Now we are updating irods.config..."
