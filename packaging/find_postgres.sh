80d179ce895f2d67c544f434b5e7b8d7b5a8ecdf
Jason Coposky <jasonc@renci.org>
2012-04-05 17:02:16 -0400

    [#701] finds system level postgres directory for ubuntu and centos

--- packaging/find_postgres.sh
+++ packaging/find_postgres.sh
@@ -1,19 +1,5 @@
 #!/bin/bash
 
-result=$(which psql)
-
-newdir=$(dirname $result)
-cd $newdir
-
-while [[ -L $result ]]; do
-    link=$(ls -l $result | awk '/->/ {print $NF}');
-#    echo "$result pointing to $link"
-    newdir=$(dirname $link)
-    filename=$(basename $link)
-    cd $newdir
-    fulldir=$(pwd)
-    result=$fulldir/$filename
-done
-
+# locates system postgres on centos and ubuntu
+result=$(locate plpgsql.so | grep usr)
 echo $result
-
