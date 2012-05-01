0191be8b906c3370edd973e949de6db39fa86586
Terrell Russell <terrellrussell@gmail.com>
2012-05-01 15:57:38 -0400

    [#737] second attempt at the correct ftp location - no version needed in path, auto generated

--- iRODS/scripts/perl/installPostgres.pl
+++ iRODS/scripts/perl/installPostgres.pl
@@ -351,7 +351,7 @@ $POSTGRES_FTP_HOST             = "ftp5.us.postgresql.org";
 #$POSTGRES_FTP_HOST             = "ftp10.us.postgresql.org";
 
 # RENCI updates for E-iRODS testing
-$POSTGRES_FTP_POSTGRES_DIR     = "pub/source/v9.0.3";
+$POSTGRES_FTP_POSTGRES_DIR     = "pub/source";
 $POSTGRES_FTP_ODBC_DIR         = "pub/odbc/versions/src";
 $POSTGRES_FTP_HOST             = "ftp.postgresql.org";
 
