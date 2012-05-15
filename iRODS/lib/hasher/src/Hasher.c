706e5009bae846f4aae79559c496369a263b05a4
Harry Johnson <hcjiv1@renci.org>
2012-05-14 23:24:46 -0400

    [#719] Created an MD5Strategy for creating md5 hashes.

--- iRODS/lib/hasher/src/Hasher.c
+++ iRODS/lib/hasher/src/Hasher.c
@@ -1,4 +1,4 @@
-/* -*- mode: c++; fill-column: 72; c-basic-offset: 2; indent-tabs-mode: nil -*- */
+/* -*- mode: c++; fill-column: 72; c-basic-offset: 4; indent-tabs-mode: nil -*- */
 
 
 #include "Hasher.h"
@@ -6,90 +6,90 @@
 Hasher::
 Hasher(void)
 {
-  // empty
+    // empty
 }
 
 Hasher::
 ~Hasher(void)
 {
-  for(vector<HashStrategy*>::iterator it = _strategies.begin(); it != _strategies.end(); ++it) {
-    HashStrategy* strategy = *it;
-    delete strategy;
-  }
+    for(std::vector<HashStrategy*>::iterator it = _strategies.begin(); it != _strategies.end(); ++it) {
+        HashStrategy* strategy = *it;
+        delete strategy;
+    }
 }
 
 unsigned int Hasher::
 listStrategies(
-  vector<string>& strategies) const
+    std::vector<std::string>& strategies) const
 {
-  unsigned int result = 0;
-  for(vector<HashStrategy*>::const_iterator it = _strategies.begin(); it != _strategies.end(); ++it) {
-    HashStrategy* strategy = *it;
-    strategies.push_back(strategy->name());
-  }
-  return result;
+    unsigned int result = 0;
+    for(std::vector<HashStrategy*>::const_iterator it = _strategies.begin(); it != _strategies.end(); ++it) {
+        HashStrategy* strategy = *it;
+        strategies.push_back(strategy->name());
+    }
+    return result;
 }
 
 unsigned int Hasher::
 init(void)
 {
-  unsigned int result = 0;
-  for(vector<HashStrategy*>::iterator it = _strategies.begin();
-      result == 0 && it != _strategies.end();
-      ++it) {
-    HashStrategy* strategy = *it;
-    result = strategy->init();
-  }
-  return result;
+    unsigned int result = 0;
+    for(std::vector<HashStrategy*>::iterator it = _strategies.begin();
+        result == 0 && it != _strategies.end();
+        ++it) {
+        HashStrategy* strategy = *it;
+        result = strategy->init();
+    }
+    return result;
 }
 
 unsigned int Hasher::
 update(
-  const string& data)
+    const std::string& data)
 {
-  unsigned int result = 0;
-  for(vector<HashStrategy*>::iterator it = _strategies.begin();
-      result == 0 && it != _strategies.end();
-      ++it) {
-    HashStrategy* strategy = *it;
-    result = strategy->update(data);
-  }
-  return result;
+    unsigned int result = 0;
+    for(std::vector<HashStrategy*>::iterator it = _strategies.begin();
+        result == 0 && it != _strategies.end();
+        ++it) {
+        HashStrategy* strategy = *it;
+        result = strategy->update(data);
+    }
+    return result;
 }
 
 unsigned int Hasher::
 digest(
-  const string& name,
-  string& messageDigest)
+    const std::string& name,
+    std::string& messageDigest)
 {
-  unsigned result = 0;
-  bool found = false;
-  for(vector<HashStrategy*>::iterator it = _strategies.begin();
-      !found && it != _strategies.end();
-      ++it) {
-    HashStrategy* strategy = *it;
-    if(name == strategy->name()) {
-      found = true;
-      result = strategy->digest(messageDigest);
+    unsigned result = 0;
+    bool found = false;
+    for(std::vector<HashStrategy*>::iterator it = _strategies.begin();
+        !found && it != _strategies.end();
+        ++it) {
+        HashStrategy* strategy = *it;
+        if(name == strategy->name()) {
+            found = true;
+            result = strategy->digest(messageDigest);
+        }
     }
-  }
-  return result;
+    return result;
 }
 
 unsigned int Hasher::
 catDigest(
-  string& messageDigest)
+    std::string& messageDigest)
 {
-  unsigned int result = 0;
-  for(vector<HashStrategy*>::iterator it = _strategies.begin();
-      result == 0 && it != _strategies.end();
-      ++it) {
-    HashStrategy* strategy = *it;
-    string tmpDigest;
-    result = strategy->digest(tmpDigest);
-    if(result == 0) {
-      messageDigest += tmpDigest;
+    unsigned int result = 0;
+    for(std::vector<HashStrategy*>::iterator it = _strategies.begin();
+        result == 0 && it != _strategies.end();
+        ++it) {
+        HashStrategy* strategy = *it;
+        std::string tmpDigest;
+        result = strategy->digest(tmpDigest);
+        if(result == 0) {
+            messageDigest += tmpDigest;
+        }
     }
-  }
-  return result;
+    return result;
 }
