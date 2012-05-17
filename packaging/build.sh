917aaf51aabc9442768c5c00cf07c95167373617
Terrell Russell <terrellrussell@gmail.com>
2012-05-17 09:13:10 -0400

    [#780] refactor build.sh for better output and skipping the E-iRODS make stage.

--- packaging/build.sh
+++ packaging/build.sh
@@ -1,22 +1,59 @@
 #!/bin/bash
 
 SCRIPTNAME=`basename $0`
+BUILDEIRODS="1"
+
+#read -d '' USAGE <<"EOF"
+USAGE="
+E-iRODS Build Script
+
+Usage: $SCRIPTNAME [OPTIONS] <serverType> [databaseType]
+
+Options:
+ -s      Skip compilation of E-iRODS source.
+
+Examples:
+ $SCRIPTNAME icat postgres
+ $SCRIPTNAME resource
+ $SCRIPTNAME -s icat postgres
+ $SCRIPTNAME -s resource
+"
+
+while getopts ":hs" opt; do
+  case $opt in
+    h)
+      echo "$USAGE"
+      ;;
+    s)
+      BUILDEIRODS="0"
+      echo "-s detected -- Skipping E-iRODS compilation"
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
+
+# remove options from $@
+shift $((OPTIND-1))
 
 # check arguments
 if [ $# -ne 1 -a $# -ne 2 ] ; then
-  echo "Usage: $SCRIPTNAME icat {databasetype}   OR   $SCRIPTNAME resource" 1>&2
+  echo "$USAGE" 1>&2
   exit 1
 fi
 
 if [ $1 != "icat" -a $1 != "resource" ] ; then
-  echo "Usage: $SCRIPTNAME icat {databasetype}   OR   $SCRIPTNAME resource" 1>&2
+    echo "ERROR :: Invalid serverType [$1]" 1>&2
+    echo "      :: Only 'icat' or 'resource' available at this time" 1>&2
+    exit 1
   exit 1
 fi
 
 if [ "$1" == "icat" ]; then
-#  if [ $2 != "postgres" -a $2 != "mysql" ]
-  if [ $2 != "postgres" ]; then
-    echo "ERROR :: Invalid iCAT database type [$2]" 1>&2
+#  if [ "$2" != "postgres" -a "$2" != "mysql" ]
+  if [ "$2" != "postgres" ]; then
+    echo "ERROR :: Invalid iCAT databaseType [$2]" 1>&2
     echo "      :: Only 'postgres' available at this time" 1>&2
     exit 1
   fi
@@ -55,6 +92,7 @@ if [ "$?" -ne "0" ]; then
 fi
 
 
+
 # get into the correct directory 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $DIR/../iRODS
@@ -76,39 +114,40 @@ if [ $1 == "icat" ] ; then
     DB_TYPE=$2
     EPMFILE="../packaging/irods.config.icat.epm"
 
-    # =-=-=-=-=-=-=-
-    # bake SQL files for different database types
-    # NOTE:: icatSysInserts.sql is handled by the packager as we rely on the default zone name
-    serverSqlDir="./server/icat/src"
-    convertScript="$serverSqlDir/convertSql.pl"
+    if [ "$BUILDEIRODS" == "1" ]; then
+        # =-=-=-=-=-=-=-
+        # bake SQL files for different database types
+        # NOTE:: icatSysInserts.sql is handled by the packager as we rely on the default zone name
+        serverSqlDir="./server/icat/src"
+        convertScript="$serverSqlDir/convertSql.pl"
 
-    echo "Converting SQL: [$convertScript] [$DB_TYPE] [$serverSqlDir]"
-    `perl $convertScript $2 $serverSqlDir &> /dev/null`
-	if [ "$?" -ne "0" ]; then
-        echo "Failed to convert SQL forms" 1>&2
-	    exit 1
-	fi
+        echo "Converting SQL: [$convertScript] [$DB_TYPE] [$serverSqlDir]"
+        `perl $convertScript $2 $serverSqlDir &> /dev/null`
+        if [ "$?" -ne "0" ]; then
+            echo "Failed to convert SQL forms" 1>&2
+            exit 1
+        fi
 
-    # =-=-=-=-=-=-=-
-    # insert postgres path into list file
-    if [ "$DB_TYPE" == "postgres" ] ; then
-        # need to do a dirname here, as the irods.config is expected to have a path
-        # which will be appended with a /bin
-        EIRODSPOSTGRESPATH=`../packaging/find_postgres_bin.sh`
-        EIRODSPOSTGRESPATH=`dirname $EIRODSPOSTGRESPATH`
-        EIRODSPOSTGRESPATH="$EIRODSPOSTGRESPATH/"
+        # =-=-=-=-=-=-=-
+        # insert postgres path into list file
+        if [ "$DB_TYPE" == "postgres" ] ; then
+            # need to do a dirname here, as the irods.config is expected to have a path
+            # which will be appended with a /bin
+            EIRODSPOSTGRESPATH=`../packaging/find_postgres_bin.sh`
+            EIRODSPOSTGRESPATH=`dirname $EIRODSPOSTGRESPATH`
+            EIRODSPOSTGRESPATH="$EIRODSPOSTGRESPATH/"
 
-        echo "Detecting PostgreSQL Path: [$EIRODSPOSTGRESPATH]"
-        sed -e s,EIRODSPOSTGRESPATH,$EIRODSPOSTGRESPATH, $EPMFILE > $TMPCONFIGFILE
-    else
-        echo "TODO: irods.config for DBTYPE other than postgres"
+            echo "Detecting PostgreSQL Path: [$EIRODSPOSTGRESPATH]"
+            sed -e s,EIRODSPOSTGRESPATH,$EIRODSPOSTGRESPATH, $EPMFILE > $TMPCONFIGFILE
+        else
+            echo "TODO: irods.config for DBTYPE other than postgres"
+        fi
     fi
 # set up variables for resource configuration
 else
-
-  SERVER_TYPE="RESOURCE"
-  EPMFILE="../packaging/irods.config.resource.epm"
-  cp $EPMFILE $TMPCONFIGFILE
+    SERVER_TYPE="RESOURCE"
+    EPMFILE="../packaging/irods.config.resource.epm"
+    cp $EPMFILE $TMPCONFIGFILE
 fi
 
 
@@ -116,83 +155,86 @@ fi
 # generate random password for database
 RANDOMDBPASS=`cat /dev/urandom | base64 | head -c15`
 
-# =-=-=-=-=-=-=-
-# run configure to create Makefile, config.mk, platform.mk, etc.
-./scripts/configure
-# overwrite with our values
-cp $TMPCONFIGFILE ./config/irods.config
-# run with our updated irods.config
-./scripts/configure
-# again to reset IRODS_HOME
-cp $TMPCONFIGFILE ./config/irods.config
+if [ "$BUILDEIRODS" == "1" ]; then
+    # =-=-=-=-=-=-=-
+    # run configure to create Makefile, config.mk, platform.mk, etc.
+    ./scripts/configure
+    # overwrite with our values
+    cp $TMPCONFIGFILE ./config/irods.config
+    # run with our updated irods.config
+    ./scripts/configure
+    # again to reset IRODS_HOME
+    cp $TMPCONFIGFILE ./config/irods.config
 
-# change password for database to be consistent with that within the e-irods.list file 
-# for installation
-sed -e "s,SOMEPASSWORD,$RANDOMDBPASS," ./config/irods.config > /tmp/irods.config
-mv /tmp/irods.config ./config/irods.config
+    # change password for database to be consistent with that within the e-irods.list file 
+    # for installation
+    sed -e "s,SOMEPASSWORD,$RANDOMDBPASS," ./config/irods.config > /tmp/irods.config
+    mv /tmp/irods.config ./config/irods.config
 
-# handle issue with IRODS_HOME being overwritten by the configure script    
-sed -e "\,^IRODS_HOME,s,^.*$,IRODS_HOME=\`./scripts/find_irods_home.sh\`," ./irodsctl > /tmp/irodsctl.tmp
-mv /tmp/irodsctl.tmp ./irodsctl
-chmod 755 ./irodsctl
-
-
-
-# =-=-=-=-=-=-=-
-# modify the eirods_ms_home.h file with the proper path to the binary directory
-irods_msvc_home=`./scripts/find_irods_home.sh`
-irods_msvc_home="$irods_msvc_home/server/bin/"
-sed -e s,EIRODSMSVCPATH,$irods_msvc_home, server/re/include/eirods_ms_home.h.src > /tmp/eirods_ms_home.h
-mv /tmp/eirods_ms_home.h server/re/include/
+    # handle issue with IRODS_HOME being overwritten by the configure script    
+    sed -e "\,^IRODS_HOME,s,^.*$,IRODS_HOME=\`./scripts/find_irods_home.sh\`," ./irodsctl > /tmp/irodsctl.tmp
+    mv /tmp/irodsctl.tmp ./irodsctl
+    chmod 755 ./irodsctl
 
 
 
-
-###########################################
-# single 'make' time on an 8 core machine
-###########################################
-#        time make           1m55.508s
-#        time make -j 1      1m55.023s
-#        time make -j 2      0m17.199s
-#        time make -j 3      0m11.873s
-#        time make -j 4      0m9.894s   <-- inflection point
-#        time make -j 5      0m9.164s
-#        time make -j 6      0m8.515s
-#        time make -j 7      0m8.042s
-#        time make -j 8      0m7.898s
-#        time make -j 9      0m7.911s
-#        time make -j 10     0m7.898s
-#        time make -j        0m30.920s
-###########################################
-# single 'make' time on a single core VM
-###########################################
-#        time make           3m1.410s
-#        time make -j 2      2m13.481s
-#        time make -j 4      1m52.533s
-#        time make -j 5      1m48.611s
-###########################################
-make -j 4
-make -j 4
-if [ "$?" != "0" ]; then
- exit 1
-fi
-
-# generate randomized database password, replacing hardcoded placeholder
-cd $DIR/../
-sed -e "s,SOMEPASSWORD,$RANDOMDBPASS," ./packaging/e-irods.list.template > /tmp/eirodslist.tmp
-mv /tmp/eirodslist.tmp ./packaging/e-irods.list
+    # =-=-=-=-=-=-=-
+    # modify the eirods_ms_home.h file with the proper path to the binary directory
+    irods_msvc_home=`./scripts/find_irods_home.sh`
+    irods_msvc_home="$irods_msvc_home/server/bin/"
+    sed -e s,EIRODSMSVCPATH,$irods_msvc_home, server/re/include/eirods_ms_home.h.src > /tmp/eirods_ms_home.h
+    mv /tmp/eirods_ms_home.h server/re/include/
 
 
-# generate manual in pdf format
-cd $DIR/../
-rst2pdf manual.rst -o manual.pdf
 
-# generate doxygen for microservices
-cd $DIR/../iRODS
-doxygen ./config/doxygen-saved.cfg
-if [ "$?" != "0" ]; then
-  echo "Error: Failed generating doxygen output." 1>&2
-fi
+
+    ###########################################
+    # single 'make' time on an 8 core machine
+    ###########################################
+    #        time make           1m55.508s
+    #        time make -j 1      1m55.023s
+    #        time make -j 2      0m17.199s
+    #        time make -j 3      0m11.873s
+    #        time make -j 4      0m9.894s   <-- inflection point
+    #        time make -j 5      0m9.164s
+    #        time make -j 6      0m8.515s
+    #        time make -j 7      0m8.042s
+    #        time make -j 8      0m7.898s
+    #        time make -j 9      0m7.911s
+    #        time make -j 10     0m7.898s
+    #        time make -j        0m30.920s
+    ###########################################
+    # single 'make' time on a single core VM
+    ###########################################
+    #        time make           3m1.410s
+    #        time make -j 2      2m13.481s
+    #        time make -j 4      1m52.533s
+    #        time make -j 5      1m48.611s
+    ###########################################
+    make -j 4
+    make -j 4
+    if [ "$?" != "0" ]; then
+     exit 1
+    fi
+
+    # generate randomized database password, replacing hardcoded placeholder
+    cd $DIR/../
+    sed -e "s,SOMEPASSWORD,$RANDOMDBPASS," ./packaging/e-irods.list.template > /tmp/eirodslist.tmp
+    mv /tmp/eirodslist.tmp ./packaging/e-irods.list
+
+
+    # generate manual in pdf format
+    cd $DIR/../
+    rst2pdf manual.rst -o manual.pdf
+
+    # generate doxygen for microservices
+    cd $DIR/../iRODS
+    doxygen ./config/doxygen-saved.cfg
+    if [ "$?" != "0" ]; then
+      echo "ERROR :: Failed generating doxygen output" 1>&2
+    fi
+
+fi # if $BUILDEIRODS
 
 
 # run EPM for package type of this machine
@@ -200,8 +242,13 @@ fi
 # md5sum 3805b1377f910699c4914ef96b273943
 
 cd $DIR/../epm
-./configure
-make
+echo "Configuring EPM"
+./configure > /dev/null
+if [ "$?" != "0" ]; then
+ exit 1
+fi
+echo "Building EPM"
+make > /dev/null
 if [ "$?" != "0" ]; then
  exit 1
 fi
