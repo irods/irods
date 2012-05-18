2f2e49914f7f4194063d187d28fadebac47ba152
Terrell Russell <terrellrussell@gmail.com>
2012-05-18 14:12:52 -0400

    [#730] detect postgres odbc driver correctly for SuSE

--- packaging/find_psqlodbc.sh
+++ packaging/find_psqlodbc.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
+
+# CentOS and Ubuntu
 ODBC=`find /usr -name "psqlodbc*.so" 2> /dev/null | grep -v "w"`
 NONEWLINES=`echo $ODBC | perl -ne 'chomp and print'`
-echo $NONEWLINES
+
+# SuSE
+if [ "$NONEWLINES" == "" ]; then
+  ODBC=`find /usr -name "libodbcpsql.so" 2> /dev/null | grep -v "w"`
+  NONEWLINES=`echo $ODBC | perl -ne 'chomp and print'`
+fi
+
+echo "$NONEWLINES"
+
