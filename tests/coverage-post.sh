bc95702551c0e3a4f2e746a8b3c403423b639c60
Terrell Russell <terrellrussell@gmail.com>
2012-07-13 10:31:23 -0400

    [#904] add baseline (zero out) coverage report, then run tests and report

--- tests/coverage-post.sh
+++ tests/coverage-post.sh
@@ -2,7 +2,9 @@
 
 # variables
 OUTDIR="coverageresults"
-INFOFILE="coverage.info"
+BASEINFOFILE="coverage-base.info"
+TESTINFOFILE="coverage-test.info"
+TOTALINFOFILE="coverage-total.info"
 
 # get into the correct directory
 DETECTEDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
@@ -17,9 +19,9 @@ if [ "$?" -ne "0" ] ; then
     echo "ERROR :: gcov is not in your path" 1>&2
     exit 1
 fi
-GENINFO=`which geninfo`
+GENINFO=`which lcov`
 if [ "$?" -ne "0" ] ; then
-    echo "ERROR :: geninfo is not in your path" 1>&2
+    echo "ERROR :: lcov is not in your path" 1>&2
     echo "      :: lcov source: http://downloads.sourceforge.net/ltp/lcov-1.9.tar.gz" 1>&2
     exit 1
 fi
@@ -32,6 +34,13 @@ fi
 
 # generate the lcov results
 for f in $(find . -name "*.gcno"); do foo=`pwd`;cd `dirname $f`; gcov `basename $f`; cd $foo; done
-geninfo -f -o $INFOFILE .
-genhtml -o $OUTDIR $INFOFILE
-rm $INFOFILE
+lcov -f -c --directory . -o $TESTINFOFILE                       # generate coverage file
+lcov -a $BASEINFOFILE -a $TESTINFOFILE -o $TOTALINFOFILE        # combine base and test
+lcov -r $TOTALINFOFILE "/usr/*" --directory . -o $TOTALINFOFILE # remove /usr/* lines from file
+genhtml -o $OUTDIR $TOTALINFOFILE                               # generate html report
+
+# clean up
+rm $BASEINFOFILE
+rm $TESTINFOFILE
+rm $TOTALINFOFILE
+
