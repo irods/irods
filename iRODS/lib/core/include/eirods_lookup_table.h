30cb6b9fb9c570b2343ddb626d3132584cf3cead
Harry Johnson <hcjiv1@renci.org>
2012-09-25 16:42:15 -0400

    [#985] First rev of the generic cache class and cache filler abc. re-indented some files

--- iRODS/lib/core/include/eirods_lookup_table.h
+++ iRODS/lib/core/include/eirods_lookup_table.h
@@ -1,3 +1,5 @@
+/* -*- mode: c++; fill-column: 132; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
 
 #ifndef EIRODS_PLUGIN_TABLE_H
 #define EIRODS_PLUGIN_TABLE_H
@@ -13,147 +15,147 @@
 
 namespace eirods {
 
-	// =-=-=-=-=-=-=-
-	// class to manage tables of plugins.  employing a class in order to use
-	// RAII for adding entries to the table now that it is not a static array
-	template< typename ValueType, typename KeyType=std::string, typename HashType=eirods_string_hash >
-	class lookup_table {
-		protected:
-			hash_map< KeyType, ValueType, HashType > table_;
+    // =-=-=-=-=-=-=-
+    // class to manage tables of plugins.  employing a class in order to use
+    // RAII for adding entries to the table now that it is not a static array
+    template< typename ValueType, typename KeyType=std::string, typename HashType=eirods_string_hash >
+    class lookup_table {
+    protected:
+        hash_map< KeyType, ValueType, HashType > table_;
 			
-		public:
-            typedef typename hash_map< KeyType, ValueType, HashType >::iterator iterator;
-			lookup_table(){};
-			virtual ~lookup_table() {}
-			ValueType& operator[]( KeyType _k ) {
-				return table_[ _k ];
-			}
-			int size() {
-				return table_.size();
-			}
-			bool has_entry( KeyType _k ) {
-				return !( table_.end() == table_.find( _k ) );
-			}
-			size_t erase( KeyType _k ) {
-		        return table_.erase( _k );	
-			}
-            void clear() {
-				table_.clear();
-			}
-			bool empty() {
-				return table_.empty();
-			}
-			iterator begin() { return table_.begin(); }
-			iterator end()   { return table_.end();   }
+    public:
+        typedef typename hash_map< KeyType, ValueType, HashType >::iterator iterator;
+        lookup_table(){};
+        virtual ~lookup_table() {}
+        ValueType& operator[]( KeyType _k ) {
+            return table_[ _k ];
+        }
+        int size() {
+            return table_.size();
+        }
+        bool has_entry( KeyType _k ) {
+            return !( table_.end() == table_.find( _k ) );
+        }
+        size_t erase( KeyType _k ) {
+            return table_.erase( _k );	
+        }
+        void clear() {
+            table_.clear();
+        }
+        bool empty() {
+            return table_.empty();
+        }
+        iterator begin() { return table_.begin(); }
+        iterator end()   { return table_.end();   }
  
-			// =-=-=-=-=-=-=-
-			// accessor function
-            error get( std::string _key, ValueType& _val ) {
-				if( !has_entry( _key ) ) {
-                    return ERROR( false, -1, "key not found" );
-				}
+        // =-=-=-=-=-=-=-
+        // accessor function
+        error get( std::string _key, ValueType& _val ) {
+            if( !has_entry( _key ) ) {
+                return ERROR( false, -1, "key not found" );
+            }
 
-				_val = table_[ _key ];
+            _val = table_[ _key ];
 
-				return SUCCESS();
-			}
+            return SUCCESS();
+        }
             
-			// =-=-=-=-=-=-=-
-			// mutator function 
-			error set( std::string _key, const ValueType& _val ) {
-                table_[ _key ] = _val;
-			} // set
+        // =-=-=-=-=-=-=-
+        // mutator function 
+        error set( std::string _key, const ValueType& _val ) {
+            table_[ _key ] = _val;
+        } // set
 
-	}; // class lookup_table
+    }; // class lookup_table
 
 
-	// =-=-=-=-=-=-=-
-	// partial specialization created to support templating the get/set
-	// functions which need to manage exception handling etc from 
-	// a boost::any_cast
+    // =-=-=-=-=-=-=-
+    // partial specialization created to support templating the get/set
+    // functions which need to manage exception handling etc from 
+    // a boost::any_cast
     template< typename KeyType, typename HashType >
-	class lookup_table < boost::any, KeyType, HashType > {
-		protected:
-			hash_map< KeyType, boost::any, HashType > table_;
+    class lookup_table < boost::any, KeyType, HashType > {
+    protected:
+        hash_map< KeyType, boost::any, HashType > table_;
 			
-		public:
-            typedef typename hash_map< KeyType, boost::any, HashType >::iterator iterator;
-			lookup_table(){};
-			virtual ~lookup_table() {}
-			boost::any& operator[]( KeyType _k ) {
-				return table_[ _k ];
-			}
-			int size() {
-				return table_.size();
-			}
-			bool has_entry( KeyType _k ) {
-				return !( table_.end() == table_.find( _k ) );
-			}
-			size_t erase( KeyType _k ) {
-		        return table_.erase( _k );	
-			}
-            void clear() {
-				table_.clear();
-			}
-			bool empty() {
-				return table_.empty();
-			}
-			iterator begin() { return table_.begin(); }
-			iterator end()   { return table_.end();   }
+    public:
+        typedef typename hash_map< KeyType, boost::any, HashType >::iterator iterator;
+        lookup_table(){};
+        virtual ~lookup_table() {}
+        boost::any& operator[]( KeyType _k ) {
+            return table_[ _k ];
+        }
+        int size() {
+            return table_.size();
+        }
+        bool has_entry( KeyType _k ) {
+            return !( table_.end() == table_.find( _k ) );
+        }
+        size_t erase( KeyType _k ) {
+            return table_.erase( _k );	
+        }
+        void clear() {
+            table_.clear();
+        }
+        bool empty() {
+            return table_.empty();
+        }
+        iterator begin() { return table_.begin(); }
+        iterator end()   { return table_.end();   }
  
-			// =-=-=-=-=-=-=-
-			// get a property from the table if it exists.  catch the exception in the case where
-			// the template types may not match and return sucess/fail
-			template< typename T >
-			error get( std::string _key, T& _val ) {
-				// =-=-=-=-=-=-=-
-				// check params
-				#ifdef DEBUG
-				if( _key.empty() ) {
-					return ERROR( false, -1, "empty key" );
-				}
-				#endif
+        // =-=-=-=-=-=-=-
+        // get a property from the table if it exists.  catch the exception in the case where
+        // the template types may not match and return sucess/fail
+        template< typename T >
+        error get( std::string _key, T& _val ) {
+            // =-=-=-=-=-=-=-
+            // check params
+#ifdef DEBUG
+            if( _key.empty() ) {
+                return ERROR( false, -1, "empty key" );
+            }
+#endif
 				
-				// =-=-=-=-=-=-=-
-				// attempt to any_cast property value to given type.  catch exception and log
-				// failure then exit
-				try {
-					_val = boost::any_cast< T >( table_[ _key ] );
-					return SUCCESS();
-				} catch ( const boost::bad_any_cast & ) {
-					return ERROR( false, -1, "lookup_table::get - type and property key mistmatch" );
-				}
+            // =-=-=-=-=-=-=-
+            // attempt to any_cast property value to given type.  catch exception and log
+            // failure then exit
+            try {
+                _val = boost::any_cast< T >( table_[ _key ] );
+                return SUCCESS();
+            } catch ( const boost::bad_any_cast & ) {
+                return ERROR( false, -1, "lookup_table::get - type and property key mistmatch" );
+            }
 		 
-				return ERROR( false, -1, "lookup_table::get - shouldn't get here." );
+            return ERROR( false, -1, "lookup_table::get - shouldn't get here." );
 
-			} // get_property
+        } // get_property
 
-			// =-=-=-=-=-=-=-
-			// set a property in the table
-			template< typename T >
-			error set( std::string _key, const T& _val ) {
-				// =-=-=-=-=-=-=-
-				// check params	
-				#ifdef DEBUG
-				if( _key.empty() ) {
-					return ERROR( false, -1, "lookup_table::set - empty key" );
-				}
+        // =-=-=-=-=-=-=-
+        // set a property in the table
+        template< typename T >
+        error set( std::string _key, const T& _val ) {
+            // =-=-=-=-=-=-=-
+            // check params	
+#ifdef DEBUG
+            if( _key.empty() ) {
+                return ERROR( false, -1, "lookup_table::set - empty key" );
+            }
 				
-				if( table_.has_entry( _key ) ) {
-					std::cout << "[!]\tlookup_table::set - overwriting entry for key [" 
-							  << key << "]" << std::endl;
-				}
-				#endif	
+            if( table_.has_entry( _key ) ) {
+                std::cout << "[!]\tlookup_table::set - overwriting entry for key [" 
+                          << key << "]" << std::endl;
+            }
+#endif	
 			
-				// =-=-=-=-=-=-=-
-				// add property to map
-				table_[ _key ] = _val;
+            // =-=-=-=-=-=-=-
+            // add property to map
+            table_[ _key ] = _val;
 					
-				return SUCCESS() ;
+            return SUCCESS() ;
 
-			} // set_property
+        } // set_property
             
-	}; // class lookup_table
+    }; // class lookup_table
 	
 }; // namepsace eirods
 
--- iRODS/server/re/src/eirods_ms_plugin.c
+++ iRODS/server/re/src/eirods_ms_plugin.c
@@ -1,3 +1,5 @@
+/* -*- mode: c++; fill-column: 132; c-basic-offset: 4; indent-tabs-mode: nil -*- */
+
 // =-=-=-=-=-=-=-
 // My Includes
 #include "eirods_ms_home.h"
@@ -11,75 +13,75 @@
 #include <sstream>
 
 namespace eirods {
-	// =-=-=-=-=-=-=-
-	// ms_table_entry definition
-	ms_table_entry::ms_table_entry( ) : 
-	    plugin_base( "" ),
-		action_(""), 
-		numberOfStringArgs_( 0 ), 
-		callAction_( 0 ) { 
-	} // def ctor
+    // =-=-=-=-=-=-=-
+    // ms_table_entry definition
+    ms_table_entry::ms_table_entry( ) : 
+        plugin_base( "" ),
+        action_(""), 
+        numberOfStringArgs_( 0 ), 
+        callAction_( 0 ) { 
+    } // def ctor
 
-	ms_table_entry::ms_table_entry( std::string _s, int _n, ms_func_ptr _fp ) :
-	    plugin_base(""),
-		action_(_s), 
-		numberOfStringArgs_( _n ), 
-		callAction_( _fp ) {
-	} // ctor
+    ms_table_entry::ms_table_entry( std::string _s, int _n, ms_func_ptr _fp ) :
+        plugin_base(""),
+        action_(_s), 
+        numberOfStringArgs_( _n ), 
+        callAction_( _fp ) {
+    } // ctor
 
-	ms_table_entry::ms_table_entry( const ms_table_entry& _rhs ) :
-	    plugin_base( _rhs.context_ ),
-		action_( _rhs.action_ ), 
-		numberOfStringArgs_( _rhs.numberOfStringArgs_ ), 
-		callAction_( _rhs.callAction_ ) {
-	} // cctor
+    ms_table_entry::ms_table_entry( const ms_table_entry& _rhs ) :
+        plugin_base( _rhs.context_ ),
+        action_( _rhs.action_ ), 
+        numberOfStringArgs_( _rhs.numberOfStringArgs_ ), 
+        callAction_( _rhs.callAction_ ) {
+    } // cctor
 
-	ms_table_entry& ms_table_entry::operator=( const ms_table_entry& _rhs ) { 
-		plugin_base::operator=( _rhs );
-		action_             = _rhs.action_;
-		numberOfStringArgs_ = _rhs.numberOfStringArgs_;
-		callAction_         = _rhs.callAction_;
-		return *this;
-	} // operator=
+    ms_table_entry& ms_table_entry::operator=( const ms_table_entry& _rhs ) { 
+        plugin_base::operator=( _rhs );
+        action_             = _rhs.action_;
+        numberOfStringArgs_ = _rhs.numberOfStringArgs_;
+        callAction_         = _rhs.callAction_;
+        return *this;
+    } // operator=
 
-	ms_table_entry::~ms_table_entry() {
-	} // dtor
+    ms_table_entry::~ms_table_entry() {
+    } // dtor
 
-	error ms_table_entry::delay_load( void* _h ) {
-		if( !_h ) {
-			return ERROR( false, -1, "delay_load - null handle parameter" );
-		}
+    error ms_table_entry::delay_load( void* _h ) {
+        if( !_h ) {
+            return ERROR( false, -1, "delay_load - null handle parameter" );
+        }
 
-		callAction_ = reinterpret_cast< ms_func_ptr >( dlsym( _h, action_.c_str() ) );
+        callAction_ = reinterpret_cast< ms_func_ptr >( dlsym( _h, action_.c_str() ) );
 		
-		if( !callAction_ ) {
-			std::stringstream msg;
-			msg << "delay_load :: failed to load msvc function [" << action_ << "]";
-			return ERROR( false, -1, msg.str() );
-		} else { 
-			#ifdef DEBUG
-			std::cout << "[+]\teirods::ms_table_entry::delay_load :: [" << action_ << "]" << std::endl;
-			#endif
-		}
+        if( !callAction_ ) {
+            std::stringstream msg;
+            msg << "delay_load :: failed to load msvc function [" << action_ << "]";
+            return ERROR( false, -1, msg.str() );
+        } else { 
+#ifdef DEBUG
+            std::cout << "[+]\teirods::ms_table_entry::delay_load :: [" << action_ << "]" << std::endl;
+#endif
+        }
 
-		return SUCCESS();
-	} // delay_load
+        return SUCCESS();
+    } // delay_load
 
-	// =-=-=-=-=-=-=-
-	// given the name of a microservice, try to load the shared object
-	// and then register that ms with the table
-	error load_microservice_plugin( ms_table& _table, const std::string _ms ) {
+    // =-=-=-=-=-=-=-
+    // given the name of a microservice, try to load the shared object
+    // and then register that ms with the table
+    error load_microservice_plugin( ms_table& _table, const std::string _ms ) {
 
-        ms_table_entry* entry;
-		error load_err = load_plugin< ms_table_entry >( _ms, EIRODS_MS_HOME, entry, "" );
+        ms_table_entry* entry = 0;
+	error load_err = load_plugin< ms_table_entry >( _ms, EIRODS_MS_HOME, entry, "" );
         if( load_err.ok() && entry ) {
             _table[ _ms ] = entry;
-		    return SUCCESS();
-		} else {
-			error ret = PASS( false, -1, "load_microservice_plugin - Failed to create ms plugin entry.", load_err );
-			// JMC :: spams the log - log( ret );
-			return ret;
-		}
-	} // load_microservice_plugin
+            return SUCCESS();
+        } else {
+            error ret = PASS( false, -1, "load_microservice_plugin - Failed to create ms plugin entry.", load_err );
+            // JMC :: spams the log - log( ret );
+            return ret;
+        }
+    } // load_microservice_plugin
 
 }; // namespace eirods
