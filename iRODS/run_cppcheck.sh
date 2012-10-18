4960de4acf9cbe337f4cb9303b23538870037f78
Terrell Russell <terrellrussell@gmail.com>
2012-10-18 15:12:22 -0400

    [#763] made run cppcheck much smarter, detects os, cpucount, cppcheck binary

--- iRODS/run_cppcheck.sh
+++ iRODS/run_cppcheck.sh
@@ -1,5 +1,47 @@
-#!/bin/sh
+#!/bin/bash -e
 
-H="~/dev/irods/E-iRODS/e-irods/iRODS/"
+# preflight
+CPPCHECK=`which cppcheck`
+if [[ "$?" != "0" || `echo $CPPCHECK | awk '{print $1}'` == "no" ]] ; then
+    echo "cppcheck is required on this machine"
+    if [ "$DETECTEDOS" == "MacOSX" ] ; then
+        echo "try:"
+        echo "  sudo apt-get install cppcheck"
+    fi
+    echo ""
+    exit 1
+fi
 
-~/bin/cppcheck -j 8 -f -I/usr/include -I$H/nt/include -I$H/lib/md5/include -I$H/lib/rbudp/include -I$H/lib/api/include -I$H/lib/core/include -I$H/lib/isio/include -I$H/server/re/include -I$H/server/drivers/include -I$H/server/core/include -I$H/server/icat/include -i nt/ -i modules/msoDrivers/ -i modules/webservices/ -i boost_irods/ -i clients/icommands/rulegen/ -i modules . | grep error
+# find proper directory
+DETECTEDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+cd $DETECTEDDIR
+H=`pwd`
+echo "Detected Directory [$H]"
+
+# detect operating system
+DETECTEDOS=`$DETECTEDDIR/../packaging/find_os.sh`
+echo "Detected OS [$DETECTEDOS]"
+
+
+# find number of cpus
+if [ "$DETECTEDOS" == "MacOSX" ] ; then
+    DETECTEDCPUCOUNT=`sysctl -n hw.ncpu`
+elif [ "$DETECTEDOS" == "Solaris" ] ; then
+    DETECTEDCPUCOUNT=`/usr/sbin/psrinfo -p`
+else
+    DETECTEDCPUCOUNT=`cat /proc/cpuinfo | grep processor | wc -l`
+fi
+if [ "$DETECTEDCPUCOUNT" \< "2" ] ; then
+    DETECTEDCPUCOUNT=1
+fi
+CPUCOUNT=$(( $DETECTEDCPUCOUNT + 3 ))
+MAKEJCMD="make -j $CPUCOUNT"
+echo "-----------------------------------"
+echo "Detected CPUs:          $DETECTEDCPUCOUNT"
+echo "Running cppcheck with:  $CPUCOUNT"
+echo "-----------------------------------"
+sleep 1
+
+
+# run cppcheck
+cppcheck -j $CPUCOUNT -f -I/usr/include -I$H/nt/include -I$H/lib/md5/include -I$H/lib/rbudp/include -I$H/lib/api/include -I$H/lib/core/include -I$H/lib/isio/include -I$H/server/re/include -I$H/server/drivers/include -I$H/server/core/include -I$H/server/icat/include -i nt/ -i modules/msoDrivers/ -i modules/webservices/ -i boost_irods/ -i clients/icommands/rulegen/ -i modules . | grep error | grep -v "error\.c"
