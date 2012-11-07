0522113204f1e2b5995c4bebed6def5f96b146b4
Terrell Russell <terrellrussell@gmail.com>
2012-11-07 12:28:52 -0500

    [#1252] binary psql detection now smarter, looks for ELF rather than non-symbolic-link

--- packaging/find_postgres_bin.sh
+++ packaging/find_postgres_bin.sh
@@ -2,29 +2,28 @@
 
 # =-=-=-=-=-=-=-
 # determine how many hits we have for finding psql
-num_res=$( find /usr -name "psql" -print 2> /dev/null | grep usr | wc -l )
+num_res=$( find /usr -name "psql" -print 2> /dev/null | wc -l )
 
 # =-=-=-=-=-=-=-
-# filter out all symlink candidates
+# count only binary ELF files
 if [ $num_res -gt 1 ]; then
 	ctr=0
 	for file in `find /usr -name "psql" -print 2> /dev/null`
 	do
-		if [ -L $file ]; then
-			continue
-		else
-			not_links[$ctr]=$file
-			ctr=$ctr+1
+		elfstatus=`file $file | grep ELF | wc -l`
+		if [ "$elfstatus" == "1" ]; then
+			elf_links[$ctr]=$file
+       			ctr=$ctr+1
 		fi
 	done
 else
-	not_links[0]=`find /usr -name "psql" -print 2> /dev/null`
+	elf_links[0]=`find /usr -name "psql" -print 2> /dev/null`
 fi
 
 # prepopulate the return variable with the proper values,
 # in the case of a failure this will be overwritten with
 # the error message
-ret=${not_links[0]}
+ret=${elf_links[0]}
 
 
 # =-=-=-=-=-=-=-
@@ -36,9 +35,9 @@ if [ "$ret" == "" ]; then
 fi
 
 # =-=-=-=-=-=-=-
-# if there are still more than one candidate, then something terrible has happened
-# we shall bail and eschew all responsibility, silly human.
-if [ ${#not_links[@]} -gt 1 ]; then
+# if there is still more than one candidate, we cannot continue.
+# set return value accordingly
+if [ ${#elf_links[@]} -gt 1 ]; then
 	echo "Multiple versions of postgres found, aborting installation" 1>&2
 	echo -e `find /usr -name "psql" -print 2> /dev/null ` 1>&2
 	ret="FAIL/FAIL"
