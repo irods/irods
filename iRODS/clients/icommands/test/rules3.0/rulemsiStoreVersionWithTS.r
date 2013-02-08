3524d7543b71467819b71684ed9aec57e6be4ff4
Jason Coposky <jasonc@renci.org>
2013-02-08 18:31:35 -0500

    [#1193] r4894:4899 lisa's microservice additions

---
+++ iRODS/clients/icommands/test/rules3.0/rulemsiStoreVersionWithTS.r
@@ -0,0 +1,16 @@
+myTestRule {
+# Input parameters are:
+#   Source data object file path
+#   Destination object path
+# Output parameter is:
+#    Status
+# Output from running the example is
+#   File /tempZone/home/rods/put_test.txt copied to /tempZone/home/rods/SaveVersions
+  msiStoreVersionWithTS(*SourceFile,*DestPath,*Status);
+  writeLine("stdout","File *SourceFile copied to *DestPath");
+}
+INPUT *SourceFile="/tempZone/home/rods/put_test.txt", *DestPath="/tempZone/home/rods/SaveVersions"
+OUTPUT ruleExecOut
+
+
+
