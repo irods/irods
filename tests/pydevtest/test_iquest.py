54a96fb3b25f865473e1d65547a11b1c5b257007
Terrell Russell <terrellrussell@gmail.com>
2013-05-23 11:08:53 -0400

    [#904] refactor resource tests into disk related tests and virtual file system tests into separate files

--- tests/pydevtest/test_iquest.py
+++ tests/pydevtest/test_iquest.py
@@ -1,13 +1,17 @@
-import pydevtest_sessions as s
-from nose.tools import with_setup
 from nose.plugins.skip import SkipTest
-from pydevtest_common import assertiCmd, assertiCmdFail
+from pydevtest_common import assertiCmd, assertiCmdFail, interruptiCmd
+import pydevtest_sessions as s
 import commands
 
-@with_setup(s.adminonly_up,s.adminonly_down)
-def test_iquest_totaldatasize():
-    assertiCmd(s.adminsession,"iquest \"select sum(DATA_SIZE) where COLL_NAME like '/tempZone/home/%'\"","LIST","DATA_SIZE") # selects total data size
-    
-@with_setup(s.adminonly_up,s.adminonly_down)
-def test_iquest_bad_format():
-    assertiCmd(s.adminsession,"iquest \"bad formatting\"","ERROR","INPUT_ARG_NOT_WELL_FORMED_ERR") # bad request
+class Test_iquest(object):
+
+    ###################
+    # iquest
+    ###################
+
+    def test_iquest_totaldatasize(self):
+        assertiCmd(s.adminsession,"iquest \"select sum(DATA_SIZE) where COLL_NAME like '/tempZone/home/%'\"","LIST","DATA_SIZE") # selects total data size
+
+    def test_iquest_bad_format(self):
+        assertiCmd(s.adminsession,"iquest \"bad formatting\"","ERROR","INPUT_ARG_NOT_WELL_FORMED_ERR") # bad request
+
