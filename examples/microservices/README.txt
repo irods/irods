a5b6d0b11f04ac3d4468cbb40268b96d1c575b72
Jason Coposky <jasonc@renci.org>
2013-06-05 17:09:24 -0400

    [#1469] add resource plugin example to the development package

--- examples/microservices/README.txt
+++ examples/microservices/README.txt
@@ -6,11 +6,11 @@ the screen.
 
 To install this example plugin:
  $ make
- $ cp libeirods_msvc_test.so /var/lib/eirods/iRODS/server/bin/
- $ chown eirods:eirods /var/lib/eirods/iRODS/server/bin/libeirods_msvc_test.so
+ $ cp libeirods_msvc_test.so /var/lib/eirods/plugins/microservices
+ $ chown eirods:eirods /var/lib/eirods/plugins/microservices/libeirods_msvc_test.so
 
 Running make will generate the plugin, which will then
-need be copied to the /var/lib/eirods/iRODS/server/bin directory
+need be copied to the /var/lib/eirods/plugins/microservices directory
 and chown'ed to eirods:eirods by the Data Grid Administrator for the
 plugin to be loadable and the microservice made available to be
 executed.
--- examples/microservices/eirods_msvc_test.cpp
+++ examples/microservices/eirods_msvc_test.cpp
@@ -10,26 +10,38 @@
 
 extern "C" {
 
-    double EIRODS_PLUGIN_INTERFACE_VERSION=1.0;
-    
     // =-=-=-=-=-=-=-
-    // 1.  Create your microservice
+    // 1. Set the version of the plugin.  1.0 is currently the standard
+    double EIRODS_PLUGIN_INTERFACE_VERSION=1.0;
+	
+    // =-=-=-=-=-=-=-
+    // 2. Write a standard issue microservice	
     int eirods_msvc_test( msParam_t* _a, msParam_t* _b, msParam_t* _c, ruleExecInfo_t* _rei ) {
         std::cout << "eirods_msvc_test :: " << parseMspForStr( _a ) << " " << parseMspForStr( _b ) << " " << parseMspForStr( _c ) << std::endl;
         return 0;
     }
 
     // =-=-=-=-=-=-=-
-    // 2.  Create the plugin factory function which will return a microservice
-    //     table entry containing the name of the microservice and the number
-    //     of parameters that the microservice takes.  This will be called by
-    //     the plugin loader in the iRODS server to create the entry in the
-    //     table when the plugin is requested.
+    // 3.  Create the plugin factory function which will return a microservice
+    //     table entry 
     eirods::ms_table_entry*  plugin_factory( ) {
-        eirods::ms_table_entry* ms_plugin = new eirods::ms_table_entry( 3 );
-        ms_plugin->add_operation( "eirods_msvc_test" );
-        return ms_plugin;
+        // =-=-=-=-=-=-=-
+        // 4.  allocate a microservice plugin which takes the number of function 
+        //     params as a parameter to the constructor 
+        eirods::ms_table_entry* msvc = new eirods::ms_table_entry( 3 );	
+
+        // =-=-=-=-=-=-=-
+        // 5. add the microservice function as an operation to the plugin
+        //    the first param is the name / key of the operation, the second
+        //    is the name of the function which will be the microservice
+        msvc->add_operation( "eirods_msvc_test", "eirods_msvc_test" );
+        
+        // =-=-=-=-=-=-=-
+        // 6. return the newly created microservice plugin
+        return msvc;
     }
 
-}; // extern "C"
+}; // extern "C" 
+
+
 
