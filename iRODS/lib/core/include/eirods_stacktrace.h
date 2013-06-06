88c64d0214038f2df5875802e75da472b1d9ae4d
Harry Johnson <hcjiv1@renci.org>
2013-06-06 14:02:03 -0400

    [#1115] Added code to demangle the backtrace symbols. Now stores more granular data for each symbol so that the dump can do fancier output.

--- iRODS/lib/core/include/eirods_stacktrace.h
+++ iRODS/lib/core/include/eirods_stacktrace.h
@@ -16,8 +16,6 @@ namespace eirods {
     class stacktrace {
     public:
 
-        typedef std::list<std::string> stacklist;
-        
         /// @brief constructor
         stacktrace(void);
         virtual ~stacktrace(void);
@@ -29,6 +27,17 @@ namespace eirods {
         error dump(void);
 
     private:
+        /// @brief function to demangle the c++ function names
+        error demangle_symbol(const std::string& _symbol, std::string& _rtn_name, std::string& _rtn_offset);
+
+        typedef struct stack_entry_s {
+            std::string function;
+            std::string offset;
+            void* address;
+        } stack_entry_t;
+        
+        typedef std::list<stack_entry_t> stacklist;
+        
         stacklist stack_;
     };
 }; // namespace eirods
