89531f1f5b92a73fcb98c84262516f31f72b6c93
Terrell Russell <terrellrussell@gmail.com>
2013-07-17 22:54:08 -0400

    [#904] add continuous integration parameter to devtest.sh

--- tests/devtest.sh
+++ tests/devtest.sh
@@ -8,6 +8,13 @@
 #
 ###################################################################
 
+# check for continuous integration parameter
+if [ "$1" != "ci" ] ; then
+    OPTS=" -c -f -b "
+else
+    OPTS=""
+fi
+
 # detect E-iRODS root directory
 EIRODSROOT=$( dirname $( cd $(dirname $0) ; pwd -P ) )
 
@@ -24,7 +31,7 @@ $EIRODSROOT/iRODS/irodsctl restart
 # run RENCI developed python-based devtest suite
 # ( equivalent of original icommands and irules )
 cd $EIRODSROOT/tests/pydevtest
-python -m unittest -v -b -c -f test_eirods_resource_types
+python -m unittest -v $OPTS test_eirods_resource_types
 nosetests -v test_allrules.py
 
 # run DICE developed perl-based devtest suite
