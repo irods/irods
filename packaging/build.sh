b0023cb7649d893e325f36f53e0ca38bb45ea3c9
Terrell Russell <terrellrussell@gmail.com>
2013-07-30 10:04:34 -0400

    [#1607] remove automake, use cmake, add -fPIC to libarchive the cmake way

--- packaging/build.sh
+++ packaging/build.sh
@@ -393,26 +393,6 @@ else
     GREPCMD="grep"
 fi
 
-AUTOMAKE=`which automake`
-if [[ "$?" != "0" || `echo $AUTOMAKE | awk '{print $1}'` == "no" ]] ; then
-    if [ "$DETECTEDOS" == "Ubuntu" -o "$DETECTEDOS" == "Debian" ] ; then
-        PREFLIGHT="$PREFLIGHT automake"
-    elif [ "$DETECTEDOS" == "RedHatCompatible" ] ; then
-        PREFLIGHT="$PREFLIGHT automake"
-    elif [ "$DETECTEDOS" == "SuSE" ] ; then
-        PREFLIGHT="$PREFLIGHT automake"
-    elif [ "$DETECTEDOS" == "Solaris" ] ; then
-        PREFLIGHT="$PREFLIGHT automake"
-    elif [ "$DETECTEDOS" == "MacOSX" ] ; then
-        PREFLIGHT="$PREFLIGHT automake"
-    else
-        PREFLIGHTDOWNLOAD=$'\n'"$PREFLIGHTDOWNLOAD      :: download from: http://www.gnu.org/software/automake/#downloading"
-    fi
-else
-    AUTOMAKEVERSION=`automake --version | head -n1 | awk '{print $4}'`
-    echo "Detected automake [$AUTOMAKE] v[$AUTOMAKEVERSION]"
-fi
-
 LIBCURLDEV=`find /usr -name curl.h 2> /dev/null`
 if [ "$LIBCURLDEV" == "" ] ; then
     if [ "$DETECTEDOS" == "Ubuntu" -o "$DETECTEDOS" == "Debian" ] ; then
@@ -708,6 +688,31 @@ sleep 1
 # LOCAL COMPILATIONS - in ./external
 if [ "$BUILDEIRODS" == "1" ] ; then
 
+    # build a copy of cmake
+    EIRODS_BUILD_CMAKEVERSIONNUMBER="2.8.11.2"
+    EIRODS_BUILD_CMAKEVERSION="cmake-$EIRODS_BUILD_CMAKEVERSIONNUMBER"
+    cd $BUILDDIR/external/
+    if [ -d "$EIRODS_BUILD_CMAKEVERSION" ] ; then
+        echo "${text_green}${text_bold}Detected copy of [$EIRODS_BUILD_CMAKEVERSION]${text_reset}"
+    else
+        echo "${text_green}${text_bold}Downloading [$EIRODS_BUILD_CMAKEVERSION] from cmake.org${text_reset}"
+        if [ -e "$EIRODS_BUILD_CMAKEVERSION.tar.gz" ] ; then
+            echo "Using existing copy"
+        else
+            wget http://www.cmake.org/files/v2.8/cmake-$EIRODS_BUILD_CMAKEVERSION.tar.gz
+        fi
+#        gunzip $EIRODS_BUILD_CMAKEVERSION.tar.gz
+        tar xf $EIRODS_BUILD_CMAKEVERSION.tar.gz # this version wasn't zipped
+    fi
+    echo "${text_green}${text_bold}Building [$EIRODS_BUILD_CMAKEVERSION]${text_reset}"
+    cd $BUILDDIR/external/$EIRODS_BUILD_CMAKEVERSION
+    if [[ ( ! -e "Makefile" ) || ( "$FULLPATHSCRIPTNAME" -nt "Makefile" ) ]] ; then
+        ./bootstrap
+        $MAKEJCMD
+    else
+        echo "Nothing to build - all files up to date."
+    fi
+
     # build a copy of libarchive
     EIRODS_BUILD_LIBARCHIVEVERSIONNUMBER="3.1.2"
     EIRODS_BUILD_LIBARCHIVEVERSION="libarchive-$EIRODS_BUILD_LIBARCHIVEVERSIONNUMBER"
@@ -727,8 +732,7 @@ if [ "$BUILDEIRODS" == "1" ] ; then
     echo "${text_green}${text_bold}Building [$EIRODS_BUILD_LIBARCHIVEVERSION]${text_reset}"
     cd $BUILDDIR/external/$EIRODS_BUILD_LIBARCHIVEVERSION
     if [[ ( ! -e "Makefile" ) || ( "$FULLPATHSCRIPTNAME" -nt "Makefile" ) ]] ; then
-        ./build/autogen.sh
-        CFLAGS="-fPIC" ./configure
+        ../$EIRODS_BUILD_CMAKEVERSION/bin/cmake -D CMAKE_C_FLAGS:STRING=-fPIC .
         $MAKEJCMD
     else
         echo "Nothing to build - all files up to date."
