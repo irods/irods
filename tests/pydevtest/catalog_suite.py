e2017e7c6f55721a23b1b283f08bd200bec4d5b2
Terrell Russell <terrellrussell@gmail.com>
2013-07-31 12:57:05 -0400

    [#904] get the zone from the session info

--- tests/pydevtest/catalog_suite.py
+++ tests/pydevtest/catalog_suite.py
@@ -162,7 +162,7 @@ class Test_CatalogSuite(unittest.TestCase, ResourceBase):
     ###################
 
     def test_iquest_totaldatasize(self):
-        assertiCmd(s.adminsession,"iquest \"select sum(DATA_SIZE) where COLL_NAME like '/tempZone/home/%'\"","LIST","DATA_SIZE") # selects total data size
+        assertiCmd(s.adminsession,"iquest \"select sum(DATA_SIZE) where COLL_NAME like '/"+s.adminsession.getZoneName()+"/home/%'\"","LIST","DATA_SIZE") # selects total data size
 
     def test_iquest_bad_format(self):
         assertiCmd(s.adminsession,"iquest \"bad formatting\"","ERROR","INPUT_ARG_NOT_WELL_FORMED_ERR") # bad request
