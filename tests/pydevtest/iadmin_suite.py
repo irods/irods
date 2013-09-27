8023c4c93ff99a0e50fefb5861916c8992e913ba
Terrell Russell <terrellrussell@gmail.com>
2013-09-27 09:19:49 -0400

    [#1693] more expressive colon notation for replication test

--- tests/pydevtest/iadmin_suite.py
+++ tests/pydevtest/iadmin_suite.py
@@ -99,7 +99,7 @@ class Test_iAdminSuite(unittest.TestCase, ResourceBase):
         assertiCmdFail(s.adminsession,"iadmin lr","LIST",testresc1) # should not be listed
         output = commands.getstatusoutput("hostname")
         hostname = output[1]
-        assertiCmd(s.adminsession,"iadmin mkresc "+testresc1+" replication ContextString:Because:Colons") # replication
+        assertiCmd(s.adminsession,"iadmin mkresc "+testresc1+" replication ContextString:Because:Multiple:Colons") # replication
         assertiCmd(s.adminsession,"iadmin lr","LIST",testresc1) # should be listed
         assertiCmd(s.adminsession,"iadmin lr "+testresc1,"LIST",["resc_net","EMPTY_RESC_HOST"]) # should have empty host
         assertiCmd(s.adminsession,"iadmin lr "+testresc1,"LIST",["resc_def_path","EMPTY_RESC_PATH"]) # should have empty path
