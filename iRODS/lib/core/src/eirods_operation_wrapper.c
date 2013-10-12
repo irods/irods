03ba9e037e6faa0a3cafc420113ab79322234837
Jason Coposky <jasonc@renci.org>
2013-10-11 22:07:28 -0400

    [#1737] refactor operation wrapper to take an instance of the rule exec mgrs to solve template generation issues for client vs server side invocaiton

--- iRODS/lib/core/src/eirods_operation_wrapper.c
+++ iRODS/lib/core/src/eirods_operation_wrapper.c
@@ -22,9 +22,11 @@ namespace eirods {
     // =-=-=-=-=-=-=-
 	// public - ctor with opreation
     operation_wrapper::operation_wrapper( 
-        const std::string& _inst_name,
-        const std::string& _op_name,
+        oper_rule_exec_mgr_ptr _rule_exec, 
+        const std::string&     _inst_name,
+        const std::string&     _op_name,
         plugin_operation _op ) : 
+        rule_exec_mgr_( _rule_exec ),
         operation_( _op ),
         instance_name_( _inst_name ),
         operation_name_( _op_name ) {
@@ -34,6 +36,7 @@ namespace eirods {
 	// public - cctor
     operation_wrapper::operation_wrapper( 
         const operation_wrapper& _rhs ) {
+        rule_exec_mgr_  = _rhs.rule_exec_mgr_;
 		operation_      = _rhs.operation_;
 		instance_name_  = _rhs.instance_name_;
 		operation_name_ = _rhs.operation_name_;
@@ -48,6 +51,7 @@ namespace eirods {
 	// public - assignment for stl container
 	operation_wrapper& operation_wrapper::operator=( 
         const operation_wrapper& _rhs ) {
+        rule_exec_mgr_  = _rhs.rule_exec_mgr_;
 		operation_      = _rhs.operation_;
 		instance_name_  = _rhs.instance_name_;
 		operation_name_ = _rhs.operation_name_;
