4b4b252c9f28d5b2b1797afa16cc122ab2dcbc8e
Harry Johnson <hcjiv1@renci.org>
2013-10-19 14:44:42 -0400

    [#1566] Forgot to add the auth plugin files for objects managers etc

---
+++ iRODS/lib/core/include/eirods_auth_home.h
@@ -0,0 +1,13 @@
+
+#ifndef __EIRODS_AUTH_HOME_H__
+#define __EIRODS_AUTH_HOME_H__
+
+#include <string>
+
+namespace eirods {
+
+	const std::string EIRODS_AUTH_HOME( "/var/lib/eirods/plugins/auth/" );
+
+}; // namespace eirods
+
+#endif // __EIRODS_AUTH_HOME_H__
