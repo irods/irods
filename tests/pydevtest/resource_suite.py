acf6971a1713e98996d6cf72d0aa14fb36f321b9
Jason Coposky <jasonc@renci.org>
2013-10-30 15:55:09 -0400

    [#1763] re-enable SSL testing for CI.  parallel xfer is now more reliable

--- tests/pydevtest/resource_suite.py
+++ tests/pydevtest/resource_suite.py
@@ -207,7 +207,7 @@ class ResourceSuite(ResourceBase):
     # iput
     ###################
 
-    @unittest.skip("TEMPORARY: Too Hard for Tiny VMs")
+    # Now with more working.... @unittest.skip("TEMPORARY: Too Hard for Tiny VMs")
     def test_ssl_iput_small_and_large_files(self):
         # set up client and server side for ssl handshake
 
