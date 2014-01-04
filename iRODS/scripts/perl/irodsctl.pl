ab9089fa4ed5a9d557394a673d76e7abdb4fbe00
Jason Coposky <jasonc@renci.org>
2014-01-04 14:37:16 -0500

    [#1870] improve reaper techonolgy in irodsctl.  add reaping step during stop when no PIDs were found.  also needed to escape the $ in the awk call

--- iRODS/scripts/perl/irodsctl.pl
+++ iRODS/scripts/perl/irodsctl.pl
@@ -1551,6 +1551,7 @@ sub stopIrods
 	}
 	if ( ! $found )
 	{
+        system( "pgrep -l -u irods irods | grep -v irodsctl | awk '{print \$1}' | xargs kill -9 > /dev/null 2>&1" );
 		printStatus( "    There are no iRODS servers running.\n" );
 		return 1;
 	}
@@ -1569,7 +1570,7 @@ sub stopIrods
         # no regard for PIDs
         # iRODS must kill all owned processes for packaging purposes
 #        printStatus( "\tKilling any remaining Zombies... Silently.\n" );
-        system( "pgrep -l -u irods irods | grep -v irodsctl | awk '{print $1}' | xargs kill -9 > /dev/null 2>&1" );
+        system( "pgrep -l -u irods irods | grep -v irodsctl | awk '{print \$1}' | xargs kill -9 > /dev/null 2>&1" );
 
 	# Report if there are any left.
 	my $didNotDie = 0;
