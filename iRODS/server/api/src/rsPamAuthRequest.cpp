4ee862b6708c5067202312e7d55abdc0ef487eb9
Antoine de Torcy <adetorcy@renci.org>
2014-02-06 21:34:28 -0500

    [#1880] fixed run as root code in rsPamAuthRequest.cpp

--- iRODS/server/api/src/rsPamAuthRequest.cpp
+++ iRODS/server/api/src/rsPamAuthRequest.cpp
@@ -10,6 +10,8 @@
 #include "reGlobalsExtern.hpp"
 #include "icatHighLevelRoutines.hpp"
 #include "miscServerFunct.hpp"
+#include "irods_server_properties.hpp"
+#include "readServerConfig.hpp"
 
 
 int
@@ -107,6 +109,7 @@ _rsPamAuthRequest( rsComm_t *rsComm, pamAuthRequestInp_t *pamAuthRequestInp,
                    pamAuthRequestOut_t **pamAuthRequestOut ) {
     int status = 0;
     pamAuthRequestOut_t *result;
+    bool run_server_as_root = false;
 
     *pamAuthRequestOut = ( pamAuthRequestOut_t * )
                          malloc( sizeof( pamAuthRequestOut_t ) );
@@ -116,19 +119,21 @@ _rsPamAuthRequest( rsComm_t *rsComm, pamAuthRequestInp_t *pamAuthRequestInp,
 
 #if defined(PAM_AUTH)
 
-#ifdef RUN_SERVER_AS_ROOT
-    /* uid == euid is needed for some plugins e.g. libpam-sss */
-    status = changeToRootUser();
-    if ( status < 0 ) {
-        return ( status );
+    irods::server_properties::getInstance().get_property<bool>(RUN_SERVER_AS_ROOT_KW, run_server_as_root);
+
+    if (run_server_as_root) {
+		/* uid == euid is needed for some plugins e.g. libpam-sss */
+		status = changeToRootUser();
+		if ( status < 0 ) {
+			return ( status );
+		}
     }
-#endif
     /* Normal mode, fork/exec setuid program to do the Pam check */
     status = runPamAuthCheck( pamAuthRequestInp->pamUser,
                               pamAuthRequestInp->pamPassword );
-#ifdef RUN_SERVER_AS_ROOT
-    changeToServiceUser();
-#endif
+    if (run_server_as_root) {
+    	changeToServiceUser();
+    }
     if ( status == 256 ) {
         status = PAM_AUTH_PASSWORD_FAILED;
     }
