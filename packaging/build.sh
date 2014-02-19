030cafee680d761270872c16a38ee19c9ae0a27c
Terrell Russell <terrellrussell@gmail.com>
2014-02-19 16:24:25 -0500

    [#1931] add temp directory itself for tmpfiles

--- packaging/build.sh
+++ packaging/build.sh
@@ -206,6 +206,7 @@ echo "Detected OS Version [$DETECTEDOSVERSION]"
 
 # creates a timestamped tempfile for quick usage
 set_tmpfile() {
+  mkdir -p /tmp/$USER
   TMPFILE=/tmp/$USER/$(date "+%Y%m%d-%H%M%S.%N.irods.tmp")
 }
 
