69dddafaace4b1464de2e52f6177b849315c29ce
Jason Coposky <jasonc@renci.org>
2014-03-25 17:42:24 +0000

    [#2008] resolve resource hierarchy for both cross zone create and write operations

--- iRODS/server/api/src/rsGetRemoteZoneResc.cpp
+++ iRODS/server/api/src/rsGetRemoteZoneResc.cpp
@@ -16,96 +16,101 @@
 #include "dataObjOpen.hpp"
 
 #include "irods_resource_redirect.hpp"
+#include "irods_resource_backport.hpp"
 
 int
 rsGetRemoteZoneResc( rsComm_t *rsComm, dataObjInp_t *dataObjInp,
                      rodsHostAddr_t **rescAddr ) {
-    char *remoteOprType;
-    int status;
-    rescInfo_t *rescInfo = NULL;
-    rescGrpInfo_t *myRescGrpInfo = NULL;
-    dataObjInfo_t *dataObjInfoHead = NULL;
+    //rescInfo_t *rescInfo = NULL;
+    //rescGrpInfo_t *myRescGrpInfo = NULL;
 
-    *rescAddr = NULL;
-    if ( ( remoteOprType =
-                getValByKey( &dataObjInp->condInput, REMOTE_ZONE_OPR_KW ) ) == NULL ) {
+    // =-=-=-=-=-=-=-
+    // acquire the operation requested
+    char* remoteOprType = getValByKey(
+                              &dataObjInp->condInput,
+                              REMOTE_ZONE_OPR_KW );
+    if ( !remoteOprType ) {
         rodsLog( LOG_ERROR,
-                 "rsGetRemoteZoneResc: EMOTE_ZONE_OPR_KW not defined for %s",
+                 "rsGetRemoteZoneResc: REMOTE_ZONE_OPR_KW not defined for %s",
                  dataObjInp->objPath );
         return ( USER_BAD_KEYWORD_ERR );
     }
 
-    if ( strcmp( remoteOprType, REMOTE_CREATE ) == 0 ) {
-        status = getRescGrpForCreate( rsComm, dataObjInp, &myRescGrpInfo );
-        if ( status < 0 || NULL == myRescGrpInfo ) {
-            return status;    // JMC cppcheck - nullptr
+    // =-=-=-=-=-=-=-
+    // try to open the object in question, otherwise it is a create
+    int status = 0;
+    bool do_sort = false;  // only on open
+    std::string oper = irods::CREATE_OPERATION;
+    dataObjInfo_t *dataObjInfoHead = NULL;
+    if ( strcmp( remoteOprType, REMOTE_OPEN ) == 0 ) {
+        status = getDataObjInfoIncSpecColl(
+                     rsComm,
+                     dataObjInp,
+                     &dataObjInfoHead );
+        if ( status >= 0 ) {
+            do_sort = true;
+            oper = irods::OPEN_OPERATION;
         }
-        rescInfo = myRescGrpInfo->rescInfo;
     }
-    else if ( strcmp( remoteOprType, REMOTE_OPEN ) == 0 ) {
-        status = getDataObjInfoIncSpecColl( rsComm, dataObjInp,
-                                            &dataObjInfoHead );
+
+    // =-=-=-=-=-=-=-
+    // determine the hier string for the dest data obj inp
+    std::string hier;
+    irods::error ret = irods::resolve_resource_hierarchy(
+                           oper,
+                           rsComm,
+                           dataObjInp,
+                           hier );
+    if ( !ret.ok() ) {
+        std::stringstream msg;
+        msg << "failed for [";
+        msg << dataObjInp->objPath << "]";
+        irods::log( PASSMSG( msg.str(), ret ) );
+        return ret.code();
+    }
+
+    // =-=-=-=-=-=-=-
+    // we resolved the hier str for subsequent api calls, etc.
+    addKeyVal( &dataObjInp->condInput, RESC_HIER_STR_KW, hier.c_str() );
+
+    // =-=-=-=-=-=-=-
+    // we resolved the hier str for subsequent api calls, etc.
+    std::string location;
+    ret = irods::get_loc_for_hier_string( hier, location );
+    if ( !ret.ok() ) {
+        irods::log( PASS( ret ) );
+        return ret.code();
+    }
+
+    // =-=-=-=-=-=-=-
+    // if this was an open then run the sort and override the location
+    if ( do_sort ) {
+        int writeFlag;
+        writeFlag = getWriteFlag( dataObjInp->openFlags );
+        status = sortObjInfoForOpen(
+                     rsComm,
+                     &dataObjInfoHead,
+                     &dataObjInp->condInput,
+                     writeFlag );
         if ( status < 0 ) {
-            /* does not exist */
-            status = getRescGrpForCreate( rsComm, dataObjInp, &myRescGrpInfo );
-            if ( status < 0 || NULL == myRescGrpInfo ) {
-                return status;    // JMC cppcheck - nullptr
-            }
-            rescInfo = myRescGrpInfo->rescInfo;
+            rodsLog(
+                LOG_ERROR,
+                "rsGetRemoteZoneResc - sortObjInfoForOpen failed for [%s]",
+                dataObjInp->objPath );
+            return status;
         }
-        else {
 
-            // =-=-=-=-=-=-=-
-            // determine the hier string for the dest data obj inp
-            std::string hier;
-            irods::error ret = irods::resolve_resource_hierarchy( irods::OPEN_OPERATION, rsComm,
-                               dataObjInp, hier );
-            if ( !ret.ok() ) {
-                std::stringstream msg;
-                msg << "failed in irods::resolve_resource_hierarchy for [";
-                msg << dataObjInp->objPath << "]";
-                irods::log( PASSMSG( msg.str(), ret ) );
-                return ret.code();
-            }
-
-            // =-=-=-=-=-=-=-
-            // we resolved the hier str for subsequent api calls, etc.
-            addKeyVal( &dataObjInp->condInput, RESC_HIER_STR_KW, hier.c_str() );
-
-            int writeFlag;
-            /* exist */
-            writeFlag = getWriteFlag( dataObjInp->openFlags );
-            status = sortObjInfoForOpen( rsComm, &dataObjInfoHead, &dataObjInp->condInput, writeFlag );
-
-            if ( status < 0 ) {
-                return status;
-            }
-            status = applyPreprocRuleForOpen( rsComm, dataObjInp,
-                                              &dataObjInfoHead );
-            if ( status < 0 || NULL == dataObjInfoHead ) { // JMC cppcheck - nullptr
-                freeAllDataObjInfo( dataObjInfoHead );
-                return status;
-            }
-            else {
-                rescInfo = dataObjInfoHead->rescInfo;
-                freeAllDataObjInfo( dataObjInfoHead );
-            }
-        }
+        location = dataObjInfoHead->rescInfo->rescLoc;
     }
-    else {
-        rodsLog( LOG_ERROR,
-                 "rsGetRemoteZoneResc: bad EMOTE_ZONE_OPR_KW %s for %s",
-                 remoteOprType, dataObjInp->objPath );
-        return ( USER_BAD_KEYWORD_ERR );
-    }
+
+    // =-=-=-=-=-=-=-
+    // set the out variable
     *rescAddr = ( rodsHostAddr_t* )malloc( sizeof( rodsHostAddr_t ) );
     bzero( *rescAddr, sizeof( rodsHostAddr_t ) );
-    rstrcpy( ( *rescAddr )->hostAddr, rescInfo->rescLoc, NAME_LEN );
+    rstrcpy( ( *rescAddr )->hostAddr, location.c_str(),       NAME_LEN );
     rstrcpy( ( *rescAddr )->zoneName, ZoneInfoHead->zoneName, NAME_LEN );
-    if ( myRescGrpInfo != NULL ) {
-        freeAllRescGrpInfo( myRescGrpInfo );
-    }
 
-    return ( 0 );
+    return 0;
+
 }
 
