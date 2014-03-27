793bfa3432d0d85de06d16fd7c69784fa3ddcce0
Jason Coposky <jasonc@renci.org>
2014-03-27 14:06:47 +0000

    [#2014] refactor server_api_call to directly call the api table from every implementation

--- iRODS/server/core/include/irods_server_api_call.hpp
+++ iRODS/server/core/include/irods_server_api_call.hpp
@@ -7,34 +7,95 @@
 namespace irods {
 
     template< typename INP_T, typename OUT_T >
-    int server_api_call( int _api_index, rsComm_t * _comm, INP_T  * _input, bytesBuf_t * _input_buffer, OUT_T ** _output, bytesBuf_t * _output_buffer ) {
+    int server_api_call(
+        int          _api_index,
+        rsComm_t *   _comm,
+        INP_T  *     _input,
+        bytesBuf_t * _input_buffer,
+        OUT_T **     _output,
+        bytesBuf_t * _output_buffer ) {
         api_entry_table& table = get_server_api_table();
         bool has_entry = table.has_entry( _api_index );
-        if ( !has_entry )
-        {
+        if ( !has_entry ) {
             return SYS_UNMATCHED_API_NUM;
         }
-        return table[_api_index]->svrHandler( _input, _comm, _input_buffer, _output, _output_buffer );
+        return table[_api_index]->svrHandler(
+                   _comm,
+                   _input,
+                   _input_buffer,
+                   _output,
+                   _output_buffer );
     }
 
     template< typename INP_T, typename OUT_T >
-    int server_api_call( int _api_index, rsComm_t * _comm, INP_T * _input, OUT_T ** _output ) {
-        return server_api_call( _api_index, _comm, _input, NULL, _output, NULL );
+    int server_api_call(
+        int        _api_index,
+        rsComm_t * _comm,
+        INP_T *    _input,
+        OUT_T **   _output ) {
+        api_entry_table& table = get_server_api_table();
+        bool has_entry = table.has_entry( _api_index );
+        if ( !has_entry ) {
+            return SYS_UNMATCHED_API_NUM;
+        }
+
+        return table[_api_index]->svrHandler(
+                   _comm,
+                   _input,
+                   _output );
     }
 
     template< typename INP_T, typename OUT_T >
-    int server_api_call( int _api_index, rsComm_t * _comm, INP_T * _input, bytesBuf_t * _input_buffer, OUT_T ** _output ) {
-        return server_api_call( _api_index, _comm, _input, _input_buffer, _output );
+    int server_api_call(
+        int          _api_index,
+        rsComm_t *   _comm,
+        INP_T *      _input,
+        bytesBuf_t * _input_buffer,
+        OUT_T **     _output ) {
+        api_entry_table& table = get_server_api_table();
+        bool has_entry = table.has_entry( _api_index );
+        if ( !has_entry ) {
+            return SYS_UNMATCHED_API_NUM;
+        }
+        return table[_api_index]->svrHandler(
+                   _comm,
+                   _input,
+                   _input_buffer,
+                   _output );
     }
 
     template< typename INP_T, typename OUT_T >
-    int server_api_call( int _api_index, rsComm_t * _comm, INP_T * _input, OUT_T ** _output, bytesBuf_t * _output_buffer ) {
-        return server_api_call( _api_index, _comm, _input, NULL, _output, _output_buffer );
+    int server_api_call(
+        int          _api_index,
+        rsComm_t *   _comm,
+        INP_T *      _input,
+        OUT_T **     _output,
+        bytesBuf_t * _output_buffer ) {
+        api_entry_table& table = get_server_api_table();
+        bool has_entry = table.has_entry( _api_index );
+        if ( !has_entry ) {
+            return SYS_UNMATCHED_API_NUM;
+        }
+        return table[_api_index]->svrHandler(
+                   _comm,
+                   _input,
+                   _output,
+                   _output_buffer );
     }
 
     template< typename INP_T >
-    int server_api_call( int _api_index, rsComm_t * _comm, INP_T * _input ) {
-        return server_api_call< INP_T, char >( _api_index, _comm, _input, NULL, NULL, NULL );
+    int server_api_call(
+        int        _api_index,
+        rsComm_t * _comm,
+        INP_T *    _input ) {
+        api_entry_table& table = get_server_api_table();
+        bool has_entry = table.has_entry( _api_index );
+        if ( !has_entry ) {
+            return SYS_UNMATCHED_API_NUM;
+        }
+        return table[_api_index]->svrHandler(
+                   _comm,
+                   _input );
     }
 
 }; // namespace irods
