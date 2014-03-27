dada250b19a2a29482e979f27401627ae6af3080
Zoey Greer <tempoz@renci.org>
2014-03-27 17:10:52 -0400

    [#2015] separated the pack_table and client_api_table from the rest of the client library

--- packaging/make_irods_dev_archive.sh
+++ packaging/make_irods_dev_archive.sh
@@ -16,15 +16,36 @@ ar d  $ar_file dummy
 rm dummy
 
 # =-=-=-=-=-=-=-
-# Archive all Client Objects 
-ar qS $ar_file $(find iRODS/lib -name \*.o )
+# Archive all Client Objects
+ar qS $ar_file $(find iRODS/lib -name \*.o ! -name irods_pack_table.o ! -name irods_client_api_table.o)
 
 # =-=-=-=-=-=-=-
 # Generate index
 ranlib $ar_file
 
+# =-=-=-=-=-=-=-
+# Build Client Library
+ar_file="libirods_client_api.a"
 
+# =-=-=-=-=-=-=-
+# clean up any old files
+rm -f $ar_file
 
+# =-=-=-=-=-=-=-
+# init the ar file
+touch dummy
+ar cqS $ar_file dummy
+ar d  $ar_file dummy
+rm dummy
+
+# =-=-=-=-=-=-=-
+# Archive all Client Objects
+ar qS $ar_file $(find iRODS/lib -name irods_pack_table.o)
+ar qS $ar_file $(find iRODS/lib -name irods_client_api_table.o)
+
+# =-=-=-=-=-=-=-
+# Generate index
+ranlib $ar_file
 
 
 # =-=-=-=-=-=-=-
