a5e403b4a456858fcb2722bda23691f0614b6156
Terrell Russell <terrellrussell@gmail.com>
2014-03-27 23:53:34 -0400

    [#2011] proper sql comments use --

--- plugins/database/postgres/upgrade-3.3.xto4.0.0.sql
+++ plugins/database/postgres/upgrade-3.3.xto4.0.0.sql
@@ -1,21 +1,20 @@
-# add new resource hierarchy columns and index
+-- add new resource hierarchy columns and index
 alter table r_resc_main add column resc_children varchar(1000);
 alter table r_resc_main add column resc_context varchar(1000);
 alter table r_resc_main add column resc_parent varchar(1000);
 alter table r_data_main add column resc_hier varchar(1000);
 alter table r_resc_main add column resc_objcount bigint DEFAULT 0;
-create index idx_data_main6 on r_data_main (data_path);
 
-# update leaf node object counts
+-- update leaf node object counts
 update r_resc_main as r SET resc_objcount = ( select count(*) from r_data_main as d where d.resc_name=r.resc_name );
 
-# reset resc_group for consistency
+-- reset resc_group for consistency
 update r_data_main SET resc_group_name=resc_name;
 
-# populate the resc_hier field
+-- populate the resc_hier field
 update r_data_main SET resc_hier=resc_name;
 
-# define and populate new configuration table
+-- define and populate new configuration table
 create table R_GRID_CONFIGURATION
  (
    namespace varchar(2700),
