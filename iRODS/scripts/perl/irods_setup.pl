a8f76594de07cbdb6a227e2abfbc3835174166c6
Jason Coposky <jasonc@renci.org>
2014-04-02 11:16:48 +0000

    [#2021] configuration of exec string for test_cll no longer needs customized for an oracle install

--- iRODS/scripts/perl/irods_setup.pl
+++ iRODS/scripts/perl/irods_setup.pl
@@ -1187,9 +1187,9 @@ sub testDatabase()
 	#my ($status,$output) = run( "$test_cll $DATABASE_ADMIN_NAME '$DATABASE_ADMIN_PASSWORD'" );
 
 	$exec_str = "$test_cll $DATABASE_ADMIN_NAME '$DATABASE_ADMIN_PASSWORD'"; 
-	if ( $DATABASE_TYPE eq "oracle" ) {
-	    $exec_str = "$test_cll $DATABASE_ADMIN_NAME"."@".$DATABASE_HOST.":$DATABASE_PORT '$DATABASE_ADMIN_PASSWORD'"; 
-        }
+	#if ( $DATABASE_TYPE eq "oracle" ) {
+	#    $exec_str = "$test_cll $DATABASE_ADMIN_NAME"."@".$DATABASE_HOST.":$DATABASE_PORT '$DATABASE_ADMIN_PASSWORD'"; 
+    #    }
 
 	my ($status,$output) = run( "$exec_str" );
 	printLog( "    ", $output );
