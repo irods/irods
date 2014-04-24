02b394e3db86988f05379b239564130e282bfc81
Terrell Russell <terrellrussell@gmail.com>
2014-04-24 09:36:03 -0400

    [#2042] better environment detection within irods_setup.pl

--- iRODS/scripts/perl/irods_setup.pl
+++ iRODS/scripts/perl/irods_setup.pl
@@ -29,9 +29,20 @@ use File::Copy;
 use Cwd;
 use Cwd "abs_path";
 use Config;
+use File::Basename;
 
 $version{"irods_setup.pl"} = "Mar 2014";
 
+
+# =-=-=-=-=-=-=-
+# detect whether running in a consistent environment (usually the service account user)
+$scriptfullpath = abs_path(__FILE__);
+$scripttoplevel = dirname(dirname(dirname(dirname($scriptfullpath))));
+if( $scripttoplevel !~ /$ENV{HOME}/ ) {
+    print "The current environment [$ENV{HOME}] does not match script location [$scripttoplevel].\n";
+    print "Please switch user and run this script again.\n";
+    exit(1);
+}
 
 # =-=-=-=-=-=-=-
 # for testing later...
