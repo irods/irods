df149cfd2e84ed230353d56fc646e8b79c08c50a
xu-hao <xuh@email.unc.edu>
2014-07-08 14:42:55 -0400

    [#2080] Update parser.cpp

--- iRODS/server/re/src/parser.cpp
+++ iRODS/server/re/src/parser.cpp
@@ -3037,7 +3037,15 @@ LOOP_END( args )
 TTEXT( ")" )
 OPTIONAL_END( argList )
 BUILD_NODE( T_CONS, cons, &start, n, n );
-END_TRY( type )
+END_TRY
+            OPTIONAL_BEGIN(kind)
+             TTEXT(":");
+             NT2(_Type, 1, 0);
+             SWAP;
+             node = POP;
+             node->exprType = POP;
+             PUSH(node);
+            OPTIONAL_END(kind)
 if ( !cont ) {
     arity ++;
     TRY( typeEnd )
