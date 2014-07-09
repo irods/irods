5a4d0db502fb6117e450215e060efccf51bb545e
Ben Keller <irods.ben.keller@gmail.com>
2014-07-09 15:42:22 -0400

    [#858] Set method.__name__ in test metaclass
    Older pythons were not able to have unittest run single dynamically
    generated tests

--- tests/pydevtest/metaclass_unittest_test_case_generator.py
+++ tests/pydevtest/metaclass_unittest_test_case_generator.py
@@ -3,5 +3,6 @@ class MetaclassUnittestTestCaseGenerator(type):
         test_generators = [v for k, v in dct.items() if k.startswith('generate_tests')]
         for g in test_generators:
             for method_name, method in g():
+                method.__name__ = method_name
                 dct[method_name] = method
         return super(MetaclassUnittestTestCaseGenerator, meta).__new__(meta, name, bases, dct)
