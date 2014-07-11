faf687739aa2ce1ddc9a274392ead889cfee6c6b
Terrell Russell <terrellrussell@gmail.com>
2014-07-11 16:14:19 -0400

    [#2203] fix parseMspForPhyPathReg to care about "unmount"
    The msiPhyPathReg claims support for the "unmount" keyword, but does
    not succeed when called.  This update forces the parseMspForPhyPathReg
    function to properly watch for this keyword and set the appropriate
    flag on the myDataObjInp.

--- iRODS/lib/core/src/msParam.cpp
+++ iRODS/lib/core/src/msParam.cpp
@@ -866,6 +866,9 @@ parseMspForPhyPathReg( msParam_t * inpParam, keyValPair_t * condInput ) {
                 else if ( strcmp( tmpStr, LINK_POINT_STR ) == 0 ) {
                     addKeyVal( condInput, COLLECTION_TYPE_KW, LINK_POINT_STR );
                 }
+                else if ( strcmp( tmpStr, UNMOUNT_STR ) == 0 ) {
+                    addKeyVal( condInput, COLLECTION_TYPE_KW, UNMOUNT_STR );
+                }
             }
         }
         else {
