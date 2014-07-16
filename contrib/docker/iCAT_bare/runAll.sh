6081aa8e800db930016c8cd0c704b3b37e24b359
Zoey Greer <tempoz@renci.org>
2014-07-16 13:15:11 -0400

    [#858] removing files that are now housed in a separate repo

--- contrib/docker/iCAT_bare/runAll.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-supervisorctl start sshd
-supervisorctl start postgresql
-supervisorctl start irodsServer
-sudo su -c "iadmin modresc demoResc host $HOSTNAME" irods
-
-
--- contrib/docker/iCAT_bare/server.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-while read e; do
-  export $e
-done </var/lib/irods/env.tmp
-cd /var/lib/irods/iRODS/server/bin
-env PATH="$PATH:/var/lib/irods/iRODS/server/bin" ./irodsServer.actual -u
-rm /var/lib/irods/env.tmp
