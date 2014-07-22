72b673a47114ec7ef1be810f59eda359dd86e547
Zoey Greer <tempoz@renci.org>
2014-07-22 15:52:37 -0400

    [#2212] CID26227:
    freed 'stream' on exiting the 'msiPrintGenQueryOutToBuffer' fuction

--- iRODS/server/re/src/genQueryMS.cpp
+++ iRODS/server/re/src/genQueryMS.cpp
@@ -1203,6 +1203,7 @@ msiPrintGenQueryOutToBuffer( msParam_t *queryOut, msParam_t *format, msParam_t *
     rei->status = printGenQueryOut( stream, format_str, NULL, genQueryOut );
     if ( rei->status < 0 ) {
         rodsLog( LOG_ERROR, "msiPrintGenQueryOutToBuffer: printGenQueryOut() failed, status = %d", rei->status );
+        free( stream );
         return( rei->status );
     }
 
@@ -1224,6 +1225,7 @@ msiPrintGenQueryOutToBuffer( msParam_t *queryOut, msParam_t *format, msParam_t *
 
     /* Fill bytesBuf in our buffer output */
     fillBufLenInMsParam( buffer, bytesBuf->len, bytesBuf );
+    free( stream );
 
     return 0;
 
