03ad26d61b50fcc365468a6d431431df29b14e55
Ben Keller <irods.ben.keller@gmail.com>
2014-08-01 13:30:08 -0400

    [#2222] Cleanup resetMutex
    boost::interprocess::named_mutex::remove() will remove a locked mutex,
    there is no need to unlock it first. Also, unlock() will unlock a
    mutex even if it is owned by another process, so there is no reason
    to attempt to unlock the mutex before remove()'ing it

--- iRODS/server/re/src/locks.cpp
+++ iRODS/server/re/src/locks.cpp
@@ -17,20 +17,12 @@ void unlockMutex( mutex_type **mutex ) {
     ( *mutex )->unlock();
     delete *mutex;
 }
-/* This function can be used during intialization to unlock previous held mutex that has not been released.
- * This can only be used when there is no other process using the mutex */
+/* This function can be used during initialization to remove a previously held mutex that has not been released.
+ * This should only be used when there is no other process using the mutex */
 void resetMutex( mutex_type **mutex ) {
     char sem_name[1024];
     getResourceName( sem_name, SEM_NAME );
-    try {
-        mutex_type *mutex0 = new boost::interprocess::named_mutex( boost::interprocess::open_only, sem_name );
-        mutex0->unlock();
-        delete mutex0;
-        boost::interprocess::named_mutex::remove( sem_name );
-    }
-    catch ( boost::interprocess::interprocess_exception e ) {
-
-    }
+    boost::interprocess::named_mutex::remove( sem_name );
 }
 
 
