c97bd2c77a7e97a5a5e53fb05070902d7a45e4d5
Zoey Greer <tempoz@renci.org>
2014-08-01 14:40:16 -0400

    [#2212] CID26255:
    Initialized start and stop operations in the copy constructor of the
    network plugin

--- iRODS/lib/core/src/irods_network_plugin.cpp
+++ iRODS/lib/core/src/irods_network_plugin.cpp
@@ -41,6 +41,8 @@ network::~network( ) {
 network::network(
     const network& _rhs ) :
     plugin_base( _rhs ) {
+    start_operation_    = _rhs.start_operation_;
+    stop_operation_     = _rhs.stop_operation_;
     operations_         = _rhs.operations_;
     ops_for_delay_load_ = _rhs.ops_for_delay_load_;
 
