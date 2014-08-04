7f44f1958e3afc01c90ac4f650f5318ba43f83d0
Zoey Greer <tempoz@renci.org>
2014-08-03 23:16:20 -0400

    [#2212] CID25779:
    Fixed flags in msParam.hpp to not overlap

--- iRODS/lib/core/include/msParam.hpp
+++ iRODS/lib/core/include/msParam.hpp
@@ -48,10 +48,10 @@ extern "C" {
 #define ExecCmdOut_MS_T         "ExecCmdOut_PI"
 #define RodsObjStat_MS_T        "RodsObjStat_PI"
 #define VaultPathPolicy_MS_T    "VaultPathPolicy_PI"
-#define StrArray_MS_T		"StrArray_PI"
-#define IntArray_MS_T		"IntArray_PI"
-#define GenQueryInp_MS_T	"GenQueryInp_PI"
-#define GenQueryOut_MS_T	"GenQueryOut_PI"
+#define StrArray_MS_T           "StrArray_PI"
+#define IntArray_MS_T           "IntArray_PI"
+#define GenQueryInp_MS_T        "GenQueryInp_PI"
+#define GenQueryOut_MS_T        "GenQueryOut_PI"
 #define XmsgTicketInfo_MS_T     "XmsgTicketInfo_PI"
 #define SendXmsgInfo_MS_T       "SendXmsgInfo_PI"
 #define GetXmsgTicketInp_MS_T   "GetXmsgTicketInp_PI"
@@ -59,25 +59,25 @@ extern "C" {
 #define RcvXmsgInp_MS_T         "RcvXmsgInp_PI"
 #define RcvXmsgOut_MS_T         "RcvXmsgOut_PI"
 #define StructFileExtAndRegInp_MS_T         "StructFileExtAndRegInp_PI"
-#define RuleSet_MS_T "RuleSet_PI"
+#define RuleSet_MS_T            "RuleSet_PI"
 #define RuleStruct_MS_T         "RuleStruct_PI"
 #define DVMapStruct_MS_T        "DVMapStruct_PI"
 #define FNMapStruct_MS_T        "FNMapStruct_PI"
-#define MsrvcStruct_MS_T         "MsrvcStruct_PI"
-#define NcOpenInp_MS_T         "NcOpenInp_PI"
-#define NcInqIdInp_MS_T                "NcInqIdInp_PI"
-#define NcInqWithIdOut_MS_T    "NcInqWithIdOut_PI"
-#define NcInqInp_MS_T          "NcInqInp_PI"
-#define NcInqOut_MS_T          "NcInqOut_PI"
-#define NcCloseInp_MS_T                "NcCloseInp_PI"
-#define NcGetVarInp_MS_T       "NcGetVarInp_PI"
-#define NcGetVarOut_MS_T       "NcGetVarOut_PI"
-#define NcInqOut_MS_T          "NcInqOut_PI"
-#define NcInqGrpsOut_MS_T      "NcInqGrpsOut_PI"
-#define Dictionary_MS_T                "Dictionary_PI"
-#define DictArray_MS_T         "DictArray_PI"
-#define GenArray_MS_T          "GenArray_PI"
-#define DataObjInfo_MS_T       "DataObjInfo_PI"
+#define MsrvcStruct_MS_T        "MsrvcStruct_PI"
+#define NcOpenInp_MS_T          "NcOpenInp_PI"
+#define NcInqIdInp_MS_T         "NcInqIdInp_PI"
+#define NcInqWithIdOut_MS_T     "NcInqWithIdOut_PI"
+#define NcInqInp_MS_T           "NcInqInp_PI"
+#define NcInqOut_MS_T           "NcInqOut_PI"
+#define NcCloseInp_MS_T         "NcCloseInp_PI"
+#define NcGetVarInp_MS_T        "NcGetVarInp_PI"
+#define NcGetVarOut_MS_T        "NcGetVarOut_PI"
+#define NcInqOut_MS_T           "NcInqOut_PI"
+#define NcInqGrpsOut_MS_T       "NcInqGrpsOut_PI"
+#define Dictionary_MS_T         "Dictionary_PI"
+#define DictArray_MS_T          "DictArray_PI"
+#define GenArray_MS_T           "GenArray_PI"
+#define DataObjInfo_MS_T        "DataObjInfo_PI"
 
     /* micro service input/output parameter */
     typedef struct MsParam {
@@ -94,12 +94,12 @@ extern "C" {
         msParam_t **msParam;
     } msParamArray_t;
 
-#define MS_INP_SEP_STR	"++++"		/* the separator str for msInp */
-#define MS_NULL_STR	"null"		/* no input */
+#define MS_INP_SEP_STR    "++++"        /* the separator str for msInp */
+#define MS_NULL_STR       "null"        /* no input */
     typedef struct ParsedMsKeyValStr {
         char *inpStr;
-        char *endPtr;	/* end pointer */
-        char *curPtr;	/* current position */
+        char *endPtr;    /* end pointer */
+        char *curPtr;    /* current position */
         char *kwPtr;
         char *valPtr;
     } parsedMsKeyValStr_t;
@@ -111,36 +111,36 @@ extern "C" {
 
     /* valid keyWd flags for dataObjInp_t */
 
-#define RESC_NAME_FLAG		0x1
-#define DEST_RESC_NAME_FLAG 	0x2
-#define BACKUP_RESC_NAME_FLAG 	0x4
-#define FORCE_FLAG_FLAG 	0x8
-#define ALL_FLAG 		0x10
-#define LOCAL_PATH_FLAG 	0x20
-#define VERIFY_CHKSUM_FLAG	0x40
-#define ADMIN_FLAG 	0x80
-#define UPDATE_REPL_FLAG 	0x100
-#define REPL_NUM_FLAG 		0x200
-#define DATA_TYPE_FLAG 		0x400
-#define CHKSUM_ALL_FLAG 	0x800
-#define FORCE_CHKSUM_FLAG 	0x1000
-#define FILE_PATH_FLAG 		0x2000
-#define CREATE_MODE_FLAG 	0x4000
-#define OPEN_FLAGS_FLAG		0x8000
-#define COLL_FLAGS_FLAG		0x8000
-#define DATA_SIZE_FLAGS		0x10000
-#define NUM_THREADS_FLAG	0x20000
-#define OPR_TYPE_FLAG		0x40000
-#define OBJ_PATH_FLAG		0x80000
-#define COLL_NAME_FLAG		0x80000
-#define RMTRASH_FLAG	0x100000
-#define ADMIN_RMTRASH_FLAG 0x200000
-#define DEF_RESC_NAME_FLAG 	0x400000
-#define RBUDP_TRANSFER_FLAG 	0x800000
-#define RBUDP_SEND_RATE_FLAG 	0x1000000
-#define RBUDP_PACK_SIZE_FLAG 	0x2000000
-#define BULK_OPR_FLAG 		0x4000000
-#define UNREG_FLAG             0x8000000
+#define RESC_NAME_FLAG          0x1
+#define DEST_RESC_NAME_FLAG     0x2
+#define BACKUP_RESC_NAME_FLAG   0x4
+#define FORCE_FLAG_FLAG         0x8
+#define ALL_FLAG                0x10
+#define LOCAL_PATH_FLAG         0x20
+#define VERIFY_CHKSUM_FLAG      0x40
+#define ADMIN_FLAG              0x80
+#define UPDATE_REPL_FLAG        0x100
+#define REPL_NUM_FLAG           0x200
+#define DATA_TYPE_FLAG          0x400
+#define CHKSUM_ALL_FLAG         0x800
+#define FORCE_CHKSUM_FLAG       0x1000
+#define FILE_PATH_FLAG          0x2000
+#define CREATE_MODE_FLAG        0x4000
+#define OPEN_FLAGS_FLAG         0x8000
+#define COLL_FLAGS_FLAG         0x10000
+#define DATA_SIZE_FLAGS         0x20000
+#define NUM_THREADS_FLAG        0x40000
+#define OPR_TYPE_FLAG           0x80000
+#define OBJ_PATH_FLAG           0x100000
+#define COLL_NAME_FLAG          0x200000
+#define RMTRASH_FLAG            0x400000
+#define ADMIN_RMTRASH_FLAG      0x800000
+#define DEF_RESC_NAME_FLAG      0x1000000
+#define RBUDP_TRANSFER_FLAG     0x2000000
+#define RBUDP_SEND_RATE_FLAG    0x4000000
+#define RBUDP_PACK_SIZE_FLAG    0x8000000
+#define BULK_OPR_FLAG           0x10000000
+#define UNREG_FLAG              0x20000000
 
 
     int
@@ -253,4 +253,4 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
-#endif	/* MS_PARAM_H */
+#endif    /* MS_PARAM_H */
