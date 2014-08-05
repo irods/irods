5b04790369e5c828817b7a8368beba58c5381528
Zoey Greer <tempoz@renci.org>
2014-08-05 17:14:00 -0400

    [#2212] CID37585:
    Removing vestigial error checking

--- plugins/database/src/low_level_odbc.cpp
+++ plugins/database/src/low_level_odbc.cpp
@@ -518,9 +518,8 @@ cllExecSqlNoResult( icatSessionStruct *icss, const char *sql ) {
 */
 void
 logTheBindVariables( int level ) {
-    int i;
-    char tmpStr[TMP_STR_LEN + 2];
-    for ( i = 0; i < cllBindVarCountPrev; i++ ) {
+    for ( int i = 0; i < cllBindVarCountPrev; i++ ) {
+        char tmpStr[TMP_STR_LEN + 2];
         snprintf( tmpStr, TMP_STR_LEN, "bindVar[%d]=%s", i + 1, cllBindVars[i] );
         rodsLog( level, tmpStr );
     }
@@ -531,43 +530,34 @@ logTheBindVariables( int level ) {
 */
 int
 bindTheVariables( HSTMT myHstmt, const char *sql ) {
-    int myBindVarCount;
-    RETCODE stat;
-    int i;
-    char tmpStr[TMP_STR_LEN + 2];
 
-    myBindVarCount = cllBindVarCount;
+    int myBindVarCount = cllBindVarCount;
     cllBindVarCountPrev = cllBindVarCount; /* save in case we need to log error */
     cllBindVarCount = 0; /* reset for next call */
 
     if ( myBindVarCount > 0 ) {
         rodsLogSql( "SQLPrepare" );
-        stat = SQLPrepare( myHstmt, ( unsigned char * )sql, SQL_NTS );
+        RETCODE stat = SQLPrepare( myHstmt, ( unsigned char * )sql, SQL_NTS );
         if ( stat != SQL_SUCCESS ) {
             rodsLog( LOG_ERROR, "bindTheVariables: SQLPrepare failed: %d",
                      stat );
-            return( -1 );
+            return -1;
         }
 
-        for ( i = 0; i < myBindVarCount; i++ ) {
+        for ( int i = 0; i < myBindVarCount; i++ ) {
             stat = SQLBindParameter( myHstmt, i + 1, SQL_PARAM_INPUT, SQL_C_CHAR,
                                      SQL_C_CHAR, 0, 0, cllBindVars[i], 0, 0 );
+            char tmpStr[TMP_STR_LEN + 2];
             snprintf( tmpStr, TMP_STR_LEN, "bindVar[%d]=%s", i + 1, cllBindVars[i] );
             rodsLogSql( tmpStr );
             if ( stat != SQL_SUCCESS ) {
                 rodsLog( LOG_ERROR,
                          "bindTheVariables: SQLBindParameter failed: %d", stat );
-                return( -1 );
+                return -1;
             }
         }
-
-        if ( stat != SQL_SUCCESS ) {
-            rodsLog( LOG_ERROR, "bindTheVariables: SQLAllocStmt failed: %d",
-                     stat );
-            return( -1 );
-        }
     }
-    return( 0 );
+    return 0;
 }
 
 /*
