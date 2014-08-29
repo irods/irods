ee1db3f09921ac08d1b8f51ba730436197758526
Zoey Greer <tempoz@renci.org>
2014-08-29 11:24:37 -0400

    [#2212] CID49416:
    Split null check and arg count check into two checks to make username ==
    NULL a sufficient condition for failure

--- iRODS/clients/icommands/src/genOSAuth.cpp
+++ iRODS/clients/icommands/src/genOSAuth.cpp
@@ -12,59 +12,57 @@
 
 int
 main( int argc, char *argv[] ) {
-    char *username;
-    char username2[NAME_LEN];
-    char *challenge, *keybuf;
-    int uid, nb, challenge_len, key_len;
-    char authenticator[16];  /* hard coded at 16 bytes .. size of an md5 hash */
 
     /* make sure the provided username matches what the OS thinks */
-    username = getenv( OS_AUTH_ENV_USER );
-    if ( username == NULL && argc > 0) {
-        /* probably means someone has run from command-line */
-        printf( "%s is run through an iRODS library call, and probably won't do anything useful for you.\n",
-                argv[0] );
-        printf( "%s exiting.\n", argv[0] );
-        exit( 1 );
+    char * username = getenv( OS_AUTH_ENV_USER );
+    if ( username == NULL ) {
+        if ( argc > 0) {
+            /* probably means someone has run from command-line */
+            printf( "%s is run through an iRODS library call, and probably won't do anything useful for you.\n",
+                    argv[0] );
+            printf( "%s exiting.\n", argv[0] );
+            return 1;
+        }
+        printf( "Username is null" );
+        return 1;
     }
-    uid = osauthGetUsername( username2, NAME_LEN );
+    char username2[NAME_LEN];
+    int uid = osauthGetUsername( username2, NAME_LEN );
     if ( uid == -1 ) {
-        exit( 1 );
+        return 1;
     }
     if ( strcmp( username, username2 ) ) {
         printf( "Username %s does not match OS user %s",
                 username, username2 );
-        exit( 1 );
+        return 1;
     }
 
     /* read the challenge from stdin */
-    nb = read( 0, ( void* )&challenge_len, sizeof( int ) );
-    if ( nb != sizeof( int ) ) {
+    int challenge_len;
+    if ( sizeof( int ) != read( 0, ( void* )&challenge_len, sizeof( int ) ) ) {
         printf( "Couldn't read challenge length from stdin: %s",
                 strerror( errno ) );
-        exit( 1 );
+        return 1;
     }
-    challenge = ( char* )malloc( challenge_len );
-    if ( challenge == NULL ) {
-        printf( "Error allocating memory for challenge: %s", strerror( errno ) );
-        exit( 1 );
-    }
-    nb = read( 0, challenge, challenge_len );
-    if ( nb != challenge_len ) {
+    char * challenge = ( char* )malloc( challenge_len );
+    if ( challenge_len != read( 0, challenge, challenge_len ) ) {
         printf( "Couldn't read challenge from stdin: %s",
                 strerror( errno ) );
-        exit( 1 );
+        return 1;
     }
 
     /* read the key from the key file */
+    char * keybuf = NULL;
+    int key_len;
     if ( osauthGetKey( &keybuf, &key_len ) ) {
         printf( "Error retrieving key. Exiting." );
-        exit( 1 );
+        return 1;
     }
 
+    char authenticator[16];  /* hard coded at 16 bytes .. size of an md5 hash */
     if ( osauthGenerateAuthenticator( username, uid, challenge, keybuf, key_len, authenticator, 16 ) ) {
         printf( "Could not generate the authenticator" );
-        exit( 1 );
+        return 1;
     }
 
     /* write out the authenticator to stdout */
@@ -72,9 +70,9 @@ main( int argc, char *argv[] ) {
         int errsv = errno;
         printf( "Error %s writing the authenticator to stdout.",
                 strerror( errsv ) );
-        exit( 1 );
+        return 1;
     }
 
-    exit( 0 );
+    return 0;
 }
 
