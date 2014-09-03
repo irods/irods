21ff87d8d94b4194022a9e1a2591c73e8c2f2314
Terrell Russell <terrellrussell@gmail.com>
2014-09-03 13:07:55 -0400

    [#2316] add test for dynamic PEP with rsComm usage

--- tests/pydevtest/rulebase_suite.py
+++ tests/pydevtest/rulebase_suite.py
@@ -89,3 +89,21 @@ class Test_RulebaseSuite(unittest.TestCase, ResourceBase):
         time.sleep(1) # remove once file hash fix is commited #2279
         os.system("cp /etc/irods/core.re.orig /etc/irods/core.re")
         time.sleep(1) # remove once file hash fix is commited #2279
+
+    @unittest.skip("[#2316] skipping until fixed")
+    def test_dynamic_pep_with_rscomm_usage(self):
+        # save original core.re
+        os.system("cp /etc/irods/core.re /etc/irods/core.re.orig")
+
+        # add dynamic PEP with rscomm usage
+        time.sleep(1) # remove once file hash fix is commited #2279
+        os.system('''echo "pep_resource_open_pre(*OUT) { msiGetSystemTime( *junk, '' ); }" >> /etc/irods/core.re''')
+        time.sleep(1) # remove once file hash fix is commited #2279
+
+        # check rei functioning
+        assertiCmd(s.adminsession,"iget "+self.testfile+" - ","STDOUT", self.testfile)
+
+        # restore core.re
+        time.sleep(1) # remove once file hash fix is commited #2279
+        os.system("cp /etc/irods/core.re.orig /etc/irods/core.re")
+        time.sleep(1) # remove once file hash fix is commited #2279
