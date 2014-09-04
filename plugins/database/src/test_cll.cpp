aace459b18154931e9b249fa393e981e45c2a631
Zoey Greer <tempoz@renci.org>
2014-09-04 15:24:40 -0400

    [#858] removed if 0'd code

--- plugins/database/src/test_cll.cpp
+++ plugins/database/src/test_cll.cpp
@@ -26,55 +26,7 @@ extern "C" int cllTest( char*, char* );
 
 int
 main( int argc, char **argv ) {
-#if 0
-    rodsServerConfig_t cfg;
-    int status = readServerConfig( &cfg );
-    if ( status < 0 ) {
-        printf( "readServerConfig failed horribly\n" );
-        exit( 0 );
-    }
-
-    irods::plugin_name_generator name_gen;
-
-    std::string so_name;
-    irods::error ret = name_gen(
-                           cfg.catalog_database_type,
-                           irods::IRODS_DATABASE_HOME,
-                           so_name );
-    if ( !ret.ok() ) {
-        printf( "name_gen failed" );
-        return -1;
-    }
-
-    void* handle = dlopen( so_name.c_str(), RTLD_LAZY );
-    if ( !handle ) {
-        std::stringstream msg;
-        msg << "failed to open shared object file [" << so_name
-            << "] :: dlerror: is [" << dlerror() << "]";
-        std::cout << msg.str() << std::endl;
-        return -1;
-    }
-
-
-    typedef int ( *test_type )( char*, char* );
-    test_type cll_test = reinterpret_cast< test_type >( dlsym( handle, "cllTest" ) );
-    char* err = 0;
-    if ( ( err = dlerror() ) != 0 ) {
-        std::stringstream msg;
-        msg << "failed to load symbol from shared object handle - cllTest"
-            << " :: dlerror is [" << err << "]";
-        dlclose( handle );
-        std::cout << msg.str() << std::endl;
-        return -1;
-    }
-
-    //int i = cllTest( argv[1], argv[2] );
-    return cll_test( argv[1], argv[2] );
-
-    dlclose( handle );
-#else
     return cllTest( argv[1], argv[2] );
-#endif
 }
 
 /* This is a dummy version of icatApplyRule for this test program so
