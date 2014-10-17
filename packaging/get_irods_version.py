4ee130a2cb09c73f40375f90785868d32fb0945a
Ben Keller <irods.ben.keller@gmail.com>
2014-10-17 11:26:42 -0400

    [#2222] Use python26'isms in get_irods_version.py

--- packaging/get_irods_version.py
+++ packaging/get_irods_version.py
@@ -1,9 +1,9 @@
+from __future__ import print_function
+
 import os
 import json
 
-irodstoplevel = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-#print irodstoplevel
-fh = open(irodstoplevel+"/VERSION.json")
-data = json.load(fh)
-print data['irods_version']
-
+irods_top_level = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+with open(os.path.join(irods_top_level, 'VERSION.json')) as fh:
+    data = json.load(fh)
+print(data['irods_version'])
