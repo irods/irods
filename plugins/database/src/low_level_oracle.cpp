0bb6b5358637bbbfb4f69b222382dba32366287b
Ben Keller <irods.ben.keller@gmail.com>
2014-11-10 21:04:18 -0500

    [#2407] Fixed const assignment error

--- plugins/database/src/low_level_oracle.cpp
+++ plugins/database/src/low_level_oracle.cpp
@@ -320,12 +320,10 @@ cllDisconnect( icatSessionStruct *icss ) {
    Oracle, ? becomes :1, :2, etc. */
 int
 convertSqlToOra( const char *sql, char *sqlOut ) {
-    char *cp1, *cp2, *cpEnd;
-    int i, hundreds, tens, ones;
-    cp1 = sql;
-    cp2 = sqlOut;
-    cpEnd = cp2 + MAX_SQL_SIZE - 2;
-    i = 1;
+    const char *cp1 = sql;
+    char *cp2 = sqlOut;
+    const char * const cpEnd = cp2 + MAX_SQL_SIZE - 2;
+    int i = 1;
     while ( *cp1 != '\0' ) {
         if ( *cp1 != '?' ) {
             *cp2++ = *cp1++;
@@ -333,9 +331,9 @@ convertSqlToOra( const char *sql, char *sqlOut ) {
         else {
             *cp2++ = ':';
             /* handle cases with up to 999 bind variables */
-            tens = i / 10 ;
-            ones = i % 10;
-            hundreds = i / 100;
+            int tens = i / 10 ;
+            const int ones = i % 10;
+            const int hundreds = i / 100;
             if ( hundreds > 0 ) {
                 tens = ( i - ( hundreds * 100 ) ) / 10;
                 *cp2++ = hundreds + '0';
