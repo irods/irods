717bae164f166217b1346e4dc2baeba6d71f452c
Terrell Russell <terrellrussell@gmail.com>
2014-11-24 14:29:16 -0500

    [#2342] updates oracle Makefile to use ORACLE_HOME
    Satisfies #1 of https://github.com/irods/irods/issues/2342#issuecomment-59072854
    
    Still need to prompt for ORACLE_HOME on installation, and export for the client
    library for runtime.

--- plugins/database/packaging/find_odbc_oracle.sh
+++ plugins/database/packaging/find_odbc_oracle.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-echo "/usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1"
+if [ $ORACLE_HOME ] ; then
+    echo "${ORACLE_HOME}/lib/libsqora.so.11.1"
+else
+    echo "/usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1"
+fi
