23e1469ca3a2b05216a7a8a1d9bccfde227245f1
Hao Xu <xuh@email.unc.edu>
2015-02-27 20:46:49 -0500

    [#2544] allow '-' in plugin names

--- iRODS/lib/core/src/irods_plugin_name_generator.cpp
+++ iRODS/lib/core/src/irods_plugin_name_generator.cpp
@@ -18,7 +18,7 @@ namespace irods {
 // =-=-=-=-=-=-=-
 // predicate to determine if a char is not alphanumeric
     static bool not_allowed_char( char _c ) {
-        return !std::isalnum( _c ) && !( '_' == _c );
+        return !std::isalnum( _c ) && !( '_' == _c ) && !( '-' == _c );
     } // not_allowed_char
 
 
