6f3c177f9d903b5e4b89a8763c67207a5911d97c
Ben Keller <irods.ben.keller@gmail.com>
2015-03-03 16:07:16 -0500

    [#2547] Remove delay from rulemsiservermonperf so msi is tested

--- iRODS/clients/icommands/test/rules3.0/rulemsiServerMonPerf.r
+++ iRODS/clients/icommands/test/rules3.0/rulemsiServerMonPerf.r
@@ -1,9 +1,8 @@
 acServerMonPerf {
 #This microservice invokes a command in iRODS/server/bin/cmd
 #    irodsServerMonPerf     - a perl script to get monitoring information
-	delay("<PLUSET>30s</PLUSET>< EF>1h</EF>") {
     	msiServerMonPerf("default","default");
-	}
+    	msiServerMonPerf("verbose","default");
 }
 INPUT null
 OUTPUT ruleExecOut
