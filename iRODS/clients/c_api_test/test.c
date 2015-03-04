f4d8c6354597c338054a60aa3966ce5b60046439
Terrell Russell <terrellrussell@gmail.com>
2015-03-04 12:12:19 -0500

    [#2535] new c api test client
    This new C API test client will ensure the C API remains consistent
    and will not include any C++ symbols.
    
    This new client is part of the main Makefile.

---
+++ iRODS/clients/c_api_test/test.c
@@ -0,0 +1,33 @@
+
+// A Haiku for C
+// This test keeps C++ out
+// Or it breaks the build
+
+#include "rodsType.hpp"
+#include "rodsErrorTable.hpp"
+#include "rodsClient.hpp"
+#include "miscUtil.hpp"
+#include "rodsPath.hpp"
+#include "rcConnect.hpp"
+#include "dataObjOpen.hpp"
+#include "dataObjRead.hpp"
+#include "dataObjChksum.hpp"
+#include "dataObjClose.hpp"
+
+int main () {
+    rodsEnv myEnv;
+    getRodsEnv( &myEnv );
+    rErrMsg_t errMsg;
+
+    rcComm_t* conn = rcConnect( myEnv.rodsHost, myEnv.rodsPort, myEnv.rodsUserName, myEnv.rodsZone, 1, &errMsg );
+
+    if ( ! conn ) {
+        printf( "rcConnect failed\n");
+        return -1;
+    }
+    else {
+        printf( "Success!\n");
+        rcDisconnect( conn );
+    }
+    return 0;
+}
