irods/irods

commit e36c63ded853a36b0290756b6c0bcf48b741eea8

[#2212] CID49433:
removed possible deadlock condition, removed race condition, moved logic
for better response times

iRODS/clients/fuse/src/iFuseLib.Conn.cpp
@@ -19,15 +19,16 @@
  */
 concurrentList_t *ConnectedConn;
 concurrentList_t *FreeConn;
-concurrentList_t *ConnReqWaitQue;
+connReqWait_t connReqWait;
 static PathCacheTable *pctable;
 
 static int ConnManagerStarted = 0;
 
 void initConn() {
+    bzero( &connReqWait, sizeof( connReqWait ) );
+    initConnReqWaitMutex( &connReqWait );
     ConnectedConn = newConcurrentList();
     FreeConn = newConcurrentList();
-    ConnReqWaitQue = newConcurrentList();
     pctable = initPathCache();
 }
 /* getIFuseConnByPath - try to use the same conn as opened desc of the
@@ -106,63 +107,37 @@ int getAndUseIFuseConn( iFuseConn_t **iFuseConn ) {
 
 }
 
-void _waitForConn() {
-    connReqWait_t myConnReqWait;
-    bzero( &myConnReqWait, sizeof( myConnReqWait ) );
-    initConnReqWaitMutex( &myConnReqWait );
-    addToConcurrentList( ConnReqWaitQue, &myConnReqWait );
-
-    untimedWait( &myConnReqWait.mutex, &myConnReqWait.cond );
-
-    deleteConnReqWaitMutex( &myConnReqWait );
-}
-
 int _getAndUseIFuseConn( iFuseConn_t **iFuseConn ) {
     int status;
     iFuseConn_t *tmpIFuseConn;
 
     *iFuseConn = NULL;
 
-    while ( *iFuseConn == NULL ) {
-        /* get a free IFuseConn */
+    LOCK( WaitForConnLock );
+    while ( listSize( ConnectedConn ) >= MAX_NUM_CONN && listSize( FreeConn ) == 0 ) {
+        untimedWait( &connReqWait.mutex, &connReqWait.cond );
+    }
 
-        if ( listSize( ConnectedConn ) >= MAX_NUM_CONN && listSize( FreeConn ) == 0 ) {
-            /* have to wait */
-            _waitForConn();
-            /* start from begining */
-            continue;
-        }
-        else {
-            tmpIFuseConn = ( iFuseConn_t * ) removeFirstElementOfConcurrentList( FreeConn );
-            if ( tmpIFuseConn == NULL ) {
-                if ( listSize( ConnectedConn ) < MAX_NUM_CONN ) {
-                    /* may cause num of conn > max num of conn */
-                    /* get here when nothing free. make one */
-                    tmpIFuseConn = newIFuseConn( &status );
-                    if ( status < 0 ) {
-                        _freeIFuseConn( tmpIFuseConn );
-                        return status;
-                    }
-
-                    _useFreeIFuseConn( tmpIFuseConn );
-                    addToConcurrentList( ConnectedConn, tmpIFuseConn );
-
-                    *iFuseConn = tmpIFuseConn;
-                    break;
-
-                }
-                _waitForConn();
-                continue;
+    tmpIFuseConn = ( iFuseConn_t * ) removeFirstElementOfConcurrentList( FreeConn );
+    if ( tmpIFuseConn == NULL ) {
+        if ( listSize( ConnectedConn ) < MAX_NUM_CONN ) {
+            tmpIFuseConn = newIFuseConn( &status );
+            if ( status < 0 ) {
+                _freeIFuseConn( tmpIFuseConn );
+                return status;
             }
-            else {
-                useIFuseConn( tmpIFuseConn );
-                *iFuseConn = tmpIFuseConn;
-                break;
-            }
+            _useFreeIFuseConn( tmpIFuseConn );
+            addToConcurrentList( ConnectedConn, tmpIFuseConn );
+            *iFuseConn = tmpIFuseConn;
         }
-
-
-    }	/* while *iFuseConn */
+        rodsLog( LOG_ERROR, "failure to acquire fuse connection; maximum fuse connections exceeded." );
+        return SYS_MAX_CONNECT_COUNT_EXCEEDED;
+    }
+    else {
+        useIFuseConn( tmpIFuseConn );
+        *iFuseConn = tmpIFuseConn;
+    }
+    UNLOCK( WaitForConnLock );
 
     if ( ++ConnManagerStarted == HIGH_NUM_CONN ) {
         /* don't do it the first time */
@@ -373,21 +348,8 @@ connManager() {
             clearListNoRegion( TimeOutList );
         }
 
-        while ( listSize( ConnectedConn ) <= MAX_NUM_CONN || listSize( FreeConn ) != 0 ) {
-            /* signal one in the wait queue */
-            connReqWait_t *myConnReqWait = ( connReqWait_t * ) removeFirstElementOfConcurrentList( ConnReqWaitQue );
-            /* if there is no conn req left, exit loop */
-            if ( myConnReqWait == NULL ) {
-                break;
-            }
-            notifyWait( &myConnReqWait->mutex, &myConnReqWait->cond );
-        }
-#if 0
-        rodsSleep( CONN_MANAGER_SLEEP_TIME, 0 );
-#else
+        notifyWait( &connReqWait.mutex, &connReqWait.cond );
         timeoutWait( &ConnManagerLock, &ConnManagerCond, CONN_MANAGER_SLEEP_TIME );
-#endif
-
 
     }
     deleteListNoRegion( TimeOutList );

iRODS/clients/fuse/include/iFuseLib.Lock.hpp
@@ -42,12 +42,14 @@ typedef struct ConcurrentList {
 extern boost::mutex* PathCacheLock;
 extern boost::thread*            ConnManagerThr;
 extern boost::mutex*             ConnManagerLock;
+extern boost::mutex*             WaitForConnLock;
 extern boost::condition_variable ConnManagerCond;
 #else
 #include <pthread.h>
 extern pthread_mutex_t PathCacheLock;
 extern pthread_t ConnManagerThr;
 extern pthread_mutex_t ConnManagerLock;
+extern pthread_mutex_t WaitForConnLock;
 extern pthread_cond_t ConnManagerCond;
 #endif
 

iRODS/clients/fuse/src/iFuseLib.Lock.cpp
@@ -21,6 +21,7 @@ boost::mutex* PathCacheLock = new boost::mutex();
 /* boost::mutex FileCacheLock; */
 boost::thread*            ConnManagerThr;
 boost::mutex*             ConnManagerLock = new boost::mutex();
+boost::mutex*             WaitForConnLock = new boost::mutex();
 boost::condition_variable ConnManagerCond;
 #else
 /*pthread_mutex_t DescLock;*/
@@ -29,6 +30,7 @@ boost::condition_variable ConnManagerCond;
 /* pthread_mutex_t FileCacheLock; */
 pthread_t ConnManagerThr;
 pthread_mutex_t ConnManagerLock;
+pthread_mutex_t WaitForConnLock;
 pthread_cond_t ConnManagerCond;
 #endif
 

commit 6a5b9eff0a4dc4bf23eb219600e23c35fee4770a

Actually use iFuseConn if opened
if an iFuseConn can be opened, there is no reason to fail with `SYS_MAX_CONNECT_COUNT_EXCEEDED`. I think the intent is to have this as the `else` for the `if ( listSize( ConnectedConn ) < MAX_NUM_CONN)`.

irodsFuse seems to be completely broken since e36c63ded853a36b0290756b6c0bcf48b741eea8 without this change, despite several subsequent commits. Do people test locally before committing to master?

iRODS/clients/fuse/src/iFuseLib.Conn.cpp
@@ -127,8 +127,10 @@ int _getAndUseIFuseConn( iFuseConn_t **iFuseConn ) {
             addToConcurrentList( ConnectedConn, tmpIFuseConn );
             *iFuseConn = tmpIFuseConn;
         }
-        rodsLog( LOG_ERROR, "failure to acquire fuse connection; maximum fuse connections exceeded." );
-        return SYS_MAX_CONNECT_COUNT_EXCEEDED;
+        else {
+            rodsLog( LOG_ERROR, "failure to acquire fuse connection; maximum fuse connections exceeded." );
+            return SYS_MAX_CONNECT_COUNT_EXCEEDED;
+        }
     }
     else {
         useIFuseConn( tmpIFuseConn );