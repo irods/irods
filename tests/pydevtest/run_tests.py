9c983cf21b25ef5f5aff26444b650b16f6c0bf95
Ben Keller <irods.ben.keller@gmail.com>
2015-04-05 23:56:36 -0400

    [#858] Add no_buffer option to run_tests.py

--- tests/pydevtest/run_tests.py
+++ tests/pydevtest/run_tests.py
@@ -43,11 +43,11 @@ def optparse_callback_topology_test(option, opt_str, value, parser):
     pydevtest_common.irods_test_constants.HOSTNAME_2 = 'resource2.example.org'
     pydevtest_common.irods_test_constants.HOSTNAME_3 = 'resource3.example.org'
 
-def run_tests_from_names(names):
+def run_tests_from_names(names, buffer_test_output):
     loader = unittest.TestLoader()
     suites = [loader.loadTestsFromName(name) for name in names]
     super_suite = unittest.TestSuite(suites)
-    runner = unittest.TextTestRunner(verbosity=2, failfast=True, buffer=True, resultclass=RegisteredTestResult)
+    runner = unittest.TextTestRunner(verbosity=2, failfast=True, buffer=buffer_test_output, resultclass=RegisteredTestResult)
     results = runner.run(super_suite)
     return results
 
@@ -63,14 +63,15 @@ class RegisteredTestResult(unittest.TextTestResult):
 
 if __name__ == '__main__':
     parser = optparse.OptionParser()
-    parser.add_option("--run_specific_test", metavar='dotted name')
-    parser.add_option("--run_python_suite", action='store_true')
-    parser.add_option("--include_auth_suite_tests", action='store_true')
-    parser.add_option("--include_fuse_suite_tests", action='store_true')
-    parser.add_option("--run_devtesty", action='store_true')
-    parser.add_option("--topology_test", type='choice', choices=['icat', 'resource'], action='callback', callback=optparse_callback_topology_test, metavar='<icat|resource>')
-    parser.add_option("--catch_keyboard_interrupt", action='callback', callback=optparse_callback_catch_keyboard_interrupt)
-    parser.add_option("--use_ssl", action='callback', callback=optparse_callback_use_ssl)
+    parser.add_option('--run_specific_test', metavar='dotted name')
+    parser.add_option('--run_python_suite', action='store_true')
+    parser.add_option('--include_auth_suite_tests', action='store_true')
+    parser.add_option('--include_fuse_suite_tests', action='store_true')
+    parser.add_option('--run_devtesty', action='store_true')
+    parser.add_option('--topology_test', type='choice', choices=['icat', 'resource'], action='callback', callback=optparse_callback_topology_test, metavar='<icat|resource>')
+    parser.add_option('--catch_keyboard_interrupt', action='callback', callback=optparse_callback_catch_keyboard_interrupt)
+    parser.add_option('--use_ssl', action='callback', callback=optparse_callback_use_ssl)
+    parser.add_option('--no_buffer', action='store_false', dest='buffer_test_output', default=True)
     options, _ = parser.parse_args()
 
     if len(sys.argv) == 1:
@@ -82,13 +83,13 @@ if __name__ == '__main__':
         test_identifiers.append(options.run_specific_test)
     if options.run_python_suite:
         test_identifiers.extend(['test_xmsg', 'iadmin_suite', 'test_mso_suite', 'test_resource_types', 'catalog_suite', 'rulebase_suite',
-                                 'test_resource_tree', 'test_load_balanced_suite', 'test_icommands_file_operations', 'test_imeta_set', 'test_allrules', 'test_iscan', "test_control_plane"])
+                                 'test_resource_tree', 'test_load_balanced_suite', 'test_icommands_file_operations', 'test_imeta_set', 'test_allrules', 'test_iscan', 'test_control_plane'])
     if options.include_auth_suite_tests:
         test_identifiers.append('auth_suite')
     if options.include_fuse_suite_tests:
         test_identifiers.append('test_fuse_suite')
 
-    results = run_tests_from_names(test_identifiers)
+    results = run_tests_from_names(test_identifiers, options.buffer_test_output)
     print(results)
     if not results.wasSuccessful():
         sys.exit(1)
