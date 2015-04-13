529b722b394fdf053daf3d3c2fb7b0021c7a99a9
Zoey Greer <tempoz@renci.org>
2015-04-13 11:35:01 -0400

    [#2064] added guards to metadata_serialization.hpp

--- iRODS/lib/core/include/irods_metadata_serialization.hpp
+++ iRODS/lib/core/include/irods_metadata_serialization.hpp
@@ -1,11 +1,16 @@
+#ifndef IRODS_METADATA_SERIALIZATION_HPP__
+#define IRODS_METADATA_SERIALIZATION_HPP__
 #include <string>
 #include <vector>
 
+#ifdef __cplusplus
 namespace irods {
     std::string serialize_metadata( const std::vector<std::string>& metadata );
     std::vector<std::string> deserialize_metadata( const std::string& metadata );
 }
+#endif
 
 extern "C" {
     char* serialize_metadata_c( const char** metadata, size_t metadata_len );
 }
+#endif
