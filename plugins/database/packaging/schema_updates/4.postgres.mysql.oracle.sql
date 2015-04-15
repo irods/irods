b484f7d710e38441a59299d7aed4f0a4d4801bcf
Terrell Russell <terrellrussell@gmail.com>
2015-04-14 23:33:37 -0400

    [#2400] remove role on upgrade

--- plugins/database/packaging/schema_updates/4.postgres.mysql.oracle.sql
+++ plugins/database/packaging/schema_updates/4.postgres.mysql.oracle.sql
@@ -1,3 +1,4 @@
 create index idx_quota_main1 on R_QUOTA_MAIN (user_id);
 delete from r_tokn_main where token_name = 'domainadmin';
 delete from r_tokn_main where token_name = 'rodscurators';
+delete from r_tokn_main where token_name = 'storageadmin';
