1acf3b6d62148331f92685b6f998d127cdcc2d40
Terrell Russell <terrellrussell@gmail.com>
2015-04-16 12:54:07 -0400

    [#2608] throw warning on install with short hostname

--- packaging/setup_irods.sh
+++ packaging/setup_irods.sh
@@ -7,6 +7,19 @@ else
     PYTHON=`type -P python`
 fi
 
+# throw STDERR warning if short hostname found
+MYHOST=`hostname`
+if [[ ! $MYHOST == *.* ]] ; then
+    echo "" 1>&2
+    echo "********************************************************" 1>&2
+    echo "*" 1>&2
+    echo "* iRODS Setup WARNING:" 1>&2
+    echo "*       hostname [$MYHOST] may need to be a FQDN" 1>&2
+    echo "*" 1>&2
+    echo "********************************************************" 1>&2
+    echo "" 1>&2
+fi
+
 # locate current directory
 DETECTEDDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
