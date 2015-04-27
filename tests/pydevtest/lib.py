6dc54d448e64a588aaa3fbebe0411c694bc03c5d
Ben Keller <irods.ben.keller@gmail.com>
2015-04-27 16:31:38 -0400

    [#858] Move irods_session into lib
    Removes circular import dependency between the two

--- tests/pydevtest/lib.py
+++ tests/pydevtest/lib.py
@@ -1,6 +1,6 @@
-import sys
 import commands
 import contextlib
+import datetime
 import itertools
 import json
 import mmap
@@ -11,11 +11,11 @@ import shlex
 import shutil
 import socket
 import subprocess
+import sys
 import tempfile
 import time
 
 import configuration
-import irods_session
 
 
 def update_json_file_from_dict(filename, update_dict):
@@ -336,7 +336,7 @@ def make_session_for_existing_admin():
     username = service_env['irods_user_name']
     zone_name = service_env['irods_zone_name']
     env_dict = make_environment_dict(username, configuration.ICAT_HOSTNAME, zone_name)
-    return irods_session.IrodsSession(env_dict, configuration.PREEXISTING_ADMIN_PASSWORD, False)
+    return IrodsSession(env_dict, configuration.PREEXISTING_ADMIN_PASSWORD, False)
 
 def mkuser_and_return_session(user_type, username, password, hostname):
     service_env = get_service_account_environment_file_contents()
@@ -345,7 +345,7 @@ def mkuser_and_return_session(user_type, username, password, hostname):
         admin_session.assert_icommand(['iadmin', 'mkuser', username, user_type])
         admin_session.assert_icommand(['iadmin', 'moduser', username, 'password', password])
         env_dict = make_environment_dict(username, hostname, zone_name)
-        return irods_session.IrodsSession(env_dict, password, True)
+        return IrodsSession(env_dict, password, True)
 
 def mkgroup_and_add_users(group_name, usernames):
     with make_session_for_existing_admin() as admin_session:
@@ -378,3 +378,160 @@ def make_sessions_mixin(rodsadmin_name_password_list, rodsuser_name_password_lis
                     admin_session.assert_icommand(['iadmin', 'rmuser', session.username])
             super(SessionsMixin, self).tearDown()
     return SessionsMixin
+
+class IrodsSession(object):
+    def __init__(self, environment_file_contents, password, manage_irods_data):
+        self._environment_file_contents = environment_file_contents
+        self._password = password
+        self._manage_irods_data = manage_irods_data
+
+        self._environment_file_invalid = True
+        self._local_session_dir = tempfile.mkdtemp(prefix='irods-testing-')
+        self._environment_file_path = os.path.join(self._local_session_dir, 'irods_environment.json')
+        self._authentication_file_path = os.path.join(self._local_session_dir, 'irods_authentication')
+        self._session_id = datetime.datetime.utcnow().strftime('%Y-%m-%dZ%H:%M:%S--') + os.path.basename(self._local_session_dir)
+
+        self.assert_icommand(['iinit', self._password])
+        if self._manage_irods_data:
+            self.assert_icommand(['imkdir', self.session_collection])
+            self.assert_icommand(['icd', self.session_collection])
+
+    @property
+    def environment_file_contents(self):
+        self._environment_file_invalid = True
+        return self._environment_file_contents
+
+    @environment_file_contents.setter
+    def environment_file_contents(self, value):
+        self._environment_file_invalid = True
+        self._environment_file_contents = value
+
+    @property
+    def zone_name(self):
+        return self._environment_file_contents['irods_zone_name']
+
+    @property
+    def username(self):
+        return self._environment_file_contents['irods_user_name']
+
+    @property
+    def password(self):
+        return self._password
+
+    @property
+    def default_resource(self):
+        return self._environment_file_contents['irods_default_resource']
+
+    @property
+    def local_session_dir(self):
+        return self._local_session_dir
+
+    @property
+    def session_collection(self):
+        return os.path.join('/', self.zone_name, 'home', self.username, self._session_id)
+
+    @property
+    def home_collection(self):
+        return os.path.join('/', self.zone_name, 'home', self.username)
+
+    def run_icommand(self, *args, **kwargs):
+        self._prepare_run_icommand(args[0], kwargs)
+        return run_command(*args, **kwargs)
+
+    def assert_icommand(self, *args, **kwargs):
+        self._prepare_run_icommand(args[0], kwargs)
+        assert_command(*args, **kwargs)
+
+    def assert_icommand_fail(self, *args, **kwargs):
+        self._prepare_run_icommand(args[0], kwargs)
+        assert_command_fail(*args, **kwargs)
+
+    def _prepare_run_icommand(self, arg, kwargs):
+        self._log_run_icommand(arg)
+        self._write_environment_file()
+        if 'env' not in kwargs:
+            environment = os.environ.copy()
+            environment['IRODS_ENVIRONMENT_FILE'] = self._environment_file_path
+            environment['IRODS_AUTHENTICATION_FILE'] = self._authentication_file_path
+            kwargs['env'] = environment
+
+    def _log_run_icommand(self, arg):
+        valid_icommands = ['iinit', 'ienv', 'ihelp', 'ils', 'icd',
+                           'imkdir', 'ichmod', 'imeta', 'iget', 'iput',
+                           'imv', 'icp', 'irepl', 'iquest', 'irm',
+                           'irmtrash', 'iexit', 'ilsresc', 'imiscsvrinfo',
+                           'iuserinfo', 'ipwd', 'ierror', 'iexecmd', 'ips',
+                           'iqstat', 'ichksum', 'itrim', 'iphymv', 'ibun',
+                           'iphybun', 'ireg', 'imcoll', 'irsync', 'ixmsg',
+                           'irule', 'iqdel', 'iticket', 'iapitest', 'iscan',
+                           'isysmeta', 'iadmin', 'ifsck',]
+
+        if isinstance(arg, basestring):
+            icommand = shlex.split(arg)[0]
+            log_string = arg
+        else:
+            icommand = arg[0]
+            log_string = ' '.join(arg)
+        assert icommand in valid_icommands, icommand
+        write_to_log('server', ' --- run_icommand by [{0}] [{1}] --- \n'.format(self.username, log_string))
+
+    def _write_environment_file(self):
+        if self._environment_file_invalid:
+            with open(self._environment_file_path, 'w') as f:
+                json.dump(self._environment_file_contents, f)
+            self._environment_file_invalid = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type=None, exc_value=None, traceback=None):
+        if self._manage_irods_data:
+            self.assert_icommand('icd')
+            self.assert_icommand(['irm', '-rf', self.session_collection])
+            self.assert_icommand('irmtrash')
+        self.assert_icommand('iexit full')
+        shutil.rmtree(self._local_session_dir)
+
+    def interrupt_icommand(self, fullcmd, filename, filesize):
+        ''' Runs an icommand, but does not let it complete.
+
+        This function terminates the icommand once filename reaches (>=)
+        filesize in bytes.
+
+        Asserts that the icommand was successfully terminated early.
+        '''
+        filename = os.path.abspath(filename)
+        parameters = shlex.split(fullcmd)
+        print "\n"
+        print "INTERRUPTING iCMD"
+        print "running icommand: " + self.username + "[" + fullcmd + "]"
+        print "  filename set to: [" + filename + "]"
+        print "  filesize set to: [" + str(filesize) + "] bytes"
+
+        write_to_log('server', ' --- interrupt icommand [{0}] --- \n'.format(fullcmd))
+
+        env = os.environ.copy()
+        env['IRODS_ENVIRONMENT_FILE'] = self._environment_file_path
+        env['IRODS_AUTHENTICATION_FILE'] = self._authentication_file_path
+        self._write_environment_file()
+
+        timeout = 30
+        begin = time.time()
+        granularity = 0.005
+
+        p = subprocess.Popen(parameters, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
+
+        while time.time() - begin < timeout and (not os.path.exists(filename) or os.stat(filename).st_size < filesize):
+            time.sleep(granularity)
+        if (time.time() - begin) >= timeout:
+            print run_command(['ls', '-l', os.path.dirname(filename)])[1]
+            out, err = p.communicate()
+            print out, err
+            print self.run_icommand(['ils', '-l'])[1]
+            assert False
+        elif p.poll() is None:
+            p.terminate()
+        else:
+            assert False
+
+        return 0
