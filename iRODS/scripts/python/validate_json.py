88c5a003791f86188213bd80c3c4f6cf1be08eb4
Terrell Russell <terrellrussell@gmail.com>
2015-04-28 12:49:53 -0400

    [#2664] workaround for pre-1.0.0 requests

--- iRODS/scripts/python/validate_json.py
+++ iRODS/scripts/python/validate_json.py
@@ -6,38 +6,58 @@ import requests
 if len(sys.argv) != 3:
     sys.exit('Usage: {0} <configuration_file> <schema_url>'.format(sys.argv[0]))
 
+config_file = sys.argv[1]
+schema_uri = sys.argv[2]
+
 def print_error(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
 
 try:
     import jsonschema
 except ImportError:
-    print_error('WARNING: Validation Failed for ['+sys.argv[1]+'] -- jsonschema not installed')
+    print_error('WARNING: Validation Failed for [%s] -- jsonschema not installed' % config_file)
     sys.exit(0)
 
 try:
     # load configuration file
-    with open(sys.argv[1], 'r+') as f:
+    with open(config_file, 'r') as f:
         config = json.load(f)
     # load the schema url
-    response = requests.get(sys.argv[2])
-    schema = json.loads(response.text)
+    response = requests.get(schema_uri)
+    # check response values
+    try:
+        # modern requests
+        schema = json.loads(response.text)
+    except AttributeError:
+        # requests pre-v1.0.0
+        response.encoding = 'utf8'
+        schema = json.loads(response.content)
+
     # validate
     jsonschema.validate( config, schema )
 except (jsonschema.exceptions.RefResolutionError) as e:
-    print_error('WARNING: Validation Failed for ['+sys.argv[1]+']')
+    # could not resolve recursive schema $ref
+    print_error('WARNING: Validation Failed for [%s]' % config_file)
+    print_error('       : against [%s]' % schema_uri)
+    print_error("  {0}: {1}".format(e.__class__.__name__, e))
+    sys.exit(0)
+except (ValueError) as e:
+    # most network errors and 404s
+    print_error('WARNING: Validation Failed for [%s]' % config_file)
+    print_error('       : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
     sys.exit(0)
 except (
-        ValueError,
         jsonschema.exceptions.ValidationError,
         jsonschema.exceptions.SchemaError
         ) as e:
-    print_error('ERROR: Validation Failed for ['+sys.argv[1]+']')
+    print_error('ERROR: Validation Failed for [%s]' % config_file)
+    print_error('     : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
     sys.exit(1)
 except Exception as e:
-    print_error('ERROR: Validation Failed for ['+sys.argv[1]+']')
+    print_error('ERROR: Validation Failed for [%s]' % config_file)
+    print_error('     : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
     sys.exit(1)
 except:
