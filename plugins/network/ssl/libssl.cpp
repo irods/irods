27c0115346484330881b946fa4caf6c14fea269f
Ben Keller <irods.ben.keller@gmail.com>
2015-05-04 11:19:55 -0400

    [#2457] Allow only TLSv1.2

--- plugins/network/ssl/libssl.cpp
+++ plugins/network/ssl/libssl.cpp
@@ -215,7 +215,7 @@ static SSL_CTX* ssl_init_context(
 
     ctx = SSL_CTX_new( SSLv23_method() );
 
-    SSL_CTX_set_options( ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_SINGLE_DH_USE );
+    SSL_CTX_set_options( ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2 | SSL_OP_NO_SSLv3 | SSL_OP_NO_TLSv1 | SSL_OP_NO_TLSv1_1 | SSL_OP_SINGLE_DH_USE );
 
     /* load our keys and certificates if provided */
     if ( certfile ) {
