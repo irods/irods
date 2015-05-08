cf6ffb4d873c92ff26efab2984ef8d403c6ce4ca
Ben Keller <irods.ben.keller@gmail.com>
2015-05-08 11:53:48 -0400

    [#2410] All caps table names for MySQL

--- plugins/database/packaging/schema_updates/4.postgres.mysql.oracle.sql
+++ plugins/database/packaging/schema_updates/4.postgres.mysql.oracle.sql
@@ -1,4 +1,4 @@
 create index idx_quota_main1 on R_QUOTA_MAIN (user_id);
-delete from r_tokn_main where token_name = 'domainadmin';
-delete from r_tokn_main where token_name = 'rodscurators';
-delete from r_tokn_main where token_name = 'storageadmin';
+delete from R_TOKN_MAIN where token_name = 'domainadmin';
+delete from R_TOKN_MAIN where token_name = 'rodscurators';
+delete from R_TOKN_MAIN where token_name = 'storageadmin';
