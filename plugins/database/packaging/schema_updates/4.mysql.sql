94228e46807c6ad9d273368a41f828b2dcfa6040
Terrell Russell <terrellrussell@gmail.com>
2015-05-11 12:03:19 -0400

    [#2483] remove DataObjInCollReCur specific query from MySQL
    This specific query was shipped in schema_update 3, however,
    the WITH statement is not supported in MySQL.
    
    This requires splitting the schema_updates file into different
    flavors and updating the EPM scripts.
    
    The current iRODS behavior is maintained, just with less logging
    of an error in the server log.

---
+++ plugins/database/packaging/schema_updates/4.mysql.sql
@@ -0,0 +1,5 @@
+create index idx_quota_main1 on R_QUOTA_MAIN (user_id);
+delete from R_TOKN_MAIN where token_name = 'domainadmin';
+delete from R_TOKN_MAIN where token_name = 'rodscurators';
+delete from R_TOKN_MAIN where token_name = 'storageadmin';
+delete from R_SPECIFIC_QUERY where alias = 'DataObjInCollReCur';
