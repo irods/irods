fa18012036df50de6c619aa9256cef43ef157f2e
Jason Coposky <jasonc@renci.org>
2015-05-15 15:01:56 -0400

    [#858] helper script for user space icommands

---
+++ packaging/make_icommands_for_distribution.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# this script bundles the icommands and required client-side plugins
+# for distribution to users who do not have the necessary root privileges
+# to install the OS level binary packages
+
+# the script will create a tgz file ( icommands.tgz ) which may be extracted in
+# the user's home directory.  the irods_plugins_home variable in the 
+# .irods/irods_environment.json file must be set to the fully qualified path
+# of the icommands/plugins/ directory:
+#
+# {
+#     "irods_plugins_home" : "/home/joeuser/bin/icommands/plugins/"
+# 
+# }
+#
+# once this file is created, iinit may be run to finish setting up the
+# irods environment.
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+mkdir -p $SCRIPTPATH/icommands
+mkdir -p $SCRIPTPATH/icommands/plugins
+mkdir -p $SCRIPTPATH/icommands/plugins/auth
+mkdir -p $SCRIPTPATH/icommands/plugins/network
+mkdir -p $SCRIPTPATH/icommands/plugins/api
+
+cp $SCRIPTPATH/iRODS/clients/icommands/bin/* $SCRIPTPATH/icommands
+
+cp $SCRIPTPATH/plugins/auth/*.so $SCRIPTPATH/icommands/plugins/auth
+cp $SCRIPTPATH/plugins/network/*.so $SCRIPTPATH/icommands/plugins/network
+cp $SCRIPTPATH/plugins/api/*.so $SCRIPTPATH/icommands/plugins/api
+
+tar zcf icommands.tgz $SCRIPTPATH/icommands/
+
+rm -rf $SCRIPTPATH/icommands
+
