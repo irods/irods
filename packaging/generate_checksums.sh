ada7aa556f020445f9c19c1277ca98777fdcabd9
Terrell Russell <terrellrussell@gmail.com>
2015-05-29 11:54:04 -0400

    [#2222] refactor checksum generation for nested directories

--- packaging/generate_checksums.sh
+++ packaging/generate_checksums.sh
@@ -5,11 +5,21 @@
 # Output just goes to stdout - redirect accordingly
 #
 
+checksum_file() {
+    if [ ! -d "$1" ] ; then
+        FILENAME="$1"
+        echo ""
+        echo "$FILENAME"
+        echo "  md5   "`openssl md5 $FILENAME | awk '{print $2}'`
+        echo "  sha1  "`openssl sha1 $FILENAME | awk '{print $2}'`
+    fi
+}
+
 # make sure openssl is installed
 OPENSSL=`which openssl`
 if [[ "$?" != "0" ]] ; then
-  echo "OpenSSL required" 1>&2
-  exit 1
+    echo "OpenSSL required" 1>&2
+    exit 1
 fi
 
 echo ""
@@ -17,33 +27,13 @@ echo "Generated: "`date`
 
 # is this a directory?
 if [ -d "$1" ] ; then
-  # if so, jump in and loop through it
-  cd $1
-  for FILENAME in *
-  do
-    if [ -d "$FILENAME" ] ; then
-      echo ""
-      echo "$FILENAME/ -- Skipping"
-    else
-      echo ""
-      echo "$FILENAME"
-      echo "  md5   "`openssl md5 $FILENAME | awk '{print $2}'`
-      echo "  sha1  "`openssl sha1 $FILENAME | awk '{print $2}'`
-    fi
-  done
+    for FILENAME in `find $1`
+    do
+        checksum_file $FILENAME
+    done
 # else, a single file
 else
-  # is this a real file?
-  if [ -f "$1" ] ; then
-    FILENAME="$1"
-    echo ""
-    echo "$FILENAME"
-    echo "  md5   "`openssl md5 $FILENAME | awk '{print $2}'`
-    echo "  sha1  "`openssl sha1 $FILENAME | awk '{print $2}'`
-  else
-    echo "ERROR: [$1] is not a file or directory" 1>&2
-    exit 1
-  fi
+    checksum_file $1
 fi
 
 echo ""
