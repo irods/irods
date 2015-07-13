bc7f18c07eb3a2afbdcbcd0ed7bf8daf849772b5
Zoey Greer <tempoz@renci.org>
2015-07-13 17:38:04 -0400

    [#2794] change out fuser for lsof

--- iRODS/scripts/python/terminate_irods_processes.py
+++ iRODS/scripts/python/terminate_irods_processes.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 import subprocess
 import os
+import sys
 import psutil
 
 #
@@ -9,36 +10,37 @@ import psutil
 # checks their executing state, then pauses and kills each one.
 #
 
-def get_fuser_output(fullpath):
-   p = subprocess.Popen(['fuser', fullpath], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-   stdout, _ = p.communicate()
-   return stdout
+def get_pids_executing_binary_file(binary_file_path):
+    # get lsof listing of pids
+    p = subprocess.Popen(['lsof', '-F', 'pf', binary_file_path],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    out = out.decode() if p.returncode == 0 else ''
+    parsed_out = parse_formatted_lsof_output(out)
+    try:
+        # we only want pids in executing state
+        return [int(d['p']) for d in parsed_out if d['f'] == 'txt']
+    except (ValueError, KeyError):
+        raise IrodsControllerError('\n\t'.join([
+            'non-conforming lsof output:',
+            '{0}'.format(out),
+            '{0}'.format(err)]),
+            sys.exc_info()[2])
 
-def get_pids(fuser_output):
-    # get fuser listing of pids
-    pid_string = fuser_output.split(':')[-1].strip()
-    if not pid_string:
-        return []
-    pid_strings = pid_string.split()
-    # we only want pids in executing state
-    def get_pid_if_executing(s):
-        if not s[-1] == 'e':
-            return None
-        try:
-            return int(s[:-1])
-        except ValueError:
-            return False
-    pids = []
-    for s in pid_strings:
-        p = get_pid_if_executing(s)
-        if p:
-            pids.append(p)
-    return pids
+def parse_formatted_lsof_output(output):
+    parsed_output = []
+    if output.strip():
+        for line in output.split():
+            if line[0] == output[0]:
+                parsed_output.append({})
+            parsed_output[-1][line[0]] = line[1:]
+    return parsed_output
 
 top_level_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
 for binary in ["irodsServer", "irodsReServer", "irodsXmsgServer", "irodsAgent"]:
     full_path = os.path.join(top_level_dir, "iRODS/server/bin/"+binary)
-    for pid in get_pids(get_fuser_output(full_path)):
+    for pid in get_pids_executing_binary_file(full_path):
         #print("killing %d %s" % (pid, full_path))
         p = psutil.Process(pid)
         p.suspend()
