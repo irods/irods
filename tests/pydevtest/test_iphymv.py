ca7896c40ce3241a7344e922be70e2caaf325506
Jason Coposky <jasonc@renci.org>
2015-07-29 14:38:16 -0400

    [#2820] iphymv test

--- tests/pydevtest/test_iphymv.py
+++ tests/pydevtest/test_iphymv.py
@@ -25,12 +25,25 @@ class Test_iPhymv(ResourceBase, unittest.TestCase):
         shutil.rmtree(self.testing_tmp_dir)
         super(Test_iPhymv, self).tearDown()
 
-    def test_iphymv_invalid_resource(self):
+    def test_iphymv_invalid_resource__2821(self):
         filepath = os.path.join(self.admin.local_session_dir, 'file')
         lib.make_file(filepath, 1)
         dest_path = self.admin.session_collection + '/file'
         self.admin.assert_icommand('iput ' + filepath)
         self.admin.assert_icommand('iphymv -S invalidResc -R demoResc '+dest_path, 'STDERR_SINGLELINE', 'SYS_RESC_DOES_NOT_EXIST')
         self.admin.assert_icommand('iphymv -S demoResc -R invalidResc '+dest_path, 'STDERR_SINGLELINE', 'SYS_RESC_DOES_NOT_EXIST')
+
+    def test_iphymv_unable_to_unlink__2820(self):
+        filepath = os.path.join(self.admin.local_session_dir, 'file')
+        lib.make_file(filepath, 1)
+        dest_path = self.admin.session_collection + '/file'
+        self.admin.assert_icommand('ireg '+filepath+' '+dest_path)
+
+        os.chmod(filepath, 0444)
+        self.admin.assert_icommand('iphymv -S demoResc -R pydevtest_TestResc '+dest_path, 'STDERR_SINGLELINE', 'SYS_USER_NO_PERMISSION')
+        
+        os.chmod(filepath, 0666)
+        self.admin.assert_icommand('iphymv -S demoResc -R pydevtest_TestResc '+dest_path)
+
 
 
