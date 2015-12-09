cb69d5cb6fe9f482e0e392d686989e33dfa9fafc
Terrell Russell <terrellrussell@gmail.com>
2015-12-09 16:57:04 -0500

    [#2984] schema connection errors should be warnings

--- iRODS/scripts/python/validate_json.py
+++ iRODS/scripts/python/validate_json.py
@@ -45,7 +45,7 @@ def get_initial_schema(schema_uri):
         sys.exit(1)
 
 def get_initial_schema_from_web(schema_uri):
-    response = requests.get(schema_uri)
+    response = requests.get(schema_uri, timeout=5)
     # check response values
     try:
         # modern requests
@@ -75,8 +75,17 @@ except (jsonschema.exceptions.RefResolutionError) as e:
     print_error('       : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
     sys.exit(0)
+except (
+        requests.exceptions.ConnectionError,
+        requests.exceptions.Timeout
+) as e:
+    # network connection error or timeout
+    print_error('WARNING: Validation Failed for [%s]' % config_file)
+    print_error('       : against [%s]' % schema_uri)
+    print_error("  {0}: {1}".format(e.__class__.__name__, e))
+    sys.exit(0)
 except (ValueError) as e:
-    # most network errors and 404s
+    # 404s and bad JSON
     print_error('WARNING: Validation Failed for [%s]' % config_file)
     print_error('       : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
@@ -85,11 +94,13 @@ except (
         jsonschema.exceptions.ValidationError,
         jsonschema.exceptions.SchemaError
 ) as e:
+    # actual bad validation, hard stop
     print_error('ERROR: Validation Failed for [%s]' % config_file)
     print_error('     : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
     sys.exit(1)
 except Exception as e:
+    # anything else, hard stop
     print_error('ERROR: Validation Failed for [%s]' % config_file)
     print_error('     : against [%s]' % schema_uri)
     print_error("  {0}: {1}".format(e.__class__.__name__, e))
