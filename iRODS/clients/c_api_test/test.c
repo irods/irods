935c7170cf68e81d379d068ce22ff9a0857458f8
Terrell Russell <terrellrussell@gmail.com>
2015-12-17 16:11:22 -0500

    [#2999] bump version for 4.1.8

--- iRODS/clients/c_api_test/test.c
+++ iRODS/clients/c_api_test/test.c
@@ -14,7 +14,7 @@
 #include "dataObjChksum.h"
 #include "dataObjClose.h"
 
-#if IRODS_VERSION_INTEGER != 4001007
+#if IRODS_VERSION_INTEGER != 4001008
     #error "IRODS_VERSION_INTEGER needs attention"
 #endif
 
