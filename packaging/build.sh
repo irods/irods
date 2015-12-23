25818eb1d315e389310c152a47668335561fd8cc
Jason Coposky <jasonc@renci.org>
2015-12-22 19:14:13 -0500

    [#2971] check db type for RIP deps

--- packaging/build.sh
+++ packaging/build.sh
@@ -1108,8 +1108,10 @@ if [ "$RUNINPLACE" == "1" ] ; then
             check_package_installed "dpkg -s" "python-jsonschema"
         fi
         # externals
-        check_package_installed "dpkg -s" "libmysqlclient-dev"
-        check_package_installed "dpkg -s" "libpcre3-dev"
+        if [ "$DATABASE_PLUGIN_TYPE" == "mysql" ] ; then
+		check_package_installed "dpkg -s" "libmysqlclient-dev"
+		check_package_installed "dpkg -s" "libpcre3-dev"
+        fi
         check_package_installed "dpkg -s" "libtool"
         check_package_installed "dpkg -s" "automake"
     elif [ "$DETECTEDOS" == "RedHatCompatible" ] ; then # CentOS and RHEL and Fedora
@@ -1123,8 +1125,10 @@ if [ "$RUNINPLACE" == "1" ] ; then
         check_package_installed "rpm -q" "fuse-libs"
         check_package_installed "rpm -q" "perl-JSON"
         # externals
-        check_package_installed "rpm -q" "mysql-devel"
-        check_package_installed "rpm -q" "pcre-devel"
+        if [ "$DATABASE_PLUGIN_TYPE" == "mysql" ] ; then
+            check_package_installed "rpm -q" "mysql-devel"
+            check_package_installed "rpm -q" "pcre-devel"
+        fi
         check_package_installed "rpm -q" "libtool"
     elif [ "$DETECTEDOS" == "SuSE" ] ; then
         # all os
@@ -1138,8 +1142,10 @@ if [ "$RUNINPLACE" == "1" ] ; then
         check_package_installed "rpm -q" "libfuse2"
         check_package_installed "rpm -q" "perl-JSON"
         # externals
-        check_package_installed "rpm -q" "libmysqlclient-devel"
-        check_package_installed "rpm -q" "pcre-devel"
+        if [ "$DATABASE_PLUGIN_TYPE" == "mysql" ] ; then
+            check_package_installed "rpm -q" "libmysqlclient-devel"
+            check_package_installed "rpm -q" "pcre-devel"
+        fi
         check_package_installed "rpm -q" "libtool"
     elif [ "$DETECTEDOS" == "MacOSX" ] ; then
         # externals
@@ -1156,8 +1162,10 @@ if [ "$RUNINPLACE" == "1" ] ; then
                 exit 1
             fi
         fi
-        check_package_installed "brew list" "mysql"
-        check_package_installed "brew list" "pcre"
+        if [ "$DATABASE_PLUGIN_TYPE" == "mysql" ] ; then
+            check_package_installed "brew list" "mysql"
+            check_package_installed "brew list" "pcre"
+        fi
     fi
 
 fi
