949bf6a6fcc2ab80892cc0a05d70580e4e64edb6
Simon Fraser <srf@sanger.ac.uk>
2016-02-13 12:07:24 +0000

    Ensure that oracle include directory can be found
    As the instant client library doesn't put the includes
    directly in ORACLE_HOME

--- plugins/database/packaging/find_oracle_include.sh
+++ plugins/database/packaging/find_oracle_include.sh
@@ -4,7 +4,7 @@ if [ ${ORACLE_HOME} ] ; then
     oci=`find $ORACLE_HOME -name "oci.h"`
     if [ ! -z ${oci} ] ; then
         if [ -e ${oci} ] ; then
-            echo ${ORACLE_HOME}
+            dirname $oci
 	    else
 		echo "/usr/include/oracle/11.2/client64"
 	    fi
