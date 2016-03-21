2ebc7b599c4a825f4074300697b13e801b8545b5
Zoey Greer <tempoz@renci.org>
2016-03-21 16:17:04 -0400

    [#2917] remove unused shell scripts

--- plugins/database/packaging/find_odbc_oracle.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $ORACLE_HOME ] ; then
-    echo "${ORACLE_HOME}/lib/libsqora.so.11.1"
-else
-    echo "/usr/lib/oracle/11.2/client64/lib/libsqora.so.11.1"
-fi
--- plugins/database/packaging/find_oracle_lib.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ $ORACLE_HOME ] ; then
-    echo ${ORACLE_HOME}
-else
-    echo /usr/lib/oracle/11.2/client64
-fi
