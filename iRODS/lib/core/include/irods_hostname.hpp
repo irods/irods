ee5de09b70b401d838484901e4edd079f6a463f1
Ben Keller <irods.ben.keller@gmail.com>
2016-03-28 00:33:31 -0400

    [#3090] Implement hostname_resolves_to_local_address

---
+++ iRODS/lib/core/include/irods_hostname.hpp
@@ -0,0 +1,7 @@
+#ifndef IRODS_HOSTNAME_HPP
+#define IRODS_HOSTNAME_HPP
+
+bool
+hostname_resolves_to_local_address(const char* hostname);
+
+#endif
---
+++ iRODS/server/core/src/hostname_resolves_to_local_address.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+#include "irods_exception.hpp"
+#include "irods_hostname.hpp"
+
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        std::cerr << "Call with hostname argument" << std::endl;
+        return -1;
+    }
+
+    try {
+        const bool hostname_is_local = hostname_resolves_to_local_address(argv[1]);
+        if (hostname_is_local) {
+            std::cout << "yes" << std::endl;
+        } else {
+            std::cout << "no" << std::endl;
+        }
+        return 0;
+    } catch ( const irods::exception& e ) {
+        std::cerr << e.what() << std::endl;
+    }
+    return -1;
+}
