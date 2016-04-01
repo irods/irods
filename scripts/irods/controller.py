e42aab31db17f36fb908a2ebd6e8bf0d477901f1
Zoey Greer <tempoz@renci.org>
2016-04-01 15:37:23 -0400

    [#2917] make sure iRODS does not start while it is already running

--- scripts/irods/controller.py
+++ scripts/irods/controller.py
@@ -36,6 +36,8 @@ class IrodsController(object):
     def start(self):
         l = logging.getLogger(__name__)
         l.debug('Calling start on IrodsController')
+        if self.status():
+            raise IrodsError('iRODS already running')
 
         self.config.clear_cache()
         if not os.path.exists(self.config.server_executable):
