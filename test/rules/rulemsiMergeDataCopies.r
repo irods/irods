883b4431068afa61390e7a8cdc99d8ed15794a85
Ben Keller <irods.ben.keller@gmail.com>
2016-04-05 17:46:50 -0400

    Remove "iRODS" intermediate directory

--- test/rules/rulemsiMergeDataCopies.r
+++ test/rules/rulemsiMergeDataCopies.r
@@ -5,7 +5,7 @@ myTestRule {
 #  Master collection path
 #  Note that the directory structures within the current collection are mirrored into the master
 #  Merges one file at a time, if file already exists creates a replica
-#  The microservice uses two commands in iRODS/server/bin/cmd:
+#  The microservice uses two commands in server/bin/cmd:
 #    ln    - softlink
 #    mkdir - make directory
 #Output parameter is:
