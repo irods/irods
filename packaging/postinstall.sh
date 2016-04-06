776458a5e1d98c8f4b77e1f0116f175a97a5bc5e
Zoey Greer <tempoz@renci.org>
2016-04-06 18:11:53 -0400

    [#3120] perform upgrade from 4.1 to 4.2

--- packaging/postinstall.sh
+++ packaging/postinstall.sh
@@ -53,14 +53,6 @@ rm -f /tmp/irodsServer.*
 rm -f /var/run/shm/*var_lib_irods_iRODS_server_config*
 
 # =-=-=-=-=-=-=-
-# wrap previous VERSION.json with new VERSION.json
-if [ "$UPGRADE_FLAG" == "true" ] ; then
-    python -c "from __future__ import print_function; import json; data=json.load(open('$IRODS_HOME/VERSION.json')); data['previous_version'] = json.load(open('$IRODS_HOME/VERSION.json.previous')); print(json.dumps(data, indent=4, sort_keys=True))" > $IRODS_HOME/VERSION.json.tmp
-    mv $IRODS_HOME/VERSION.json.tmp $IRODS_HOME/VERSION.json
-    rm $IRODS_HOME/VERSION.json.previous
-fi
-
-# =-=-=-=-=-=-=-
 # explode tarball of necessary coverage files if it exists
 if [ -f "$IRODS_HOME/gcovfiles.tgz" ] ; then
     cd $IRODS_HOME
@@ -83,56 +75,22 @@ if [ "$UPGRADE_FLAG" == "true" ] ; then
     # get service account information
     source /etc/irods/service_account.config 2> /dev/null
 
-    # make sure the service acount owns everything except the PAM executable, once again
+    # make sure the service acount owns everything
     chown -R $IRODS_SERVICE_ACCOUNT_NAME:$IRODS_SERVICE_GROUP_NAME /etc/irods
     chown -R $IRODS_SERVICE_ACCOUNT_NAME:$IRODS_SERVICE_GROUP_NAME $IRODS_HOME
-    chmod 4755 /usr/bin/genOSAuth
 
 
     # convert any legacy configuration files
     su - $IRODS_SERVICE_ACCOUNT_NAME -c "python $IRODS_HOME/packaging/convert_configuration_to_json.py $SERVER_TYPE"
-    # update the configuration files
-    su - $IRODS_SERVICE_ACCOUNT_NAME -c "python $IRODS_HOME/packaging/update_configuration_schema.py"
 
-    # stop server
-    su - $IRODS_SERVICE_ACCOUNT_NAME -c "$IRODS_HOME/irodsctl stop"
-    # update the database schema if an icat server
-    if [ "$SERVER_TYPE" == "icat" ] ; then
-        # =-=-=-=-=-=-=-
-        # update the catalog schema
-        su - $IRODS_SERVICE_ACCOUNT_NAME -c "python $IRODS_HOME/scripts/python/setup_irods.py update_catalog_schema"
-    fi
-    # re-start server
-    su - $IRODS_SERVICE_ACCOUNT_NAME -c "$IRODS_HOME/irodsctl start"
 else
-    # copy packaged template files into live config directory
-    cp /var/lib/irods/packaging/core.re.template /etc/irods/core.re
-    cp /var/lib/irods/packaging/core.dvm.template /etc/irods/core.dvm
-    cp /var/lib/irods/packaging/core.fnm.template /etc/irods/core.fnm
-    cp /var/lib/irods/packaging/server_config.json.template /etc/irods/server_config.json
-    cp /var/lib/irods/packaging/hosts_config.json.template /etc/irods/hosts_config.json
-    cp /var/lib/irods/packaging/host_access_control_config.json.template /etc/irods/host_access_control_config.json
-
-    # =-=-=-=-=-=-=-
-    if [ "$SERVER_TYPE" == "icat" ] ; then
-        # copy packaged template database file into live config directory
-        cp /var/lib/irods/packaging/database_config.json.template /etc/irods/database_config.json
-        # give user some guidance regarding database selection and installation
-        cat $IRODS_HOME/packaging/user_icat.txt
-    elif [ "$SERVER_TYPE" == "resource" ] ; then
-        # give user some guidance regarding resource configuration
-        cat $IRODS_HOME/packaging/user_resource.txt
-    fi
+    cat $IRODS_HOME/packaging/setup_instructions.txt
 fi
 
 # =-=-=-=-=-=-=-
 # remove temporary files
 rm -f $IRODS_HOME/plaintext_database_password.txt
 rm -f $UPGRADE_FLAG_FILE
-
-# =-=-=-=-=-=-=-
-# chown the binary_installation.flag file
-chown $IRODS_SERVICE_ACCOUNT_NAME:$IRODS_SERVICE_GROUP_NAME $BINARY_INSTALL_FLAG_FILE
 
 # =-=-=-=-=-=-=-
 # exit with success
