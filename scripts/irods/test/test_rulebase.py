546615ce3d7ebf851776de2d641fa9f64e2113d2
Antoine de Torcy <adetorcy@email.unc.edu>
2016-04-11 16:41:54 -0400

    [#2316] fix number of arguments in dynPEP test

--- scripts/irods/test/test_rulebase.py
+++ scripts/irods/test/test_rulebase.py
@@ -140,7 +140,7 @@ replicateMultiple(*destRgStr) {
 
         # add dynamic PEP with rscomm usage
         time.sleep(1)  # remove once file hash fix is commited #2279
-        os.system('''echo "pep_resource_open_pre(*OUT) { msiGetSystemTime( *junk, '' ); }" >> ''' + corefile)
+        os.system('''echo "pep_resource_open_pre(*OUT,*FOO,*BAR) { msiGetSystemTime( *junk, '' ); }" >> ''' + corefile)
         time.sleep(1)  # remove once file hash fix is commited #2279
 
         # check rei functioning
