d73f33c730ff507aeb65534c33e871f88c1a887c
Zoey Greer <tempoz@renci.org>
2016-04-18 15:15:16 -0400

    [#3121] use d in ls to find service account filename

--- packaging/preremove.sh
+++ packaging/preremove.sh
@@ -35,8 +35,8 @@ if [ -f /etc/irods/service_account.config ] ; then
     # get service account information
     source /etc/irods/service_account.config 2> /dev/null
 else
-    IRODS_SERVICE_ACCOUNT_NAME=`ls -l /var/lib/irods | awk '{print $3}'`
-    IRODS_SERVICE_GROUP_NAME=`ls -l /var/lib/irods | awk '{print $4}'`
+    IRODS_SERVICE_ACCOUNT_NAME=`ls -ld /var/lib/irods | awk '{print $3}'`
+    IRODS_SERVICE_GROUP_NAME=`ls -ld /var/lib/irods | awk '{print $4}'`
 fi
 
 # =-=-=-=-=-=-=-
