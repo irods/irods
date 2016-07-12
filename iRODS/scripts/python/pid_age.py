2071aab02ee2304941c7ad52a8d031c9d9926d39
Ben Keller <irods.ben.keller@gmail.com>
2016-07-12 11:47:07 -0400

    [#3219] pid_age.py compat w/ psutil>=2.0

--- iRODS/scripts/python/pid_age.py
+++ iRODS/scripts/python/pid_age.py
@@ -1,6 +1,12 @@
 import sys
 import psutil
 import time
+
+def get_create_time(process):
+    if psutil.version_info >= (2,0):
+        return process.create_time()
+    return process.create_time
+
 pid = int(sys.argv[1])
 p = psutil.Process(pid)
-print time.time() - p.create_time
+print time.time() - get_create_time(p)
