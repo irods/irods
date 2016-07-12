065d4bc6518b0d727d8b961cefaa79ac15ba9ed0
Ben Keller <irods.ben.keller@gmail.com>
2016-07-12 12:10:12 -0400

    [#3195] Check unix free space & inodes during create vote

--- iRODS/lib/core/include/rodsErrorTable.h
+++ iRODS/lib/core/include/rodsErrorTable.h
@@ -253,6 +253,7 @@
 #define USER_NOT_ALLOWED_TO_EXEC_CMD                -370000
 #define USER_HASH_TYPE_MISMATCH                     -380000
 #define USER_INVALID_CLIENT_ENVIRONMENT             -390000
+#define USER_INSUFFICIENT_FREE_INODES               -400000
 /** @} */
 
 /* 500,000 to 800,000 - file driver error */
