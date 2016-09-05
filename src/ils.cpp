ae953ced8a7889867e76539e78df32db69fc59ff
Ben Keller <irods.ben.keller@gmail.com>
2016-09-05 14:12:30 -0400

    Update cmake for irods lib condense

--- src/ils.cpp
+++ src/ils.cpp
@@ -72,15 +72,11 @@ main( int argc, char **argv ) {
     if ( conn == NULL ) {
         exit( 2 );
     }
-#if 0
-    init_client_api_table();
-#else
     // =-=-=-=-=-=-=-
     // initialize pluggable api table
     irods::api_entry_table&  api_tbl = irods::get_client_api_table();
     irods::pack_entry_table& pk_tbl  = irods::get_pack_table();
     init_api_table( api_tbl, pk_tbl );
-#endif
     if ( strcmp( myEnv.rodsUserName, PUBLIC_USER_NAME ) != 0 ) {
         status = clientLogin( conn );
         if ( status != 0 ) {
