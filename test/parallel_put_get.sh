1882bdcac5064a05c7e8cae5e7af886f22a3ca8f
Terrell Russell <terrellrussell@gmail.com>
2016-10-31 12:26:37 -0400

    [#3347] new parallel test script, iput/iget

---
+++ test/parallel_put_get.sh
@@ -0,0 +1,72 @@
+#!/bin/bash -e
+
+###############################
+#
+# This script will attempt parallel puts and gets
+# using the environment's iput/iget iCommands.
+#
+# This script will exit only when encountering an error.
+#
+# - JOBS is the number of parallel jobs
+#
+# - Creates FILES_TO_CREATE FILESIZE_IN_MB MiB files in FILES_FULLPATH
+# - Puts the files into BENCHMARKS_DIR
+# - Gets the files into BENCHMARKS_FULLPATH
+#
+###############################
+
+SCRIPTPATH=$( cd $(dirname $0) ; pwd -P )
+
+FILESIZE_IN_MB=40
+FILES_TO_CREATE=256
+FILES_DIR=files.dir
+BENCHMARKS_DIR=benchmarks.dir
+TARGET_RESOURCE=demoResc
+JOBS=30
+
+FILES_FULLPATH=${SCRIPTPATH}/${FILES_DIR}
+BENCHMARKS_FULLPATH=${SCRIPTPATH}/${BENCHMARKS_DIR}
+
+####################################
+# generate files
+####################################
+mkdir -p ${FILES_FULLPATH}
+for i in `seq -w ${FILES_TO_CREATE}`
+do
+    truncate -s${FILESIZE_IN_MB}M ${FILES_FULLPATH}/bigfile.${i}
+    echo ${i} >> ${FILES_FULLPATH}/bigfile.${i}
+done
+
+####################################
+# generate scripts
+####################################
+mkdir -p ${BENCHMARKS_FULLPATH}
+for i in `ls ${FILES_FULLPATH}`
+do
+    echo iput -R ${TARGET_RESOURCE} -K -f ${FILES_FULLPATH}/${i} ${BENCHMARKS_DIR}/
+done > ${BENCHMARKS_FULLPATH}/iput-all
+for i in `ls ${FILES_FULLPATH}`
+do
+    echo iget -R ${TARGET_RESOURCE} -K -f ${BENCHMARKS_DIR}/${i} ${BENCHMARKS_FULLPATH}/
+done > ${BENCHMARKS_FULLPATH}/iget-all
+
+####################################
+# parallel put and get
+####################################
+imkdir -p ${BENCHMARKS_DIR}
+irm -rf ${BENCHMARKS_DIR}
+imkdir -p ${BENCHMARKS_DIR}
+COUNT=0
+time while [ "$?" == "0" ]; do
+    COUNT=$((COUNT+1))
+    date
+    echo "Starting loop ${COUNT}"
+    echo " put"
+    cat ${BENCHMARKS_FULLPATH}/iput-all | parallel -j ${JOBS}
+    if [ "$?" != "0" ]; then
+        date
+        exit 1
+    fi
+    echo " get"
+    cat ${BENCHMARKS_FULLPATH}/iget-all | parallel -j ${JOBS}
+done
