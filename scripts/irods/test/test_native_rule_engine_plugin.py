9563d20773325c59a699db1789aaf5a3cbe1f698
Rick Skarbez <skarbez@gmail.com>
2016-11-11 14:08:20 -0500

    [#3119] Fix and enable test_native_rule_engine_plugin.py

--- scripts/irods/test/test_native_rule_engine_plugin.py
+++ scripts/irods/test/test_native_rule_engine_plugin.py
@@ -22,7 +22,7 @@ class Test_Native_Rule_Engine_Plugin(resource_suite.ResourceBase, unittest.TestC
     def tearDown(self):
         super(Test_Native_Rule_Engine_Plugin, self).tearDown()
 
-    def helper_test_pep(self, rules_to_prepend, icommand, strings_to_check_for=['THIS IS AN OUT VARIABLE']):
+    def helper_test_pep(self, rules_to_prepend, icommand, strings_to_check_for=['THIS IS AN OUT VARIABLE'], number_of_strings_to_look_for=1):
         corefile = paths.core_re_directory() + "/core.re"
 
         with lib.file_backed_up(corefile):
@@ -32,10 +32,11 @@ class Test_Native_Rule_Engine_Plugin(resource_suite.ResourceBase, unittest.TestC
 
             initial_size_of_server_log = lib.get_file_size_by_path(paths.server_log_path())
 
-            self.admin.assert_icommand(icommand, 'STDOUT_SINGLELINE', '')
+            self.admin.run_icommand(icommand)
 
         for s in strings_to_check_for:
-            assert(1 == lib.count_occurrences_of_string_in_log(paths.server_log_path(), s, start_index=initial_size_of_server_log))
+            count = lib.count_occurrences_of_string_in_log(paths.server_log_path(), s, start_index=initial_size_of_server_log)
+            assert number_of_strings_to_look_for == count, 'Found {0} instead of {1} occurrences of {2}'.format(count, number_of_strings_to_look_for, s)
 
     def test_network_pep(self):
         self.helper_test_pep("""
@@ -55,7 +56,7 @@ pep_resource_resolve_hierarchy_pre(*INSTANCE,*CONTEXT,*OUT,*OPERATION,*HOST,*PAR
 pep_resource_resolve_hierarchy_post(*INSTANCE,*CONTEXT,*OUT,*OPERATION,*HOST,*PARSER,*VOTE){
 writeLine( 'serverLog', '*OUT')
 }
-""", "iput -f --metadata ATTR;VALUE;UNIT "+self.testfile)
+""", "iput -f --metadata ATTR;VALUE;UNIT "+self.testfile, ['THIS IS AN OUT VARIABLE'], 2)
 
     def test_out_variable(self):
         self.helper_test_pep("""
@@ -65,7 +66,7 @@ pep_resource_resolve_hierarchy_pre(*INSTANCE,*CONTEXT,*OUT,*OPERATION,*HOST,*PAR
 pep_resource_resolve_hierarchy_post(*INSTANCE,*CONTEXT,*OUT,*OPERATION,*HOST,*PARSER,*VOTE){
 writeLine( 'serverLog', '*OUT')
 }
-""", "iput -f --metadata ATTR;VALUE;UNIT "+self.testfile)
+""", "iput -f --metadata ATTR;VALUE;UNIT "+self.testfile, ['THIS IS AN OUT VARIABLE'], 2)
 
     def test_re_serialization(self):
         self.helper_test_pep("""
@@ -73,27 +74,65 @@ pep_resource_resolve_hierarchy_pre(*INSTANCE,*CONTEXT,*OUT,*OPERATION,*HOST,*PAR
 writeLine("serverLog", "pep_resource_resolve_hierarchy_pre - [*INSTANCE] [*CONTEXT] [*OUT] [*OPERATION] [*HOST] [*PARSER] [*VOTE]");
 }
 """, "iput -f --metadata ATTR;VALUE;UNIT "+self.testfile,
-            ['user_auth_info_auth_flag=5', 'user_rods_zone=tempZone', 'user_user_name=otherrods', 'ATTR;VALUE;UNIT'])
+            ['user_auth_info_auth_flag=5', 'user_rods_zone=tempZone', 'user_user_name=otherrods', 'metadataIncluded=ATTR;VALUE;UNIT'], 2)
 
+#pep_rs_hello_world_pre(*INST,*OUT,*COMM,*HELLO_IN,*HELLO_OUT) {
+#    writeLine("serverLog", "pep_rs_hello_world_pre - *INST *OUT *HELLO_IN, *HELLO_OUT");
+#}
+#pep_rs_hello_world_post(*INST,*OUT,*COMM,*HELLO_IN,*HELLO_OUT) {
+#    writeLine("serverLog", "pep_rs_hello_world_post - *INST *OUT *HELLO_IN, *HELLO_OUT");
+#}
     def test_api_plugin(self):
         self.helper_test_pep("""
-pep_rs_hello_world_pre(*INST,*OUT,*COMM,*HELLO_IN,*HELLO_OUT) {
-    writeLine("serverLog", "pep_rs_hello_world_pre - *INST *OUT *HELLO_IN, *HELLO_OUT");
+pep_api_rs_hello_world_pre(*INST, *OUT, *HELLO_IN, *HELLO_OUT) {
+    writeLine("serverLog", "pep_api_rs_hello_world_pre - *INST *OUT *HELLO_IN, *HELLO_OUT");
 }
-pep_rs_hello_world_post(*INST,*OUT,*COMM,*HELLO_IN,*HELLO_OUT) {
-    writeLine("serverLog", "pep_rs_hello_world_post - *INST *OUT *HELLO_IN, *HELLO_OUT");
+pep_api_rs_hello_world_post(*INST, *OUT, *HELLO_IN, *HELLO_OUT) {
+    writeLine("serverLog", "pep_api_rs_hello_world_post - *INST *OUT *HELLO_IN, *HELLO_OUT");
 }
 """, "iapitest",
-            ['pep_rs_hello_world_pre - api_instance <unconvertible> that=hello, world.++++this=42, null_value', 'HELLO WORLD', 'pep_rs_hello_world_post - api_instance <unconvertible> that=hello, world.++++this=42, that=hello, world.++++this=42++++value=128'])
+            ['pep_api_rs_hello_world_pre - api_instance auth_scheme=native', 'that=hello, world.++++this=42, null_value', 'HELLO WORLD', 'pep_api_rs_hello_world_post - api_instance auth_scheme=native', 'that=hello, world.++++this=42, that=hello, world.++++this=42++++value=128'])
 
     def test_rule_engine_2242(self):
-        self.admin.assert_icommand("irule -F rule1_2242.r", 'STDOUT_SINGLELINE', "Update session variable $userNameClient not allowed")
-        self.admin.assert_icommand("irule -F rule2_2242.r", "EMPTY")
+        rule_file1 = "rule1_2242.r"
+        rule_string1 = """
+test {
+    $userNameClient = \"foobar\";
+}
+
+INPUT *A=\"status\"
+OUTPUT ruleExecOut
+}
+"""
+        with open(rule_file1, 'wt') as f:
+            print(rule_string1, file=f, end='')
+
+        out, _, _ = self.admin.run_icommand("irule -F " + rule_file1)
+        assert 'Update session variable $userNameClient not allowed' in out
+
+        rule_file2 = "rule2_2242.r"
+        rule_string2 = """
+test {
+    $status = \"1\";
+}
+
+acPreProcForWriteSessionVariable(*x) {
+    writeLine(\"stdout\", \"bwahahaha\");
+    succeed;
+}
+
+INPUT *A=\"status\"
+OUTPUT ruleExecOut
+"""
+        with open(rule_file2, 'wt') as f:
+            print(rule_string2, file=f, end='')
+
+        self.admin.assert_icommand("irule -F " + rule_file2, "EMPTY")
 
     @unittest.skipIf(test.settings.TOPOLOGY_FROM_RESOURCE_SERVER, 'Skip for topology testing from resource server: reads re server log')
     def test_rule_engine_2309(self):
-        corefile = lib.get_core_re_dir() + "/core.re"
-        coredvm = lib.get_core_re_dir() + "/core.dvm"
+        corefile = paths.core_re_directory() + "/core.re"
+        coredvm = paths.core_re_directory() + "/core.dvm"
         with lib.file_backed_up(coredvm):
             lib.prepend_string_to_file('oprType||rei->doinp->oprType\n', coredvm)
             with lib.file_backed_up(corefile):
