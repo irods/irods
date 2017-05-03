b551ea618d246147e9620b53c17c4c711e219321
Rick Skarbez <skarbez@gmail.com>
2017-05-03 13:40:54 -0400

    [irods/irods_client_icommands#14] Added tests for irmdir
    Conflicts:
    	scripts/run_tests.py

--- scripts/run_tests.py
+++ scripts/run_tests.py
@@ -122,7 +122,8 @@ if __name__ == '__main__':
         test_identifiers.extend(['test_xmsg', 'test_iadmin', 'test_resource_types', 'test_catalog', 'test_rulebase',
                                  'test_resource_tree', 'test_load_balanced_suite', 'test_icommands_file_operations', 'test_imeta_set',
                                  'test_all_rules', 'test_iscan', 'test_ichmod', 'test_iput_options', 'test_ireg', 'test_irsync',
-                                 'test_iticket', 'test_irodsctl', 'test_resource_configuration', 'test_control_plane', 'test_native_rule_engine_plugin', 'test_quotas', 'test_ils'])
+                                 'test_iticket', 'test_irodsctl', 'test_resource_configuration', 'test_control_plane', 
+                                 'test_native_rule_engine_plugin', 'test_quotas', 'test_ils', 'test_irmdir'])
 
     results = run_tests_from_names(test_identifiers, options.buffer_test_output, options.xml_output)
     print(results)
