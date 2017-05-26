7f02680104299bcb86a3ae8c4d5000328aef364a
Ben Keller <irods.ben.keller@gmail.com>
2017-05-26 15:01:39 -0400

    [#3499] ichksum -a reports which replica failed checksum validation

--- server/api/src/rsDataObjChksum.cpp
+++ server/api/src/rsDataObjChksum.cpp
@@ -266,9 +266,10 @@ verifyDatObjChksum( rsComm_t *rsComm, dataObjInfo_t *dataObjInfo,
     }
 
     if ( strcmp( *outChksumStr, dataObjInfo->chksum ) != 0 ) {
-        rodsLog( LOG_ERROR,
-                 "verifyDatObjChksum: computed chksum %s != icat value %s for %s",
-                 *outChksumStr, dataObjInfo->chksum, dataObjInfo->objPath );
+        std::stringstream error_message;
+        error_message << "verifyDatObjChksum: computed chksum [" << *outChksumStr << "] != icat value [" << dataObjInfo->chksum << "] for [" << dataObjInfo->objPath << "] hierarchy [" << dataObjInfo->rescHier << "] replNum [" << dataObjInfo->replNum << "]";
+        rodsLog( LOG_ERROR, "%s", error_message.str().c_str());
+        addRErrorMsg(&rsComm->rError, USER_CHKSUM_MISMATCH, error_message.str().c_str());
         return USER_CHKSUM_MISMATCH;
     }
     else {
