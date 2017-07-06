1c9302831ad4e0560201de1a57912943282e82d4
Ben Keller <irods.ben.keller@gmail.com>
2017-07-06 11:27:02 -0400

    [#3479] Turn segfault stacktrace back on in release build
    The stacktrace is worth possible hanging agents

--- server/core/src/irods_signal.cpp
+++ server/core/src/irods_signal.cpp
@@ -14,22 +14,17 @@ extern "C" {
     /// @brief Signal handler for seg faults
     static void segv_handler(
         int signal ) {
-#if 0
-        // RTS - output in signal handlers are unsafe - #3326 
         std::cerr << "Caught signal [" << signal << "]. Dumping stacktrace and exiting" << std::endl;
         std::cerr << irods::stacktrace().dump();
-#endif
-        exit( signal );
+        exit(signal);
     }
 
     void register_handlers( void ) {
         struct sigaction action;
-        memset( &action, 0, sizeof( action ) );
+        memset(&action, 0, sizeof(action));
         action.sa_handler = segv_handler;
-#ifndef RELEASE_FLAG
         sigaction( SIGSEGV, &action, 0 );
         sigaction( SIGABRT, &action, 0 );
         sigaction( SIGINT, &action, 0 );
-#endif
     }
-};
+}
