840e0cb94b74200d9ac0d1fa902eb007dfb0a2d8
Ben Keller <irods.ben.keller@gmail.com>
2017-07-27 14:52:56 -0400

    [#3537] rsFileChksum() condInput option to compare vault size to database size

--- lib/core/include/rodsKeyWdDef.h
+++ lib/core/include/rodsKeyWdDef.h
@@ -80,6 +80,7 @@
 #define REPL_DATA_OBJ_INP_KW   	"replDataObjInp"
 #define CROSS_ZONE_CREATE_KW   	"replDataObjInp"  /* use the same for backward
 * compatibility */
+#define VERIFY_VAULT_SIZE_EQUALS_DATABASE_SIZE_KW    "verifyVaultSizeEqualsDatabaseSize"
 #define QUERY_BY_DATA_ID_KW   	"queryByDataID"
 #define SU_CLIENT_USER_KW   	"suClientUser"
 #define RM_BUN_COPY_KW   	"rmBunCopy"
