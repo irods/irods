29a735f56c7f20b1cd36e4f7341b2ea339466371
Jason Coposky <jasonc@renci.org>
2017-09-26 11:39:59 -0400

    [#3667] add tests

--- scripts/irods/test/test_imeta_set.py
+++ scripts/irods/test/test_imeta_set.py
@@ -28,6 +28,9 @@ class Test_ImetaSet(ResourceBase, unittest.TestCase):
 
         super(Test_ImetaSet, self).tearDown()
 
+    def mod_avu(self, user_name, a, v, u, newv):
+        self.admin.assert_icommand('imeta mod -u %s %s %s %s v:%s' % (user_name, a, v, u, newv))
+
     def set_avu(self, user_name, a, v, u):
         self.admin.assert_icommand('imeta set -u %s %s %s %s' % (user_name, a, v, u))
 
@@ -55,6 +58,17 @@ class Test_ImetaSet(ResourceBase, unittest.TestCase):
     def add_and_check_avu(self, user_name, a, v, u):
         self.add_avu(user_name, a, v, u)
         self.check_avu(user_name, a, v, u)
+
+    def mod_and_check_avu(self, user_name, a, v, u, newv):
+        self.mod_avu(user_name, a, v, u, newv)
+        self.check_avu(user_name, a, newv, u)
+
+    def test_imeta_set_and_mod_single_object_triple(self, user=None):
+        if user is None:
+            user = self.user0.username
+
+        self.set_and_check_avu(user, 'att0', 'val0', 'unt0')
+        self.mod_and_check_avu(user, 'att0', 'val0', 'unt0', 'val5')
 
     def test_imeta_set_single_object_triple(self, user=None):
         if user is None:
