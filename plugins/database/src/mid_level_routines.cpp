718aaaed3b94343fa6e54545533382e9f875e6e7
Terrell Russell <terrellrussell@gmail.com>
2018-02-03 08:29:35 -0500

    [#3717] uppercase table names to be MySQL-friendly

--- plugins/database/src/mid_level_routines.cpp
+++ plugins/database/src/mid_level_routines.cpp
@@ -1667,7 +1667,7 @@ int cmlGetGroupMemberCount( const char *groupName, icatSessionStruct *icss ) {
     std::vector<std::string> bindVars;
     bindVars.push_back( groupName );
     status = cmlGetIntegerValueFromSql(
-                 "select count(user_id) from r_user_group where  group_user_id != user_id and group_user_id in (select user_id from r_user_main where user_name=? and user_type_name='rodsgroup')",
+                 "select count(user_id) from R_USER_GROUP where group_user_id != user_id and group_user_id in (select user_id from R_USER_MAIN where user_name=? and user_type_name='rodsgroup')",
                  &iVal, bindVars, icss );
     if ( status == 0 ) {
         status = iVal;
