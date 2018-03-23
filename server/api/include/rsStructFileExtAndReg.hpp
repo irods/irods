6361838c9bec09a8e80628aa318743154497f717
Alan King <alanking@renci.org>
2018-03-23 12:34:54 -0400

    [#3855] Add dataMode in dataObjectInfo for ibun -x
    Fixes a bug where attempting to replicate files extracted from a
    bundle in a rebalance operation would fail with a BAD_LEXICAL_CAST
    due to a missing DATA_MODE column. Struct file extraction and
    registration now includes data mode in the data object information
    when registering subfiles.
    
    Many dependencies between rsBulkDataObjPut and rsStructFileExtAndReg
    were not needed, so they were removed as appropriate. Some formatting
    cleanup at the tops of the files as well.
    
    (cherry-picked from SHA: c233f20f1b2aecc3f2f18f4b2c10d284cf3ca568)

--- server/api/include/rsStructFileExtAndReg.hpp
+++ server/api/include/rsStructFileExtAndReg.hpp
@@ -2,19 +2,16 @@
 #define RS_STRUCT_FILE_EXT_AND_REG_HPP
 
 #include "rcConnect.h"
-#include "rodsGenQuery.h"
-#include "rodsType.h"
 #include "objStat.h"
-#include "bulkDataObjPut.h"
 #include "structFileExtAndReg.h"
 
-int rsStructFileExtAndReg( rsComm_t *rsComm, structFileExtAndRegInp_t *structFileExtAndRegInp );
-int chkCollForExtAndReg( rsComm_t *rsComm, char *collection, rodsObjStat_t **rodsObjStatOut );
-int regUnbunSubfiles( rsComm_t *rsComm, const char *_resc_name, const char* rescHier, char *collection, char *phyBunDir, int flags, genQueryOut_t *attriArray );
-int regSubfile( rsComm_t *rsComm, const char *_resc_name, const char* rescHier, char *subObjPath, char *subfilePath, rodsLong_t dataSize, int flags );
-int addRenamedPhyFile( char *subObjPath, char *oldFileName, char *newFileName, renamedPhyFiles_t *renamedPhyFiles );
-int postProcRenamedPhyFiles( renamedPhyFiles_t *renamedPhyFiles, int regStatus );
-int cleanupBulkRegFiles( rsComm_t *rsComm, genQueryOut_t *bulkDataObjRegInp );
-int postProcBulkPut( rsComm_t *rsComm, genQueryOut_t *bulkDataObjRegInp, genQueryOut_t *bulkDataObjRegOut );
+int rsStructFileExtAndReg(
+    rsComm_t *rsComm,
+    structFileExtAndRegInp_t *structFileExtAndRegInp );
+
+int chkCollForExtAndReg(
+    rsComm_t *rsComm,
+    char *collection,
+    rodsObjStat_t **rodsObjStatOut );
 
 #endif
