4b54de51840d673824d80494172218ce907421be
Kory Draughn <korydraughn@renci.org>
2018-04-03 15:26:14 -0400

    [#3485] Converts null pointers to empty strings.

--- scripts/irods/test/test_ichksum.py
+++ scripts/irods/test/test_ichksum.py
@@ -7,11 +7,11 @@ else:
 
 import os
 
-from . import session
-from . import settings
 from . import resource_suite
 from .. import lib
 from .. import test
+from .. import paths
+from ..configuration import IrodsConfig
 
 class Test_Ichksum(resource_suite.ResourceBase, unittest.TestCase):
 
@@ -81,7 +81,6 @@ class Test_Ichksum(resource_suite.ResourceBase, unittest.TestCase):
         # Mess with file in vault
         full_path = os.path.join(self.admin.get_vault_session_path(), filename)
         lib.cat(full_path, 'XXXXX')
-        new_chksum = lib.file_digest(full_path, 'sha256', encoding='base64')
         self.admin.assert_icommand_fail(['ichksum', '-K', filename], 'STDOUT_SINGLELINE', 'Failed checksum = 1')
         self.admin.assert_icommand(['ichksum', '-K', filename], 'STDERR_SINGLELINE', 'USER_CHKSUM_MISMATCH')
 
@@ -92,4 +91,44 @@ class Test_Ichksum(resource_suite.ResourceBase, unittest.TestCase):
         lib.make_file(filename_path, 1024, 'arbitrary')
         self.admin.assert_icommand(['iput', filename_path])
         self.admin.assert_icommand(['ichksum', filename], 'STDOUT_SINGLELINE', filename)
+
+    @unittest.skipIf(test.settings.RUN_IN_TOPOLOGY, "Skip for topology testing")
+    def test_ichksum_fails_with_dyn_pep__issue_3485(self):
+        config = IrodsConfig()
+
+        # Create a file and make iRODS aware of it.
+        # If this were done after adding the rule to the rulebase, an
+        # error would be returned.
+        filename = 'test_file_issue_3485.txt'
+        lib.make_file(filename, 1024, 'arbitrary')
+        chksum = lib.file_digest(filename, 'sha256', encoding='base64')
+        self.admin.assert_icommand(['iput', filename])
+
+        with lib.file_backed_up(config.server_config_path):
+            # Create a new rule file that contains a pep with the wrong signature.
+            # This particular pep is required in order to show that a more appropriate
+            # error code/status is returned instead of [SYS_INTERNAL_ERR].
+            rule_filename = 'issue_3485_rule.re'
+            rule_filename_path = os.path.join(paths.core_re_directory(), rule_filename)
+            with open(rule_filename_path, 'w') as f:
+                f.write("pep_database_mod_data_obj_meta_pre() {}\n")
+
+            # Add the newly created rule file to the rule engine rulebase.
+            rule_engine = config.server_config['plugin_configuration']['rule_engines'][0]
+            rule_engine['plugin_specific_configuration']['re_rulebase_set'][0] = rule_filename[:-3]
+            lib.update_json_file_from_dict(config.server_config_path, config.server_config)
+
+            self.admin.assert_icommand(['ichksum', filename], 'STDERR', 'status = -1097000 NO_RULE_OR_MSI_FUNCTION_FOUND_ERR')
+
+            # Update the rule to the correct signature.
+            # Running ichksum after this change should work as expected and print
+            # the requested information.
+            with open(rule_filename_path, 'w') as f:
+                f.write("pep_database_mod_data_obj_meta_pre(*a, *b, *c, *d, *e) {}\n")
+
+            self.admin.assert_icommand(['ichksum', filename], 'STDOUT', [filename, chksum])
+
+            os.remove(rule_filename_path)
+
+        self.admin.assert_icommand(['irm', '-f', filename])
 
