5275359df3a7957edd3c4e2328657a55fa21db13
Alan King <alanking@renci.org>
2018-05-08 14:14:36 -0400

    [#3928] Add tests surrounding root dir and vaults
    This change adds a test which moves the vault path of demoResc to a
    non-existent directory under root directory and manually executes a
    rule to call the msi_update_unixfilesystem_resource_free_space
    microservice. The result should be an error along with an unchanged
    free_space context value.
    
    Also adds tests to ensure that using localhost as a host for a
    resource produces a warning to the user. Adds tests to ensure that
    root directory cannot be used as a vault path. Tests created for
    both creating and modifying storage resources.

--- tests/pydevtest/test_resource_types.py
+++ tests/pydevtest/test_resource_types.py
@@ -695,6 +695,33 @@ class Test_Resource_Unixfilesystem(ResourceSuite, ChunkyDevTest, unittest.TestCa
             admin_session.assert_icommand("iadmin modresc origResc name demoResc", 'STDOUT_SINGLELINE', 'rename', stdin_string='yes\n')
         shutil.rmtree(lib.get_irods_top_level_dir() + "/demoRescVault", ignore_errors=True)
 
+    def test_unix_filesystem_free_space_on_root__3928(self):
+        free_space = '100'
+        self.admin.assert_icommand(['iadmin', 'modresc', 'demoResc', 'path', '/demoRescVault'], 'STDOUT_SINGLELINE', 'Previous resource path')
+        self.admin.assert_icommand(['iadmin', 'modresc', 'demoResc', 'free_space', free_space])
+
+        rule_file_path = 'test_free_space_on_root.r'
+        rule_str = '''
+test_free_space_on_root {{
+    msi_update_unixfilesystem_resource_free_space(*leaf_resource);
+}}
+
+INPUT *leaf_resource="demoResc"
+OUTPUT ruleExecOut
+        '''
+        with open(rule_file_path, 'w') as rule_file:
+            rule_file.write(rule_str)
+
+        initial_log_size = lib.get_log_size('server')
+        rc,_,stderr = self.admin.assert_icommand_fail(['irule', '-F', rule_file_path])
+
+        self.admin.assert_icommand(['ilsresc', '-l', 'demoResc'], 'STDOUT_SINGLELINE', ['free space', free_space])
+        self.assertTrue(0 != rc)
+        self.assertTrue('status = -32000 SYS_INVALID_RESC_INPUT' in stderr)
+        self.assertTrue(1 == lib.count_occurrences_of_string_in_log('server', 'could not find existing non-root path from vault path', start_index=initial_log_size))
+
+        os.unlink(rule_file_path)
+
     def test_unix_filesystem_free_space__3306(self):
         filename = 'test_unix_filesystem_free_space__3306.txt'
         filesize = 3000000
