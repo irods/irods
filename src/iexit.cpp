ac1eec3c6998a679b995b096758de913485fbb69
Terrell Russell <terrellrussell@gmail.com>
2019-02-06 16:16:58 -0500

    [irods/irods#2082] update iexit to always remove saved credential file

--- src/iexit.cpp
+++ src/iexit.cpp
@@ -11,12 +11,9 @@ main( int argc, char **argv ) {
 
     signal( SIGPIPE, SIG_IGN );
 
-    int status, ix;
     rodsArguments_t myRodsArgs;
-    rodsEnv myEnv;
-    char *envFile;
 
-    status = parseCmdLineOpt( argc, argv, "Vvh", 0, &myRodsArgs );
+    int status = parseCmdLineOpt( argc, argv, "Vvh", 0, &myRodsArgs );
     if ( status ) {
         printf( "Use -h for help\n" );
         exit( 1 );
@@ -25,29 +22,20 @@ main( int argc, char **argv ) {
         usage( argv[0] );
         exit( 0 );
     }
-    ix = myRodsArgs.optind;
 
-    status = getRodsEnv( &myEnv );
-
-    envFile = getRodsEnvFileName();
+    rodsEnv myEnv;
+    getRodsEnv( &myEnv );
+    char* envFile = getRodsEnvFileName();
     status = unlink( envFile );
     if ( myRodsArgs.verbose == True ) {
-        printf( "Deleting (if it exists) session envFile:%s\n", envFile );
-        printf( "unlink status = %d\n", status );
+        printf( "Deleting (if it exists) session envFile: %s\n", envFile );
+        printf( "unlink status [%d]\n", status );
+        // prompt before removing
+        obfRmPw( 0 );
     }
-
-    if ( ix < argc ) {
-        if ( strcmp( argv[ix], "full" ) == 0 ) {
-            if ( myRodsArgs.verbose == True ) {
-                obfRmPw( 0 );
-            }
-            else {
-                obfRmPw( 1 );
-            }
-        }
-        else {
-            printf( "option %s is unrecognized\n", argv[ix] );
-        }
+    else {
+        // do not prompt
+        obfRmPw( 1 );
     }
 
     exit( 0 );
@@ -55,10 +43,9 @@ main( int argc, char **argv ) {
 
 
 void usage( char *prog ) {
-    printf( "Exits iRODS session (cwd) and optionally removes\n" );
+    printf( "Exits iRODS session (cwd) and removes\n" );
     printf( "the scrambled password file produced by iinit.\n" );
-    printf( "Usage: %s [-vh] [full]\n", prog );
-    printf( "If 'full' is included the scrambled password is also removed.\n" );
+    printf( "Usage: %s [-vVh]\n", prog );
     printf( " -v  verbose\n" );
     printf( " -V  very verbose\n" );
     printf( " -h  this help\n" );
