71a7ab866c4fecf14e972ca940b1a732e357066c
Kory Draughn <korydraughn@renci.org>
2019-02-12 10:27:22 -0500

    [#4270] Added new api plugin that allows fetching info about a given iRODS file descriptor.
    Includes the following:
    - Convenience functions for client/server-side use.
    - Unit test

---
+++ server/api/src/rs_get_file_descriptor_info.cpp
@@ -0,0 +1,31 @@
+#include "rs_get_file_descriptor_info.hpp"
+
+#include "api_plugin_number.h"
+#include "rodsErrorTable.h"
+
+#include "irods_server_api_call.hpp"
+
+#include <cstring>
+
+extern "C"
+auto rs_get_file_descriptor_info(rsComm_t* _comm, const char* _json_input, char** _json_output) -> int
+{
+    if (!_json_input || !_json_output) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t input{};
+    input.buf = const_cast<char*>(_json_input);
+    input.len = static_cast<int>(std::strlen(_json_input));
+
+    bytesBuf_t* output{};
+
+    const auto ec = irods::server_api_call(GET_FILE_DESCRIPTOR_INFO_APN, _comm, &input, &output);
+
+    if (ec == 0) {
+        *_json_output = static_cast<char*>(output->buf);
+    }
+
+    return ec;
+}
+
