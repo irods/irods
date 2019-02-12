7a394067640329ab1cc0232e994888d0452e37b6
Kory Draughn <korydraughn@renci.org>
2019-02-12 10:27:22 -0500

    [#4805] Made get_file_descriptor_info API plugin more accessible.
    - Externalized client-side function into separate file.
    - Added missing error codes on failures.
    - Cleaned up code structure.
    - Corrected operation name in apidef_t config.
    - Added unit-test.

---
+++ lib/api/src/rc_get_file_descriptor_info.cpp
@@ -0,0 +1,33 @@
+#include "get_file_descriptor_info.h"
+
+#include "api_plugin_number.h"
+#include "rodsDef.h"
+#include "rcConnect.h"
+#include "procApiRequest.h"
+#include "rodsErrorTable.h"
+
+#include <cstring>
+
+extern "C"
+auto rc_get_file_descriptor_info(rcComm_t* _comm, const char* _json_input, char** _json_output) -> int
+{
+    if (!_json_input || !_json_output) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t input_buf{};
+    input_buf.buf = const_cast<char*>(_json_input);
+    input_buf.len = static_cast<int>(std::strlen(_json_input));
+
+    bytesBuf_t* output_buf{};
+
+    const int ec = procApiRequest(_comm, GET_FILE_DESCRIPTOR_INFO_APN,
+                                  &input_buf, nullptr,
+                                  reinterpret_cast<void**>(&output_buf), nullptr);
+
+    if (ec == 0) {
+        *_json_output = static_cast<char*>(output_buf->buf);
+    }
+
+    return ec;
+}
