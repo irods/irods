430b886d4db07d4878b6058ade4eb67e3ea18fcf
Kory Draughn <korydraughn@renci.org>
2019-05-04 18:55:32 -0400

    [#4369] Added new query_processor library.

--- lib/core/include/rodsErrorTable.h
+++ lib/core/include/rodsErrorTable.h
@@ -233,6 +233,7 @@ NEW_ERROR(SYS_SOCK_WRITE_ERR,                          -161000)
 NEW_ERROR(SYS_SOCK_CONNECT_ERR,                        -162000)
 NEW_ERROR(SYS_OPERATION_IN_PROGRESS,                   -163000)
 NEW_ERROR(SYS_REPLICA_DOES_NOT_EXIST,                  -164000)
+NEW_ERROR(SYS_UNKNOWN_ERROR,                           -165000)
 /** @} */
 
 /* 300,000 - 499,000 - user input type error */
