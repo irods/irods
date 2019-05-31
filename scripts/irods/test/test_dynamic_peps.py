eb63dcfe0e5357bd3294cbd0208e21a1e60be3d2
Kory Draughn <korydraughn@renci.org>
2019-05-31 11:55:13 -0400

    [#4370] Skip test for PREP.

--- scripts/irods/test/test_dynamic_peps.py
+++ scripts/irods/test/test_dynamic_peps.py
@@ -15,6 +15,8 @@ from ..configuration import IrodsConfig
 
 class Test_Dynamic_PEPs(session.make_sessions_mixin([('otherrods', 'rods')], []), unittest.TestCase):
 
+    plugin_name = IrodsConfig().default_rule_engine_plugin
+
     def setUp(self):
         super(Test_Dynamic_PEPs, self).setUp()
         self.admin = self.admin_sessions[0]
@@ -22,7 +24,7 @@ class Test_Dynamic_PEPs(session.make_sessions_mixin([('otherrods', 'rods')], [])
     def tearDown(self):
         super(Test_Dynamic_PEPs, self).tearDown()
 
-    @unittest.skipIf(test.settings.RUN_IN_TOPOLOGY, "Skip for Topology Testing")
+    @unittest.skipIf(plugin_name == 'irods_rule_engine_plugin-python' or test.settings.RUN_IN_TOPOLOGY, "Skip for Topology Testing")
     def test_if_collInp_t_is_exposed__issue_4370(self):
         config = IrodsConfig()
 
