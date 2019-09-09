ae795fd227610889376d90381ab4e0c59d202de0
Kory Draughn <korydraughn@renci.org>
2019-09-09 18:45:28 -0400

    [#4467][#4060][#3060] Removed special handling of characters in logical paths.

--- lib/core/src/lsUtil.cpp
+++ lib/core/src/lsUtil.cpp
@@ -131,12 +131,7 @@ lsDataObjUtil( rcComm_t *conn, rodsPath_t *srcPath,
 
 int
 printLsStrShort( char *srcPath ) {
-    char srcElement[MAX_NAME_LEN];
-
-    getLastPathElement( srcPath, srcElement );
-
     printf( "  %s\n", srcPath );
-
     return 0;
 }
 
@@ -558,13 +553,11 @@ printCollCollEnt( collEnt_t *collEnt, int flags ) {
 int
 printSpecLsLong( char *objPath, char *ownerName, char *objSize,
                  char *modifyTime, specColl_t *specColl, rodsArguments_t *rodsArgs ) {
-    char srcElement[MAX_NAME_LEN];
     collEnt_t collEnt;
     int queryFlags;
 
     bzero( &collEnt, sizeof( collEnt ) );
 
-    getLastPathElement( objPath, srcElement );
     collEnt.dataName = objPath;
     collEnt.ownerName = ownerName;
     collEnt.dataSize = strtoll( objSize, 0, 0 );
---
+++ unit_tests/src/test_logical_paths_and_special_characters.cpp
@@ -0,0 +1,228 @@
+#include "catch.hpp"
+
+#include "getRodsEnv.h"
+#include "rodsPath.h"
+#include "miscUtil.h"
+
+#include "filesystem/path.hpp"
+
+#include <cstring>
+#include <string>
+
+TEST_CASE("logical paths and special characters")
+{
+    using namespace std::string_literals;
+
+    namespace fs = irods::experimental::filesystem;
+
+    rodsEnv env;
+    REQUIRE(getRodsEnv(&env) == 0);
+
+    rodsPath_t input{};
+    
+    SECTION("expand tilde")
+    {
+        const std::string in_path = "~";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(fs::path{env.rodsHome}.string() == input.outPath);
+    }
+
+    SECTION("hat should not expand to home collection")
+    {
+        const std::string in_path = "^";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(fs::path{env.rodsHome} / in_path == input.outPath);
+    }
+
+    SECTION("keep leading tilde")
+    {
+        const std::string in_path = "/zone/~data_object";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+
+    SECTION("keep leading period")
+    {
+        const std::string in_path = "/zone/.data_object";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+
+    SECTION("keep leading hat")
+    {
+        const std::string in_path = "/zone/^data_object";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+
+    SECTION("keep trailing tilde")
+    {
+        const std::string in_path = "/zone/data_object~";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+
+    SECTION("keep trailing period")
+    {
+        const std::string in_path = "/zone/data_object.";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+
+    SECTION("keep trailing hat")
+    {
+        const std::string in_path = "/zone/data_object^";
+        std::strncpy(input.inPath, in_path.data(), in_path.size());
+
+        REQUIRE(parseRodsPath(&input, &env) == 0);
+        REQUIRE(in_path == input.outPath);
+    }
+}
+
+TEST_CASE("test getLastPathElement")
+{
+    using namespace std::string_literals;
+
+    char last_path_element[MAX_NAME_LEN]{};
+
+    SECTION("/path/to/.")
+    {
+        std::string path = "/path/to/.";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/path/to/..")
+    {
+        std::string path = "/path/to/..";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/path/to/foo")
+    {
+        std::string path = "/path/to/foo";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo"s);
+    }
+
+    SECTION("/path/to/foo/.")
+    {
+        std::string path = "/path/to/foo/.";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/path/to/foo/..")
+    {
+        std::string path = "/path/to/foo/..";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/path/to/foo/")
+    {
+        std::string path = "/path/to/foo/";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "/path/to/foo"s);
+    }
+
+    SECTION("/path/to/foo.")
+    {
+        std::string path = "/path/to/foo.";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo."s);
+    }
+
+    SECTION("/path/to/foo~")
+    {
+        std::string path = "/path/to/foo~";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo~"s);
+    }
+
+    SECTION("/path/to/foo^")
+    {
+        std::string path = "/path/to/foo^";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo^"s);
+    }
+
+    SECTION("/.")
+    {
+        std::string path = "/.";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/..")
+    {
+        std::string path = "/..";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/")
+    {
+        std::string path = "/";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("/foo")
+    {
+        std::string path = "/foo";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo"s);
+    }
+
+    SECTION("/foo/")
+    {
+        std::string path = "/foo/";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "/foo"s);
+    }
+
+    SECTION("foo/")
+    {
+        std::string path = "foo/";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == "foo"s);
+    }
+
+    SECTION(".")
+    {
+        std::string path = ".";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("..")
+    {
+        std::string path = "..";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+
+    SECTION("")
+    {
+        std::string path = "";
+        REQUIRE(getLastPathElement(&path[0], last_path_element) == 0);
+        REQUIRE(last_path_element == ""s);
+    }
+}
+
