1df43ca9d7bf6e0c167c4f87a592483f0c919db9
Kory Draughn <korydraughn@renci.org>
2019-09-11 08:41:18 -0400

    [#5005] Implemented new API plugin: replica_close.

---
+++ server/api/src/rs_replica_close.cpp
@@ -0,0 +1,22 @@
+#include "rs_replica_close.hpp"
+
+#include "api_plugin_number.h"
+#include "rodsErrorTable.h"
+
+#include "irods_server_api_call.hpp"
+
+#include <cstring>
+
+auto rs_replica_close(RsComm* _comm, const char* _json_input) -> int
+{
+    if (!_json_input) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t input{};
+    input.buf = const_cast<char*>(_json_input);
+    input.len = static_cast<int>(std::strlen(_json_input));
+
+    return irods::server_api_call(REPLICA_CLOSE_APN, _comm, &input);
+}
+
