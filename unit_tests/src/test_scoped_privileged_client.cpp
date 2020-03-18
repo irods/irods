5c96b2e1cb9421e2729f3d8be40934e5c6d8b85b
Kory Draughn <korydraughn@renci.org>
2020-03-18 07:43:30 -0400

    [#4819] Added new library that elevates privileges and restores them at scope exit.

---
+++ unit_tests/src/test_scoped_privileged_client.cpp
@@ -0,0 +1,19 @@
+#include "catch.hpp"
+
+#include "scoped_privileged_client.hpp"
+#include "irods_rs_comm_query.hpp"
+
+TEST_CASE("scoped privileged client")
+{
+    rsComm_t conn{};
+
+    REQUIRE_FALSE(irods::is_privileged_client(conn));
+
+    {
+        irods::experimental::scoped_privileged_client spc{conn};
+        REQUIRE(irods::is_privileged_client(conn));
+    }
+
+    REQUIRE_FALSE(irods::is_privileged_client(conn));
+}
+
