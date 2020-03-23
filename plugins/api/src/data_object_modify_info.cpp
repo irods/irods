112e80909c75432f2c463ba30fe8cafd92f086a4
Kory Draughn <korydraughn@renci.org>
2020-03-23 14:49:11 -0400

    [#4832] New API Plugin - data_object_modify_info.
    A version of modDataObjMeta that limits the scope of what can be updated
    in the catalog. Implemented for the client-side API whitelist.
    
    Includes unit-test.

---
+++ plugins/api/src/data_object_modify_info.cpp
@@ -0,0 +1,156 @@
+#include "api_plugin_number.h"
+#include "rodsDef.h"
+#include "rcConnect.h"
+#include "rodsPackInstruct.h"
+#include "rcMisc.h"
+#include "client_api_whitelist.hpp"
+
+#include "apiHandler.hpp"
+
+#include <functional>
+
+#ifdef RODS_SERVER
+
+//
+// Server-side Implementation
+//
+
+#include "objDesc.hpp"
+#include "irods_stacktrace.hpp"
+#include "irods_server_api_call.hpp"
+#include "irods_re_serialization.hpp"
+#include "rsModDataObjMeta.hpp"
+#include "scoped_privileged_client.hpp"
+#include "key_value_proxy.hpp"
+
+#include <string>
+#include <tuple>
+
+namespace
+{
+    //
+    // Function Prototypes
+    //
+
+    auto call_data_object_modify_info(irods::api_entry*, rsComm_t*, modDataObjMeta_t*) -> int;
+    auto is_input_valid(const bytesBuf_t*) -> std::tuple<bool, std::string>;
+    auto rs_data_object_modify_info(rsComm_t*, modDataObjMeta_t*) -> int;
+
+    //
+    // Function Implementations
+    //
+
+    auto call_data_object_modify_info(irods::api_entry* api,
+                                      rsComm_t* comm,
+                                      modDataObjMeta_t* input) -> int
+    {
+        return api->call_handler<modDataObjMeta_t*>(comm, input);
+    }
+
+    auto is_input_valid(const modDataObjMeta_t* input) -> std::tuple<bool, std::string>
+    {
+        if (!input) {
+            return {false, "Input is null"};
+        }
+
+        irods::experimental::key_value_proxy proxy{*input->regParam};
+
+        const auto is_invalid_keyword = [](const auto& handle)
+        {
+            // To allow modification of a column, comment out the condition
+            // referencing that column.
+            return handle.key() == CHKSUM_KW ||
+                   handle.key() == COLL_ID_KW ||
+                   //handle.key() == DATA_COMMENTS_KW ||
+                   handle.key() == DATA_CREATE_KW ||
+                   //handle.key() == DATA_EXPIRY_KW ||
+                   handle.key() == DATA_ID_KW ||
+                   handle.key() == DATA_MAP_ID_KW ||
+                   handle.key() == DATA_MODE_KW ||
+                   //handle.key() == DATA_MODIFY_KW ||
+                   handle.key() == DATA_NAME_KW ||
+                   handle.key() == DATA_OWNER_KW ||
+                   handle.key() == DATA_OWNER_ZONE_KW ||
+                   //handle.key() == DATA_RESC_GROUP_NAME_KW || // Not defined.
+                   handle.key() == DATA_SIZE_KW ||
+                   //handle.key() == DATA_TYPE_KW ||
+                   handle.key() == FILE_PATH_KW ||
+                   handle.key() == REPL_NUM_KW ||
+                   handle.key() == REPL_STATUS_KW ||
+                   handle.key() == RESC_HIER_STR_KW ||
+                   handle.key() == RESC_ID_KW ||
+                   handle.key() == RESC_NAME_KW ||
+                   handle.key() == STATUS_STRING_KW ||
+                   handle.key() == VERSION_KW;
+        };
+
+        if (std::any_of(std::begin(proxy), std::end(proxy), is_invalid_keyword)) {
+            return {false, "Invalid keyword found"};
+        }
+
+        return {true, ""};
+    }
+
+    auto rs_data_object_modify_info(rsComm_t* comm, modDataObjMeta_t* input) -> int
+    {
+        if (auto [valid, msg] = is_input_valid(input); !valid) {
+            rodsLog(LOG_ERROR, msg.data());
+            return USER_BAD_KEYWORD_ERR;
+        }
+
+        irods::experimental::scoped_privileged_client spc{*comm};
+        return rsModDataObjMeta(comm, input);
+    }
+
+    using operation = std::function<int(rsComm_t*, modDataObjMeta_t*)>;
+    const operation op = rs_data_object_modify_info;
+    #define CALL_DATA_OBJECT_MODIFY_INFO call_data_object_modify_info
+} // anonymous namespace
+
+#else // RODS_SERVER
+
+//
+// Client-side Implementation
+//
+
+#include "modDataObjMeta.h"
+
+namespace
+{
+    using operation = std::function<int(rsComm_t*, modDataObjMeta_t*)>;
+    const operation op{};
+    #define CALL_DATA_OBJECT_MODIFY_INFO nullptr
+} // anonymous namespace
+
+#endif // RODS_SERVER
+
+// The plugin factory function must always be defined.
+extern "C"
+auto plugin_factory(const std::string& _instance_name,
+                    const std::string& _context) -> irods::api_entry*
+{
+#ifdef RODS_SERVER
+    irods::client_api_whitelist::instance().add(DATA_OBJECT_MODIFY_INFO_APN);
+#endif // RODS_SERVER
+
+    // clang-format off
+    irods::apidef_t def{DATA_OBJECT_MODIFY_INFO_APN,     // API number
+                        RODS_API_VERSION,                // API version
+                        NO_USER_AUTH,                    // Client auth
+                        NO_USER_AUTH,                    // Proxy auth
+                        "ModDataObjMeta_PI", 0,          // In PI / bs flag
+                        nullptr, 0,                      // Out PI / bs flag
+                        op,                              // Operation
+                        "data_object_modify_info",       // Operation name
+                        clearModDataObjMetaInp,          // Null clear function
+                        (funcPtr) CALL_DATA_OBJECT_MODIFY_INFO};
+    // clang-format on
+
+    auto* api = new irods::api_entry{def};
+
+    api->in_pack_key = "ModDataObjMeta_PI";
+    api->in_pack_value = ModDataObjMeta_PI;
+
+    return api;
+}
+
---
+++ unit_tests/src/test_data_object_modify_info.cpp
@@ -0,0 +1,87 @@
+#include "catch.hpp"
+
+#include "getRodsEnv.h"
+#include "irods_client_api_table.hpp"
+#include "irods_pack_table.hpp"
+#include "connection_pool.hpp"
+#include "filesystem.hpp"
+#include "dstream.hpp"
+#include "transport/default_transport.hpp"
+#include "rodsErrorTable.h"
+#include "irods_at_scope_exit.hpp"
+#include "data_object_modify_info.h"
+
+TEST_CASE("data_object_modify_info")
+{
+    namespace fs = irods::experimental::filesystem;
+    namespace io = irods::experimental::io;
+
+    auto& api_table = irods::get_client_api_table();
+    auto& pack_table = irods::get_pack_table();
+    init_api_table(api_table, pack_table);
+
+    auto conn_pool = irods::make_connection_pool();
+    auto conn = conn_pool->get_connection();
+
+    rodsEnv env;
+    _getRodsEnv(env);
+
+    const auto sandbox = fs::path{env.rodsHome} / "irods_unit_tests_sandbox";
+    const auto path = sandbox / "dstream_data_object.txt";
+
+    fs::client::create_collection(conn, sandbox);
+
+    irods::at_scope_exit cleanup{[&] {
+        fs::client::remove_all(conn, sandbox, fs::remove_options::no_trash);
+    }};
+
+    // Create a data object in iRODS.
+    {
+        io::client::default_transport tp{conn};
+        io::odstream out{tp, path};
+    }
+
+    dataObjInfo_t info{};
+    std::strcpy(info.objPath, path.c_str());
+
+    const auto invalid_keywords = {
+        CHKSUM_KW,
+        COLL_ID_KW,
+        //DATA_COMMENTS_KW,
+        DATA_CREATE_KW,
+        //DATA_EXPIRY_KW,
+        DATA_ID_KW,
+        DATA_MAP_ID_KW,
+        DATA_MODE_KW,
+        //DATA_MODIFY_KW,
+        DATA_NAME_KW,
+        DATA_OWNER_KW,
+        DATA_OWNER_ZONE_KW,
+        //DATA_RESC_GROUP_NAME_KW, // Not defined.
+        DATA_SIZE_KW,
+        //DATA_TYPE_KW,
+        FILE_PATH_KW,
+        REPL_NUM_KW,
+        REPL_STATUS_KW,
+        RESC_HIER_STR_KW,
+        RESC_ID_KW,
+        RESC_NAME_KW,
+        STATUS_STRING_KW,
+        VERSION_KW
+    };
+
+    for (auto&& kw : invalid_keywords) {
+        DYNAMIC_SECTION("error on invalid keyword [" << kw << ']')
+        {
+            keyValPair_t reg_params{};
+            addKeyVal(&reg_params, kw, "");
+
+            modDataObjMeta_t input{};
+            input.dataObjInfo = &info;
+            input.regParam = &reg_params;
+
+            REQUIRE(rc_data_object_modify_info(static_cast<rcComm_t*>(conn), &input) == USER_BAD_KEYWORD_ERR);
+        }
+    }
+}
+
