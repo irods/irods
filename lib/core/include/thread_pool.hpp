8614d119ab0d979320a3c3cd7178450a3ebb7fc1
Alan King <alanking@renci.org>
2020-03-25 10:55:10 -0400

    [#4824] Use boost::asio::thread_pool in irods::thread_pool

--- lib/core/include/thread_pool.hpp
+++ lib/core/include/thread_pool.hpp
@@ -1,64 +1,50 @@
 #ifndef IRODS_THREAD_POOL_HPP
 #define IRODS_THREAD_POOL_HPP
 
+#include <utility>
+
 #include <boost/asio.hpp>
-#include <boost/thread.hpp>
 
-#include <memory>
+namespace irods
+{
+    class thread_pool
+    {
+    public:
+        explicit thread_pool(int _size)
+            : pool_{static_cast<std::size_t>(_size)}
+        {
+        }
 
-namespace irods {
-    class thread_pool {
-        public:
-            explicit thread_pool(int _size)
-                : io_service_{std::make_shared<boost::asio::io_service>()}
-                , work_{std::make_shared<boost::asio::io_service::work>(*io_service_)}
-            {
-                for (decltype(_size) i{}; i < _size; i++) {
-                    thread_group_.create_thread([this] {
-                        io_service_->run();
-                    });
-                }
-            }
+        void join()
+        {
+            pool_.join();
+        }
 
-            ~thread_pool() {
-                stop();
-                join();
-            }
+        void stop()
+        {
+            pool_.stop();
+        }
 
-            template <typename Function>
-            static inline void dispatch(thread_pool& _pool, Function&& _func) {
-                _pool.dispatch(std::forward<Function>(_func));
-            }
+        template <typename Function>
+        static void dispatch(thread_pool& _pool, Function&& _func)
+        {
+            boost::asio::dispatch(_pool.pool_, std::forward<Function>(_func));
+        }
 
-            template <typename Function>
-            static inline void post(thread_pool& _pool, Function&& _func) {
-                _pool.post(std::forward<Function>(_func));
-            }
+        template <typename Function>
+        static void post(thread_pool& _pool, Function&& _func)
+        {
+            boost::asio::post(_pool.pool_, std::forward<Function>(_func));
+        }
 
-            void join() {
-                thread_group_.join_all();
-            }
+        template <typename Function>
+        static void defer(thread_pool& _pool, Function&& _func)
+        {
+            boost::asio::defer(_pool.pool_, std::forward<Function>(_func));
+        }
 
-            void stop() {
-                if (io_service_) {
-                    io_service_->stop();
-                }
-            }
-
-        private:
-            boost::thread_group thread_group_;
-            std::shared_ptr<boost::asio::io_service> io_service_;
-            std::shared_ptr<boost::asio::io_service::work> work_;
-
-            template <typename Function>
-            void dispatch(Function&& _func) {
-                io_service_->dispatch(std::forward<Function>(_func));
-            }
-
-            template <typename Function>
-            void post(Function&& _func) {
-                io_service_->post(std::forward<Function>(_func));
-            }
+    private:
+        boost::asio::thread_pool pool_;
     };
 } // namespace irods
 
