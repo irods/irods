bf98f9f4bd1c17cc117ba359e2b5e09a5180ddcd
Jaspreet Gill <jessi.k.gill@gmail.com>
2020-04-08 17:54:32 -0400

    [#4846] fmt dependency version number updated

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -30,7 +30,7 @@ def install_building_dependencies(externals_directory):
         'irods-externals-clang-runtime6.0-0',
         'irods-externals-clang6.0-0',
         'irods-externals-cppzmq4.2.3-0',
-        'irods-externals-fmt6.1.2-0',
+        'irods-externals-fmt6.1.2-1',
         'irods-externals-json3.7.3-0',
         'irods-externals-libarchive3.3.2-1',
         'irods-externals-nanodbc2.13.0-0',
