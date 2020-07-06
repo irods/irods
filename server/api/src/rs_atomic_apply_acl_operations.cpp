eb0db9d4fb822e73a72415dda567cd02cb2116af
Kory Draughn <korydraughn@renci.org>
2020-07-06 14:27:07 -0400

    [#5001] Implemented new API plugin: atomic_apply_acl_operations.

---
+++ server/api/src/rs_atomic_apply_acl_operations.cpp
@@ -0,0 +1,27 @@
+#include "rs_atomic_apply_acl_operations.hpp"
+
+#include "api_plugin_number.h"
+#include "rodsErrorTable.h"
+
+#include "irods_server_api_call.hpp"
+
+#include <cstring>
+
+auto rs_atomic_apply_acl_operations(RsComm* _comm, const char* _json_input, char** _json_output) -> int
+{
+    if (!_json_input || !_json_output) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t input{};
+    input.buf = const_cast<char*>(_json_input);
+    input.len = static_cast<int>(std::strlen(_json_input)) + 1;
+
+    bytesBuf_t* output{};
+
+    const auto ec = irods::server_api_call(ATOMIC_APPLY_ACL_OPERATIONS_APN, _comm, &input, &output);
+
+    *_json_output = static_cast<char*>(output->buf);
+
+    return ec;
+}
---
+++ unit_tests/cmake/test_config/irods_atomic_apply_acl_operations.cmake
@@ -0,0 +1,21 @@
+set(IRODS_TEST_TARGET irods_atomic_apply_acl_operations)
+
+set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_atomic_apply_acl_operations.cpp)
+
+set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
+                            ${CMAKE_SOURCE_DIR}/lib/core/include
+                            ${CMAKE_SOURCE_DIR}/lib/api/include
+                            ${CMAKE_SOURCE_DIR}/lib/filesystem/include
+                            ${CMAKE_SOURCE_DIR}/server/core/include
+                            ${CMAKE_SOURCE_DIR}/server/icat/include
+                            ${CMAKE_SOURCE_DIR}/server/re/include
+                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
+                            ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
+ 
+set(IRODS_TEST_LINK_LIBRARIES irods_common
+                              irods_client
+                              irods_plugin_dependencies
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+                              c++abi)
