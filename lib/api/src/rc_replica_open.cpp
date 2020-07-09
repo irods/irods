c47e108a8c09cb3a2edf3292b6ae3dbe469035d0
Kory Draughn <korydraughn@renci.org>
2020-07-09 12:44:47 -0400

    [#5004] Implemented new API plugin: replica_open
    Includes unit-test for replica_open and replica_close API plugins.

---
+++ lib/api/src/rc_replica_open.cpp
@@ -0,0 +1,26 @@
+#include "replica_open.h"
+
+#include "api_plugin_number.h"
+#include "procApiRequest.h"
+#include "rodsErrorTable.h"
+
+#include <cstring>
+
+auto rc_replica_open(RcComm* _comm, DataObjInp* _input, char** _json_output) -> int
+{
+    if (!_input || !_json_output) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t* output{};
+
+    const int ec = procApiRequest(_comm, REPLICA_OPEN_APN,
+                                  _input, nullptr,
+                                  reinterpret_cast<void**>(&output), nullptr);
+
+    if (ec >= 3) {
+        *_json_output = static_cast<char*>(output->buf);
+    }
+
+    return ec;
+}
