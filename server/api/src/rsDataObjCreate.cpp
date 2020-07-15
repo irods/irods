5231cf77e4daec36941419e8f6abe61c9060e977
Kory Draughn <korydraughn@renci.org>
2020-07-15 12:59:04 -0400

    [#5012] O_TRUNC flag is now set on single buffer PUTs and calls to rxDataObjCreate.

--- server/api/src/rsDataObjCreate.cpp
+++ server/api/src/rsDataObjCreate.cpp
@@ -4,7 +4,7 @@
 #include "rsDataObjOpen.hpp"
 
 int rsDataObjCreate(rsComm_t *rsComm, dataObjInp_t *dataObjInp) {
-    dataObjInp->openFlags |= O_CREAT | O_RDWR;
+    dataObjInp->openFlags |= (O_CREAT | O_RDWR | O_TRUNC);
     return rsDataObjOpen(rsComm, dataObjInp);
 } // rsDataObjCreate
 
