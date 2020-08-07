ff30a2c3b5dbb4d1062d609dfe1470c8e8dabbd6
Alan King <alanking@renci.org>
2020-08-07 15:30:46 -0400

    [#4331] Add finalize_data_object API plugin
    Adds a new API plugin which finalizes a data object by atomically
    updating all rows in r_data_main mapped to a particular data_id.
    
    The new API endpoint takes a JSON string of the following form:
    
    {
        "data_id": "XXXXX"
        "replicas": [
            {
                "before": {
                    "data_id": "XXXXX",
                    "coll_id": "XXXXX",
                    ...
                }
                "after": {
                    "data_id": "XXXXX",
                    "coll_id": "XXXXX",
                    ...
                }
            },
            ...
        ]
    }
    
    The API plugin takes the array of replicas and updates each corresponding
    row in the catalog with the column values provided in the "after" piece.
    The "before" piece is mainly in place for the caller to keep track of the
    state of a given replica before any changes are made to it at the time of
    opening.
    
    Note: A replica's mtime can be updated to the time of the actual catalog
    update by providing the string in SET_TIME_TO_NOW_KW to the "modify_ts"
    field of the JSON input.

---
+++ lib/api/include/data_object_finalize.h
@@ -0,0 +1,99 @@
+#ifndef IRODS_DATA_OBJECT_FINALIZE_HPP
+#define IRODS_DATA_OBJECT_FINALIZE_HPP
+
+/// \file
+
+const char* SET_TINE_TO_NOW_KW = "set time to now";
+
+struct RcComm;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Set the state of every replica for a data object in the catalog, atomically.
+///
+/// A complete set of the columns in their current state as well as the desired modifications
+/// is required in the form of a JSON structure.
+///
+/// \p json_input must have the following JSON structure:
+/// \code{.js}
+/// {
+///   "data_id": string,
+///   "replicas": [
+///     {
+///       "before": {
+///         "data_id": string,
+///         "coll_id": string
+///         "data_name": string
+///         "data_repl_num": string
+///         "data_version": string
+///         "data_type_name": string
+///         "data_size": string
+///         "data_path": string
+///         "data_owner_name": string
+///         "data_owner_zone": string
+///         "data_is_dirty": string
+///         "data_status": string
+///         "data_checksum": string
+///         "data_expiry_ts": string
+///         "data_map_id": string
+///         "data_mode": string
+///         "r_comment": string
+///         "create_ts": string
+///         "modify_ts": string
+///         "resc_id: string
+///       },
+///       "after": {
+///         "data_id": string,
+///         "coll_id": string
+///         "data_name": string
+///         "data_repl_num": string
+///         "data_version": string
+///         "data_type_name": string
+///         "data_size": string
+///         "data_path": string
+///         "data_owner_name": string
+///         "data_owner_zone": string
+///         "data_is_dirty": string
+///         "data_status": string
+///         "data_checksum": string
+///         "data_expiry_ts": string
+///         "data_map_id": string
+///         "data_mode": string
+///         "r_comment": string
+///         "create_ts": string
+///         "modify_ts": string
+///         "resc_id: string
+///       }
+///     }
+///   ]
+/// }
+/// \endcode
+///
+/// The "modify_ts" field can be populated with the string value for SET_TIME_TO_NOW_KW and
+/// the API plugin will fill the value with the current time.
+///
+/// On error, \p json_output will have the following JSON structure:
+/// \code{.js}
+/// {
+///   "error_message": string
+/// }
+/// \endcode
+///
+/// \since 4.2.9
+///
+/// \param[in]  _comm        A pointer to a RcComm.
+/// \param[in]  _json_input  A JSON string containing the replicas with before and after states.
+/// \param[out] _json_output A JSON string containing the error information on failure.
+///
+/// \return An integer.
+/// \retval 0        On success.
+/// \retval non-zero On failure.
+int rc_data_object_finalize(RcComm* _comm, const char* _json_input, char** _json_output);
+
+#ifdef __cplusplus
+} // extern "C"
+#endif
+
+#endif // IRODS_DATA_OBJECT_FINALIZE_HPP
---
+++ lib/api/src/rc_data_object_finalize.cpp
@@ -0,0 +1,28 @@
+#include "data_object_finalize.h"
+
+#include "api_plugin_number.h"
+#include "procApiRequest.h"
+#include "rodsErrorTable.h"
+
+#include <cstring>
+
+auto rc_data_object_finalize(RcComm* _comm, const char* _json_input, char** _json_output) -> int
+{
+    if (!_json_input || !_json_output) {
+        return SYS_INVALID_INPUT_PARAM;
+    }
+
+    bytesBuf_t input_buf{};
+    input_buf.buf = const_cast<char*>(_json_input);
+    input_buf.len = static_cast<int>(std::strlen(_json_input)) + 1;
+
+    bytesBuf_t* output_buf{};
+
+    const int ec = procApiRequest(_comm, DATA_OBJECT_FINALIZE_APN,
+                                  &input_buf, nullptr,
+                                  reinterpret_cast<void**>(&output_buf), nullptr);
+
+    *_json_output = static_cast<char*>(output_buf->buf);
+
+    return ec;
+}
