4e09c7f75cfe019ef5fb6cef386608e08d367072
Jason Coposky <jasonc@renci.org>
2020-08-13 09:14:49 -0400

    [#5007] build fixes

--- plugins/experimental/remove.cpp
+++ plugins/experimental/remove.cpp
@@ -1,61 +1,26 @@
-
-#include "experimental_plugin_framework.hpp"
-#include "parallel_remove_collection.hpp"
-
-#include "thread_pool.hpp"
-#include "connection_pool.hpp"
-
-#define IRODS_FILESYSTEM_ENABLE_SERVER_SIDE_API
-#include "filesystem.hpp"
+#include "parallel_filesystem_operation.hpp"
 
 namespace irods::experimental::api {
-    namespace fs   = irods::experimental::filesystem;
-    namespace fsvr = irods::experimental::filesystem::server;
-
-    class remove : public base {
-
-        bool enable_asynchronous_operation() { return true; }
-
-        auto operation(rsComm_t* comm, const json req) -> json
+    class recursive_remove : public parallel_filesystem_operation {
+        void process_object(rcComm_t& comm, const fs::path& path, const json& req)
         {
-            try {
-                blackboard.set({{constants::status, states::running}});
+            auto ur = get<bool>("unregister", req, false);
+            auto nt = get<bool>("no_trash",   req, false);
+            fscl::remove(comm, path, {nt, false, false, false, ur});
+        }
 
-                fs::path path = get<std::string>("logical_path", req);
+        void collection_predicate(rcComm_t& comm, const json& req)
+        {
+            fscl::remove_all(comm, req.at("logical_path"), {false, false, false, false});
+        }
 
-                auto unregister = get<bool>("unregister", req, false);
-
-                auto no_trash = get<bool>("no_trash", req, false);
-
-                auto thread_count = get<int> ("thread_count", req, 4);
-
-                if(fsvr::is_collection(*comm, path)) {
-                    parallel_remove_collection(blackboard, {path, unregister, no_trash, thread_count});
-                }
-                else {
-                    bool verbose{false}, progress{false}, recursive{false};
-                    fsvr::remove(*comm, path, {no_trash, verbose, progress, recursive, unregister});
-                }
-
-                blackboard.update({{constants::status, states::complete}});
-            }
-            catch(const irods::exception& e) {
-                rodsLog(LOG_ERROR, "%s", e.what());
-                blackboard.set({{constants::status,  states::failed},
-                                {constants::errors, {
-                                {constants::code,    e.code()},
-                                {constants::message, e.what()}}}});
-            }
-
-            return blackboard.get();
-
-        } // operation
-
-    }; // remove
+        public:
+        recursive_remove() : parallel_filesystem_operation("remove") {}
+    }; // recursive_remove
 
 } // namespace irods::experimental::api
 
 extern "C"
-irods::experimental::api::remove* plugin_factory(const std::string&, const std::string&) {
-    return new irods::experimental::api::remove{};
+irods::experimental::api::recursive_remove* plugin_factory(const std::string&, const std::string&) {
+    return new irods::experimental::api::recursive_remove{};
 }
