74f4fe4a262e90fdffc2098895f4ff129ea54486
Alan King <alanking@renci.org>
2020-09-17 18:55:54 -0400

    [#5142][#5143][#5151] New replica library features
    [#5142] Add resource name overloads to replica lib
    
    Adds overloads for most of the replica library functions which take a
    leaf resource name (std::string_view) in order to filter replica results
    by DATA_RESC_NAME in addition to the replica number alternatives.
    
    [#5143] Add to_leaf_resource and to_replica_number
    
    Adds two new functions to the replica library.
    
    to_leaf_resource: Takes a logical path and a replica number and
    returns that replica's resource name.
    
    to_replica_number: Takes a logical path and a leaf resource name and
    returns that replica's replica number.
    
    [#5151] Add replica::replica_exists
    
    Add a function which selects a DATA_ID based on the replica information
    passed by the user. Separated from the get_data_object_info functions
    in order to reduce generating unused data.

--- unit_tests/src/test_replica.cpp
+++ unit_tests/src/test_replica.cpp
@@ -5,6 +5,7 @@
 #include "dstream.hpp"
 #include "irods_at_scope_exit.hpp"
 #include "replica.hpp"
+#include "replica_proxy.hpp"
 #include "resource_administration.hpp"
 #include "rodsClient.h"
 #include "transport/default_transport.hpp"
@@ -14,6 +15,8 @@
 #include <iostream>
 #include <string_view>
 #include <thread>
+
+#define very_long_string "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 
 namespace fs = irods::experimental::filesystem;
 namespace io = irods::experimental::io;
@@ -28,19 +31,20 @@ namespace replica = irods::experimental::replica;
 
 TEST_CASE("replica", "[replica]")
 {
+    using namespace std::chrono_literals;
+
     load_client_api_plugins();
 
-    auto conn_pool = irods::make_connection_pool();
-    auto conn_proxy = conn_pool->get_connection();
-    rcComm_t& conn = conn_proxy;
+    irods::experimental::client_connection setup_conn;
+    RcComm& setup_comm = static_cast<RcComm&>(setup_conn);
 
     rodsEnv env;
     _getRodsEnv(env);
 
     const auto sandbox = fs::path{env.rodsHome} / "test_replica";
 
-    if (!fs::client::exists(conn, sandbox)) {
-        REQUIRE(fs::client::create_collection(conn, sandbox));
+    if (!fs::client::exists(setup_comm, sandbox)) {
+        REQUIRE(fs::client::create_collection(setup_comm, sandbox));
     }
 
     irods::at_scope_exit remove_sandbox{[&sandbox] {
@@ -50,72 +54,196 @@ TEST_CASE("replica", "[replica]")
 
     const auto target_object = sandbox / "target_object";
 
-    SECTION("library operations")
+    std::string_view expected_checksum = "sha2:z4DNiu1ILV0VJ9fccvzv+E5jJlkoSER9LcCw6H38mpA=";
+    std::string_view object_content = "testing";
+
+    std::string_view updated_object_content = "something else";
+    std::string_view updated_checksum = "sha2:9B8/piX/Eg3cp+9Fa/ZjcezqI8Ep9OTDI2cQHttRbPg=";
+
     {
-        using namespace std::chrono_literals;
-
-        std::string_view expected_checksum = "sha2:47DEQpj8HBSa+/TImW+5JCeuQeRkm5NMpJWZG3hSuFU=";
-
-        {
-            io::client::default_transport tp{conn};
-            io::odstream{tp, target_object};
-        }
-
-        // size
-        REQUIRE(replica::is_replica_empty(conn, target_object, 0));
-
-        // checksum
-        REQUIRE(replica::replica_checksum(conn, target_object, 0) == expected_checksum);
-
-        // mtime (access)
-        const auto old_mtime = replica::last_write_time(conn, target_object, 0);
-        std::this_thread::sleep_for(2s);
-
-        const std::string_view resc_name = "unit_test_ufs";
-
-        {
-            // create resource.
-            irods::experimental::client_connection conn;
-            REQUIRE(unit_test_utils::add_ufs_resource(conn, resc_name, "unit_test_vault"));
-        }
-
-        {
-            // replicate replica.
-            irods::experimental::client_connection conn;
-            REQUIRE(unit_test_utils::replicate_data_object(conn, target_object.c_str(), resc_name));
-        }
-
-        irods::at_scope_exit clean_up{[&target_object, &resc_name] {
-            namespace ix = irods::experimental;
-            ix::client_connection conn;
-            fs::client::remove(conn, target_object, fs::remove_options::no_trash);
-            ix::administration::client::remove_resource(conn, resc_name);
-        }};
-
-        irods::experimental::client_connection conn;
-
-        // the replica number of the second replica.
-        const auto second_replica = 1;
-
-        // show that the mtime of each replica is different.
-        REQUIRE(old_mtime != replica::last_write_time<RcComm>(conn, target_object, second_replica));
-
-        // mtime (modification)
-        replica::last_write_time<RcComm>(conn, target_object, second_replica, old_mtime);
-        REQUIRE(old_mtime == replica::last_write_time<RcComm>(conn, target_object, second_replica));
+        io::client::default_transport tp{setup_comm};
+        io::odstream{tp, target_object} << object_content;
     }
 
-    SECTION("replica size")
-    {
-        std::string_view s = "testing";
+    std::this_thread::sleep_for(2s);
 
+    const std::string_view resc_name = "unit_test_ufs";
+
+    {
+        // create resource.
+        irods::experimental::client_connection conn;
+        REQUIRE(unit_test_utils::add_ufs_resource(conn, resc_name, "unit_test_vault"));
+    }
+
+    {
+        // replicate replica.
+        irods::experimental::client_connection conn;
+        REQUIRE(unit_test_utils::replicate_data_object(conn, target_object.c_str(), resc_name));
+    }
+
+    irods::at_scope_exit clean_up{[&target_object, &resc_name] {
+        namespace ix = irods::experimental;
+        ix::client_connection conn;
+        fs::client::remove(conn, target_object, fs::remove_options::no_trash);
+        ix::administration::client::remove_resource(conn, resc_name);
+    }};
+
+    // the replica number of the second replica.
+    constexpr auto second_replica = 1;
+
+    SECTION("perform library operations using replica number")
+    {
+        constexpr int invalid_replica_number = -1;
+
+        irods::experimental::client_connection conn;
+        RcComm& comm = static_cast<RcComm&>(conn);
+
+        // invalid inputs
+        REQUIRE_THROWS(replica::replica_exists(comm, std::string{}, second_replica));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, std::string{}, second_replica));
+        REQUIRE_THROWS(replica::replica_checksum(comm, std::string{}, second_replica));
+        REQUIRE_THROWS(replica::last_write_time(comm, std::string{}, second_replica));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, very_long_string, second_replica));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, very_long_string, second_replica));
+        REQUIRE_THROWS(replica::replica_checksum(comm, very_long_string, second_replica));
+        REQUIRE_THROWS(replica::last_write_time(comm, very_long_string, second_replica));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, sandbox, second_replica));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, sandbox, second_replica));
+        REQUIRE_THROWS(replica::replica_checksum(comm, sandbox, second_replica));
+        REQUIRE_THROWS(replica::last_write_time(comm, sandbox, second_replica));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, target_object, invalid_replica_number));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, target_object, invalid_replica_number));
+        REQUIRE_THROWS(replica::replica_checksum(comm, target_object, invalid_replica_number));
+        REQUIRE_THROWS(replica::last_write_time(comm, target_object, invalid_replica_number));
+
+        REQUIRE(std::nullopt == replica::to_leaf_resource(comm, target_object, 42));
+
+        // existence
+        REQUIRE(replica::replica_exists(comm, target_object, second_replica));
+
+        // size
+        REQUIRE(!replica::is_replica_empty(comm, target_object, second_replica));
+        REQUIRE(object_content.length() == replica::replica_size(comm, target_object, second_replica));
+
+        // checksum a specific replica
+        REQUIRE(replica::replica_checksum(comm, target_object, second_replica) == expected_checksum);
+
+        // test replica::verification_calculation
+        // overwrite second replica with new contents
         {
-            io::client::default_transport tp{conn};
-            io::odstream{tp, target_object} << s;
+            std::ios_base::openmode mode = std::ios_base::out | std::ios_base::trunc;
+            io::client::default_transport tp{comm};
+            io::odstream{tp, target_object, io::replica_number{second_replica}, mode} << updated_object_content;
         }
 
-        REQUIRE(!replica::is_replica_empty(conn, target_object, 0));
-        REQUIRE(s.length() == replica::replica_size(conn, target_object, 0));
+        // show that the replica was updated
+        REQUIRE(updated_object_content.length() == replica::replica_size(comm, target_object, second_replica));
+
+        // show that the checksum has not updated in the catalog, yet
+        REQUIRE(replica::replica_checksum(comm, target_object, second_replica) == expected_checksum);
+
+        // now update the catalog using the force flag
+        REQUIRE(replica::replica_checksum(comm, target_object, second_replica, replica::verification_calculation::always) == updated_checksum);
+
+        // show that the catalog updated
+        REQUIRE(replica::replica_checksum(comm, target_object, second_replica) == updated_checksum);
+
+        // show that the other replica did not receive a checksum in the catalog
+        {
+            const auto [rp, lm] = replica::make_replica_proxy(comm, target_object, 0);
+            REQUIRE(rp.checksum() == std::string{});
+        }
+
+        // show that the mtime of each replica is different.
+        const auto old_mtime = replica::last_write_time(comm, target_object, 0);
+        REQUIRE(old_mtime != replica::last_write_time(comm, target_object, second_replica));
+
+        // update the mtime for the second replica
+        replica::last_write_time(comm, target_object, second_replica, old_mtime);
+        REQUIRE(old_mtime == replica::last_write_time(comm, target_object, second_replica));
+
+        // get resource name from replica number
+        REQUIRE(resc_name == replica::to_leaf_resource(comm, target_object, second_replica));
+    }
+
+    SECTION("perform library operations using resource name")
+    {
+
+        irods::experimental::client_connection conn;
+        RcComm& comm = static_cast<RcComm&>(conn);
+
+        // invalid inputs
+        REQUIRE_THROWS(replica::replica_exists(comm, std::string{}, resc_name));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, std::string{}, resc_name));
+        REQUIRE_THROWS(replica::replica_checksum(comm, std::string{}, resc_name));
+        REQUIRE_THROWS(replica::last_write_time(comm, std::string{}, resc_name));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, very_long_string, resc_name));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, very_long_string, resc_name));
+        REQUIRE_THROWS(replica::replica_checksum(comm, very_long_string, resc_name));
+        REQUIRE_THROWS(replica::last_write_time(comm, very_long_string, resc_name));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, sandbox, resc_name));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, sandbox, resc_name));
+        REQUIRE_THROWS(replica::replica_checksum(comm, sandbox, resc_name));
+        REQUIRE_THROWS(replica::last_write_time(comm, sandbox, resc_name));
+
+        REQUIRE_THROWS(replica::replica_exists(comm, target_object, std::string{}));
+        REQUIRE_THROWS(replica::is_replica_empty(comm, target_object, std::string{}));
+        REQUIRE_THROWS(replica::replica_checksum(comm, target_object, std::string{}));
+        REQUIRE_THROWS(replica::last_write_time(comm, target_object, std::string{}));
+
+        REQUIRE(std::nullopt == replica::to_replica_number(comm, target_object, "nope"));
+
+        // existence
+        REQUIRE(replica::replica_exists(comm, target_object, resc_name));
+
+        // size
+        REQUIRE(!replica::is_replica_empty(comm, target_object, resc_name));
+        REQUIRE(object_content.length() == replica::replica_size(comm, target_object, resc_name));
+
+        // checksum a specific replica
+        REQUIRE(replica::replica_checksum(comm, target_object, resc_name) == expected_checksum);
+
+        // test replica::verification_calculation
+        // overwrite second replica with new contents
+        {
+            std::ios_base::openmode mode = std::ios_base::out | std::ios_base::trunc;
+            io::client::default_transport tp{comm};
+            io::odstream{tp, target_object, io::leaf_resource_name{resc_name.data()}, mode} << updated_object_content;
+        }
+
+        // show that the replica was updated
+        REQUIRE(updated_object_content.length() == replica::replica_size(comm, target_object, second_replica));
+
+        // show that the checksum has not updated in the catalog, yet
+        REQUIRE(replica::replica_checksum(comm, target_object, resc_name) == expected_checksum);
+
+        // now update the catalog using the force flag
+        REQUIRE(replica::replica_checksum(comm, target_object, resc_name, replica::verification_calculation::always) == updated_checksum);
+
+        // show that the catalog updated
+        REQUIRE(replica::replica_checksum(comm, target_object, resc_name) == updated_checksum);
+
+        // show that the other replica did not receive a checksum in the catalog
+        {
+            const auto [rp, lm] = replica::make_replica_proxy(comm, target_object, 0);
+            REQUIRE(rp.checksum() == std::string{});
+        }
+
+        // show that the mtime of each replica is different.
+        const auto old_mtime = replica::last_write_time(comm, target_object, 0);
+        REQUIRE(old_mtime != replica::last_write_time(comm, target_object, resc_name));
+
+        // update the mtime for the second replica
+        replica::last_write_time(comm, target_object, resc_name, old_mtime);
+        REQUIRE(old_mtime == replica::last_write_time(comm, target_object, resc_name));
+
+        // get replica number from resource name
+        REQUIRE(second_replica == replica::to_replica_number(comm, target_object, resc_name));
     }
 }
 
