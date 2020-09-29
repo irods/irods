e5c4986e6a43aba139ea45082ad18d237241f924
Kory Draughn <korydraughn@renci.org>
2020-09-29 15:13:03 -0400

    [#5233] Added include guards to delay queue header.

--- server/core/include/irods_delay_queue.hpp
+++ server/core/include/irods_delay_queue.hpp
@@ -1,3 +1,6 @@
+#ifndef IRODS_DELAY_QUEUE_HPP
+#define IRODS_DELAY_QUEUE_HPP
+
 #include <algorithm>
 #include <mutex>
 #include <string>
@@ -7,37 +10,35 @@ namespace irods {
     class delay_queue {
         public:
             delay_queue() = default;
-            delay_queue(delay_queue&) = delete;
-            delay_queue& operator=(delay_queue&) = delete;
+            delay_queue(const delay_queue&) = delete;
+            delay_queue& operator=(const delay_queue&) = delete;
 
             bool contains_rule_id(const std::string& _rule_id) {
-                auto rules_lock{get_queue_lock()};
-                return std::any_of(queued_rules.begin(), queued_rules.end(),
-                           [&](const std::string& q_rule){return q_rule == _rule_id;});
+                std::lock_guard rules_lock{rules_mutex_};
+                return std::any_of(queued_rules_.begin(), queued_rules_.end(),
+                           [&](const std::string& q_rule) { return q_rule == _rule_id; });
             }
 
             void enqueue_rule(const std::string& rule_id) {
-                auto rules_lock{get_queue_lock()};
-                queued_rules.emplace_back(rule_id);
+                std::lock_guard rules_lock{rules_mutex_};
+                queued_rules_.emplace_back(rule_id);
             }
 
             void dequeue_rule(const std::string& rule_id) {
-                auto rules_lock{get_queue_lock()};
-                for(auto it = queued_rules.begin(); it != queued_rules.end(); ++it) {
-                    const auto& rule = *it;
-                    if(rule == std::string(rule_id)) {
-                        queued_rules.erase(it);
+                std::lock_guard rules_lock{rules_mutex_};
+                for (auto it = queued_rules_.begin(); it != queued_rules_.end(); ++it) {
+                    if (*it == rule_id) {
+                        queued_rules_.erase(it);
                         break;
                     }
                 }
             }
 
         private:
-            std::mutex rules_mutex;
-            std::vector<std::string> queued_rules;
-
-            std::unique_lock<std::mutex> get_queue_lock() {
-                return std::unique_lock<std::mutex>(rules_mutex);
-            }
+            std::mutex rules_mutex_;
+            std::vector<std::string> queued_rules_;
     };
-}
+} // namespace irods
+
+#endif // IRODS_DELAY_QUEUE_HPP
+
