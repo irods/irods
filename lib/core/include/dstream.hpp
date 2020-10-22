02e9e4715682a7ffb1e04c27d24adcc307e315c5
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2020-10-22 13:07:22 -0500

    [#5213] dstream: Specify type of all std::ios_base::openmode constexprs
    Clang 10 needs this for bitwise operators

--- lib/core/include/dstream.hpp
+++ lib/core/include/dstream.hpp
@@ -433,13 +433,13 @@ namespace io {
     constexpr std::ios_base::openmode default_openmode{};
 
     template <typename T>
-    constexpr auto default_openmode<std::basic_istream<T>> = std::ios_base::in;
+    constexpr std::ios_base::openmode default_openmode<std::basic_istream<T>> = std::ios_base::in;
 
     template <typename T>
-    constexpr auto default_openmode<std::basic_ostream<T>> = std::ios_base::out;
+    constexpr std::ios_base::openmode default_openmode<std::basic_ostream<T>> = std::ios_base::out;
 
     template <typename T>
-    constexpr auto default_openmode<std::basic_iostream<T>> = std::ios_base::in | std::ios_base::out;
+    constexpr std::ios_base::openmode default_openmode<std::basic_iostream<T>> = std::ios_base::in | std::ios_base::out;
 
     // A concrete stream class template that wraps a basic_data_object_buf object.
     // The general stream used to instantiate this type must use "char" for the underlying
@@ -454,10 +454,10 @@ namespace io {
         static constexpr std::ios_base::openmode mandatory_openmode{};
 
         template <typename T>
-        static constexpr auto mandatory_openmode<std::basic_istream<T>> = std::ios_base::in;
+        static constexpr std::ios_base::openmode mandatory_openmode<std::basic_istream<T>> = std::ios_base::in;
 
         template <typename T>
-        static constexpr auto mandatory_openmode<std::basic_ostream<T>> = std::ios_base::out;
+        static constexpr std::ios_base::openmode mandatory_openmode<std::basic_ostream<T>> = std::ios_base::out;
 
     public:
         // clang-format off
