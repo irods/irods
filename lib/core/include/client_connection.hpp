544d6dda124cfdb760c435087af187b845c42c74
Kory Draughn <korydraughn@renci.org>
2020-11-20 17:40:13 -0500

    [#5092] Fixed client_connection.hpp documentation for doxygen.

--- lib/core/include/client_connection.hpp
+++ lib/core/include/client_connection.hpp
@@ -6,33 +6,35 @@
 #include <string_view>
 #include <memory>
 
-/// \brief Forward declaration of the underlying connection type.
+/// Forward declaration of the underlying connection type.
 struct RcComm;
 
-/// \brief A namespace that encapsulates experimental implementations that 
-///        may experience frequent changes.
+/// A namespace that encapsulates experimental implementations that 
+/// may experience frequent changes.
 namespace irods::experimental
 {
-    /// \brief A tag type that indicates whether or not a client connection
-    ///        should allow the user to control when the connection and
-    ///        authentication occurs.
+    /// A tag type that indicates whether or not a client connection
+    /// should allow the user to control when the connection and
+    /// authentication occurs.
+    ///
+    /// \since 4.2.9
     struct defer_connection {} defer_connection;
 
-    /// \brief This move-only class provides a convenient way to connect to and
-    ///        disconnect from an iRODS server in a safe manner.
+    /// This move-only class provides a convenient way to connect to and
+    /// disconnect from an iRODS server in a safe manner.
+    ///
+    /// \since 4.2.9
     class client_connection
     {
     public:
-        /// \brief Connects to an iRODS server.
-        ///
         /// Connects and authenticates using the information and credentials
         /// found in the user's irods_environment.json file.
         ///
         /// \throws irods::exception If an error occurred.
+        ///
+        /// \since 4.2.9
         client_connection();
 
-        /// \brief Connects to an iRODS server.
-        ///
         /// Connects to the iRODS server identified by the passed arguments
         /// and authenticates the user using the information and credentials
         /// found in the user's irods_environment.json file.
@@ -43,37 +45,42 @@ namespace irods::experimental
         /// \param[in] _port     The port to connect to.
         /// \param[in] _username The user to connect as.
         /// \param[in] _zone     The zone managed by the iRODS server.
+        ///
+        /// \since 4.2.9
         client_connection(const std::string_view _host,
                           const int _port,
                           const std::string_view _username,
                           const std::string_view _zone);
 
-        /// \brief Takes ownership of a raw iRODS connection and provides
-        ///        all of the safety guarantees found in other instantations.
+        /// Takes ownership of a raw iRODS connection and provides
+        /// all of the safety guarantees found in other instantations.
+        ///
+        /// \since 4.2.9
         explicit client_connection(RcComm& _conn);
 
-        /// \brief Constructs a client connection, but does not connect to a server.
-        ///
+        /// Constructs a client connection, but does not connect to a server.
         /// Instantiations of this class via this constructor must call \ref connect
         /// to establish a connection and authenticate the user.
+        ///
+        /// \since 4.2.9
         explicit client_connection(struct defer_connection);
 
         client_connection(client_connection&& _other) = default;
         auto operator=(client_connection&& _other) -> client_connection& = default;
 
-        /// \brief Closes the underlying connection if active.
+        /// Closes the underlying connection if active.
+        ///
+        /// \since 4.2.9
         ~client_connection() = default;
 
-        /// \brief Connects to an iRODS server.
-        ///
         /// Connects and authenticates using the information and credentials
         /// found in the user's irods_environment.json file.
         ///
         /// \throws irods::exception If an error occurred.
+        ///
+        /// \since 4.2.9
         auto connect() -> void;
 
-        /// \brief Connects to an iRODS server.
-        ///
         /// Connects to the iRODS server identified by the passed arguments
         /// and authenticates the user using the information and credentials
         /// found in the user's irods_environment.json file.
@@ -84,27 +91,37 @@ namespace irods::experimental
         /// \param[in] _port     The port to connect to.
         /// \param[in] _username The user to connect as.
         /// \param[in] _zone     The zone managed by the iRODS server.
+        ///
+        /// \since 4.2.9
         auto connect(const std::string_view _host,
                      const int _port,
                      const std::string_view _username,
                      const std::string_view _zone) -> void;
 
-        /// \brief Closes the underlying connection if active.
+        /// Closes the underlying connection if active.
+        ///
+        /// \since 4.2.9
         auto disconnect() noexcept -> void;
 
-        /// \brief Checks whether \p *this is holding an active connection.
+        /// Checks whether \p *this is holding an active connection.
         ///
         /// \returns A boolean.
         /// \retval true  If the \p *this contains an active connection.
         /// \retval false Otherwise.
+        ///
+        /// \since 4.2.9
         operator bool() const noexcept;
 
-        /// \brief Returns a reference to the underlying connection.
+        /// Returns a reference to the underlying connection.
         ///
         /// \throws irods::exception If the underlying connection is null.
+        ///
+        /// \since 4.2.9
         operator RcComm&() const;
 
-        /// \brief Returns a pointer to the underlying connection.
+        /// Returns a pointer to the underlying connection.
+        ///
+        /// \since 4.2.9
         explicit operator RcComm*() const noexcept;
 
     private:
