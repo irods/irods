3a7831df8edb43341f847fbd764d999b0cbbcf02
Kory Draughn <korydraughn@renci.org>
2020-11-24 10:10:17 -0500

    [irods/irods#5269] istream name now shows up in ips output.

---
+++ src/utility.hpp
@@ -0,0 +1,23 @@
+#ifndef IRODS_ICOMMANDS_UTILITY_HPP
+#define IRODS_ICOMMANDS_UTILITY_HPP
+
+#include "rodsDef.h"
+
+#include <cstdlib>
+#include <string_view>
+#include <iostream>
+
+namespace utils
+{
+    inline auto set_ips_display_name(const std::string_view _display_name) -> void
+    {
+        // Setting this environment variable is required so that "ips" can display
+        // the command name alongside the connection information.
+        if (setenv(SP_OPTION, _display_name.data(), /* overwrite */ 1)) {
+            std::cout << "Warning: Could not set environment variable [spOption] for ips.";
+        }
+    }
+} // namespace utils
+
+#endif // IRODS_ICOMMANDS_UTILITY_HPP
+
