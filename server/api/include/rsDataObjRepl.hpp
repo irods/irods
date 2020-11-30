1a246cb13f4cc46dee684f0e7bf431a03e666164
Alan King <alanking@renci.org>
2020-11-30 11:23:13 -0500

    [#858] Add const to args, whitespace, etc.

--- server/api/include/rsDataObjRepl.hpp
+++ server/api/include/rsDataObjRepl.hpp
@@ -8,7 +8,7 @@
 int rsDataObjRepl( rsComm_t *rsComm, dataObjInp_t *dataObjInp, transferStat_t **transferStat );
 int dataObjCopy( rsComm_t *rsComm, int l1descInx );
 int replToCacheRescOfCompObj( rsComm_t *rsComm, dataObjInp_t *dataObjInp, dataObjInfo_t *srcDataObjInfoHead, dataObjInfo_t *compObjInfo, dataObjInfo_t *oldDataObjInfo, dataObjInfo_t **outDestDataObjInfo );
-int unbunAndStageBunfileObj( rsComm_t *rsComm, char *bunfileObjPath, char **outCacheRescName );
+int unbunAndStageBunfileObj(rsComm_t* rsComm, const char* bunfileObjPath, char** outCacheRescName);
 int _unbunAndStageBunfileObj( rsComm_t *rsComm, dataObjInfo_t **bunfileObjInfoHead, keyValPair_t* condInput, char **outCacheRescName, int rmBunCopyFlag );
 int stageDataFromCompToCache( rsComm_t *rsComm, dataObjInfo_t *compObjInfo, dataObjInfo_t *outCacheObjInfo );
 int stageAndRequeDataToCache( rsComm_t *rsComm, dataObjInfo_t **compObjInfoHead );
