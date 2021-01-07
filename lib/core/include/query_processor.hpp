e6415785435bd119c899f0fe1c8cceba229c23fe
Kory Draughn <korydraughn@renci.org>
2021-01-07 13:24:05 -0500

    [#4902] query_processor::future now reports the size of a result set.

--- lib/core/include/query_processor.hpp
+++ lib/core/include/query_processor.hpp
@@ -26,7 +26,44 @@ namespace irods
         using query_type   = typename query<ConnectionType>::query_type;
         // clang-format on
 
-        query_processor(const std::string& _query, job _job, uint32_t _limit = 0, query_type _type = query_type::GENERAL)
+        class future
+        {
+        public:
+            auto get() -> errors
+            {
+                errors errs;
+                errs.reserve(promises.size());
+
+                for (auto&& p : promises) {
+                    auto&& e = p->get_future().get();
+                    if (std::get<0>(e) < 0) {
+                        errs.push_back(std::move(e));
+                    }
+                }
+
+                return errs;
+            }
+
+            auto size() const noexcept -> std::uint32_t
+            {
+                return promises.size();
+            }
+
+            friend query_processor;
+
+        private:
+            auto push_back(std::shared_ptr<std::promise<error>> p) -> void
+            {
+                promises.push_back(p);
+            }
+
+            std::vector<std::shared_ptr<std::promise<error>>> promises;
+        }; // class future
+
+        query_processor(const std::string& _query,
+                        job _job,
+                        uint32_t _limit = 0,
+                        query_type _type = query_type::GENERAL)
             : query_{_query}
             , job_{_job}
             , limit_{_limit}
@@ -36,45 +73,17 @@ namespace irods
 
         query_processor(const query_processor&) = delete;
         query_processor& operator=(const query_processor&) = delete;
-        uint32_t size() { return size_; }
 
-        class future
-        {
-            friend query_processor;
-
-            std::vector<std::shared_ptr<std::promise<error>>> promises;
-
-            void push_back(std::shared_ptr<std::promise<error>> p) {
-                promises.push_back(p);
-            }
-
-            public:
-            auto get() {
-                errors errs{};
-                errs.reserve(promises.size());
-                for(auto&& p : promises) {
-                    auto&& e = p->get_future().get();
-                    if(std::get<0>(e) < 0) {
-                        errs.push_back(std::move(e));
-                    }
-                }
-
-                return errs;
-            }
-        };
-
-        auto execute(thread_pool& _thread_pool, ConnectionType& _conn)
+        auto execute(thread_pool& _thread_pool, ConnectionType& _conn) -> future
         {
             future f;
             query<ConnectionType> q{&_conn, query_, limit_, 0, type_};
-
-            size_ = q.size();
 
             for (auto&& r : q) {
                auto p = std::make_shared<std::promise<error>>();
                f.push_back(p);
 
-               thread_pool::post(_thread_pool, [this, p, r]() mutable {
+               thread_pool::post(_thread_pool, [this, p, r]() mutable noexcept {
                     try {
                         job_(r);
                         p->set_value({0, ""});
@@ -96,12 +105,11 @@ namespace irods
         }
 
     private:
-        uint32_t size_;
         std::string query_;
         job job_;
         uint32_t limit_;
         query_type type_;
-    };
+    }; // class query_processor
 } // namespace irods
 
 #endif // IRODS_QUERY_PROCESSOR_HPP
