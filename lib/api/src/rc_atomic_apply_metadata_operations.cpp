9891b7146faa832d870d25cebd8eae293fc7bf0b
Kory Draughn <korydraughn@renci.org>
2021-01-13 14:47:32 -0500

    [#5370] Fixed memory leaks in rx_atomic_apply_metadata_operations.

--- lib/api/src/rc_atomic_apply_metadata_operations.cpp
+++ lib/api/src/rc_atomic_apply_metadata_operations.cpp
@@ -4,6 +4,7 @@
 #include "procApiRequest.h"
 #include "rodsErrorTable.h"
 
+#include <cstdlib>
 #include <cstring>
 
 auto rc_atomic_apply_metadata_operations(RcComm* _comm, const char* _json_input, char** _json_output) -> int
@@ -23,6 +24,7 @@ auto rc_atomic_apply_metadata_operations(RcComm* _comm, const char* _json_input,
                                   reinterpret_cast<void**>(&output_buf), nullptr);
 
     *_json_output = static_cast<char*>(output_buf->buf);
+    std::free(output_buf);
 
     return ec;
 }
