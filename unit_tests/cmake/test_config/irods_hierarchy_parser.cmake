d1b290bb27ea3c6712c235710e0fc8bd54034ee5
Alan King <alanking@renci.org>
2021-02-09 16:00:26 -0500

    [#5427] Move rError utilities to rodsError.h
    Moves rError stack utilities to rodsError.h and adds a new .cpp file for
    them of the same name. Added unit tests to ensure that things work
    properly and easily test whether memory is managed well.
    
    Limits the number of error messages which can be added to the stack via
    addRErrorMsg to the defined maximum.
    
    Add TRACE_LOG macro for easily adding trace log messages. To use, simply
    add TRACE_LOG() to the line you would like to see in the log.
    TRACE_LOG() calls should never be merged upstream.

--- unit_tests/cmake/test_config/irods_hierarchy_parser.cmake
+++ unit_tests/cmake/test_config/irods_hierarchy_parser.cmake
@@ -7,5 +7,5 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
                             ${CMAKE_SOURCE_DIR}/lib/core/include
                             ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
                             ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
- 
+
 set(IRODS_TEST_LINK_LIBRARIES irods_common)
