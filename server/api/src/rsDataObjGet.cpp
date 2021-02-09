12f818c95f0d3387add1eece8b1a0f8a0caa6178
Alan King <alanking@renci.org>
2021-02-09 16:24:33 -0500

    [#5195] Adjust open/close for new RST interfaces
    Adjustments made to rsDataObjOpen and rsDataObjClose due to changes in
    how the replica state table works. Allows for multiple replicas to be
    open on the same data object for things like replication. Fencing in
    special collections and bundle resource codepaths so that they do not
    use intermediate replica states.
    
    Change source/destination close order in copy so that it can use
    finalize in the future.
    
    Adjusts rsDataObjGet to work with some of the changes in rsDataObjOpen.
    
    Also populates expiry_ts in database plugin at registration so that the
    information is in an expected state.
    
    Performs the following operations on agent teardown for all open L1
    descriptors:
    
     - Size calculation
     - Checksum calculation
     - Stale replica
    
    ...and updates the catalog via rs_data_object_finalize.

--- server/api/src/rsDataObjGet.cpp
+++ server/api/src/rsDataObjGet.cpp
@@ -1,67 +1,64 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-/* This is script-generated code (for the most part).  */
-/* See dataObjGet.h for a description of this API call.*/
-
-#include "dataObjGet.h"
-#include "rodsLog.h"
 #include "dataGet.h"
-#include "fileGet.h"
-#include "dataObjOpen.h"
 #include "dataObjClose.h"
-#include "rsGlobalExtern.hpp"
-#include "rcGlobalExtern.h"
-#include "rsApiHandler.hpp"
+#include "dataObjGet.h"
+#include "dataObjOpen.h"
+#include "fileGet.h"
+#include "getRemoteZoneResc.h"
 #include "objMetaOpr.hpp"
 #include "physPath.hpp"
-#include "specColl.hpp"
-#include "subStructFileGet.h"
-#include "getRemoteZoneResc.h"
+#include "rcGlobalExtern.h"
+#include "rodsLog.h"
+#include "rsApiHandler.hpp"
+#include "rsDataGet.hpp"
+#include "rsDataObjClose.hpp"
 #include "rsDataObjGet.hpp"
 #include "rsDataObjOpen.hpp"
-#include "rsDataObjClose.hpp"
-#include "rsDataGet.hpp"
-#include "rsFileLseek.hpp"
-#include "rsSubStructFileGet.hpp"
 #include "rsFileGet.hpp"
+#include "rsFileLseek.hpp"
+#include "rsGlobalExtern.hpp"
+#include "rsSubStructFileGet.hpp"
+#include "specColl.hpp"
+#include "subStructFileGet.h"
 
 // =-=-=-=-=-=-=-
 #include "irods_resource_redirect.hpp"
 #include "irods_hierarchy_parser.hpp"
 #include "irods_resource_backport.hpp"
+#include "key_value_proxy.hpp"
 
-namespace {
+namespace
+{
     int _rsDataObjGet(
         rsComm_t *rsComm,
         dataObjInp_t *dataObjInp,
         portalOprOut_t **portalOprOut,
-        bytesBuf_t *dataObjOutBBuf,
-        int handlerFlag) {
-
+        bytesBuf_t *dataObjOutBBuf)
+    {
         char *chksumStr = NULL;
 
-        /* PHYOPEN_BY_SIZE ask it to check whether "dataInclude" should be done */
-        addKeyVal( &dataObjInp->condInput, PHYOPEN_BY_SIZE_KW, "" );
-        int l1descInx = rsDataObjOpen(rsComm, dataObjInp);
+        auto cond_input = irods::experimental::make_key_value_proxy(dataObjInp->condInput);
 
-        if ( l1descInx < 0 ) {
-            return l1descInx;
+        /* PHYOPEN_BY_SIZE ask it to check whether "dataInclude" should be done */
+        cond_input[PHYOPEN_BY_SIZE_KW] = "";
+
+        dataObjInp->openFlags = O_RDONLY;
+        int fd = rsDataObjOpen(rsComm, dataObjInp);
+        if (fd < 0) {
+            return fd;
         }
 
-        L1desc[l1descInx].oprType = GET_OPR;
+        L1desc[fd].oprType = GET_OPR;
 
-        dataObjInfo_t* dataObjInfo = L1desc[l1descInx].dataObjInfo;
-        copyKeyVal(
-            &dataObjInp->condInput,
-            &dataObjInfo->condInput );
+        dataObjInfo_t* dataObjInfo = L1desc[fd].dataObjInfo;
+        copyKeyVal( &dataObjInp->condInput, &dataObjInfo->condInput );
 
         if ( getStructFileType( dataObjInfo->specColl ) >= 0 && // JMC - backport 4682
-                L1desc[l1descInx].l3descInx > 0 ) {
+                L1desc[fd].l3descInx > 0 ) {
             /* l3descInx == 0 if included */
             *portalOprOut = ( portalOprOut_t * ) malloc( sizeof( portalOprOut_t ) );
             bzero( *portalOprOut,  sizeof( portalOprOut_t ) );
-            ( *portalOprOut )->l1descInx = l1descInx;
-            return l1descInx;
+            ( *portalOprOut )->l1descInx = fd;
+            return fd;
         }
 
         if ( getValByKey( &dataObjInp->condInput, VERIFY_CHKSUM_KW ) != NULL ) {
@@ -78,10 +75,9 @@ namespace {
             }
         }
 
-        if ( L1desc[l1descInx].l3descInx <= 2 ) {
+        if ( L1desc[fd].l3descInx <= 2 ) {
             /* no physical file was opened */
-            int status = l3DataGetSingleBuf( rsComm, l1descInx, dataObjOutBBuf,
-                                         portalOprOut );
+            int status = l3DataGetSingleBuf( rsComm, fd, dataObjOutBBuf, portalOprOut );
             if ( status >= 0 ) {
                 int status2;
                 status2 = applyRuleForPostProcForRead( rsComm, dataObjOutBBuf,
@@ -101,17 +97,17 @@ namespace {
         }
 
 
-        int status = preProcParaGet( rsComm, l1descInx, portalOprOut );
+        int status = preProcParaGet( rsComm, fd, portalOprOut );
 
         if ( status < 0 ) {
             openedDataObjInp_t dataObjCloseInp{};
-            dataObjCloseInp.l1descInx = l1descInx;
+            dataObjCloseInp.l1descInx = fd;
             rsDataObjClose( rsComm, &dataObjCloseInp );
             free( chksumStr );
             return status;
         }
 
-        status = l1descInx;         /* means file not included */
+        status = fd;         /* means file not included */
         if ( chksumStr != NULL ) {
             rstrcpy( ( *portalOprOut )->chksum, chksumStr, NAME_LEN );
         }
@@ -124,49 +120,46 @@ namespace {
 
         if ( retval < 0 ) {
             openedDataObjInp_t dataObjCloseInp{};
-            dataObjCloseInp.l1descInx = l1descInx;
+            dataObjCloseInp.l1descInx = fd;
             rsDataObjClose( rsComm, &dataObjCloseInp );
         }
 
-        if ( handlerFlag & INTERNAL_SVR_CALL ) {
-            /* internal call. want to know the real status */
-            return retval;
-        }
-        else {
-            /* already send the client the status */
-            return SYS_NO_HANDLER_REPLY_MSG;
-        }
+        /* already send the client the status */
+        return SYS_NO_HANDLER_REPLY_MSG;
+    } // _rsDataObjGet
+} // anonymous namespace
 
-    }
-}
-
-int
-rsDataObjGet( rsComm_t *rsComm, dataObjInp_t *dataObjInp,
-              portalOprOut_t **portalOprOut, bytesBuf_t *dataObjOutBBuf ) {
-    rodsServerHost_t *rodsServerHost;
-    specCollCache_t *specCollCache = NULL;
-    if ( dataObjOutBBuf == NULL ) {
-        rodsLog( LOG_ERROR, "dataObjOutBBuf was null in call to rsDataObjGet." );
+int rsDataObjGet(
+    rsComm_t *rsComm,
+    dataObjInp_t *dataObjInp,
+    portalOprOut_t **portalOprOut,
+    bytesBuf_t *dataObjOutBBuf)
+{
+    if (!dataObjOutBBuf) {
+        rodsLog(LOG_ERROR, "dataObjOutBBuf was null in call to rsDataObjGet.");
         return SYS_INTERNAL_NULL_INPUT_ERR;
     }
 
     remove_trailing_path_separators(dataObjInp->objPath);
 
-    resolveLinkedPath( rsComm, dataObjInp->objPath, &specCollCache,
-                       &dataObjInp->condInput );
-    int remoteFlag = getAndConnRemoteZone( rsComm, dataObjInp, &rodsServerHost,
-                                       REMOTE_OPEN );
+    auto cond_input = irods::experimental::make_key_value_proxy(dataObjInp->condInput);
 
-    if ( remoteFlag < 0 ) {
+    specCollCache_t *specCollCache = NULL;
+    resolveLinkedPath( rsComm, dataObjInp->objPath, &specCollCache, cond_input.get());
+
+    rodsServerHost_t *rodsServerHost;
+    int remoteFlag = getAndConnRemoteZone( rsComm, dataObjInp, &rodsServerHost, REMOTE_OPEN);
+
+    if (remoteFlag < 0) {
         return remoteFlag;
     }
-    else if ( remoteFlag != LOCAL_HOST ) {
-        int status = _rcDataObjGet( rodsServerHost->conn, dataObjInp, portalOprOut,
-                                dataObjOutBBuf );
+    else if (LOCAL_HOST != remoteFlag) {
+        int status = _rcDataObjGet(rodsServerHost->conn, dataObjInp, portalOprOut, dataObjOutBBuf);
 
-        if ( status < 0 ) {
+        if (status < 0) {
             return status;
         }
+
         if ( status == 0 || dataObjOutBBuf->len > 0 ) {
             /* data included in buf */
             return status;
@@ -194,19 +187,19 @@ rsDataObjGet( rsComm_t *rsComm, dataObjInp_t *dataObjInp,
     // working on the "home zone", determine if we need to redirect to a different
     // server in this zone for this operation.  if there is a RESC_HIER_STR_KW then
     // we know that the redirection decision has already been made
-    if ( getValByKey( &dataObjInp->condInput, RESC_HIER_STR_KW ) == NULL ) {
+    if (!cond_input.contains(RESC_HIER_STR_KW)) {
         try {
             auto result = irods::resolve_resource_hierarchy(irods::OPEN_OPERATION, rsComm, *dataObjInp);
-            const auto hier = std::get<std::string>(result);
-            addKeyVal( &dataObjInp->condInput, RESC_HIER_STR_KW, hier.c_str() );
+            cond_input[RESC_HIER_STR_KW] = std::get<std::string>(result);
         }
         catch (const irods::exception& e) {
             irods::log(e);
             return e.code();
         }
     }
-    return _rsDataObjGet(rsComm, dataObjInp, portalOprOut, dataObjOutBBuf, BRANCH_MSG);
-}
+
+    return _rsDataObjGet(rsComm, dataObjInp, portalOprOut, dataObjOutBBuf);
+} // rsDataObjGet
 
 
 /* preProcParaGet - preprocessing for parallel get. Basically it calls
