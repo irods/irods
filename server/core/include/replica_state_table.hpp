7686e150d3967c9e0926ff06a06c3597357b95ac
Alan King <alanking@renci.org>
2021-02-09 15:58:51 -0500

    [#3848] Add RST interfaces/documentation/tests
    As part of the effort to implement logical locking, we need the ability
    to easily and atomically update the status of all replicas for a given
    data object. This change adds several new interfaces including a
    "publish" function in order to stamp out a replica state table entry to
    the catalog as part of the finalize workflow.
    
    This change also allows multiple references to the same replica state
    table entry for things like replication. An internal reference count is
    maintained by the replica state table so that multiple opens can track
    replica states at the same time.

--- server/core/include/replica_state_table.hpp
+++ server/core/include/replica_state_table.hpp
@@ -1,20 +1,96 @@
 #ifndef IRODS_REPLICA_STATE_TABLE_HPP
 #define IRODS_REPLICA_STATE_TABLE_HPP
 
+#define IRODS_REPLICA_ENABLE_SERVER_SIDE_API
+#include "data_object_proxy.hpp"
+
 #include "json.hpp"
 
 #include <string_view>
 
-struct DataObjInfo;
+//struct DataObjInfo;
+struct RsComm;
 
 namespace irods
 {
-    /// \brief Singleton which maintains a JSON structure describing the state of change of opened data objects.
+    /// \brief Singleton that maintains a JSON structure describing the changes to opened data objects.
+    ///
+    /// \parblock
+    /// The JSON structure has the following form:
+    /// \code{.js}
+    /// {
+    ///     "replicas": [
+    ///         {
+    ///             "before": {
+    ///                 "data_id": <string>,
+    ///                 "coll_id": <string>,
+    ///                 "data_repl_num": <string>,
+    ///                 "data_version": <string>,
+    ///                 "data_type_name": <string>,
+    ///                 "data_size": <string>,
+    ///                 "data_path": <string>,
+    ///                 "data_owner_name": <string>,
+    ///                 "data_owner_zone": <string>,
+    ///                 "data_is_dirty": <string>,
+    ///                 "data_status": <string>,
+    ///                 "data_checksum": <string>,
+    ///                 "data_expiry_ts": <string>,
+    ///                 "data_map_id": <string>,
+    ///                 "data_mode": <string>,
+    ///                 "r_comment": <string>,
+    ///                 "create_ts": <string>,
+    ///                 "modify_ts": <string>,
+    ///                 "resc_id": <string>
+    ///             },
+    ///             "after": {
+    ///                 "data_id": <string>,
+    ///                 "coll_id": <string>,
+    ///                 "data_repl_num": <string>,
+    ///                 "data_version": <string>,
+    ///                 "data_type_name": <string>,
+    ///                 "data_size": <string>,
+    ///                 "data_path": <string>,
+    ///                 "data_owner_name": <string>,
+    ///                 "data_owner_zone": <string>,
+    ///                 "data_is_dirty": <string>,
+    ///                 "data_status": <string>,
+    ///                 "data_checksum": <string>,
+    ///                 "data_expiry_ts": <string>,
+    ///                 "data_map_id": <string>,
+    ///                 "data_mode": <string>,
+    ///                 "r_comment": <string>,
+    ///                 "create_ts": <string>,
+    ///                 "modify_ts": <string>,
+    ///                 "resc_id": <string>
+    ///             },
+    ///             "file_modified": {
+    ///                 <string>: <string>,
+    ///                 ...
+    ///             }
+    ///         },
+    ///         ...
+    ///     ],
+    ///     "ref_count": <int>
+    /// }
+    /// \endcode
+    ///
+    /// - "replicas" is an array of JSON structures describing replicas for a particular data object:
+    ///     - "before" refers to the replica as it appeared in the catalog on open
+    ///     - "after" refers to the current state of the replica (may or may not be representative of the catalog state)
+    ///     - "file_modified" (OPTIONAL) is a set of key-value pairs which are used in the fileModified plugin operation
+    ///
+    /// - "ref_count" refers to the number of open file descriptors which are currently referring to this entry
+    ///
+    /// \endparblock
     ///
     /// \since 4.2.9
     class replica_state_table
     {
     public:
+        inline static const std::string BEFORE_KW = "before";
+        inline static const std::string AFTER_KW = "after";
+        inline static const std::string REPLICAS_KW = "replicas";
+
         // Prevent copying replica_state_table
         replica_state_table(const replica_state_table&) = delete;
         auto operator=(const replica_state_table&) -> replica_state_table& = delete;
@@ -28,6 +104,12 @@ namespace irods
             after,
             both
         }; // enum class state_type
+
+        enum class trigger_file_modified
+        {
+            no,
+            yes
+        };
 
         /// \brief Initialize the replica_state_table by making sure it is completely cleared out
         ///
@@ -51,7 +133,18 @@ namespace irods
         /// \param[in] _obj Initial data object state to serve as both "before" and "after" at the start
         ///
         /// \since 4.2.9
-        auto insert(const DataObjInfo& _obj) -> void;
+        auto insert(const irods::experimental::data_object::data_object_proxy_t& _obj) -> void;
+
+        /// \brief Create a new replica in an existing entry in the replica_state_table
+        ///
+        /// If the specified replica already exists for this entry in the replica state table, nothing happens.
+        ///
+        /// \param[in] _obj Initial data object state to serve as both "before" and "after" at the start
+        /// \throws irods::exception If entry does not exist
+        ///
+        /// \since 4.2.9
+        auto insert(const std::string_view _logical_path,
+                    const irods::experimental::replica::replica_proxy_t& _obj) -> void;
 
         /// \brief Erase replica_state_table entry indicated by _logical_path
         ///
@@ -71,6 +164,30 @@ namespace irods
         ///
         /// \since 4.2.9
         auto contains(const std::string_view _logical_path) const -> bool;
+
+        /// \brief Returns whether or not a replica exists for an existing entry replica state table
+        ///
+        /// \param[in] _logical_path
+        /// \param[in] _leaf_resource_name
+        ///
+        /// \retval true if replica state table contains key _logical_path and a replica on the specified leaf resource
+        /// \retval false if replica state table does not contain key _logical_path or a replica on the specified leaf resource
+        ///
+        /// \since 4.2.9
+        auto contains(const std::string_view _logical_path,
+                      const std::string_view _leaf_resource_name) const -> bool;
+
+        /// \brief Returns whether or not a replica exists for an existing entry replica state table
+        ///
+        /// \param[in] _logical_path
+        /// \param[in] _replica_number
+        ///
+        /// \retval true if replica state table contains key _logical_path and a replica with the specified replica number
+        /// \retval false if replica state table does not contain key _logical_path or a replica with the specified replica number
+        ///
+        /// \since 4.2.9
+        auto contains(const std::string_view _logical_path,
+                      const int _replica_number) const -> bool;
 
         /// \brief return all information for all states of all replicas for a particular data object
         ///
@@ -306,13 +423,45 @@ namespace irods
         /// \brief Updates the specified replica with the specified changes (after state only)
         ///
         /// \param[in] _logical_path
+        /// \param[in] _leaf_resource_name Leaf resource name in the "before" entry
+        /// \param[in] _updates JSON input representing changes to be made. \parblock
+        /// Must be of the following form (only fields which need updating need to be included):
+        /// \code{.js}
+        ///     {
+        ///         "replicas": {
+        ///             <r_data_main_column>: <string>,
+        ///             ...
+        ///         },
+        ///         "file_modified": {
+        ///             <string>: <string>,
+        ///             ...
+        ///         }
+        ///     }
+        /// \endcode
+        /// \endparblock
+        ///
+        /// \since 4.2.9
+        auto update(
+            const std::string_view _logical_path,
+            const std::string_view _leaf_resource_name,
+            const nlohmann::json& _updates) -> void;
+
+        /// \brief Updates the specified replica with the specified changes (after state only)
+        ///
+        /// \param[in] _logical_path
         /// \param[in] _replica_number Replica number in the "before" entry
         /// \param[in] _updates JSON input representing changes to be made. \parblock
         /// Must be of the following form (only fields which need updating need to be included):
         /// \code{.js}
         ///     {
-        ///         <r_data_main_column>: <string>,
-        ///         ...
+        ///         "replicas": {
+        ///             <r_data_main_column>: <string>,
+        ///             ...
+        ///         },
+        ///         "file_modified": {
+        ///             <string>: <string>,
+        ///             ...
+        ///         }
         ///     }
         /// \endcode
         /// \endparblock
@@ -322,24 +471,27 @@ namespace irods
                     const int _replica_number,
                     const nlohmann::json& _updates) -> void;
 
-        /// \brief Updates the specified replica with the specified changes (after state only)
+        /// \brief Updates all columns of the specified replica
         ///
         /// \param[in] _logical_path
-        /// \param[in] _leaf_resource_name Leaf resource name in the "before" entry
-        /// \param[in] _updates JSON input representing changes to be made. \parblock
-        /// Must be of the following form (only fields which need updating need to be included):
+        /// \param[in] _replica replica_proxy which is converted to JSON format: \parblock
         /// \code{.js}
         ///     {
-        ///         <r_data_main_column>: <string>,
-        ///         ...
+        ///         "replicas": {
+        ///             <r_data_main_column>: <string>,
+        ///             ...
+        ///         },
+        ///         "file_modified": {
+        ///             <string>: <string>,
+        ///             ...
+        ///         }
         ///     }
         /// \endcode
         /// \endparblock
         ///
         /// \since 4.2.9
         auto update(const std::string_view _logical_path,
-                    const std::string_view _leaf_resource_name,
-                    const nlohmann::json& _updates) -> void;
+                    const irods::experimental::replica::replica_proxy_t& _replica) -> void;
 
         /// \brief Returns the value of a given property of the given replica in the state table
         ///
@@ -348,7 +500,7 @@ namespace irods
         /// \param[in] _property_name
         /// \param[in] _state Must be state_type::before or state_type::after
         ///
-        /// \throws irods::exception
+        /// \throws irods::exception If specified replica does not exist
         ///
         /// \since 4.2.9
         auto get_property(
@@ -364,7 +516,7 @@ namespace irods
         /// \param[in] _property_name
         /// \param[in] _state Must be state_type::before or state_type::after
         ///
-        /// \throws irods::exception
+        /// \throws irods::exception If specified replica does not exist
         ///
         /// \since 4.2.9
         auto get_property(
@@ -372,6 +524,21 @@ namespace irods
             const std::string_view _leaf_resource_name,
             const std::string_view _property_name,
             const state_type _state = state_type::before) const -> std::string;
+
+        /// \brief Prepares the specified data object as input to data_object_finalize and updates the catalog
+        ///
+        /// \param[in/out] _comm
+        /// \param[in] _logical_path
+        /// \param[in] _trigger_file_modified
+        ///
+        /// \returns error code returned by rs_data_object_finalize
+        /// \retval 0 success
+        ///
+        /// \since 4.2.9
+        auto publish_to_catalog(
+            RsComm& _comm,
+            const std::string_view _logical_path,
+            const trigger_file_modified _trigger_file_modified) -> int;
 
     private:
         // Disallow construction of the replica_state_table outside of member functions
