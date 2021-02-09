457cca753af7025581a3f79259ff4c7cd3890abb
Alan King <alanking@renci.org>
2021-02-09 15:48:46 -0500

    [#5149] Improvements for proxy classes
    Adds helper types for the replica_proxy and data_object_proxy classes so
    that they are easier to use. Up to this point, DataObjInfo was still
    easier to use in an API declaration due to the simplicity of the type
    name. This allows use of proxies directly in various interfaces.
    
    Creates a compilation unit for key_value_proxy to reduce the amount of
    code in the header file. Only contains one non-template function at this
    point in time.

---
+++ lib/core/src/key_value_proxy.cpp
@@ -0,0 +1,25 @@
+#include "key_value_proxy.hpp"
+
+namespace irods::experimental
+{
+    auto make_key_value_proxy(std::initializer_list<key_value_pair> _kvps) -> key_value_proxy_pair<KeyValPair>
+    {
+        KeyValPair* cond_input = (KeyValPair*)malloc(sizeof(KeyValPair));
+        std::memset(cond_input, 0, sizeof(KeyValPair));
+        key_value_proxy proxy{*cond_input};
+        for (const auto& kvp : _kvps) {
+            const auto& key = std::get<0>(kvp);
+            const auto& val = std::get<1>(kvp);
+            proxy[key] = val;
+        }
+        return {std::move(proxy), lifetime_manager{*cond_input}};
+    } // make_key_value_proxy
+
+    auto keyword_has_a_value(const KeyValPair& _kvp, const std::string_view _keyword) -> bool
+    {
+        const auto cond_input = irods::experimental::make_key_value_proxy(_kvp);
+
+        return cond_input.contains(_keyword) &&
+               !cond_input.at(_keyword).value().empty();
+    } // keyword_has_a_value
+} // namespace irods::experimental
