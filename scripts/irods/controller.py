a1ccaf908b32d76f0b0195d2c51d158a1bf91449
Justin James <jjames@renci.org>
2021-02-22 19:11:11 +0000

    [#5451] delete s3 shmem files on startup/shutdown

--- scripts/irods/controller.py
+++ scripts/irods/controller.py
@@ -72,6 +72,8 @@ class IrodsController(object):
         if self.get_binary_to_pids_dict():
             raise IrodsError('iRODS already running')
 
+        delete_s3_shmem()
+
         self.config.clear_cache()
         if not os.path.exists(self.config.server_executable):
             raise IrodsError(
@@ -230,6 +232,9 @@ class IrodsController(object):
                         except psutil.NoSuchProcess:
                             pass
                         delete_cache_files_by_pid(pid)
+
+            delete_s3_shmem()
+
         except IrodsError as e:
             l.info('Failure')
             six.reraise(IrodsError, e, sys.exc_info()[2])
@@ -298,3 +303,12 @@ def delete_cache_files_by_name(*filepaths):
             os.unlink(path)
         except (IOError, OSError):
             l.warning(lib.indent('Error deleting cache file: %s'), path)
+
+def delete_s3_shmem():
+    # delete s3 shared memory if any exist 
+    s3_plugin_shmem = glob.glob(os.path.join(
+        paths.root_directory(),
+        'dev',
+        'shm',
+        '*irods_s3-shm*'))
+    delete_cache_files_by_name(*s3_plugin_shmem)
