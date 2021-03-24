923f5f214f13aebe71d3203fc832a6017a54ebec
Kory Draughn <korydraughn@renci.org>
2021-03-24 08:10:32 -0400

    [#5504] Added "struct" keyword to C API plugin headers.

--- lib/api/src/rc_data_object_modify_info.cpp
+++ lib/api/src/rc_data_object_modify_info.cpp
@@ -4,7 +4,7 @@
 #include "procApiRequest.h"
 #include "rodsErrorTable.h"
 
-auto rc_data_object_modify_info(RcComm* _comm, modDataObjMeta_t* _input) -> int
+auto rc_data_object_modify_info(RcComm* _comm, ModDataObjMetaInp* _input) -> int
 {
     return _input ? procApiRequest(_comm, DATA_OBJECT_MODIFY_INFO_APN, _input, nullptr, nullptr, nullptr)
                   : SYS_INVALID_INPUT_PARAM;
