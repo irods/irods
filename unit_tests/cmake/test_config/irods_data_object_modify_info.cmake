620446a837ac57196ae4e9b44f2c4fb7e89e3d81
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-06-28 18:36:44 -0400

    [#5669, #5713] Use find_package for nlohmann-json

--- unit_tests/cmake/test_config/irods_data_object_modify_info.cmake
+++ unit_tests/cmake/test_config/irods_data_object_modify_info.cmake
@@ -13,8 +13,7 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
                             ${CMAKE_SOURCE_DIR}/server/re/include
                             ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
                             ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
-                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include
-                            ${IRODS_EXTERNALS_FULLPATH_JSON}/include)
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
 
 set(IRODS_TEST_LINK_LIBRARIES irods_common
                               irods_client
