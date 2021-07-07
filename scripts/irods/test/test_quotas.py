dc903d7eb145e50eabff4c3ad5cf11ccb812d5af
Alan King <alanking@renci.org>
2021-07-07 11:17:10 -0400

    [#858] Test cleanup
    Cleanup delay rule after test run
    
    Clean up group after test

--- scripts/irods/test/test_quotas.py
+++ scripts/irods/test/test_quotas.py
@@ -84,6 +84,9 @@ class Test_Quotas(resource_suite.ResourceBase, unittest.TestCase):
 
     def test_filter_out_groups_when_selecting_user__issue_3507(self):
         self.admin.assert_icommand(['igroupadmin', 'mkgroup', 'test_group_3507'])
-        # Attempt to set user quota passing in the name of a group; should fail
-        self.admin.assert_icommand(['iadmin', 'suq', 'test_group_3507', 'demoResc', '10000000'], 'STDERR_SINGLELINE', 'CAT_INVALID_USER')
 
+        try:
+            # Attempt to set user quota passing in the name of a group; should fail
+            self.admin.assert_icommand(['iadmin', 'suq', 'test_group_3507', 'demoResc', '10000000'], 'STDERR_SINGLELINE', 'CAT_INVALID_USER')
+        finally:
+            self.admin.assert_icommand(['iadmin', 'rmgroup', 'test_group_3507'])
