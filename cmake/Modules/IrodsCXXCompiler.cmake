4fcc4f45b9323dafa2b74291e4cde7d086b64748
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-08-10 22:30:30 -0400

    [#4083] Initial buildsystem support for gcc/libstdc++

---
+++ cmake/Modules/IrodsCXXCompiler.cmake
@@ -0,0 +1,41 @@
+#[======================================================================[.rst:
+IrodsCXXCompiler
+----------------
+
+Experimental stuff
+
+#]======================================================================]
+
+include_guard(GLOBAL)
+
+include(IrodsExternals)
+
+cmake_policy(PUSH)
+
+set(IRODS_BUILD_WITH_CLANG ON CACHE BOOL "Try to build with clang instead of gcc")
+set(CLANG_STATIC_ANALYZER OFF CACHE BOOL "Choose whether to run Clang Static Analyzer.")
+
+if (IRODS_BUILD_WITH_CLANG)
+	IRODS_MACRO_CHECK_DEPENDENCY_SET_FULLPATH(CLANG clang6.0-0)
+else()
+	set(CLANG_STATIC_ANALYZER OFF CACHE BOOL "Choose whether to run Clang Static Analyzer." FORCE)
+	message(STATUS "Setting CLANG_STATIC_ANALYZER to 'OFF'.")
+endif()
+
+if (CLANG_STATIC_ANALYZER)
+	if (NOT CMAKE_C_COMPILER)
+		set(CMAKE_C_COMPILER ${IRODS_EXTERNALS_FULLPATH_CLANG}/bin/ccc-analyzer)
+	endif()
+	if (NOT CMAKE_CXX_COMPILER)
+		set(CMAKE_CXX_COMPILER ${IRODS_EXTERNALS_FULLPATH_CLANG}/bin/c++-analyzer)
+	endif()
+elseif (IRODS_BUILD_WITH_CLANG)
+	if (NOT CMAKE_C_COMPILER)
+		set(CMAKE_C_COMPILER ${IRODS_EXTERNALS_FULLPATH_CLANG}/bin/clang)
+	endif()
+	if (NOT CMAKE_CXX_COMPILER)
+		set(CMAKE_CXX_COMPILER ${IRODS_EXTERNALS_FULLPATH_CLANG}/bin/clang++)
+	endif()
+endif()
+
+cmake_policy(POP)
