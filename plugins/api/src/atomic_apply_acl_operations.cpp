9cb44b9625d31100d78cc329f66aae8597b73f90
Kory Draughn <korydraughn@renci.org>
2021-08-16 16:33:44 -0400

    [#5774] rs_touch correctly handles redirection.
    - Fixed the redirection issue for other API plugins.
    - Adjusted the rc/rs convenience functions for other API plugins to
      include the null terminator in the request.
    - Updated BytesBuf serialization test for null terminators.

--- plugins/api/src/atomic_apply_acl_operations.cpp
+++ plugins/api/src/atomic_apply_acl_operations.cpp
@@ -31,6 +31,7 @@
 #include "rodsConnect.h"
 #include "rodsLog.h"
 #include "server_utilities.hpp"
+#include "irods_at_scope_exit.hpp"
 
 #define IRODS_QUERY_ENABLE_SERVER_SIDE_API
 #include "irods_query.hpp"
@@ -459,8 +460,9 @@ namespace
                 irods::log(LOG_DEBUG8, "Redirecting request to catalog service provider ...");
 
                 auto host_info = ic::redirect_to_catalog_provider(*_comm);
+                irods::at_scope_exit close_conn{[&host_info] { rcDisconnect(host_info.conn); }};
 
-                std::string_view json_input(static_cast<const char*>(_input->buf), _input->len);
+                const std::string json_input(static_cast<const char*>(_input->buf), _input->len);
                 char* json_output = nullptr;
 
                 const auto ec = rc_atomic_apply_acl_operations(host_info.conn, json_input.data(), &json_output);
