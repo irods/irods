1b7242a3cd0268e37c0a74627008704928c071be
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2021-08-24 13:38:57 -0400

    [#4083] fix missing irods_common library reference in test
    GCC can't find the irods::error constructor without a little help

--- unit_tests/cmake/test_config/irods_linked_list_iterator.cmake
+++ unit_tests/cmake/test_config/irods_linked_list_iterator.cmake
@@ -13,5 +13,6 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/lib/api/include
                             ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
                             ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include)
  
-set(IRODS_TEST_LINK_LIBRARIES irods_server
+set(IRODS_TEST_LINK_LIBRARIES irods_common
+                              irods_server
                               ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so)
