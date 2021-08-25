32bc555e8bd4b03d831aeb431f3b1f57eaa5520e
Alan King <alanking@renci.org>
2021-08-25 08:38:05 -0400

    [#5602] Add missing fmt include dependencies
    Unit tests were not building due to missing include dependency for the
    fmt library which was introduced when the fmt library was added as a
    dependency to irods_server_properties.hpp.

--- unit_tests/cmake/test_config/irods_linked_list_iterator.cmake
+++ unit_tests/cmake/test_config/irods_linked_list_iterator.cmake
@@ -9,10 +9,10 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_SOURCE_DIR}/lib/api/include
                             ${CMAKE_SOURCE_DIR}/server/core/include
                             ${CMAKE_SOURCE_DIR}/server/drivers/include
                             ${CMAKE_SOURCE_DIR}/server/re/include
-                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
                             ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-                            ${IRODS_EXTERNALS_FULLPATH_JANSSON}/include)
- 
+                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
+
 set(IRODS_TEST_LINK_LIBRARIES irods_common
                               irods_server
                               ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so)
--- unit_tests/cmake/test_config/irods_packstruct.cmake
+++ unit_tests/cmake/test_config/irods_packstruct.cmake
@@ -7,7 +7,8 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
                             ${CMAKE_SOURCE_DIR}/lib/core/include
                             ${CMAKE_SOURCE_DIR}/lib/api/include
                             ${CMAKE_SOURCE_DIR}/server/core/include
+                            ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
                             ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
-                            ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
- 
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
+
 set(IRODS_TEST_LINK_LIBRARIES irods_common)
