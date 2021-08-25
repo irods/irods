a7398ef38ebbcb6764d9f1867420ea5d6566f029
Alan King <alanking@renci.org>
2021-08-25 08:38:05 -0400

    [#5602] Add missing fmt include dependencies
    Unit tests were not building due to missing include dependency for the
    fmt library which was introduced when the fmt library was added as a
    dependency to irods_server_properties.hpp.

--- unit_tests/cmake/test_config/irods_data_object_modify_info.cmake
+++ unit_tests/cmake/test_config/irods_data_object_modify_info.cmake
@@ -11,10 +11,11 @@ set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
                             ${CMAKE_SOURCE_DIR}/server/core/include
                             ${CMAKE_SOURCE_DIR}/server/icat/include
                             ${CMAKE_SOURCE_DIR}/server/re/include
-                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
                             ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include
                             ${IRODS_EXTERNALS_FULLPATH_JSON}/include)
- 
+
 set(IRODS_TEST_LINK_LIBRARIES irods_common
                               irods_client
                               irods_plugin_dependencies
