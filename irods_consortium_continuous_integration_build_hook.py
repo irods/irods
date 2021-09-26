bca78765b4fac1028a3c7f65350f40907af7c44a
Kory Draughn <korydraughn@renci.org>
2021-09-26 15:26:21 -0400

    [#5881] Added spdlog dependency to CI build hook.

--- irods_consortium_continuous_integration_build_hook.py
+++ irods_consortium_continuous_integration_build_hook.py
@@ -34,6 +34,7 @@ def install_building_dependencies(externals_directory):
         'irods-externals-json3.7.3-0',
         'irods-externals-libarchive3.3.2-1',
         'irods-externals-nanodbc2.13.0-1',
+        'irods-externals-spdlog1.5.0-1',
         'irods-externals-zeromq4-14.1.6-0'
         ]
     if externals_directory is None or externals_directory == 'None':
