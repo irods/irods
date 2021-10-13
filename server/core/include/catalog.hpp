b0b252fbba40c6744f64e488cae123408cf1382d
Kory Draughn <korydraughn@renci.org>
2021-10-13 14:58:50 -0400

    [#5906] Delay server uses nanodbc to fetch r_rule_exec.exe_context.
    There is a limitation in the database plugin that stops it from being
    able to fetch the REI from the catalog. This occurs when the data in
    exe_context exceeds a certain size. Nanodbc does not have this issue.

--- server/core/include/catalog.hpp
+++ server/core/include/catalog.hpp
@@ -10,15 +10,25 @@ namespace irods::experimental::catalog
 {
     /// \brief Establish connection to database hosting the catalog
     ///
+    /// \param[in] _read_server_config If true, reads server_config.json and uses the
+    ///                                database credentials to establish a database connection.
+    ///                                Otherwise, uses the credentials stored in memory.
+    ///
     /// \returns Tuple of the database type (string) and the database connection
     ///
+    /// \throws std::runtime_error If any of the following conditions are met:
+    /// - server_config.json cannot be found.
+    /// - The database instance name is empty.
+    /// - An exception is thrown.
+    ///
     /// \since 4.2.9
-    auto new_database_connection() -> std::tuple<std::string, nanodbc::connection>;
+    auto new_database_connection(bool _read_server_config = false)
+        -> std::tuple<std::string, nanodbc::connection>;
 
     /// \brief Provides a transaction to the provided function and executes it
     ///
-    /// \param[in] _db_conn - Established connection to the database
-    /// \param[in] _func    - Function to execute using the generated transaction
+    /// \param[in] _db_conn Established connection to the database
+    /// \param[in] _func    Function to execute using the generated transaction
     ///
     /// \returns Error code resulting from the executed transaction
     ///
