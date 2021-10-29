82e9c04d9974ab3a48415da816268dfd9d05ad2b
Kory Draughn <korydraughn@renci.org>
2021-10-29 15:54:18 -0400

    [#5503] Added unit test to verify correctness of delay hints parser.
    The test proves that the parser produces deterministic results for most
    hints. However, the test does not cover the "DOUBLE" hints because they
    have issues. For that reason, the test cases for the "DOUBLE" hints have
    been disabled for now.

---
+++ unit_tests/cmake/test_config/irods_rcMisc_functions.cmake
@@ -0,0 +1,22 @@
+set(IRODS_TEST_TARGET irods_rcMisc_functions)
+
+set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_rcMisc_functions.cpp)
+
+set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
+                            ${CMAKE_SOURCE_DIR}/lib/core/include
+                            ${CMAKE_SOURCE_DIR}/lib/api/include
+                            ${CMAKE_SOURCE_DIR}/lib/filesystem/include
+                            ${CMAKE_SOURCE_DIR}/server/core/include
+                            ${CMAKE_SOURCE_DIR}/server/icat/include
+                            ${CMAKE_SOURCE_DIR}/server/re/include
+                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include
+                            ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
+
+set(IRODS_TEST_LINK_LIBRARIES irods_common
+                              irods_client
+                              irods_plugin_dependencies
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+                              ${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so)
