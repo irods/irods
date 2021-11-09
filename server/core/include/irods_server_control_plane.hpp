2f42e79c8980f96e42b255b0a40bcb627d8c6fa1
Kory Draughn <korydraughn@renci.org>
2021-11-09 09:20:47 -0500

    [#5700] Control plane resolves hostname aliases using hosts_config.json.
    The important change in this commit is the introduction and use of the
    function, resolve_hostnames_using_hosts_config.
    
    This new function is called after parsing the request. The function
    takes the incoming list of hostnames and replaces each one with the
    resolved hostname from hosts_config.json. Hostnames that do not have an
    entry in hosts_config.json remain in the hostname list as-is.
    
    This commit also cleans up formatting and simplifies logic.

--- server/core/include/irods_server_control_plane.hpp
+++ server/core/include/irods_server_control_plane.hpp
@@ -1,35 +1,34 @@
+#ifndef IRODS_SERVER_CONTROL_PLANE_HPP
+#define IRODS_SERVER_CONTROL_PLANE_HPP
 
+/// \file
 
-
-#ifndef SERVER_CONTROL_PLANE_HPP
-#define SERVER_CONTROL_PLANE_HPP
-
-#include "zmq.hpp"
-#include "boost/thread.hpp"
-#include "boost/atomic.hpp"
 #include "irods_lookup_table.hpp"
 #include "server_control_plane_command.hpp"
+
+#include "boost/thread.hpp"
+#include "boost/atomic.hpp"
 #include "boost/unordered_map.hpp"
+#include "zmq.hpp"
 
-namespace irods {
-    const std::string SERVER_CONTROL_OPTION_KW( "server_control_option" );
-    const std::string SERVER_CONTROL_HOST_KW( "server_control_host" );
-    const std::string SERVER_CONTROL_FORCE_AFTER_KW( "server_control_force_after" );
-    const std::string SERVER_CONTROL_WAIT_FOREVER_KW( "server_control_wait_forever" );
+namespace irods
+{
+    inline const std::string SERVER_CONTROL_OPTION_KW( "server_control_option" );
+    inline const std::string SERVER_CONTROL_HOST_KW( "server_control_host" );
+    inline const std::string SERVER_CONTROL_FORCE_AFTER_KW( "server_control_force_after" );
+    inline const std::string SERVER_CONTROL_WAIT_FOREVER_KW( "server_control_wait_forever" );
 
-    const std::string SERVER_CONTROL_SHUTDOWN( "server_control_shutdown" );
-    const std::string SERVER_CONTROL_PAUSE( "server_control_pause" );
-    const std::string SERVER_CONTROL_RESUME( "server_control_resume" );
-    const std::string SERVER_CONTROL_STATUS( "server_control_status" );
-    const std::string SERVER_CONTROL_PING( "server_control_ping" );
+    inline const std::string SERVER_CONTROL_SHUTDOWN( "server_control_shutdown" );
+    inline const std::string SERVER_CONTROL_PAUSE( "server_control_pause" );
+    inline const std::string SERVER_CONTROL_RESUME( "server_control_resume" );
+    inline const std::string SERVER_CONTROL_STATUS( "server_control_status" );
+    inline const std::string SERVER_CONTROL_PING( "server_control_ping" );
 
-    const std::string SERVER_CONTROL_ALL_OPT( "all" );
-    const std::string SERVER_CONTROL_HOSTS_OPT( "hosts" );
-    const std::string SERVER_CONTROL_SUCCESS( "server_control_success" );
+    inline const std::string SERVER_CONTROL_ALL_OPT( "all" );
+    inline const std::string SERVER_CONTROL_HOSTS_OPT( "hosts" );
+    inline const std::string SERVER_CONTROL_SUCCESS( "server_control_success" );
 
-
-    const std::string SERVER_PAUSED_ERROR( "The server is Paused, resume before issuing any other commands" );
-
+    inline const std::string SERVER_PAUSED_ERROR( "The server is Paused, resume before issuing any other commands" );
 
     // this is a hand-chosen polling time for the control plane
     static const size_t SERVER_CONTROL_POLLING_TIME_MILLI_SEC = 500;
@@ -37,107 +36,102 @@ namespace irods {
     // derived from above - used to wait for the server to shut down or resume
     static const size_t SERVER_CONTROL_FWD_SLEEP_TIME_MILLI_SEC = SERVER_CONTROL_POLLING_TIME_MILLI_SEC / 4.0;
 
-    class server_control_executor {
-        public:
-            // @brief constructor
-            server_control_executor( ) {}
-            server_control_executor( const std::string& ); // port property
+    class server_control_executor
+    {
+    public:
+        server_control_executor(const std::string&); // port property
 
-            // @brief callable operator for thread work
-            void operator()();
+        server_control_executor(const server_control_executor&) = delete;
+        server_control_executor& operator=(const server_control_executor&) = delete;
 
-        private:
-            // typedefs
-            typedef boost::function< error( const std::string&, const size_t, std::string& ) > ctrl_func_t;
-            typedef std::vector< std::string >                host_list_t;
+        // @brief callable operator for thread work
+        void operator()();
 
-            // members
-            server_control_executor( const server_control_executor& ) {}
-            error process_operation(
-                const zmq::message_t&, // incoming msg
-                std::string& );        // outgoing text
+    private:
+        using ctrl_func_t = boost::function<error(const std::string&, const size_t, std::string&)>;
+        using host_list_t = std::vector<std::string>;
 
-            error extract_command_parameters(
-                const irods::control_plane_command&, // incoming command
-                std::string&,                        // command name
-                std::string&,                        // command option
-                std::string&,                        // wait option
-                size_t&,                             // wait time in seconds
-                host_list_t& ); // hostnames
-            error process_host_list(
-                const std::string&, // command
-                const std::string&, // wait option
-                const size_t&,      // wait time in seconds
-                const host_list_t&, // hostnames
-                std::string& );     // outgoing text
-            error validate_host_list(
-                const host_list_t&, // irods hostnames
-                const host_list_t&, // incoming hostnames
-                host_list_t& );     // valid hostnames
-            error get_resource_host_names(
-                host_list_t& );     // fetched hostnames
-            error notify_icat_and_local_servers_preop(
-                const std::string&, // command
-                const std::string&, // command option
-                const std::string&, // wait option
-                const size_t&,      // wait seconds
-                const host_list_t&, // irods hostnames
-                std::string& );     // output
-            error notify_icat_and_local_servers_postop(
-                const std::string&, // command
-                const std::string&, // command option
-                const std::string&, // wait option
-                const size_t&,      // wait seconds
-                const host_list_t&, // irods hostnames
-                std::string& );     // output
-            error forward_command(
-                const std::string&, // command name
-                const std::string&, // host
-                const std::string&, // port keyword
-                const std::string&, // wait option
-                const size_t&,      // wait seconds
-                std::string& );     // output
+        error process_operation(
+            const zmq::message_t&, // incoming msg
+            std::string& );        // outgoing text
 
-            bool compare_host_names(
-                const std::string&,   // host name 1
-                const std::string& ); // host name 2
+        error extract_command_parameters(
+            const irods::control_plane_command&, // incoming command
+            std::string&,                        // command name
+            std::string&,                        // command option
+            std::string&,                        // wait option
+            size_t&,                             // wait time in seconds
+            host_list_t& );                      // hostnames
 
-            bool is_host_in_list(
-                const std::string&,   // host name in question
-                const host_list_t& ); // list of candidates
-            // attributes
-            const std::string port_prop_;
-            boost::unordered_map< std::string, ctrl_func_t >  op_map_;
-            std::string my_host_name_;
-            std::string icat_host_name_;
+        error process_host_list(
+            const std::string&, // command
+            const std::string&, // wait option
+            const size_t&,      // wait time in seconds
+            const host_list_t&, // hostnames
+            std::string& );     // outgoing text
 
+        error validate_host_list(
+            const host_list_t&, // irods hostnames
+            const host_list_t&, // incoming hostnames
+            host_list_t& );     // valid hostnames
+
+        error get_resource_host_names(
+            host_list_t& );     // fetched hostnames
+
+        error notify_provider_and_local_servers_preop(
+            const std::string&, // command
+            const std::string&, // command option
+            const std::string&, // wait option
+            const size_t&,      // wait seconds
+            const host_list_t&, // irods hostnames
+            std::string& );     // output
+
+        error notify_provider_and_local_servers_postop(
+            const std::string&, // command
+            const std::string&, // command option
+            const std::string&, // wait option
+            const size_t&,      // wait seconds
+            const host_list_t&, // irods hostnames
+            std::string& );     // output
+
+        error forward_command(
+            const std::string&, // command name
+            const std::string&, // host
+            const std::string&, // port keyword
+            const std::string&, // wait option
+            const size_t&,      // wait seconds
+            std::string& );     // output
+
+        bool is_host_in_list(
+            const std::string&,   // host name in question
+            const host_list_t& ); // list of candidates
+
+        const std::string port_prop_;
+        boost::unordered_map<std::string, ctrl_func_t> op_map_;
+        std::string local_server_hostname_;
+        std::string provider_hostname_;
     }; // class server_control_executor
 
-    class server_control_plane {
-        public:
-            // @brief default constructor taking the port property
-            server_control_plane( const std::string& );
+    class server_control_plane
+    {
+    public:
+        // @brief default constructor taking the port property
+        server_control_plane(const std::string&);
 
-            // @brief destructor
-            ~server_control_plane();
+        server_control_plane(const server_control_plane&) = delete;
+        server_control_plane& operator=(const server_control_plane&) = delete;
 
-        private:
+        // @brief destructor
+        ~server_control_plane();
 
-            // @brief disallow copy constructor
-            server_control_plane( server_control_plane& ) {}
+    private:
+        // @brief functor which manages the control
+        server_control_executor control_executor_;
 
-            // @brief functor which manages the control
-            server_control_executor control_executor_;
-
-            // @brief thread which manages the control loop
-            boost::thread control_thread_;
-
+        // @brief thread which manages the control loop
+        boost::thread control_thread_;
     }; // server_control_plane
+} // namespace irods
 
-}; // namespace irods
-
-#endif // SERVER_CONTROL_PLANE_HPP
-
-
-
+#endif // IRODS_SERVER_CONTROL_PLANE_HPP
 
