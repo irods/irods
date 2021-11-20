8a26cdfcb853787c02156e62e40bd5a05e32d480
Kory Draughn <korydraughn@renci.org>
2021-11-20 15:09:24 -0500

    [#5982][#6017] fixed_buffer_resource now honors memory alignment requirements.
    - Removed undefined behavior.
    - Temporarily removed tracking of overhead due to alignment handling.
    - Added code that results in a compilation if the version of Boost does
      not meet the minimum requirements.
    - Added unit test.

--- unit_tests/src/test_fixed_buffer_resource.cpp
+++ unit_tests/src/test_fixed_buffer_resource.cpp
@@ -1,8 +1,12 @@
+#include "boost/version.hpp"
+
+#if BOOST_VERSION >= 107200
+
 #include "catch.hpp"
 
 #include "fixed_buffer_resource.hpp"
 
-#include <boost/container/pmr/vector.hpp>
+#include "boost/container/pmr/vector.hpp"
 
 #include <cstddef>
 #include <sstream>
@@ -10,43 +14,28 @@
 
 namespace pmr = irods::experimental::pmr;
 
-TEST_CASE("fixed_buffer_resource")
+TEST_CASE("fixed_buffer_resource allocation and deallocation")
 {
-    std::byte buffer[1000];
-    pmr::fixed_buffer_resource mem_resc{buffer, sizeof(buffer), 100};
+    char buffer[100];
+    pmr::fixed_buffer_resource allocator{buffer, sizeof(buffer)};
 
-    {
-        boost::container::pmr::vector<int> ints{&mem_resc};
+    // No bytes allocated yet.
+    CHECK(allocator.allocated() == 0);
 
-        for (int i = 0; i < 20; ++i) {
-            ints.push_back(i);
-        }
+    // Show that allocations and deallocations are tracked correctly.
+    auto* p = allocator.allocate(10);
+    CHECK(allocator.allocated() == 10);
 
-        CHECK(mem_resc.allocated() > 0);
+    allocator.deallocate(p, 10);
+    CHECK(allocator.allocated() == 0);
 
-        // Show that the allocation table can be printed out.
-        std::ostringstream os;
-        CHECK_NOTHROW([&mem_resc, &os] { mem_resc.print(os); }());
+    // Show that the fixed_buffer_resource throws a std::bad_alloc exception
+    // when the underlying buffer is exhausted.
+    CHECK_THROWS_AS([&allocator] { allocator.allocate(101); }(), std::bad_alloc);
 
-        const auto alloc_table = os.str();
-        CHECK(alloc_table.find("  1. Header Info [") != std::string::npos);
-        CHECK(alloc_table.find("previous=") != std::string::npos);
-        CHECK(alloc_table.find("next=") != std::string::npos);
-        CHECK(alloc_table.find("used=") != std::string::npos);
-        CHECK(alloc_table.find("data_size=") != std::string::npos);
-        CHECK(alloc_table.find("data=") != std::string::npos);
-
-        // Show that the fixed_buffer_resource throws a std::bad_alloc exception
-        // when the memory is exhausted.
-        CHECK_THROWS_AS([&ints] {
-            for (int i = 0; i < 100000; ++i) {
-                ints.push_back(i);
-            }
-        }(), std::bad_alloc);
-    }
-
-    CHECK(mem_resc.allocated() == 0);
-    CHECK(mem_resc.allocation_overhead() == 25);
+    // Show that the allocator's state is not corrupted following a
+    // std::bad_alloc exception.
+    CHECK(allocator.allocated() == 0);
 }
 
 TEST_CASE("fixed_buffer_resource throws on invalid arguments")
@@ -54,7 +43,7 @@ TEST_CASE("fixed_buffer_resource throws on invalid arguments")
     SECTION("throws on invalid buffer")
     {
         CHECK_THROWS_AS(([] {
-            pmr::fixed_buffer_resource<char>{nullptr, 1, 1};
+            pmr::fixed_buffer_resource<char>{nullptr, 1};
         }()), std::invalid_argument);
     }
 
@@ -62,26 +51,51 @@ TEST_CASE("fixed_buffer_resource throws on invalid arguments")
     {
         CHECK_THROWS_AS(([] {
             char buffer;
-            pmr::fixed_buffer_resource<char>{&buffer, 0, 1};
+            pmr::fixed_buffer_resource<char>{&buffer, 0};
         }()), std::invalid_argument);
 
         CHECK_THROWS_AS(([] {
             char buffer;
-            pmr::fixed_buffer_resource<char>{&buffer, -1, 1};
-        }()), std::invalid_argument);
-    }
-
-    SECTION("throws on invalid freelist size")
-    {
-        CHECK_THROWS_AS(([] {
-            char buffer;
-            pmr::fixed_buffer_resource<char>{&buffer, 1, 0};
-        }()), std::invalid_argument);
-
-        CHECK_THROWS_AS(([] {
-            char buffer;
-            pmr::fixed_buffer_resource<char>{&buffer, 1, -1};
+            pmr::fixed_buffer_resource<char>{&buffer, -1};
         }()), std::invalid_argument);
     }
 }
+
+TEST_CASE("fixed_buffer_resource works with pmr containers")
+{
+    char buffer[64];
+    pmr::fixed_buffer_resource allocator{buffer, sizeof(buffer)};
+
+    {
+        boost::container::pmr::vector<int> ints{&allocator};
+
+        ints.push_back(1);
+        ints.push_back(2);
+        ints.push_back(3);
+
+        // Show that the allocation table can be printed out.
+        std::ostringstream os;
+        CHECK_NOTHROW([&allocator, &os] { allocator.print(os); }());
+
+        const auto alloc_table = os.str();
+        CHECK(alloc_table.find("  1. Header Info [") != std::string::npos);
+        CHECK(alloc_table.find("previous=") != std::string::npos);
+        CHECK(alloc_table.find("next=") != std::string::npos);
+        CHECK(alloc_table.find("used=") != std::string::npos);
+        CHECK(alloc_table.find("data=") != std::string::npos);
+        CHECK(alloc_table.find("data_size=") != std::string::npos);
+
+        // Show that the fixed_buffer_resource throws a std::bad_alloc exception
+        // when the memory is exhausted.
+        CHECK_THROWS_AS([&ints] {
+            for (int i = 0; i < 100; ++i) {
+                ints.push_back(i + 4);
+            }
+        }(), std::bad_alloc);
+    }
+
+    CHECK(allocator.allocated() == 0);
+}
+
+#endif // BOOST_VERSION >= 107200
 
