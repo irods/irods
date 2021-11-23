74a129d9ee6047e7884d789f5de6b4edbb51bae5
Kory Draughn <korydraughn@renci.org>
2021-11-23 14:04:32 -0500

    [#6019] Implemented new allocator: capped_memory_resource
    capped_memory_resource is an alternative PMR allocator to
    fixed_buffer_resource. It exists due to a bug in Boost.Container's PMR
    implementation.
    
    While capped_memory_resource does not match the level of performance of
    fixed_buffer_resource, it does satisfy the requirement of enforcing a
    limit on the number of bytes that can be allocated through it.

---
+++ unit_tests/src/test_capped_memory_resource.cpp
@@ -0,0 +1,61 @@
+#include "catch.hpp"
+
+#include "capped_memory_resource.hpp"
+
+#include "boost/container/pmr/vector.hpp"
+
+#include <cstddef>
+#include <stdexcept>
+
+namespace pmr = irods::experimental::pmr;
+
+TEST_CASE("capped_memory_resource allocation and deallocation")
+{
+    pmr::capped_memory_resource allocator{100};
+
+    // No bytes allocated yet.
+    CHECK(allocator.allocated() == 0);
+
+    // Show that allocations and deallocations are tracked correctly.
+    auto* p = allocator.allocate(10);
+    CHECK(allocator.allocated() == 10);
+
+    allocator.deallocate(p, 10);
+    CHECK(allocator.allocated() == 0);
+
+    // Show that the capped_memory_resource throws a std::bad_alloc exception
+    // when the memory cap will be exceeded.
+    CHECK_THROWS_AS([&allocator] { allocator.allocate(101); }(), std::bad_alloc);
+
+    // Show that the allocator's state is not corrupted following a
+    // std::bad_alloc exception.
+    CHECK(allocator.allocated() == 0);
+}
+
+TEST_CASE("capped_memory_resource throws on invalid cap")
+{
+    CHECK_THROWS_AS([] { pmr::capped_memory_resource{ 0}; }(), std::invalid_argument);
+    CHECK_THROWS_AS([] { pmr::capped_memory_resource{-1}; }(), std::invalid_argument);
+}
+
+TEST_CASE("capped_memory_resource works with pmr containers")
+{
+    pmr::capped_memory_resource allocator{64};
+
+    {
+        boost::container::pmr::vector<int> ints{&allocator};
+
+        ints.push_back(1);
+        ints.push_back(2);
+        ints.push_back(3);
+
+        CHECK_THROWS_AS([&ints] {
+            for (int i = 0; i < 100; ++i) {
+                ints.push_back(i + 4);
+            }
+        }(), std::bad_alloc);
+    }
+
+    CHECK(allocator.allocated() == 0);
+}
+
