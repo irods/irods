45efac80e29503ee5736b9b0012fd8112f9f3113
Kory Draughn <korydraughn@renci.org>
2021-11-23 14:04:32 -0500

    [#6019] Implemented new allocator: capped_memory_resource
    capped_memory_resource is an alternative PMR allocator to
    fixed_buffer_resource. It exists due to a bug in Boost.Container's PMR
    implementation.
    
    While capped_memory_resource does not match the level of performance of
    fixed_buffer_resource, it does satisfy the requirement of enforcing a
    limit on the number of bytes that can be allocated through it.

--- cmake/development_library.cmake
+++ cmake/development_library.cmake
@@ -23,6 +23,7 @@ set(
   ${CMAKE_SOURCE_DIR}/lib/core/include/apiHandler.hpp
   ${CMAKE_SOURCE_DIR}/lib/core/include/base64.h
   ${CMAKE_SOURCE_DIR}/lib/core/include/bunUtil.h
+  ${CMAKE_SOURCE_DIR}/lib/core/include/capped_memory_resource.hpp
   ${CMAKE_SOURCE_DIR}/lib/core/include/chksumUtil.h
   ${CMAKE_SOURCE_DIR}/lib/core/include/client_connection.hpp
   ${CMAKE_SOURCE_DIR}/lib/core/include/connection_pool.hpp
