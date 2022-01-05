8e6e655b61343072a517d66114bf383e239f2499
Alan King <alanking@renci.org>
2022-01-05 14:40:53 -0500

    [#5927] Add API test for rsDataObjRepl and VERIFY_CHKSUM_KW

---
+++ unit_tests/cmake/test_config/irods_rc_data_obj_repl.cmake
@@ -0,0 +1,14 @@
+set(IRODS_TEST_TARGET irods_rc_data_obj_repl)
+
+set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_rc_data_obj_repl.cpp)
+
+set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
+
+set(IRODS_TEST_LINK_LIBRARIES irods_common
+                              irods_client
+                              irods_plugin_dependencies
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_filesystem.so
+                              ${IRODS_EXTERNALS_FULLPATH_BOOST}/lib/libboost_system.so
+                              ${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so)
