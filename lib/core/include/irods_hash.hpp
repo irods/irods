56e395243a6a5c69aaed07abcddfa6a220d52117
Kory Draughn <korydraughn@renci.org>
2022-02-07 17:35:53 -0500

    [#6133] Fixed compilation issues with irods_hash.hpp.

--- lib/core/include/irods_hash.hpp
+++ lib/core/include/irods_hash.hpp
@@ -1,54 +1,52 @@
-#ifndef __IRODS_HASH_HPP__
-#define __IRODS_HASH_HPP__
+#ifndef IRODS_HASH_HPP
+#define IRODS_HASH_HPP
 
-// =-=-=-=-=-=-=-
-// hash_map include
+#include "irods_stacktrace.hpp"
+#include "rodsLog.h"
+
 #include <boost/unordered_map.hpp>
+
 #define HASH_TYPE boost::unordered_map
 
 #include <string>
-#include "irods_stacktrace.hpp"
-#include "irods_log.hpp"
+#include <iostream>
 
-namespace irods {
-
-    struct irods_string_hash {
-        enum {
+namespace irods
+{
+    struct irods_string_hash
+    {
+        enum
+        {
             // parameters for hash table
             bucket_size = 4, // 0 < bucket_size
             min_buckets = 8
-
         }; // min_buckets = 2 ^^ N, 0 < N
 
-        size_t operator()( const std::string& s1 ) const {
-            if ( s1.empty() ) {
+        ~irods_string_hash() = default;
+
+        std::size_t operator()(const std::string& s1) const
+        {
+            if (s1.empty()) {
                 std::cerr << irods::stacktrace().dump();
-                rodsLog(
-                    LOG_NOTICE,
-                    "irods_string_hash - empty string value" );
+                rodsLog(LOG_NOTICE, "irods_string_hash - empty string value");
                 return 0;
             }
 
-            // hash string s1 to size_t value
-            const unsigned char *p = ( const unsigned char * )s1.c_str();
-            size_t hashval = 0;
+            const unsigned char *p = reinterpret_cast<const unsigned char*>(s1.c_str());
+            std::size_t hashval = 0;
 
-            while ( *p != '\0' ) {
-                hashval = 31 * hashval + ( *p++ );    // or whatever
+            while (*p != '\0') {
+                hashval = 31 * hashval + *p++;
             }
 
             return hashval;
         }
 
-        bool operator()( const std::string& s1, const std::string& s2 ) const {
+        bool operator()(const std::string& s1, const std::string& s2) const
+        {
             return s1 < s2;
         }
-
-        ~irods_string_hash() {
-        }
-
     }; // struct irods_string_hash
+} // namespace irods
 
-}; // namespace irods
-
-#endif // __IRODS_HASH_HPP__
+#endif // IRODS_HASH_HPP
