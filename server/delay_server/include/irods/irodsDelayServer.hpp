17a91279498298cd50d507aeda4bb968547fbec4
Kory Draughn <korydraughn@renci.org>
2022-04-12 15:35:21 -0400

    [#6328] Added static assertions for delay server defaults.

--- server/delay_server/include/irods/irodsDelayServer.hpp
+++ server/delay_server/include/irods/irodsDelayServer.hpp
@@ -4,9 +4,12 @@
 namespace irods
 {
     // clang-format off
-    constexpr int default_delay_server_sleep_time_in_seconds   = 30;
-    constexpr int default_number_of_concurrent_delay_executors = 4;
+    constexpr const int default_delay_server_sleep_time_in_seconds   = 30;
+    constexpr const int default_number_of_concurrent_delay_executors = 4;
     // clang-format on
+
+    static_assert(default_delay_server_sleep_time_in_seconds > 0);
+    static_assert(default_number_of_concurrent_delay_executors > 0);
 } // namespace irods
 
 #endif // IRODS_DELAY_SERVER_HPP 
