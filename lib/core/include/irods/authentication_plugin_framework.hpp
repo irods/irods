771e106a77a167a275bc7abb302143e962cdc0cd
Alan King <alanking@renci.org>
2022-05-11 17:14:53 -0400

    [#6382] Remove supplied password support from PAM
    This change removes support for a password supplied via keyword to the
    plugin. The password should only be allowed through the prompt given by
    the plugin.
    
    This change adds support for a new keyword in the authentication plugins
    called force_password_prompt. As the name suggests, this forces the
    plugin to prompt the user for a password rather than using any password
    supplied by the user beforehand or in an authentication file. This is
    useful for clients like iinit which want to re-prompt the user for their
    password even when they are currently authenticated.

--- lib/core/include/irods/authentication_plugin_framework.hpp
+++ lib/core/include/irods/authentication_plugin_framework.hpp
@@ -28,6 +28,7 @@ namespace irods::experimental::auth
 {
     static const char* const flow_complete{"authentication_flow_complete"};
     static const char* const next_operation{"next_operation"};
+    static const char* const force_password_prompt{"force_password_prompt"};
 
     /// \brief Base class for authentication plugin implementations.
     ///
