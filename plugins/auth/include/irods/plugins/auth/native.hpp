1c8f7d09faba673e46ea74d3b3c9a52a7746586f
Alan King <alanking@renci.org>
2022-05-13 15:06:39 -0400

    [#5479] Add header for native auth plugin

---
+++ plugins/auth/include/irods/plugins/auth/native.hpp
@@ -0,0 +1,9 @@
+#ifndef IRODS_PLUGIN_AUTH_NATIVE_HPP
+#define IRODS_PLUGIN_AUTH_NATIVE_HPP
+
+namespace irods::experimental::auth::scheme
+{
+    constexpr char* native = "native";
+} // namespace irods::experimental::auth::scheme
+
+#endif // IRODS_PLUGIN_AUTH_NATIVE_HPP
