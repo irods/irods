d4806703ce9773ff2814ebeedb7ec82829ec7cfa
Alan King <alanking@renci.org>
2022-05-13 15:06:12 -0400

    [#5479] Rename new pam plugin to pam_password

---
+++ plugins/auth/include/irods/plugins/auth/pam_password.hpp
@@ -0,0 +1,9 @@
+#ifndef IRODS_PLUGIN_AUTH_PAM_PASSWORD_HPP
+#define IRODS_PLUGIN_AUTH_PAM_PASSWORD_HPP
+
+namespace irods::experimental::auth::scheme
+{
+    constexpr char* pam_password = "pam_password";
+} // namespace irods::experimental::auth::scheme
+
+#endif // IRODS_PLUGIN_AUTH_PAM_PASSWORD_HPP
