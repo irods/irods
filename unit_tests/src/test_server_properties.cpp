a27044eea2a226e6190346bb1c6795021ebf32c9
Violet White <violet.white.dammit@protonmail.com>
2022-06-03 14:47:20 -0400

    [#5816] Main server now reloads its config on SIGHUP signal.
    This permits configuration changes to be made to high load servers that
    cannot otherwise be shut down due to ongoing work such as file transfers.
    
    As a side effect, being able to reload the configuration permits some
    tests to be made considerably faster by replacing calls to
    "IrodsController.restart" with "IrodsController.reload_configuration".
    
    Adds a hook system which permits dispatch of arbitrary json structures
    through an event system. In the server this permits plugins to listen
    for configuration changes by using "irods::experimental::json_events::add_hook".
    
    In client programs this may be used as a more generic dispatch
    mechanism. Potentially, this could allow client programs to more easily
    respond to updated state issued from the server. This may be
    accomplished by use of both "add_hook" and "run_hook".

--- unit_tests/src/test_server_properties.cpp
+++ unit_tests/src/test_server_properties.cpp
@@ -1,10 +1,13 @@
 #include <catch2/catch.hpp>
 
+#include "irods/irods_at_scope_exit.hpp"
 #include "irods/irods_configuration_keywords.hpp"
 #include "irods/irods_configuration_parser.hpp"
 #include "irods/irods_server_properties.hpp"
 
 #include <fmt/format.h>
+
+#include <thread>
 
 TEST_CASE("server_properties", "[exceptions]")
 {
@@ -27,5 +30,29 @@ TEST_CASE("server_properties", "[exceptions]")
                                  irods::KW_CFG_PLUGIN_CONFIGURATION,
                                  irods::KW_CFG_PLUGIN_TYPE_DATABASE,
                                  irods::KW_CFG_DB_PASSWORD));
+    }
+}
+
+// The goal here is to ensure that the server_properties object is locked and unlocked
+// safely while using the normal api
+TEST_CASE("server_properties is threadsafe", "[thread-safety]")
+{
+    const auto limit = 1000;
+
+    // Start a thread that will generally get in the way a lot by writing
+    // to the irods::server_properties object
+    std::thread a([]() {
+        for (auto i = 0; i < limit; i++) {
+            irods::set_server_property(std::to_string(i), i);
+        }
+    });
+    irods::at_scope_exit([&a]() { a.join(); });
+    auto i = 0;
+    while (i < limit) {
+        auto key = std::to_string(i);
+        if (irods::server_property_exists(key)) {
+            REQUIRE(irods::get_server_property<int>(key) == i);
+            i++;
+        }
     }
 }
