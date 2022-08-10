efe32a3d0af4dfd95e29943d7ba883e2f88b397b
Justin James <jjames@renci.org>
2022-08-10 16:04:05 -0400

    [#4421] Added detached mode for unixfilesystem resource

---
+++ plugins/resources/unixfilesystem/include/irods/private/irods_is_in_host_list.hpp
@@ -0,0 +1,13 @@
+#ifndef IRODS_IS_IN_HOST_LIST_HPP
+#define IRODS_IS_IN_HOST_LIST_HPP
+
+#include "irods/irods_lookup_table.hpp"
+
+#include <string_view>
+
+namespace irods
+{
+    auto is_host_in_host_list(irods::plugin_property_map& prop_map, const std::string_view& resource_hostname) -> bool;
+} // namespace irods
+
+#endif // IRODS_IS_IN_HOST_LIST_HPP
