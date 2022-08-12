15fa433f9cdf0e3ab91571491d5f2b0e37e1e967
Awab <awab.masroor@gmail.com>
2022-08-12 15:45:51 -0400

    [#5176] Added Ticket Administration Library

---
+++ lib/administration/ticket/src/ticket_administration.cpp
@@ -0,0 +1,155 @@
+#include "irods/ticket_administration.hpp"
+
+#undef rxTicketAdmin
+
+#ifdef IRODS_TICKET_ADMINISTRATION_ENABLE_SERVER_SIDE_API
+#    include "irods/rsTicketAdmin.hpp"
+#    define rxTicketAdmin rsTicketAdmin
+#else
+#    include "irods/ticketAdmin.h"
+#    define rxTicketAdmin rcTicketAdmin
+#endif // IRODS_TICKET_ADMINISTRATION_ENABLE_SERVER_SIDE_API
+
+#include "irods/irods_random.hpp"
+#include "irods/objInfo.h"
+#include "irods/rcConnect.h"
+#include "irods/rcMisc.h"
+#include "irods/rodsError.h"
+#include "irods/rodsKeyWdDef.h"
+#include "irods/irods_exception.hpp"
+#include "irods/irods_at_scope_exit.hpp"
+
+#include <string>
+
+namespace irods::experimental::administration::ticket::NAMESPACE_IMPL
+{
+    namespace
+    {
+        void make_ticket_name(char* newTicket)
+        {
+            const int ticket_len = 15;
+            // random_bytes must be (unsigned char[]) to guarantee that following
+            // modulo result is positive (i.e. in [0, 61])
+            unsigned char random_bytes[ticket_len];
+            irods::getRandomBytes(random_bytes, ticket_len);
+
+            const char characterSet[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P',
+                                         'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f',
+                                         'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v',
+                                         'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+
+            for (int i = 0; i < ticket_len; ++i) {
+                const int ix = random_bytes[i] % sizeof(characterSet);
+                newTicket[i] = characterSet[ix];
+            }
+            newTicket[ticket_len] = '\0';
+        }
+    } // anonymous namespace
+
+    namespace detail
+    {
+        void execute_ticket_operation(RxComm& conn,
+                                      std::string_view command,
+                                      std::string_view ticket_identifier,
+                                      std::string_view command_modifier1,
+                                      std::string_view command_modifier2,
+                                      std::string_view command_modifier3,
+                                      std::string_view command_modifier4,
+                                      bool run_as_admin)
+        {
+            ticketAdminInp_t ticketAdminInp{};
+            irods::at_scope_exit free_memory{[&ticketAdminInp] { clearKeyVal(&ticketAdminInp.condInput); }};
+
+            ticketAdminInp.arg1 = const_cast<char*>(command.data());
+            ticketAdminInp.arg2 = const_cast<char*>(ticket_identifier.data());
+            ticketAdminInp.arg3 = const_cast<char*>(command_modifier1.data());
+            ticketAdminInp.arg4 = const_cast<char*>(command_modifier2.data());
+            ticketAdminInp.arg5 = const_cast<char*>(command_modifier3.data());
+            ticketAdminInp.arg6 = const_cast<char*>(command_modifier4.data());
+
+            if (run_as_admin) {
+                addKeyVal(&ticketAdminInp.condInput, ADMIN_KW, "");
+            }
+
+            int status = rxTicketAdmin(&conn, &ticketAdminInp);
+
+            if (status < 0) {
+                THROW(status, "Ticket operation failed");
+            }
+        }
+    } // namespace detail
+
+    void create_ticket(RxComm& conn, ticket_type _type, std::string_view obj_path, std::string_view ticket_name)
+    {
+        if (_type == ticket_type::read) {
+            detail::execute_ticket_operation(conn, "create", ticket_name, "read", obj_path, ticket_name, "", false);
+        }
+        else if (_type == ticket_type::write) {
+            detail::execute_ticket_operation(conn, "create", ticket_name, "write", obj_path, ticket_name, "", false);
+        }
+    }
+
+    std::string create_ticket(RxComm& conn, ticket_type _type, std::string_view obj_path)
+    {
+        char myTicket[30];
+        make_ticket_name(myTicket);
+
+        if (_type == ticket_type::read) {
+            detail::execute_ticket_operation(conn, "create", myTicket, "read", obj_path, myTicket, "", false);
+        }
+        else if (_type == ticket_type::write) {
+            detail::execute_ticket_operation(conn, "create", myTicket, "write", obj_path, myTicket, "", false);
+        }
+
+        return myTicket;
+    }
+
+    void create_ticket(admin_tag,
+                       RxComm& conn,
+                       ticket_type _type,
+                       std::string_view obj_path,
+                       std::string_view ticket_name)
+    {
+        if (_type == ticket_type::read) {
+            detail::execute_ticket_operation(conn, "create", ticket_name, "read", obj_path, ticket_name, "", true);
+        }
+        else if (_type == ticket_type::write) {
+            detail::execute_ticket_operation(conn, "create", ticket_name, "write", obj_path, ticket_name, "", true);
+        }
+    }
+
+    std::string create_ticket(admin_tag, RxComm& conn, ticket_type _type, std::string_view obj_path)
+    {
+        char myTicket[30];
+        make_ticket_name(myTicket);
+
+        if (_type == ticket_type::read) {
+            detail::execute_ticket_operation(conn, "create", myTicket, "read", obj_path, myTicket, "", true);
+        }
+        else if (_type == ticket_type::write) {
+            detail::execute_ticket_operation(conn, "create", myTicket, "write", obj_path, myTicket, "", true);
+        }
+
+        return myTicket;
+    }
+
+    void delete_ticket(RxComm& conn, std::string_view ticket_name)
+    {
+        detail::execute_ticket_operation(conn, "delete", ticket_name, "", "", "", "", false);
+    }
+
+    void delete_ticket(RxComm& conn, int ticket_id)
+    {
+        detail::execute_ticket_operation(conn, "delete", std::to_string(ticket_id), "", "", "", "", false);
+    }
+
+    void delete_ticket(admin_tag, RxComm& conn, std::string_view ticket_name)
+    {
+        detail::execute_ticket_operation(conn, "delete", ticket_name, "", "", "", "", true);
+    }
+
+    void delete_ticket(admin_tag, RxComm& conn, int ticket_id)
+    {
+        detail::execute_ticket_operation(conn, "delete", std::to_string(ticket_id), "", "", "", "", true);
+    }
+} // namespace irods::experimental::administration::ticket::NAMESPACE_IMPL
---
+++ unit_tests/src/test_ticket_administration.cpp
@@ -0,0 +1,317 @@
+#include <catch2/catch.hpp>
+
+#include "irods/connection_pool.hpp"
+#include "irods/rodsClient.h"
+#include "irods/ticket_administration.hpp"
+#include "irods/query_builder.hpp"
+#include "irods/user_administration.hpp"
+
+#include <fmt/format.h>
+
+#include <string>
+#include <cstdio>
+#include <stdexcept>
+
+TEST_CASE("ticket administration")
+{
+    namespace tic = irods::experimental::administration::ticket;
+    const std::string read_ticket_name = "readTicket";
+    const std::string write_ticket_name = "writeTicket";
+    const std::string read_string = "read";
+    const std::string read_coll_string = "/tempZone/home/rods";
+    const std::string write_string = "write";
+    const std::string write_coll_string = "/tempZone";
+
+    irods::experimental::query_builder builder;
+
+    load_client_api_plugins();
+    auto conn_pool = irods::make_connection_pool();
+    auto conn = conn_pool->get_connection();
+
+    const std::string set_ticket_name = "setTicket";
+    const std::string set_ticket_collection = "/tempZone/home";
+    const std::string user_name = "testUser";
+    const std::string group_name = "testGroup";
+
+    int set_test_number = 19;
+    std::string remove_test_number_string = "0";
+
+    irods::experimental::administration::user test_user{user_name};
+    irods::experimental::administration::client::add_user(conn, test_user);
+
+    irods::experimental::administration::group test_group{group_name};
+    irods::experimental::administration::client::add_group(conn, test_group);
+
+    SECTION("create read ticket / delete ticket")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::read, read_coll_string, read_ticket_name);
+
+        auto general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", read_ticket_name));
+
+        bool test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == read_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(test_ticket_exists);
+
+        std::string query =
+            fmt::format("select TICKET_COLL_NAME, TICKET_TYPE where TICKET_STRING = '{}'", read_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(read_coll_string == (general_query.begin().capture_results().at(0)));
+        REQUIRE(read_string == (general_query.begin().capture_results().at(1)));
+
+        tic::client::delete_ticket(conn, read_ticket_name);
+
+        general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", read_ticket_name));
+
+        test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == read_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(!test_ticket_exists);
+    }
+
+    SECTION("create write ticket/admin delete ticket")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::write, write_coll_string, write_ticket_name);
+
+        auto general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", write_ticket_name));
+
+        bool test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == write_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(test_ticket_exists);
+
+        std::string query =
+            fmt::format("select TICKET_COLL_NAME, TICKET_TYPE where TICKET_STRING = '{}'", write_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(write_coll_string == general_query.begin().capture_results().at(0));
+        REQUIRE(write_string == general_query.begin().capture_results().at(1));
+
+        tic::client::delete_ticket(tic::admin, conn, write_ticket_name);
+
+        general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", write_ticket_name));
+
+        test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == write_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(!test_ticket_exists);
+    }
+
+    SECTION("admin create read ticket")
+    {
+        tic::client::create_ticket(tic::admin, conn, tic::ticket_type::read, read_coll_string, read_ticket_name);
+
+        auto general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", read_ticket_name));
+
+        bool test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == read_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(test_ticket_exists);
+
+        std::string query =
+            fmt::format("select TICKET_COLL_NAME, TICKET_TYPE where TICKET_STRING = '{}'", read_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(read_coll_string == (general_query.begin().capture_results().at(0)));
+        REQUIRE(read_string == (general_query.begin().capture_results().at(1)));
+
+        tic::client::delete_ticket(conn, read_ticket_name);
+    }
+
+    SECTION("admin create write ticket")
+    {
+        tic::client::create_ticket(tic::admin, conn, tic::ticket_type::write, write_coll_string, write_ticket_name);
+
+        auto general_query =
+            builder.build<rcComm_t>(conn,
+                                    fmt::format("select TICKET_STRING where TICKET_STRING = '{}'", write_ticket_name));
+
+        bool test_ticket_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == write_ticket_name) {
+                test_ticket_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(test_ticket_exists);
+
+        std::string query =
+            fmt::format("select TICKET_COLL_NAME, TICKET_TYPE where TICKET_STRING = '{}'", write_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(write_coll_string == (general_query.begin().capture_results().at(0)));
+        REQUIRE(write_string == (general_query.begin().capture_results().at(1)));
+
+        tic::client::delete_ticket(conn, write_ticket_name);
+    }
+
+    SECTION("add user/remove user")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::read, set_ticket_collection, set_ticket_name);
+
+        tic::client::add_ticket_constraint(conn, set_ticket_name, tic::user_constraint{user_name});
+
+        std::string query = fmt::format("select TICKET_ALLOWED_USER_NAME where TICKET_STRING = '{}'", set_ticket_name);
+        auto general_query = builder.build<rcComm_t>(conn, query);
+
+        bool user_name_exists = false;
+
+        for (const auto& row : general_query) {
+            if (row[0] == user_name) {
+                user_name_exists = true;
+                break;
+            }
+        }
+
+        REQUIRE(user_name_exists);
+
+        tic::client::remove_ticket_constraint(conn, set_ticket_name, tic::user_constraint{user_name});
+
+        query = fmt::format("select TICKET_ALLOWED_USER_NAME where TICKET_STRING = '{}'", set_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() == 0);
+
+        tic::client::delete_ticket(conn, set_ticket_name);
+    }
+
+    SECTION("add group/remove group")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::write, set_ticket_collection, set_ticket_name);
+
+        tic::client::add_ticket_constraint(conn, set_ticket_name, tic::group_constraint{group_name});
+
+        std::string query = fmt::format("select TICKET_ALLOWED_GROUP_NAME where TICKET_STRING = '{}'", set_ticket_name);
+        auto general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(group_name == (general_query.begin().capture_results().at(0)));
+
+        tic::client::remove_ticket_constraint(conn, set_ticket_name, tic::group_constraint{group_name});
+
+        query = fmt::format("select TICKET_ALLOWED_GROUP_NAME where TICKET_STRING = '{}'", set_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() == 0);
+
+        tic::client::delete_ticket(conn, set_ticket_name);
+    }
+
+    SECTION("set uses_count/remove uses_count")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::write, set_ticket_collection, set_ticket_name);
+
+        tic::client::set_ticket_constraint(conn, set_ticket_name, tic::use_count_constraint{set_test_number});
+
+        std::string query = fmt::format("select TICKET_USES_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        auto general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(std::to_string(set_test_number) == (general_query.begin().capture_results().at(0)));
+
+        tic::client::remove_ticket_constraint(conn, set_ticket_name, tic::use_count_constraint{});
+
+        query = fmt::format("select TICKET_USES_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(remove_test_number_string == (general_query.begin().capture_results().at(0)));
+
+        tic::client::delete_ticket(conn, set_ticket_name);
+    }
+
+    SECTION("admin set write_file/remove write_file")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::write, set_ticket_collection, set_ticket_name);
+
+        tic::client::set_ticket_constraint(tic::admin,
+                                           conn,
+                                           set_ticket_name,
+                                           tic::n_writes_to_data_object_constraint{set_test_number});
+
+        std::string query = fmt::format("select TICKET_WRITE_FILE_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        auto general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(std::to_string(set_test_number) == (general_query.begin().capture_results().at(0)));
+
+        tic::client::remove_ticket_constraint(conn, set_ticket_name, tic::n_writes_to_data_object_constraint{});
+
+        query = fmt::format("select TICKET_WRITE_FILE_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(remove_test_number_string == (general_query.begin().capture_results().at(0)));
+
+        tic::client::delete_ticket(conn, set_ticket_name);
+    }
+
+    SECTION("set write_byte/admin remove write_byte")
+    {
+        tic::client::create_ticket(conn, tic::ticket_type::write, set_ticket_collection, set_ticket_name);
+
+        tic::client::set_ticket_constraint(conn, set_ticket_name, tic::n_write_bytes_constraint{set_test_number});
+
+        std::string query = fmt::format("select TICKET_WRITE_BYTE_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        auto general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(std::to_string(set_test_number) == (general_query.begin().capture_results().at(0)));
+
+        tic::client::remove_ticket_constraint(tic::admin, conn, set_ticket_name, tic::n_write_bytes_constraint{});
+
+        query = fmt::format("select TICKET_WRITE_BYTE_LIMIT where TICKET_STRING = '{}'", set_ticket_name);
+        general_query = builder.build<rcComm_t>(conn, query);
+
+        REQUIRE(general_query.size() > 0);
+        REQUIRE(remove_test_number_string == (general_query.begin().capture_results().at(0)));
+
+        tic::client::delete_ticket(conn, set_ticket_name);
+    }
+
+    irods::experimental::administration::client::remove_user(conn, test_user);
+    irods::experimental::administration::client::remove_group(conn, test_group);
+}
