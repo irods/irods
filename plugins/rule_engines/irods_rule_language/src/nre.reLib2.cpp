9a441b06219fba41c30a7e3a0e681d9307473001
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2022-08-23 03:04:07 -0400

    [#6546] remove unused and empty nre.reLib2.cpp

--- plugins/rule_engines/irods_rule_language/src/nre.reLib2.cpp
+++
@@ -1,7 +0,0 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-
-#include "irods/private/re/reGlobalsExtern.hpp"
-#include "irods/private/re/reHelpers1.hpp"
-
-
