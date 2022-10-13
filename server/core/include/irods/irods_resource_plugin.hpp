60548eec268a2766288855801d0f6113c4476a84
Kory Draughn <korydraughn@renci.org>
2022-10-13 08:58:01 -0400

    [858] Cleaned up resource plugin class.

--- server/core/include/irods/irods_resource_plugin.hpp
+++ server/core/include/irods/irods_resource_plugin.hpp
@@ -1,114 +1,95 @@
-#ifndef ___IRODS_RESC_PLUGIN_HPP__
-#define ___IRODS_RESC_PLUGIN_HPP__
+#ifndef IRODS_RESOURCE_PLUGIN_HPP
+#define IRODS_RESOURCE_PLUGIN_HPP
 
-// =-=-=-=-=-=-=-
-#include "irods/irods_file_object.hpp"
 #include "irods/irods_plugin_base.hpp"
 #include "irods/irods_resource_constants.hpp"
 #include "irods/irods_resource_types.hpp"
 
-#include <iostream>
 #include <utility>
-#include <boost/any.hpp>
+#include <vector>
 
-namespace irods {
+namespace irods
+{
+    extern const char* const RESC_CHILD_MAP_PROP;
+    extern const char* const RESC_PARENT_PROP;
 
-    const std::string RESC_CHILD_MAP_PROP( "resource_child_map_property" );
-    const std::string RESC_PARENT_PROP( "resource_parent_property" );
-    typedef lookup_table< std::pair< std::string, resource_ptr > > resource_child_map;
+    using resource_child_map = lookup_table<std::pair<std::string, resource_ptr>>;
 
-// =-=-=-=-=-=-=-
-    /**
-     * \author Jason M. Coposky
-     * \brief
-     *
-     **/
-    class resource : public plugin_base {
-        public:
-            // =-=-=-=-=-=-=-
-            /// @brief Constructors
-            resource(
-                const std::string& _inst,
-                const std::string& _ctx ) :
-                    plugin_base(
-                        _inst,
-                        _ctx ) {
-                properties_.set(
-                    RESC_CHILD_MAP_PROP,
-                    &children_ );
-                properties_.set(
-                    RESC_PARENT_PROP,
-                    parent_ );
-            } // ctor
+    class resource : public plugin_base // NOLINT(cppcoreguidelines-special-member-functions)
+    {
+    public:
+        resource(const std::string& _inst, const std::string& _ctx)
+            : plugin_base(_inst, _ctx)
+        {
+            properties_.set(RESC_CHILD_MAP_PROP, &children_);
+            properties_.set(RESC_PARENT_PROP, parent_);
+        }
 
-            // =-=-=-=-=-=-=-
-            /// @brief Destructor
-            virtual ~resource() {
-            }
+        ~resource() = default; // NOLINT(modernize-use-override)
 
-            // =-=-=-=-=-=-=-
-            /// @brief copy ctor
-            resource( const resource& _rhs ) :
-              plugin_base{ _rhs },
-              children_{_rhs.children_},
-              parent_{_rhs.parent_} {
-            } // cctor
+        resource(const resource& _rhs) = default;
 
-            // =-=-=-=-=-=-=-
-            /// @brief Assignment Operator - necessary for stl containers
-            resource& operator=( const resource& _rhs ) {
-                if ( &_rhs == this ) {
-                    return *this;
-                }
-                plugin_base::operator=( _rhs );
-                children_ = _rhs.children_;
-                parent_   = _rhs.parent_;
+        resource& operator=(const resource& _rhs)
+        {
+            if (&_rhs == this) {
                 return *this;
             }
 
-            // =-=-=-=-=-=-=-
-            /// @brief interface to add and remove children using the zone_name::resource_name
-            virtual error add_child( const std::string&, const std::string&, resource_ptr );
-            virtual error remove_child( const std::string& );
-            virtual size_t num_children() {
-                return children_.size();
-            }
-            virtual bool has_child(
-                const std::string& _name ) {
-                return children_.has_entry( _name );
-            }
-            virtual void children( std::vector<std::string>& );
+            plugin_base::operator=(_rhs);
+            children_ = _rhs.children_;
+            parent_ = _rhs.parent_;
 
-            // =-=-=-=-=-=-=-
-            /// @brief interface to get and set a resource's parent pointer
-            virtual error set_parent( const resource_ptr& );
-            virtual error get_parent( resource_ptr& );
+            return *this;
+        }
 
-        protected:
-            // =-=-=-=-=-=-=-
-            /// @brief Pointers to Child and Parent Resources
-            resource_child_map  children_;
-            resource_ptr        parent_;
+        virtual error add_child(const std::string& _name, const std::string& _data, resource_ptr _resc);
 
+        virtual error remove_child(const std::string& _name);
+
+        virtual std::size_t num_children()
+        {
+            return children_.size();
+        }
+
+        virtual bool has_child(const std::string& _name)
+        {
+            return children_.has_entry(_name);
+        }
+
+        virtual void children(std::vector<std::string>& _out);
+
+        virtual error set_parent(const resource_ptr& _resc);
+
+        virtual error get_parent(resource_ptr& _resc);
+
+    protected:
+        // NOLINTBEGIN(cppcoreguidelines-non-private-member-variables-in-classes)
+        resource_child_map children_;
+        resource_ptr parent_;
+        // NOLINTEND(cppcoreguidelines-non-private-member-variables-in-classes)
     }; // class resource
 
     /// \brief Convenience function for getting resource name from plugin context
-    /// \param[in] ctx - Plugin context from which resource name will be extracted
-    /// \throws irods::exception - thrown if the error object returned by get() is not ok()
+    ///
+    /// \param[in] ctx Plugin context from which resource name will be extracted
+    ///
+    /// \throws irods::exception thrown if the error object returned by get() is not ok()
     auto get_resource_name(plugin_context& ctx) -> std::string;
+
     /// \brief Convenience function for getting resource status from plugin context
-    /// \param[in] ctx - Plugin context from which resource status will be extracted
-    /// \throws irods::exception - thrown if the error object returned by get() is not ok()
+    ///
+    /// \param[in] ctx Plugin context from which resource status will be extracted
+    ///
+    /// \throws irods::exception thrown if the error object returned by get() is not ok()
     auto get_resource_status(plugin_context& ctx) -> int;
+
     /// \brief Convenience function for getting resource location from plugin context
-    /// \param[in] ctx - Plugin context from which resource location will be extracted
-    /// \throws irods::exception - thrown if the error object returned by get() is not ok()
+    ///
+    /// \param[in] ctx Plugin context from which resource location will be extracted
+    ///
+    /// \throws irods::exception thrown if the error object returned by get() is not ok()
     auto get_resource_location(plugin_context& ctx) -> std::string;
+} // namespace irods
 
-}; // namespace irods
-
-
-#endif // ___IRODS_RESC_PLUGIN_HPP__
-
-
+#endif // IRODS_RESOURCE_PLUGIN_HPP
 
--- server/core/src/irods_resource_plugin.cpp
+++ server/core/src/irods_resource_plugin.cpp
@@ -1,119 +1,98 @@
-// =-=-=-=-=-=-=-
-// My Includes
 #include "irods/irods_resource_plugin.hpp"
-#include "irods/irods_resource_redirect.hpp"
-#include "irods/irods_load_plugin.hpp"
 
-// =-=-=-=-=-=-=-
-// STL Includes
-#include <iostream>
-#include <sstream>
-#include <algorithm>
-#include <vector>
+#include <fmt/format.h>
 
-#include <boost/lexical_cast.hpp>
+namespace irods
+{
+    // clang-format off
+    const char* const RESC_CHILD_MAP_PROP = "resource_child_map_property";
+    const char* const RESC_PARENT_PROP    = "resource_parent_property";
+    // clang-format on
 
-// =-=-=-=-=-=-=-
-// dlopen, etc
-#include <dlfcn.h>
-
-namespace irods {
-
-    error resource::add_child(
-            const std::string& _name,
-            const std::string& _data,
-            resource_ptr       _resc ) {
-        // =-=-=-=-=-=-=-
-        // check params
-        if ( _name.empty() ) {
-            return ERROR(
-                       SYS_INVALID_INPUT_PARAM,
-                       "empty name" );
+    // NOLINTNEXTLINE(performance-unnecessary-value-param)
+    error resource::add_child(const std::string& _name, const std::string& _data, resource_ptr _resc)
+    {
+        if (_name.empty()) {
+            return ERROR(SYS_INVALID_INPUT_PARAM, "empty resource name");
         }
 
-        if ( 0 == _resc.get() ) {
-            return ERROR(
-                       SYS_INVALID_INPUT_PARAM,
-                       "null resource pointer" );
+        if (!_resc) {
+            return ERROR(SYS_INVALID_INPUT_PARAM, "null resource pointer");
         }
 
-        // =-=-=-=-=-=-=-
-        // add resource and data to the table
-        children_[ _name ] = std::make_pair( _data, _resc );
+        children_[_name] = std::make_pair(_data, _resc);
 
         return SUCCESS();
-
     } // add_child
 
-    error resource::remove_child( const std::string& _name ) {
-        // =-=-=-=-=-=-=-
+    error resource::remove_child(const std::string& _name)
+    {
         // if an entry exists, erase it otherwise issue a warning.
-        if ( children_.has_entry( _name ) ) {
-            children_.erase( _name );
+        if (children_.has_entry(_name)) {
+            children_.erase(_name);
             return SUCCESS();
         }
-        else {
-            std::stringstream msg;
-            msg << "resource has no child named [" << _name << "]";
-            return ERROR( SYS_INVALID_INPUT_PARAM, msg.str() );
-        }
 
+        return ERROR(SYS_INVALID_INPUT_PARAM, fmt::format("resource has no child named [{}]", _name));
     } // remove_child
 
-    error resource::set_parent( const resource_ptr& _resc ) {
+    error resource::set_parent(const resource_ptr& _resc)
+    {
         parent_ = _resc;
         return SUCCESS();
-
     } // set_parent
 
-    error resource::get_parent( resource_ptr& _resc ) {
+    error resource::get_parent(resource_ptr& _resc)
+    {
         _resc = parent_;
-        if ( _resc.get() ) {
-            return SUCCESS();
-        }
-        else {
-            return ERROR(
-                       SYS_INVALID_INPUT_PARAM,
-                       "null parent pointer" );
+
+        if (!_resc) {
+            return ERROR(SYS_INVALID_INPUT_PARAM, "null resource parent pointer");
         }
 
+        return SUCCESS();
     } // get_parent
 
-    void resource::children(
-        std::vector<std::string>& _out ) {
-        for( auto c_itr : children_ ) {
-            _out.push_back( c_itr.first );
+    void resource::children(std::vector<std::string>& _out)
+    {
+        for (auto&& c : children_) {
+            _out.push_back(c.first);
         }
     } // children
 
-auto get_resource_name(plugin_context& ctx) -> std::string
-{
-    std::string resc_name{};
-    if (error err = ctx.prop_map().get<std::string>(RESOURCE_NAME, resc_name); !err.ok()) {
-        THROW(err.code(), err.result());
-    }
-    return resc_name;
-} // get_resource_name
+    auto get_resource_name(plugin_context& ctx) -> std::string
+    {
+        std::string resc_name;
 
-auto get_resource_status(plugin_context& ctx) -> int
-{
-    int resc_status{}; 
-    if (error err = ctx.prop_map().get<int>(RESOURCE_STATUS, resc_status); !err.ok()) {
-        const irods::error ret = PASSMSG("Failed to get \"status\" property.", err);
-        THROW(ret.code(), ret.result());
-    }
-    return resc_status;
-} // get_resource_status
+        if (error err = ctx.prop_map().get<std::string>(RESOURCE_NAME, resc_name); !err.ok()) {
+            THROW(err.code(), err.result()); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+        }
 
-auto get_resource_location(plugin_context& ctx) -> std::string
-{
-    std::string host_name{};
-    if (error err = ctx.prop_map().get<std::string>(RESOURCE_LOCATION, host_name); !err.ok()) {
-        const irods::error ret = PASSMSG("Failed to get \"location\" property.", err);
-        THROW(ret.code(), ret.result());
-    }
-    return host_name;
-} // get_resource_location
+        return resc_name;
+    } // get_resource_name
 
+    auto get_resource_status(plugin_context& ctx) -> int
+    {
+        int resc_status; // NOLINT(cppcoreguidelines-init-variables)
+
+        if (error err = ctx.prop_map().get<int>(RESOURCE_STATUS, resc_status); !err.ok()) {
+            const irods::error ret = PASSMSG("Failed to get resource status property.", err);
+            THROW(ret.code(), ret.result()); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+        }
+
+        return resc_status;
+    } // get_resource_status
+
+    auto get_resource_location(plugin_context& ctx) -> std::string
+    {
+        std::string host_name;
+
+        if (error err = ctx.prop_map().get<std::string>(RESOURCE_LOCATION, host_name); !err.ok()) {
+            const irods::error ret = PASSMSG("Failed to get resource location property.", err);
+            THROW(ret.code(), ret.result()); // NOLINT(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+        }
+
+        return host_name;
+    } // get_resource_location
 } // namespace irods
 
