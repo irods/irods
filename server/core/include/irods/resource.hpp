06cdeea206bba305dc140cc12bb61ad65dcf4e62
Kory Draughn <korydraughn@renci.org>
2022-10-13 09:02:15 -0400

    [858] Cleaned up resource.hpp.

--- server/core/include/irods/resource.hpp
+++ server/core/include/irods/resource.hpp
@@ -1,14 +1,5 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-
-/* resource.h - header file for resource.c
- */
-
-
-
-#ifndef RESOURCE_HPP
-#define RESOURCE_HPP
-
+#ifndef IRODS_RESOURCE_HPP
+#define IRODS_RESOURCE_HPP
 
 #include "irods/rods.h"
 #include "irods/objInfo.h"
@@ -16,7 +7,6 @@
 #include "irods/ruleExecSubmit.h"
 #include "irods/rcGlobalExtern.h"
 #include "irods/rsGlobalExtern.hpp"
-//#include "irods/reIn2p3SysRule.hpp"
-//#include "irods/reSysDataObjOpr.hpp"
 
-#endif	/* RESOURCE_H */
+#endif // IRODS_RESOURCE_HPP
+
