400ce081171b13d146d1259fe284307a208e6680
Kory Draughn <korydraughn@renci.org>
2022-10-21 21:27:11 -0400

    [#6611] Added initialization function for l1desc objects.
    Calling std::memset() on a l1desc object can lead to undefined behavior
    due to the presence of a std::string member variable.

--- server/core/include/irods/objDesc.hpp
+++ server/core/include/irods/objDesc.hpp
@@ -1,10 +1,5 @@
-/*** Copyright (c), The Regents of the University of California            ***
- *** For more information please refer to files in the COPYRIGHT directory ***/
-
-/* objDesc.h - header file for objDesc.c
- */
-#ifndef OBJ_DESC_HPP
-#define OBJ_DESC_HPP
+#ifndef IRODS_OBJ_DESC_HPP
+#define IRODS_OBJ_DESC_HPP
 
 #include "irods/rods.h"
 #include "irods/objInfo.h"
@@ -28,9 +23,15 @@
 #define REG_CHKSUM      1
 #define VERIFY_CHKSUM   2
 
-/* values in l1desc_t is the desired value. values in dataObjInfo are
- * the values in rcat */
-typedef struct l1desc {
+// Values in l1desc are the desired values.
+// Values in dataObjInfo are the values in the catalog.
+//
+// If you modify this data type, you must also update init_l1desc().
+//
+// DO NOT call std::memset() on this data type. Use init_l1desc().
+// DO NOT call std::memcpy() on this data type. Use the assignment operator.
+struct l1desc
+{
     int l3descInx;
     int inuseFlag;
     int oprType;
@@ -41,77 +42,85 @@ typedef struct l1desc {
     dataObjInfo_t *dataObjInfo;
     dataObjInfo_t *otherDataObjInfo;
     int copiesNeeded;
-    rodsLong_t bytesWritten;    /* mark whether it has been written */
-    rodsLong_t dataSize;        /* this is the target size. The size in
-                                 * dataObjInfo is the registered size */
-    int replStatus;     /* the replica status */
-    int chksumFlag;     /* parsed from condition */
+    rodsLong_t bytesWritten; /* mark whether it has been written */
+    rodsLong_t dataSize; /* this is the target size. The size in dataObjInfo is the registered size */
+    int replStatus; /* the replica status */
+    int chksumFlag; /* parsed from condition */
     int srcL1descInx;
     char chksum[NAME_LEN]; /* the input chksum */
     int remoteL1descInx;
     int stageFlag;
-    int purgeCacheFlag; // JMC - backport 4537
-    int lockFd; // JMC - backport 4604
+    int purgeCacheFlag;
+    int lockFd;
     boost::any pluginData;
-    dataObjInfo_t *replDataObjInfo; /* if non NULL, repl to this dataObjInfo
-                                     * on close */
+    dataObjInfo_t* replDataObjInfo; // if not a nullptr, repl to this dataObjInfo on close.
     rodsServerHost_t *remoteZoneHost;
     char in_pdmo[MAX_NAME_LEN];
 
     std::string replica_token;
-} l1desc_t;
+};
 
-extern "C" {
+using l1desc_t = l1desc;
 
-int
-initL1Desc();
+int initL1Desc();
 
-int
-allocL1Desc();
+auto init_l1desc(l1desc& _l1d) -> void;
+
+int allocL1Desc();
 
 int freeL1desc_struct(l1desc& _l1desc);
 
 int freeL1desc(const int l1descInx);
 
-int
-fillL1desc( int l1descInx, dataObjInp_t *dataObjInp,
-            dataObjInfo_t *dataObjInfo, int replStatus, rodsLong_t dataSize );
-int
-getL1descIndexByDataObjInfo( const dataObjInfo_t * dataObjInfo );
-int
-getNumThreads( rsComm_t *rsComm, rodsLong_t dataSize, int inpNumThr,
-               keyValPair_t *condInput, char *destRescName, char *srcRescName, int oprType );
-int
-initDataOprInp( dataOprInp_t *dataOprInp, int l1descInx, int oprType );
-int
-convL3descInx( int l3descInx );
-int
-initDataObjInfoWithInp( dataObjInfo_t *dataObjInfo, dataObjInp_t *dataObjInp );
-int
-allocL1desc();
-int
-closeAllL1desc( rsComm_t *rsComm );
-int
-initSpecCollDesc();
-int
-allocSpecCollDesc();
-int
-freeSpecCollDesc( int specCollInx );
-int
-initL1desc();
-int
-allocCollHandle();
-int
-freeCollHandle( int handleInx );
-int
-rsInitQueryHandle( queryHandle_t *queryHandle, rsComm_t *rsComm );
-int
-allocAndSetL1descForZoneOpr( int l3descInx, dataObjInp_t *dataObjInp,
-                             rodsServerHost_t *remoteZoneHost, openStat_t *openStat );
-int
-isL1descInuse();
+int fillL1desc(
+    int l1descInx,
+    dataObjInp_t* dataObjInp,
+    dataObjInfo_t* dataObjInfo,
+    int replStatus,
+    rodsLong_t dataSize);
 
-}
+int getL1descIndexByDataObjInfo(const dataObjInfo_t* dataObjInfo);
+
+int getNumThreads(
+    rsComm_t* rsComm,
+    rodsLong_t dataSize,
+    int inpNumThr,
+    keyValPair_t* condInput,
+    char* destRescName,
+    char* srcRescName,
+    int oprType);
+
+int initDataOprInp(dataOprInp_t* dataOprInp, int l1descInx, int oprType);
+
+int convL3descInx(int l3descInx);
+
+int initDataObjInfoWithInp(dataObjInfo_t* dataObjInfo, dataObjInp_t* dataObjInp);
+
+int allocL1desc();
+
+int closeAllL1desc(rsComm_t* rsComm);
+
+int initSpecCollDesc();
+
+int allocSpecCollDesc();
+
+int freeSpecCollDesc(int specCollInx);
+
+int initL1desc();
+
+int allocCollHandle();
+
+int freeCollHandle(int handleInx);
+
+int rsInitQueryHandle(queryHandle_t* queryHandle, rsComm_t* rsComm);
+
+int allocAndSetL1descForZoneOpr(
+    int l3descInx,
+    dataObjInp_t* dataObjInp,
+    rodsServerHost_t* remoteZoneHost,
+    openStat_t* openStat);
+
+int isL1descInuse();
 
 namespace irods
 {
@@ -130,4 +139,4 @@ namespace irods
         const rodsLong_t _data_size) -> int;
 } // namespace irods
 
-#endif  /* OBJ_DESC_H */
+#endif // IRODS_OBJ_DESC_HPP
