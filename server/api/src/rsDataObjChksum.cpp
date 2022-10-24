a3a6654600d517050a0c9901a9715a268e3479d2
Kory Draughn <korydraughn@renci.org>
2022-10-24 19:24:04 -0400

    [#6634] Fixed memory leak in rsDataObjChksum.

--- server/api/src/rsDataObjChksum.cpp
+++ server/api/src/rsDataObjChksum.cpp
@@ -240,7 +240,7 @@ namespace
 
     int do_verification(RsComm& _comm,
                         DataObjInp& _dataObjInp,
-                        DataObjInfo* _replicas,
+                        DataObjInfo*& _replicas,
                         char** _checksum)
     {
         ix::key_value_proxy kvp{_dataObjInp.condInput};
@@ -385,7 +385,7 @@ namespace
 
     int do_lookup_or_update(RsComm& _comm,
                             DataObjInp& _dataObjInp,
-                            DataObjInfo* _replicas,
+                            DataObjInfo*& _replicas,
                             char** _computed_checksum)
     {
         ix::key_value_proxy kvp{_dataObjInp.condInput};
