a895e62435a72f4b57bf2f397c97e870ba6c205c
Kory Draughn <korydraughn@renci.org>
2022-10-24 20:30:55 -0400

    [#6634] Fixed double-free memory bug on destruction of l1desc.
    This commit also includes a new function, `copy_l1desc`, that performs a
    deep copy of an l1desc object.

--- lib/core/include/irods/rcMisc.h
+++ lib/core/include/irods/rcMisc.h
@@ -118,6 +118,8 @@ int copyKeyVal(const keyValPair_t* srcCondInput, keyValPair_t* destCondInput);
 
 int replDataObjInp(dataObjInp_t* srcDataObjInp, dataObjInp_t* destDataObjInp);
 
+int replDataObjInfo(struct DataObjInfo* _dst, struct DataObjInfo* _src);
+
 int replSpecColl(specColl_t* inSpecColl, specColl_t** outSpecColl);
 
 int addKeyVal(keyValPair_t* condInput, const char* keyWord, const char* value);
