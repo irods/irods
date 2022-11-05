6eb210b3fd6821605ee421fbd8694f1da7ebb42e
Kory Draughn <korydraughn@renci.org>
2022-11-05 14:48:13 -0400

    [#6619] Updated unit tests to handle resource administration changes.

--- unit_tests/cmake/test_config/irods_resource_administration.cmake
+++ unit_tests/cmake/test_config/irods_resource_administration.cmake
@@ -3,7 +3,9 @@ set(IRODS_TEST_TARGET irods_resource_administration)
 set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_resource_administration.cpp)
 
-set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
+set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
+                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
  
 set(IRODS_TEST_LINK_LIBRARIES irods_common
-                              irods_client)
+                              irods_client
+                              ${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so)
