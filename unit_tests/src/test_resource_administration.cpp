ed5077eecb66de432e2f857e89b0a445f409f84f
Kory Draughn <korydraughn@renci.org>
2022-11-11 17:21:27 -0500

    [#6727] Fixed usage of gethostname() and HOST_NAME_MAX.
    POSIX.1 guarantees that "Host names (not including the terminating null
    byte) are limited to HOST_NAME_MAX bytes". On Linux, HOST_NAME_MAX is
    defined with the value of 64.
    
    This ultimately means the buffers passed to gethostname() must have a
    minimum buffer size of HOST_NAME_MAX + 1.

--- unit_tests/src/test_resource_administration.cpp
+++ unit_tests/src/test_resource_administration.cpp
@@ -18,13 +18,13 @@ TEST_CASE("resource administration")
     repl_info.resource_name = "unit_test_repl";
     repl_info.resource_type = adm::resource_type::replication;
 
-    char host_name[64] {};
-    REQUIRE(gethostname(host_name, sizeof(host_name)) == 0);
+    char hostname[HOST_NAME_MAX + 1]{};
+    REQUIRE(gethostname(hostname, sizeof(hostname)) == 0);
 
     adm::resource_registration_info ufs_info;
     ufs_info.resource_name = "unit_test_ufs0";
     ufs_info.resource_type = adm::resource_type::unixfilesystem;
-    ufs_info.host_name = host_name;
+    ufs_info.host_name = hostname;
     ufs_info.vault_path = "/tmp";
 
     load_client_api_plugins();
