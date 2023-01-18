16e152ef017d1c00484171199b05b548923d65ce
Kory Draughn <korydraughn@renci.org>
2023-01-18 18:37:24 -0500

    [#6268] Made scoped_client_identity federation friendly.
    This commit allows developers to instantiate scoped_client_identity
    objects with a different username and zone.

--- plugins/microservices/src/msi_assertion_macros.hpp
+++ plugins/microservices/src/msi_assertion_macros.hpp
@@ -1,40 +1,99 @@
 #ifndef IRODS_MSI_ASSERTION_MACROS_HPP
 #define IRODS_MSI_ASSERTION_MACROS_HPP
 
+/// \file
+///
+/// This file is only for testing purposes!
+/// It MUST NOT be used outside of microservice-based unit tests.
+
+#include "irods/irods_at_scope_exit.hpp"
 #include "irods/irods_logger.hpp"
+
+#include <cstring>
 
 using log_msi_test_internal = irods::experimental::log::microservice;
 
+// clang-format off
+
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define IRODS_MSI_TEST_BEGIN(test_title) \
-  try {                                  \
+#define IRODS_MSI_TEST_CASE(func) \
+  if (func()) {                   \
+      return -1;                  \
+  }
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define IRODS_MSI_TEST_BEGIN(test_title)                              \
+  int msi_test_error_code = 0;                                        \
+                                                                      \
+  irods::at_scope_exit print_pass_or_failed{[&msi_test_error_code] {  \
+    if (msi_test_error_code) {                                        \
+      log_msi_test_internal::info("--- TEST FAILED ---");             \
+    }                                                                 \
+    else {                                                            \
+      log_msi_test_internal::info("--- TEST PASSED ---");             \
+    }                                                                 \
+  }};                                                                 \
+                                                                      \
+  try {                                                               \
     log_msi_test_internal::info(">>> TEST BEGIN: [" #test_title "]");
 
-// clang-format off
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define IRODS_MSI_TEST_END                                                       \
-    log_msi_test_internal::info("--- TEST PASSED ---");                          \
-    return 0;                                                                    \
-  }                                                                              \
-  catch (const std::exception& e) {                                              \
-    log_msi_test_internal::error(e.what());                                      \
-  }                                                                              \
-  catch (...) {                                                                  \
-    log_msi_test_internal::error("Caught an unknown exception during testing."); \
-  }                                                                              \
-                                                                                 \
-  log_msi_test_internal::error("--- TEST FAILED ---");                           \
-  return 1;
-// clang-format on
+#define IRODS_MSI_TEST_END                                                      \
+    return msi_test_error_code;                                                 \
+  }                                                                             \
+  catch (const std::exception& e) {                                             \
+    log_msi_test_internal::info(e.what());                                      \
+    return msi_test_error_code = -1;                                            \
+  }                                                                             \
+  catch (...) {                                                                 \
+    log_msi_test_internal::info("Caught an unknown exception during testing."); \
+    return msi_test_error_code = -1;                                            \
+  }                                                                             \
 
 // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
-#define IRODS_MSI_ASSERT(x)                                                            \
-  if (x) {                                                                             \
-    log_msi_test_internal::info("ASSERTION PASSED [{}:{}]: " #x, __FILE__, __LINE__);  \
-  }                                                                                    \
-  else {                                                                               \
-    log_msi_test_internal::error("ASSERTION FAILED [{}:{}]: " #x, __FILE__, __LINE__); \
-    return 1;                                                                          \
+#define IRODS_MSI_ASSERT(expr)                                                              \
+  if (expr) {                                                                               \
+    log_msi_test_internal::info("ASSERTION PASSED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+  }                                                                                         \
+  else {                                                                                    \
+    log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+    return msi_test_error_code = -1;                                                        \
   }
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define IRODS_MSI_THROWS(expr, ex_type)                                                     \
+  try {                                                                                     \
+    expr;                                                                                   \
+    log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+    return msi_test_error_code = -1;                                                        \
+  }                                                                                         \
+  catch (const ex_type&) {                                                                  \
+    log_msi_test_internal::info("ASSERTION PASSED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+  }                                                                                         \
+  catch (...) {                                                                             \
+    log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+    return msi_test_error_code = -1;                                                        \
+  }
+
+// NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
+#define IRODS_MSI_THROWS_MSG(expr, ex_type, ex_msg)                                           \
+  try {                                                                                       \
+    expr;                                                                                     \
+    log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr);   \
+    return msi_test_error_code = -1;                                                          \
+  }                                                                                           \
+  catch (const ex_type& e) {                                                                  \
+    if (!std::strstr(e.what(), ex_msg)) {                                                     \
+      log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr); \
+      return msi_test_error_code = -1;                                                        \
+    }                                                                                         \
+    log_msi_test_internal::info("ASSERTION PASSED [{}:{}]: {}", __FILE__, __LINE__, #expr);   \
+  }                                                                                           \
+  catch (...) {                                                                               \
+    log_msi_test_internal::info("ASSERTION FAILED [{}:{}]: {}", __FILE__, __LINE__, #expr);   \
+    return msi_test_error_code = -1;                                                          \
+  }
+
+// clang-format on
 
 #endif // IRODS_MSI_ASSERTION_MACROS_HPP
---
+++ plugins/microservices/src/test_scoped_client_identity.cpp
@@ -0,0 +1,147 @@
+/// \file
+
+#include "irods/getRodsEnv.h"
+#include "irods/irods_exception.hpp"
+#include "irods/irods_ms_plugin.hpp"
+#include "irods/irods_re_structs.hpp"
+#include "irods/msParam.h"
+#include "irods/scoped_client_identity.hpp"
+
+#include "msi_assertion_macros.hpp"
+
+#include <exception>
+#include <string>
+
+namespace
+{
+    auto test_basic_usage() -> int
+    {
+        IRODS_MSI_TEST_BEGIN("scoped_client_identity - basic usage")
+
+        const std::string username = "rods";
+        const std::string zone = "tempZone";
+
+        RsComm comm{};
+        std::strcpy(comm.clientUser.userName, username.c_str());
+        std::strcpy(comm.clientUser.rodsZone, zone.c_str());
+
+        // Show that the RsComm represents rods#tempZone.
+        IRODS_MSI_ASSERT(comm.clientUser.userName == username)
+        IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+
+        {
+            const std::string new_username = "otherrods";
+            irods::experimental::scoped_client_identity sci{comm, new_username};
+
+            // Show that the RsComm represents otherrods#tempZone.
+            IRODS_MSI_ASSERT(comm.clientUser.userName == new_username)
+            IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+        }
+
+        // Show that the original identity associated with the RsComm has been restored
+        // (i.e. rods#tempZone).
+        IRODS_MSI_ASSERT(comm.clientUser.userName == username)
+        IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+
+        IRODS_MSI_TEST_END
+    } // test_basic_usage
+
+    auto test_updates_zone() -> int
+    {
+        IRODS_MSI_TEST_BEGIN("#6268: scoped_client_identity updates the zone")
+
+        rodsEnv env;
+        _getRodsEnv(env);
+
+        const std::string username = "rods";
+        const std::string zone = env.rodsZone;
+
+        RsComm comm{};
+        std::strcpy(comm.clientUser.userName, username.c_str());
+
+        // Show that using the two-argument constructor results in the user
+        // being identified as a member of the local zone.
+        {
+            const std::string new_username = "alice";
+            irods::experimental::scoped_client_identity sci{comm, new_username};
+            IRODS_MSI_ASSERT(comm.clientUser.userName == new_username)
+            IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+        }
+
+        // Show that the RsComm was restored to rods#<empty_string>.
+        IRODS_MSI_ASSERT(comm.clientUser.userName == username)
+        IRODS_MSI_ASSERT(std::strlen(comm.clientUser.rodsZone) == 0)
+
+        std::strcpy(comm.clientUser.rodsZone, zone.c_str());
+
+        // Show that the RsComm represents rods#tempZone.
+        IRODS_MSI_ASSERT(comm.clientUser.userName == username)
+        IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+
+        // Show that scoped_client_identity updates both the username and zone.
+        {
+            const std::string new_username = "otherrods";
+            const std::string new_zone = "otherZone";
+            irods::experimental::scoped_client_identity sci{comm, new_username, new_zone};
+            IRODS_MSI_ASSERT(comm.clientUser.userName == new_username)
+            IRODS_MSI_ASSERT(comm.clientUser.rodsZone == new_zone)
+        }
+
+        // Show that the identity associated with the RsComm has been restored.
+        IRODS_MSI_ASSERT(comm.clientUser.userName == username)
+        IRODS_MSI_ASSERT(comm.clientUser.rodsZone == zone)
+
+        IRODS_MSI_TEST_END
+    } // test_updates_zone
+
+    auto test_invalid_input() -> int
+    {
+        IRODS_MSI_TEST_BEGIN("scoped_client_identity throws on invalid input")
+
+        const std::string username = "rods";
+        const std::string zone = "tempZone";
+
+        RsComm comm{};
+        std::strcpy(comm.clientUser.userName, username.c_str());
+        std::strcpy(comm.clientUser.rodsZone, zone.c_str());
+
+        IRODS_MSI_THROWS_MSG((irods::experimental::scoped_client_identity{comm, ""}),
+                             irods::exception,
+                             "scoped_client_identity: username is empty.")
+
+        const auto* too_long = "64_bytes_long___________________________________________________";
+
+        IRODS_MSI_THROWS_MSG((irods::experimental::scoped_client_identity{comm, too_long}),
+                             irods::exception,
+                             "scoped_client_identity: username is too long.")
+
+        IRODS_MSI_THROWS_MSG((irods::experimental::scoped_client_identity{comm, "otherrods", too_long}),
+                             irods::exception,
+                             "scoped_client_identity: zone is too long.")
+
+        IRODS_MSI_TEST_END
+    } // test_updates_zone
+
+    auto msi_impl([[maybe_unused]] RuleExecInfo* _rei) -> int
+    {
+        IRODS_MSI_TEST_CASE(test_basic_usage)
+        IRODS_MSI_TEST_CASE(test_updates_zone)
+        IRODS_MSI_TEST_CASE(test_invalid_input)
+
+        return 0;
+    } // msi_impl
+
+    template <typename... Args, typename Function>
+    auto make_msi(const std::string& _name, Function _func) -> irods::ms_table_entry*
+    {
+        // NOLINTNEXTLINE(cppcoreguidelines-owning-memory)
+        auto* msi = new irods::ms_table_entry{sizeof...(Args)};
+        msi->add_operation(_name, std::function<int(Args..., ruleExecInfo_t*)>(_func));
+        return msi;
+    } // make_msi
+} // anonymous namespace
+
+extern "C" auto plugin_factory() -> irods::ms_table_entry*
+{
+    return make_msi<>("msi_test_scoped_client_identity", msi_impl);
+} // plugin_factory
