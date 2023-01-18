3c1043678fe7a1385f16eebd7726556dc172257b
Kory Draughn <korydraughn@renci.org>
2023-01-18 18:37:24 -0500

    [#6268] Made scoped_client_identity federation friendly.
    This commit allows developers to instantiate scoped_client_identity
    objects with a different username and zone.

--- server/core/include/irods/scoped_client_identity.hpp
+++ server/core/include/irods/scoped_client_identity.hpp
@@ -1,12 +1,13 @@
 #ifndef IRODS_SCOPED_CLIENT_IDENTITY_HPP
 #define IRODS_SCOPED_CLIENT_IDENTITY_HPP
 
+#include "irods/irods_exception.hpp"
 #include "irods/rcConnect.h"
+#include "irods/rodsConnect.h"
+#include "irods/rodsErrorTable.h"
 
 #include <cstring>
 #include <string>
-#include <string_view>
-#include <stdexcept>
 
 namespace irods::experimental
 {
@@ -16,42 +17,77 @@ namespace irods::experimental
     /// Instances of this class are not copyable or moveable.
     ///
     /// \since 4.2.8
-    class scoped_client_identity
+    class scoped_client_identity // NOLINT(cppcoreguidelines-special-member-functions)
     {
-    public:
-        /// Constructs a scoped_client_identity object and sets the username of
-        /// rsComm_t::clientUser to new_username.
+      public:
+        /// Constructs a scoped_client_identity object and updates the RsComm to represent
+        /// a user in the local zone.
         ///
-        /// \param[in] comm         The server communication object.
-        /// \param[in] new_username The name of the iRODS user to become.
+        /// The zone will match the string returned by getLocalZoneName().
         ///
-        /// \exception std::length_error Thrown if new_username exceeds the buffer size
-        ///                              of rsComm_t::clientUser::userName.
-        scoped_client_identity(rsComm_t& comm, std::string_view new_username)
-            : conn_{comm}
-            , username_{conn_.clientUser.userName}
+        /// \param[in] _comm         The server communication object.
+        /// \param[in] _new_username The name of the user to become.
+        ///
+        /// \throws irods::exception If an error occurs.
+        scoped_client_identity(RsComm& _comm, const std::string& _new_username)
+            : scoped_client_identity{_comm, _new_username, ""}
         {
-            if (new_username.size() >= sizeof(conn_.clientUser.userName)) {
-                throw std::length_error{"scoped_client_identity: new_username exceeds buffer size"};
+        }
+
+        /// Constructs a scoped_client_identity object and updates the RsComm to represent
+        /// a user in a zone.
+        ///
+        /// \param[in] _comm         The server communication object.
+        /// \param[in] _new_username The name of the user to become.
+        /// \param[in] _new_zone     The name of the zone the user is a member of.
+        ///                          If _new_zone is empty, it defaults to the value returned
+        ///                          by getLocalZoneName().
+        ///
+        /// \throws irods::exception If an error occurs.
+        ///
+        /// \since 4.2.12
+        scoped_client_identity(RsComm& _comm, const std::string& _new_username, const std::string& _new_zone)
+            : comm_{_comm}
+            , old_username_{comm_.clientUser.userName}
+            , old_zone_{comm_.clientUser.rodsZone}
+        {
+            if (_new_username.empty()) {
+                THROW(SYS_INVALID_INPUT_PARAM, "scoped_client_identity: username is empty.");
             }
 
-            std::strcpy(conn_.clientUser.userName, new_username.data());
+            if (_new_username.size() >= sizeof(UserInfo::userName)) {
+                THROW(SYS_INVALID_INPUT_PARAM, "scoped_client_identity: username is too long.");
+            }
+
+            if (_new_zone.size() >= sizeof(UserInfo::rodsZone)) {
+                THROW(SYS_INVALID_INPUT_PARAM, "scoped_client_identity: zone is too long.");
+            }
+
+            const char* new_zone = _new_zone.c_str();
+
+            if (_new_zone.empty()) {
+                new_zone = getLocalZoneName();
+            }
+
+            std::strcpy(comm_.clientUser.userName, _new_username.c_str());
+            std::strcpy(comm_.clientUser.rodsZone, new_zone);
         }
 
         scoped_client_identity(const scoped_client_identity&) = delete;
         auto operator=(const scoped_client_identity&) -> scoped_client_identity& = delete;
 
-        /// Restores the username of the rsComm_t::clientUser to it's original value.
+        /// Restores the identity of the RsComm::clientUser to its original value.
         ~scoped_client_identity()
         {
-            std::strcpy(conn_.clientUser.userName, username_.data());
+            std::strcpy(comm_.clientUser.userName, old_username_.c_str());
+            std::strcpy(comm_.clientUser.rodsZone, old_zone_.c_str());
         }
 
-    private:
-        rsComm_t& conn_;
-        const std::string username_;
+      private:
+        RsComm& comm_;
+        std::string old_username_;
+        std::string old_zone_;
     }; // class scoped_client_identity
 } // namespace irods::experimental
 
 #endif // IRODS_SCOPED_CLIENT_IDENTITY_HPP
-
--- unit_tests/cmake/test_config/irods_scoped_client_identity.cmake
+++
@@ -1,6 +0,0 @@
-set(IRODS_TEST_TARGET irods_scoped_client_identity)
-
-set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
-                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_scoped_client_identity.cpp)
- 
-set(IRODS_TEST_LINK_LIBRARIES irods_server)
