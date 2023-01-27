5f7a36d7e2de17dfbbe18c3973a61bd492a0c9b0
Alan King <alanking@renci.org>
2023-01-27 13:49:38 -0500

    [#6882] Add details to key_value_proxy error messages
    This uses libfmt to add details to the error messages in
    key_value_proxy. Requires linking an additional dependency in the
    key_value_proxy unit tests.

--- unit_tests/cmake/test_config/irods_key_value_proxy.cmake
+++ unit_tests/cmake/test_config/irods_key_value_proxy.cmake
@@ -4,5 +4,8 @@ set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_irods_key_value_proxy.cpp)
 
 set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
- 
-set(IRODS_TEST_LINK_LIBRARIES irods_common)
+
+set(IRODS_TEST_LINK_LIBRARIES
+    irods_common
+    "${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so"
+    )
