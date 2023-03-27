45875e89784870055dab6285548b084a51d4a69f
Kory Draughn <korydraughn@renci.org>
2023-03-27 15:14:20 -0400

    [#6136] Fixed iexit test.

--- scripts/irods/test/test_iexit.py
+++ scripts/irods/test/test_iexit.py
@@ -13,6 +13,11 @@ class Test_iexit(unittest.TestCase):
         with session.make_session_for_existing_admin() as admin_session:
             admin_session.assert_icommand("iexit")
 
+    def test_iexit_full_does_not_delete_auth_file_if_user_is_the_service_account_user__issue_6136(self):
+        with session.make_session_for_existing_admin() as admin_session:
+            expected_output = 'WARNING: iexit appears to be running as service account. Skipping auth file deletion (pass -f to force).'
+            admin_session.assert_icommand("iexit full", 'STDOUT', expected_output)
+
     def test_iexit_verbose(self):
         with session.make_session_for_existing_admin() as admin_session:
             admin_session.assert_icommand("iexit -v", 'STDOUT_SINGLELINE', "Deleting (if it exists) session envFile:")
