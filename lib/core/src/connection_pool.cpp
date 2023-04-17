d3d355a31675c9d76259b7de8022cb70f19a07fe
Kory Draughn <korydraughn@renci.org>
2023-04-17 18:27:54 -0400

    [#5060,#5799] irods::connection_pool supports proxy users and overriding default authentication logic.

--- lib/core/src/connection_pool.cpp
+++ lib/core/src/connection_pool.cpp
@@ -5,17 +5,22 @@
 
 #include <stdexcept>
 #include <thread>
+#include <tuple> // For std::ignore.
 
 namespace irods
 {
+    //
+    // Connection Proxy Implementation
+    //
+
     connection_pool::connection_proxy::connection_proxy()
         : pool_{}
         , conn_{}
         , index_{uninitialized_index}
     {
-    }
+    } // default constructor
 
-    connection_pool::connection_proxy::connection_proxy(connection_proxy&& _other)
+    connection_pool::connection_proxy::connection_proxy(connection_proxy&& _other) noexcept
         : pool_{_other.pool_}
         , conn_{_other.conn_}
         , index_{_other.index_}
@@ -23,9 +28,9 @@ namespace irods
         _other.pool_ = nullptr;
         _other.conn_ = nullptr;
         _other.index_ = uninitialized_index;
-    }
+    } // move constructor
 
-    connection_pool::connection_proxy& connection_pool::connection_proxy::operator=(connection_proxy&& _other)
+    connection_pool::connection_proxy& connection_pool::connection_proxy::operator=(connection_proxy&& _other) noexcept
     {
         pool_ = _other.pool_;
         conn_ = _other.conn_;
@@ -36,74 +41,109 @@ namespace irods
         _other.index_ = uninitialized_index;
 
         return *this;
-    }
+    } // operator=
 
     connection_pool::connection_proxy::~connection_proxy()
     {
+        // NOLINTNEXTLINE(readability-implicit-bool-conversion)
         if (pool_ && uninitialized_index != index_) {
             pool_->return_connection(index_);
         }
-    }
+    } // destructor
 
     connection_pool::connection_proxy::operator bool() const noexcept
     {
         return nullptr != conn_;
-    }
+    } // operator bool
 
-    connection_pool::connection_proxy::operator rcComm_t&() const
+    connection_pool::connection_proxy::operator RcComm&() const
     {
-        if (!conn_) {
+        if (!conn_) { // NOLINT(readability-implicit-bool-conversion)
             throw std::runtime_error{"Invalid connection object"};
         }
 
         return *conn_;
-    }
+    } // operator RcComm&
 
-    connection_pool::connection_proxy::operator rcComm_t*() const noexcept
+    connection_pool::connection_proxy::operator RcComm*() const noexcept
     {
         return conn_;
-    }
+    } // operator RcComm*
 
-    rcComm_t* connection_pool::connection_proxy::release()
+    RcComm* connection_pool::connection_proxy::release()
     {
         pool_->release_connection(index_);
         auto* conn = conn_;
         conn_ = nullptr;
         return conn;
-    }
+    } // release
 
-    connection_pool::connection_proxy::connection_proxy(connection_pool& _pool,
-                                                        rcComm_t& _conn,
-                                                        int _index) noexcept
+    connection_pool::connection_proxy::connection_proxy(connection_pool& _pool, RcComm& _conn, int _index) noexcept
         : pool_{&_pool}
         , conn_{&_conn}
         , index_{_index}
     {
-    }
+    } // constructor
+
+    //
+    // Connection Pool Implementation
+    //
 
     connection_pool::connection_pool(int _size,
                                      const std::string& _host,
                                      const int _port,
-                                     const std::string& _username,
+                                     const std::string& _name,
                                      const std::string& _zone,
                                      const int _refresh_time)
+        : connection_pool{_size, _host, _port, std::nullopt, {_name, _zone}, _refresh_time, {}}
+    {
+    } // constructor
+
+    connection_pool::connection_pool(int _size,
+                                     const std::string& _host,
+                                     const int _port,
+                                     experimental::fully_qualified_username _username,
+                                     const int _refresh_time)
+        : connection_pool{_size, _host, _port, std::nullopt, std::move(_username), _refresh_time, {}}
+    {
+    } // constructor
+
+    connection_pool::connection_pool(int _size,
+                                     const std::string& _host,
+                                     const int _port,
+                                     experimental::fully_qualified_username _username,
+                                     const int _refresh_time,
+                                     std::function<void(RcComm&)> _auth_func)
+        : connection_pool{_size, _host, _port, std::nullopt, std::move(_username), _refresh_time, std::move(_auth_func)}
+    {
+    } // constructor
+
+    connection_pool::connection_pool(int _size,
+                                     std::string_view _host,
+                                     const int _port,
+                                     std::optional<experimental::fully_qualified_username> _proxy_username,
+                                     experimental::fully_qualified_username _username,
+                                     const int _refresh_time,
+                                     std::function<void(RcComm&)> _auth_func)
         : host_{_host}
         , port_{_port}
-        , username_{_username}
-        , zone_{_zone}
+        , proxy_username_{std::move(_proxy_username)}
+        , username_{std::move(_username)}
         , refresh_time_(_refresh_time)
+        , auth_func_{std::move(_auth_func)}
         , conn_ctxs_(_size)
     {
         if (_size < 1) {
-            throw std::runtime_error{"invalid connection pool size"};
+            throw std::runtime_error{"Invalid connection pool size"}; // TODO These should be irods::exceptions.
         }
 
         // Always initialize the first connection to guarantee that the
         // network plugin is loaded. This guarantees that asynchronous calls
         // to rcConnect do not cause a segfault.
-        create_connection(0,
-                          [] { throw std::runtime_error{"connect error"}; },
-                          [] { throw std::runtime_error{"client login error"}; });
+        create_connection(
+            0,
+            [] { throw std::runtime_error{"Connect error"}; },
+            [] { throw std::runtime_error{"Client login error"}; });
 
         // If the size of the pool is one, then return immediately.
         if (_size == 1) {
@@ -117,51 +157,68 @@ namespace irods
         std::atomic<bool> connect_error{};
         std::atomic<bool> login_error{};
 
-        for (int i = 1; i < _size; ++i) {
-            irods::thread_pool::post(thread_pool, [this, i, &connect_error, &login_error] {
-                if (connect_error.load() || login_error.load()) {
-                    return;
-                }
+        const auto on_connect_error = [&connect_error] { connect_error.store(true); };
+        const auto on_login_error = [&login_error] { login_error.store(true); };
 
-                create_connection(i,
-                                  [&connect_error] { connect_error.store(true); },
-                                  [&login_error] { login_error.store(true); });
-            });
+        for (int i = 1; i < _size; ++i) {
+            irods::thread_pool::post(
+                thread_pool, [this, i, &connect_error, &login_error, &on_connect_error, &on_login_error] {
+                    if (connect_error.load() || login_error.load()) {
+                        return;
+                    }
+
+                    create_connection(i, on_connect_error, on_login_error);
+                });
         }
 
         thread_pool.join();
 
         if (connect_error.load()) {
-            throw std::runtime_error{"connect error"};
+            throw std::runtime_error{"Connect error"};
         }
 
         if (login_error.load()) {
-            throw std::runtime_error{"client login error"};
+            throw std::runtime_error{"Client login error"};
         }
-    }
+    } // constructor
 
     void connection_pool::create_connection(int _index,
-                                            std::function<void()> _on_connect_error,
-                                            std::function<void()> _on_login_error)
+                                            const std::function<void()>& _on_connect_error,
+                                            const std::function<void()>& _on_login_error)
     {
         auto& ctx = conn_ctxs_[_index];
         ctx.creation_time = std::time(nullptr);
-        ctx.conn.reset(rcConnect(host_.c_str(),
-                                 port_,
-                                 username_.c_str(),
-                                 zone_.c_str(),
-                                 NO_RECONN,
-                                 &ctx.error));
+
+        if (proxy_username_.has_value()) {
+            ctx.conn.reset(_rcConnect(host_.c_str(),
+                                      port_,
+                                      proxy_username_->name().c_str(),
+                                      proxy_username_->zone().c_str(),
+                                      username_.name().c_str(),
+                                      username_.zone().c_str(),
+                                      &ctx.error,
+                                      1,
+                                      NO_RECONN));
+        }
+        else {
+            ctx.conn.reset(rcConnect(
+                host_.c_str(), port_, username_.name().c_str(), username_.zone().c_str(), NO_RECONN, &ctx.error));
+        }
 
         if (!ctx.conn) {
             _on_connect_error();
             return;
         }
 
+        if (auth_func_) {
+            auth_func_(*ctx.conn);
+            return;
+        }
+
         if (clientLogin(ctx.conn.get()) != 0) {
             _on_login_error();
         }
-    }
+    } // create_connection
 
     bool connection_pool::verify_connection(int _index)
     {
@@ -172,7 +229,8 @@ namespace irods
         }
 
         try {
-            query<rcComm_t>{ctx.conn.get(), "select ZONE_NAME where ZONE_TYPE = 'local'"};
+            // NOLINTNEXTLINE(bugprone-unused-raii)
+            query<RcComm>{ctx.conn.get(), "select ZONE_NAME where ZONE_TYPE = 'local'"};
             if (std::time(nullptr) - ctx.creation_time > refresh_time_) {
                 return false;
             }
@@ -182,30 +240,30 @@ namespace irods
         }
 
         return true;
-    }
+    } // verify_connection
 
-    rcComm_t* connection_pool::refresh_connection(int _index)
+    RcComm* connection_pool::refresh_connection(int _index)
     {
         auto& ctx = conn_ctxs_[_index];
         ctx.error = {};
 
         if (ctx.refresh) {
             ctx.refresh = false;
-            ctx.conn.release();
         }
 
         if (!verify_connection(_index)) {
-            create_connection(_index,
-                              [] { throw std::runtime_error{"connect error"}; },
-                              [] { throw std::runtime_error{"client login error"}; });
+            create_connection(
+                _index,
+                [] { throw std::runtime_error{"Connect error"}; },
+                [] { throw std::runtime_error{"Client login error"}; });
         }
 
         return ctx.conn.get();
-    }
+    } // refresh_connection
 
     connection_pool::connection_proxy connection_pool::get_connection()
     {
-        for (int i = 0;; i = ++i % conn_ctxs_.size()) {
+        for (int i = 0;; i = (i + 1) % static_cast<int>(conn_ctxs_.size())) {
             std::unique_lock<std::mutex> lock{conn_ctxs_[i].mutex, std::defer_lock};
 
             if (lock.try_lock()) {
@@ -215,30 +273,24 @@ namespace irods
                 }
             }
         }
-    }
+    } // get_connection
 
     void connection_pool::return_connection(int _index)
     {
         conn_ctxs_[_index].in_use.store(false);
-    }
+    } // return_connection
 
     void connection_pool::release_connection(int _index)
     {
         conn_ctxs_[_index].refresh = true;
-        conn_ctxs_[_index].conn.release();
-    }
+        std::ignore = conn_ctxs_[_index].conn.release();
+    } // release_connection
 
-    std::shared_ptr<connection_pool> make_connection_pool(int size)
+    std::shared_ptr<connection_pool> make_connection_pool(int _size)
     {
         rodsEnv env{};
         _getRodsEnv(env);
         return std::make_shared<irods::connection_pool>(
-            size,
-            env.rodsHost,
-            env.rodsPort,
-            env.rodsUserName,
-            env.rodsZone,
-            env.irodsConnectionPoolRefreshTime);
-    }
+            _size, env.rodsHost, env.rodsPort, env.rodsUserName, env.rodsZone, env.irodsConnectionPoolRefreshTime);
+    } // make_connection_pool
 } // namespace irods
-
--- unit_tests/src/test_connection_pool.cpp
+++ unit_tests/src/test_connection_pool.cpp
@@ -1,17 +1,27 @@
 #include <catch2/catch.hpp>
 
+#include "irods/client_connection.hpp"
 #include "irods/connection_pool.hpp"
 #include "irods/filesystem.hpp"
+#include "irods/fully_qualified_username.hpp"
 #include "irods/getRodsEnv.h"
 #include "irods/irods_at_scope_exit.hpp"
+#include "irods/irods_exception.hpp"
+#include "irods/rcConnect.h"
 #include "irods/rodsClient.h"
+#include "irods/rodsErrorTable.h"
+#include "irods/user_administration.hpp"
 
+// NOLINTNEXTLINE(readability-function-cognitive-complexity)
 TEST_CASE("connection pool")
 {
     rodsEnv env;
     _getRodsEnv(env);
 
     load_client_api_plugins();
+
+    constexpr int cp_size = 1;
+    constexpr int cp_refresh_time = 600;
 
     SECTION("connections are detachable")
     {
@@ -21,15 +31,15 @@ TEST_CASE("connection pool")
             REQUIRE(rcDisconnect(released_conn_ptr) == 0);
         }};
 
-        const int cp_size = 1;
-        const int cp_refresh_time = 600;
-
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
         irods::connection_pool conn_pool{cp_size,
                                          env.rodsHost,
                                          env.rodsPort,
                                          env.rodsUserName,
                                          env.rodsZone,
                                          cp_refresh_time};
+#pragma clang diagnostic pop
 
         namespace fs = irods::experimental::filesystem;
 
@@ -61,7 +71,46 @@ TEST_CASE("connection pool")
         REQUIRE(released_conn_ptr != static_cast<rcComm_t*>(conn));
     }
 
-    SECTION("connection_proxies are default constructible")
+    SECTION("connections are detachable (updated interface)")
+    {
+        rcComm_t* released_conn_ptr = nullptr;
+
+        irods::at_scope_exit at_scope_exit{[&released_conn_ptr] { REQUIRE(rcDisconnect(released_conn_ptr) == 0); }};
+
+        irods::experimental::fully_qualified_username user{env.rodsUserName, env.rodsZone};
+        irods::connection_pool conn_pool{cp_size, env.rodsHost, env.rodsPort, user, cp_refresh_time};
+
+        namespace fs = irods::experimental::filesystem;
+
+        {
+            auto conn = conn_pool.get_connection();
+            REQUIRE(static_cast<rcComm_t*>(conn) != nullptr);
+            REQUIRE(fs::client::exists(conn, env.rodsHome));
+
+            // Show that the connection is no longer being managed by the pool.
+            released_conn_ptr = conn.release();
+            REQUIRE(released_conn_ptr != nullptr);
+            REQUIRE_FALSE(conn);
+            REQUIRE(static_cast<rcComm_t*>(conn) == nullptr);
+            REQUIRE_THROWS((void) static_cast<rcComm_t&>(conn), "Invalid connection object");
+        } //namespace irods::experimental::filesystem;
+
+        // Show that the released connection is no longer managed by the
+        // connection pool, but is still usable.
+        REQUIRE(fs::client::exists(*released_conn_ptr, env.rodsHome));
+
+        // Given that the connection pool contained only one connection.
+        // Show that requesting a connection will cause the connection pool
+        // to construct a new connection in place of the released connection.
+        auto conn = conn_pool.get_connection();
+        REQUIRE(static_cast<rcComm_t*>(conn) != nullptr);
+
+        // Show that the released connection and the connection recently
+        // created by the connection pool are indeed different connections.
+        REQUIRE(released_conn_ptr != static_cast<rcComm_t*>(conn));
+    }
+
+    SECTION("connection_proxy is default constructible")
     {
         // This will not compile if the connection proxy class is not
         // default constructible. This allows connection proxies to be
@@ -107,5 +156,109 @@ TEST_CASE("connection pool")
 
         REQUIRE(released_conn_ptr);
     }
-}
 
+    SECTION("connection pools allow the default authentication method to be overridden")
+    {
+        REQUIRE_NOTHROW([&env] {
+            const auto auth_func = [](auto& _comm) {
+                if (clientLogin(&_comm) != 0) {
+                    THROW(SYS_SOCK_CONNECT_ERR, ".irodsA authentication error");
+                }
+            };
+
+            irods::experimental::fully_qualified_username user{env.rodsUserName, env.rodsZone};
+            irods::connection_pool conn_pool{cp_size, env.rodsHost, env.rodsPort, user, cp_refresh_time, auth_func};
+        }());
+    }
+
+    SECTION("connection pools support proxied connections")
+    {
+        irods::experimental::client_connection admin_conn;
+
+        namespace ia = irods::experimental::administration;
+
+        const ia::user proxy_admin{"proxy_admin"};
+        REQUIRE_NOTHROW(ia::client::add_user(admin_conn, proxy_admin, ia::user_type::rodsadmin));
+
+        irods::at_scope_exit remove_proxy_admin{
+            [&admin_conn, proxy_admin] { ia::client::remove_user(admin_conn, proxy_admin); }};
+
+        auto password = std::to_array("ppass");
+        const ia::user_password_property pwd_property{password.data()};
+        REQUIRE_NOTHROW(ia::client::modify_user(admin_conn, proxy_admin, pwd_property));
+
+        const auto auth_func = [&password](auto& _comm) {
+            if (clientLoginWithPassword(&_comm, password.data()) != 0) {
+                THROW(SYS_SOCK_CONNECT_ERR, "Password authentication error");
+            }
+        };
+
+        irods::experimental::fully_qualified_username proxy_user{proxy_admin.name, env.rodsZone};
+        irods::experimental::fully_qualified_username user{env.rodsUserName, env.rodsZone};
+
+        irods::connection_pool conn_pool{
+            cp_size, env.rodsHost, env.rodsPort, proxy_user, user, cp_refresh_time, auth_func};
+
+        auto conn = conn_pool.get_connection();
+
+        // Show that the proxied connection works by accessing the home collection of
+        // the proxied user.
+        namespace fs = irods::experimental::filesystem;
+        REQUIRE(fs::client::exists(conn, fmt::format("/{}/home/{}", env.rodsZone, env.rodsUserName)));
+    }
+
+    SECTION("connection pool throws exception on incorrect alternative authentication method")
+    {
+        const auto auth_func = [](auto& _comm) {
+            // This will cause the connection pool to throw an exception on construction.
+            if (clientLoginWithPassword(&_comm, "nope") != 0) {
+                THROW(SYS_SOCK_CONNECT_ERR, "Password authentication error");
+            }
+        };
+
+        try {
+            irods::experimental::fully_qualified_username user{env.rodsUserName, env.rodsZone};
+            irods::connection_pool conn_pool{cp_size, env.rodsHost, env.rodsPort, user, cp_refresh_time, auth_func};
+        }
+        catch (const irods::exception& e) {
+            REQUIRE(e.code() == SYS_SOCK_CONNECT_ERR);
+        }
+    }
+
+    SECTION("connection pool with proxy throws exception on incorrect alternative authentication method")
+    {
+        irods::experimental::client_connection admin_conn;
+
+        namespace ia = irods::experimental::administration;
+
+        const ia::user proxy_admin{"proxy_admin"};
+        REQUIRE_NOTHROW(ia::client::add_user(admin_conn, proxy_admin, ia::user_type::rodsadmin));
+
+        irods::at_scope_exit remove_proxy_admin{
+            [&admin_conn, proxy_admin] { ia::client::remove_user(admin_conn, proxy_admin); }};
+
+        // The property is mutable so that we can use it for clientLoginWithPassword()
+        // later in the test.
+        auto password = std::to_array("ppass");
+        const ia::user_password_property pwd_property{password.data()};
+        REQUIRE_NOTHROW(ia::client::modify_user(admin_conn, proxy_admin, pwd_property));
+
+        const auto auth_func = [](auto& _comm) {
+            // This will cause the connection pool to throw an exception on construction.
+            if (clientLoginWithPassword(&_comm, "nope") != 0) {
+                THROW(SYS_SOCK_CONNECT_ERR, "Password authentication error");
+            }
+        };
+
+        try {
+            irods::experimental::fully_qualified_username proxy_user{proxy_admin.name, env.rodsZone};
+            irods::experimental::fully_qualified_username user{env.rodsUserName, env.rodsZone};
+
+            irods::connection_pool conn_pool{
+                cp_size, env.rodsHost, env.rodsPort, proxy_user, user, cp_refresh_time, auth_func};
+        }
+        catch (const irods::exception& e) {
+            REQUIRE(e.code() == SYS_SOCK_CONNECT_ERR);
+        }
+    }
+}
