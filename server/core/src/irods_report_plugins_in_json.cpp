46292b028d9da2725fa98370cefe720c61c6bec7
Nishant Dash <nishant.dash@gmail.com>
2023-05-19 13:12:31 -0400

    [#3739] Added comments and info fields for resources. Additionally added tests to ensure fields appear. Updated the schema to include comments and info fields.

--- server/core/src/irods_report_plugins_in_json.cpp
+++ server/core/src/irods_report_plugins_in_json.cpp
@@ -167,6 +167,19 @@ namespace irods {
             return PASS(ret);
         }
 
+        std::string property_value;
+        ret = _resc->get_property<std::string>(irods::RESOURCE_COMMENTS, property_value);
+        if (!ret.ok()) {
+            return PASS(ret);
+        }
+        _entry["comments"] = property_value;
+
+        ret = _resc->get_property<std::string>(irods::RESOURCE_INFO, property_value);
+        if (!ret.ok()) {
+            return PASS(ret);
+        }
+        _entry["info"] = property_value;
+
         int status = 0;
         ret = _resc->get_property< int >( irods::RESOURCE_STATUS, status );
         if ( !ret.ok() ) {
