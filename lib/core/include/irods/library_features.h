1e638e698ad71f2b6617d15ae12e085222fa6be4
Kory Draughn <korydraughn@renci.org>
2023-06-24 22:27:35 -0400

    [#5060] Added feature test macro for client connection library enhancements.

--- lib/core/include/irods/library_features.h
+++ lib/core/include/irods/library_features.h
@@ -1,6 +1,12 @@
 #ifndef IRODS_LIBRARY_FEATURES_H
 #define IRODS_LIBRARY_FEATURES_H
 
+// This file SHOULD NOT be processed by clang-tidy because it is required to define
+// preprocessor macros for compile-time decision making, hence the use of the following
+// clang-tidy directive.
+//
+// NOLINTBEGIN
+
 /// \file
 ///
 /// \brief iRODS Feature Test Macros
@@ -27,5 +33,10 @@
 ///
 /// \since 4.3.1
 #define IRODS_HAS_LIBRARY_SYSTEM_ERROR        202301L
+
+/// Defined if the C++ client connection libraries support proxy users.
+///
+/// \since 4.3.1
+#define IRODS_HAS_FEATURE_PROXY_USER_SUPPORT_FOR_CLIENT_CONNECTION_LIBRARIES 202306L
 
 #endif // IRODS_LIBRARY_FEATURES_H
