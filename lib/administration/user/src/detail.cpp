34c0f3b74625d7b81ed6745a97154b9d18c6fcb7
Kory Draughn <korydraughn@renci.org>
2023-07-22 10:28:06 -0400

    [#7193] Relaxed dependency on obfGetPw() in user administration library.
    Changing the password of a user requires the caller's password. Before
    this commit, the user administration library had a hard requirement on
    obfGetPw(). This made it difficult to impossible to support use-cases
    where there's no .irodsA file.
    
    This commit resolves that by allowing developers to pass the caller's
    password directly through the library, bypassing the need for the
    .irodsA file.
    
    This commit also replaces the use of obfEncodeByKeyV2() with
    obfEncodeByKey(). This aligns the library's implementation with that of
    iadmin and removes the need of the client-side session signature which
    is important since the server side does not have anything equivalent to
    the client-side session signature.

--- lib/administration/user/src/detail.cpp
+++ lib/administration/user/src/detail.cpp
@@ -5,43 +5,51 @@
 #include "irods/rodsErrorTable.h"
 #include "irods/irods_exception.hpp"
 
+#include <fmt/format.h>
+
 #include <cstring>
 #include <array>
 #include <string>
-#include <string_view>
 
 namespace irods::experimental::administration::detail
 {
-    auto obfuscate_password(const std::string_view _new_password) -> std::string
+    auto obfuscate_password(const user_password_property& _property) -> std::string
     {
         // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
         std::array<char, MAX_PASSWORD_LEN + 10> plain_text_password{};
-        std::strncpy(plain_text_password.data(), _new_password.data(), MAX_PASSWORD_LEN);
+        std::strncpy(plain_text_password.data(), _property.value.data(), MAX_PASSWORD_LEN);
 
         // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-        if (const auto lcopy = MAX_PASSWORD_LEN - 10 - _new_password.size(); lcopy > 15) {
+        if (const auto count = MAX_PASSWORD_LEN - 10 - _property.value.size(); count > 15) {
             // The random string (second argument) is used for padding and must match
             // what is defined on the server-side.
-            std::strncat(plain_text_password.data(), "1gCBizHWbwIYyWLoysGzTe6SyzqFKMniZX05faZHWAwQKXf6Fs", lcopy);
+            std::strncat(plain_text_password.data(), "1gCBizHWbwIYyWLoysGzTe6SyzqFKMniZX05faZHWAwQKXf6Fs", count);
         }
 
         // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
-        std::array<char, MAX_PASSWORD_LEN + 10> admin_password{};
+        std::array<char, MAX_PASSWORD_LEN + 10> key{};
 
-        // Get the plain text password of the iRODS user.
-        // "obfGetPw" decodes the obfuscated password stored in .irods/irodsA.
-        if (const auto ec = obfGetPw(admin_password.data()); ec != 0) {
-            // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
-            THROW(ec, "Failed to decode obfuscated password in .irodsA file.");
+        if (_property.requester_password) {
+            if (_property.requester_password->size() >= MAX_PASSWORD_LEN) {
+                constexpr const char* msg_fmt = "Password key cannot exceed maximum size [{}].";
+                // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+                THROW(SYS_INVALID_INPUT_PARAM, fmt::format(msg_fmt, MAX_PASSWORD_LEN));
+            }
+
+            std::strncpy(key.data(), _property.requester_password->c_str(), MAX_PASSWORD_LEN);
+        }
+        else {
+            // Get the plain text password of the iRODS user.
+            // "obfGetPw" decodes the obfuscated password stored in .irods/irodsA.
+            if (const auto ec = obfGetPw(key.data()); ec != 0) {
+                // NOLINTNEXTLINE(cppcoreguidelines-pro-bounds-array-to-pointer-decay)
+                THROW(ec, "Failed to decode obfuscated password in .irodsA file.");
+            }
         }
 
         // NOLINTNEXTLINE(cppcoreguidelines-avoid-magic-numbers, readability-magic-numbers)
         std::array<char, MAX_PASSWORD_LEN + 100> obfuscated_password{};
-        obfEncodeByKeyV2(
-            plain_text_password.data(),
-            admin_password.data(),
-            getSessionSignatureClientside(),
-            obfuscated_password.data());
+        obfEncodeByKey(plain_text_password.data(), key.data(), obfuscated_password.data());
 
         return obfuscated_password.data();
     } // obfuscate_password
