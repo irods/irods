aa1ec6cf3ac54bc708dd60337856142f5addf91f
Kory Draughn <korydraughn@renci.org>
2023-07-25 14:46:09 -0400

    [#7206] Removed dependency on libirods_server.so from unit test.

--- unit_tests/cmake/test_config/irods_environment_variables.cmake
+++ unit_tests/cmake/test_config/irods_environment_variables.cmake
@@ -3,5 +3,4 @@ set(IRODS_TEST_TARGET irods_environment_variables)
 set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_environment_variables.cpp)
  
-set(IRODS_TEST_LINK_LIBRARIES irods_common
-                              irods_server)
+set(IRODS_TEST_LINK_LIBRARIES irods_common)
