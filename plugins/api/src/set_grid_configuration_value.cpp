12c56383c644bdb183299eb2b477b7389584135c
Alan King <alanking@renci.org>
2023-08-25 14:08:22 -0400

    [#7283] set_grid_configuration: Add protected config list
    Adds a list of grid configuration namespaces and option names which are
    not allowed to be modified via set_grid_configuration API. Any namespace
    which has an option name of empty string means that the entire namespace
    is protected.
    
    This change also adds a check for a given namespace/option_name
    modification attempt to return an appropriate error if it is not found.
    This is to notify the administrator that the namespace/option_name they
    attempted to update does not exist (whether this is due to a typo, an
    update, or otherwise).

--- plugins/api/src/set_grid_configuration_value.cpp
+++ plugins/api/src/set_grid_configuration_value.cpp
@@ -15,6 +15,8 @@
 // Server-side Implementation
 //
 
+// clang-format off
+
 #include "irods/set_grid_configuration_value.h"
 #include "irods/irods_server_api_call.hpp"
 #include "irods/icatHighLevelRoutines.hpp"
@@ -22,6 +24,10 @@
 #include "irods/irods_logger.hpp"
 
 #include <string>
+#include <string_view>
+#include <utility> // for std::pair
+
+// clang-format on
 
 namespace ic = irods::experimental::catalog;
 
@@ -37,9 +43,38 @@ namespace
 
     auto rs_set_grid_configuration_value(RsComm*, const GridConfigurationInput*) -> int;
 
+    auto is_grid_configuration_allowed_to_be_modified(const char* _namespace, const char* _option_name) -> bool;
+
     //
     // Function Implementations
     //
+
+    auto is_grid_configuration_allowed_to_be_modified(const char* _namespace, const char* _option_name) -> bool
+    {
+        constexpr auto protected_config_list = std::to_array<std::pair<std::string_view, std::string_view>>(
+            {{"database", "schema_version"}, {"delay_server", ""}});
+
+        // If none of the namespace/option name combinations in the protected list match the passed in namespace and
+        // option name, it is allowed to be modified and so the function should return true. If any instance returns
+        // true, that means a match was found in the list of protected configurations, so we should return false in this
+        // function because that means the grid configuration is not allowed to be modified.
+        return std::none_of(std::begin(protected_config_list),
+                            std::end(protected_config_list),
+                            [&_namespace, &_option_name](const auto& _pair) {
+                                const auto& [protected_namespace, protected_option_name] = _pair;
+
+                                // If this evaluates to true, this is not a protected option and is allowed to be
+                                // modified. Return false because this is not a match.
+                                if (protected_namespace != _namespace) {
+                                    return false;
+                                }
+
+                                // Empty string indicates that modifications for all options in the namespace are
+                                // forbidden. If this expression evaluates to true, this a protected option and is not
+                                // allowed to be modified.
+                                return protected_option_name.empty() || protected_option_name == _option_name;
+                            });
+    } // is_grid_configuration_allowed_to_be_modified
 
     auto call_set_grid_configuration_value(irods::api_entry* _api,
                                            RsComm* _comm,
@@ -63,6 +98,12 @@ namespace
         catch (const irods::exception& e) {
             log_api::error(e.what());
             return e.code();
+        }
+
+        if (!is_grid_configuration_allowed_to_be_modified(_input->name_space, _input->option_name)) {
+            constexpr auto ec = SYS_NOT_ALLOWED;
+            addRErrorMsg(&_comm->rError, ec, "Specified grid configuration is not allowed to be modified.");
+            return ec;
         }
 
         return chlSetGridConfigurationValue(_comm,
