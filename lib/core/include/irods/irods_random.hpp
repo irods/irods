62742b15d15937ac063826f8d36642cf5d2f5c55
Alan King <alanking@renci.org>
2023-09-15 18:49:40 +0000

    [#7098] Add utility to generate random alphanumeric strings
    Adds a function which randomly generates an alphanumeric character and concatenates
    a string which is returned to the user. Unit tests included.

--- lib/core/include/irods/irods_random.hpp
+++ lib/core/include/irods/irods_random.hpp
@@ -1,8 +1,19 @@
 #ifndef IRODS_RANDOM_HPP
 #define IRODS_RANDOM_HPP
 
+#include <string>
+
 namespace irods {
     void getRandomBytes( void * buf, int bytes );
+
+    /// \brief Generate a string of size \p _length consisting of randomly generated alphanumeric characters.
+    ///
+    /// \param[in] _length Desired length of the generated string.
+    ///
+    /// \throws irods::exception If \p _length is negative.
+    ///
+    /// \since 4.3.1
+    auto generate_random_alphanumeric_string(std::int16_t _length) -> std::string;
 
     template <typename T>
     T
