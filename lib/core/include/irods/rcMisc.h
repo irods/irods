fbac8365fcf690d13de5c8bffe091c0f2db9cf64
Alan King <alanking@renci.org>
2023-10-05 16:19:20 -0400

    [#6672] Fix struct clearing for getRescQuota API

--- lib/core/include/irods/rcMisc.h
+++ lib/core/include/irods/rcMisc.h
@@ -65,6 +65,10 @@ void clearBytesBuffer(void* _p);
 
 void clearSwitchUserInput(void* _p);
 
+void clearRescQuota(void* _p);
+
+void clearRescQuotaInp(void* _p);
+
 // clang-format off
 __attribute__((deprecated("SimpleQuery is deprecated. Use GenQuery or SpecificQuery instead.")))
 void clearSimpleQueryOut(void* _p);
