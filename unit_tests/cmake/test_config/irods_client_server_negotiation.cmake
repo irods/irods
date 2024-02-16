18816ae0e5520864d3a1232520fe3524f573d652
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2024-02-16 15:50:29 -0500

    [#6251] CMake: use find_package for fmt

--- unit_tests/cmake/test_config/irods_client_server_negotiation.cmake
+++ unit_tests/cmake/test_config/irods_client_server_negotiation.cmake
@@ -4,7 +4,6 @@ set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_client_server_negotiation.cpp)
 
 set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include
                             nlohmann_json::nlohmann_json)
 
 set(IRODS_TEST_LINK_LIBRARIES irods_client
--- unit_tests/cmake/test_config/irods_rc_check_auth_credentials.cmake
+++ unit_tests/cmake/test_config/irods_rc_check_auth_credentials.cmake
@@ -3,9 +3,7 @@ set(IRODS_TEST_TARGET irods_rc_check_auth_credentials)
 set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_rc_check_auth_credentials.cpp)
 
-set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-                            ${IRODS_EXTERNALS_FULLPATH_FMT}/include)
+set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
  
-set(IRODS_TEST_LINK_LIBRARIES irods_client
-                              ${IRODS_EXTERNALS_FULLPATH_FMT}/lib/libfmt.so)
+set(IRODS_TEST_LINK_LIBRARIES irods_client)
 
