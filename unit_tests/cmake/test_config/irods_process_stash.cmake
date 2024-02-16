cb8897efc82af735639ac2bbb3954f4d8b205d90
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2024-02-16 15:41:00 -0500

    [#7510] clean up process_stash unit test cmake

--- unit_tests/cmake/test_config/irods_process_stash.cmake
+++ unit_tests/cmake/test_config/irods_process_stash.cmake
@@ -3,9 +3,6 @@ set(IRODS_TEST_TARGET irods_process_stash)
 set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
                             ${CMAKE_CURRENT_SOURCE_DIR}/src/test_process_stash.cpp)
 
-set(IRODS_TEST_INCLUDE_PATH ${CMAKE_BINARY_DIR}/lib/core/include
-                            ${CMAKE_SOURCE_DIR}/lib/core/include
-                            ${IRODS_EXTERNALS_FULLPATH_BOOST}/include
-                            ${IRODS_EXTERNALS_FULLPATH_CATCH2}/include)
+set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
 
 set(IRODS_TEST_LINK_LIBRARIES irods_common)
