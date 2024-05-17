e5d6185d38c089e3728d694ae52887f3b35a2279
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2024-05-17 14:01:18 -0500

    [#7745,#7747] improve `irods::lookup_table` implementation
    - `typedef`s converted to `using`s
    - type aliases added
        - `key_type`
        - `value_type`
        - `size_type`
        - `hasher`
        - `iterator_value_type`
        - `const_iterator_value_type`
    - references to template type arguments in member function declarations swapped for aliases
    - `operator[](key_type)` overload replaced with `operator[](const key_type&)` and `operator[](key_type&&)`
    - `size()` is now `noexcept` and returns `size_type` instead of `int`
    - `has_entry(key_type)` overload replaced with `has_entry(const key_type&)`
    - `erase(key_type)` overload replaced with `erase(const key_type&)` that returns `size_type` instead of `size_t`
    - `clear()` is now `noexcept`
    - `empty()` is now `noexcept` and `nodiscard`
    - `begin()` and `end()` are now `noexcept` and have `const` overloads that return a `const_iterator`
    - `cbegin()` and `cend()` are now `noexcept`
    - `find(key_type)` overload replaced with `find(const key_type&)` and a `const` overload that returns a `const_iterator`
    - `get()` implementaiton optimized - uses `find()` instead of `has_entry()`, allowing re-use of the iterator.
    - `set()` implementation now calls `insert_or_assign()` instead of the `[]` operator.
    
    The implementation is now more similar to that of `std::unordered_map`.

--- lib/core/include/irods/irods_lookup_table.hpp
+++ lib/core/include/irods/irods_lookup_table.hpp
@@ -2,199 +2,276 @@
 #define PLUGIN_TABLE_HPP
 
 // =-=-=-=-=-=-=-
-// boost includes
-#include <boost/any.hpp>
-
-// =-=-=-=-=-=-=-
+// irods includes
 #include "irods/rodsErrorTable.h"
 #include "irods/irods_hash.hpp"
 #include "irods/irods_error.hpp"
 #include "irods/irods_stacktrace.hpp"
-
 #include "irods/rodsLog.h"
 
-namespace irods {
+// =-=-=-=-=-=-=-
+// stl includes
+#include <string>
 
 // =-=-=-=-=-=-=-
-// class to manage tables of plugins.  employing a class in order to use
-// RAII for adding entries to the table now that it is not a static array
-    template < typename ValueType,
-               typename KeyType = std::string,
-               typename HashType = irods_string_hash >
-    class lookup_table {
-        protected:
-            typedef HASH_TYPE< KeyType, ValueType, HashType > irods_hash_map;
+// boost includes
+#include <boost/any.hpp>
 
-            irods_hash_map table_;
+#include <fmt/format.h>
 
-        public:
-            typedef typename irods_hash_map::iterator       iterator;
-            typedef typename irods_hash_map::const_iterator const_iterator;
-            lookup_table() {};
-            virtual ~lookup_table() {}
-            ValueType& operator[]( KeyType _k ) {
-                return table_[ _k ];
-            }
-            int size() const {
-                return table_.size();
-            }
-            bool has_entry( KeyType _k ) const {
-                return !( table_.end() == table_.find( _k ) );
-            }
-            size_t erase( KeyType _k ) {
-                return table_.erase( _k );
-            }
-            void clear() {
-                table_.clear();
-            }
-            bool empty() const {
-                return table_.empty();
-            }
-            iterator begin()  {
-                return table_.begin();
-            }
-            iterator end()    {
-                return table_.end();
-            }
-            iterator cbegin() {
-                return table_.cbegin();
-            }
-            iterator cend()   {
-                return table_.cend();
-            }
-            iterator find( KeyType _k ) {
-                return table_.find( _k );
+namespace irods
+{
+
+    // =-=-=-=-=-=-=-
+    // class to manage tables of plugins.  employing a class in order to use
+    // RAII for adding entries to the table now that it is not a static array
+    template <typename ValueType, typename KeyType = std::string, typename HashType = irods_string_hash>
+    class lookup_table
+    {
+      protected:
+        using irods_hash_map = HASH_TYPE<KeyType, ValueType, HashType>;
+
+        irods_hash_map table_;
+
+      public:
+        // clang-format off
+        using key_type                     = typename irods_hash_map::key_type;
+        using value_type                   = typename irods_hash_map::mapped_type;
+        using size_type                    = typename irods_hash_map::size_type;
+        using hasher                       = typename irods_hash_map::hasher;
+        using iterator                     = typename irods_hash_map::iterator;
+        using iterator_value_type          = typename irods_hash_map::value_type;
+        using const_iterator               = typename irods_hash_map::const_iterator;
+        using const_iterator_value_type    =    const iterator_value_type;
+        // clang-format on
+
+        lookup_table() {}
+        virtual ~lookup_table() {}
+
+        value_type& operator[](const key_type& _k)
+        {
+            return table_[_k];
+        }
+
+        value_type& operator[](key_type&& _k)
+        {
+            return table_[_k];
+        }
+
+        size_type size() const noexcept
+        {
+            return table_.size();
+        }
+
+        bool has_entry(const key_type& _k) const
+        {
+            return table_.contains(_k);
+        }
+
+        size_type erase(const key_type& _k)
+        {
+            return table_.erase(_k);
+        }
+
+        void clear() noexcept
+        {
+            table_.clear();
+        }
+
+        [[nodiscard]]
+        bool empty() const noexcept
+        {
+            return table_.empty();
+        }
+
+        iterator begin() noexcept
+        {
+            return table_.begin();
+        }
+
+        const_iterator begin() const noexcept
+        {
+            return table_.begin();
+        }
+
+        iterator end() noexcept
+        {
+            return table_.end();
+        }
+
+        const_iterator end() const noexcept
+        {
+            return table_.end();
+        }
+
+        const_iterator cbegin() const noexcept
+        {
+            return table_.cbegin();
+        }
+
+        const_iterator cend() const noexcept
+        {
+            return table_.cend();
+        }
+
+        iterator find(const key_type& _k)
+        {
+            return table_.find(_k);
+        }
+
+        const_iterator find(const key_type& _k) const
+        {
+            return table_.find(_k);
+        }
+
+        // =-=-=-=-=-=-=-
+        // accessor function
+        error get(const key_type& _key, value_type& _val)
+        {
+            auto _val_itr = find(_key);
+
+            if (_val_itr == end()) {
+                return ERROR(KEY_NOT_FOUND, fmt::format("failed to find key [{}] in table", _key));
             }
 
-            // =-=-=-=-=-=-=-
-            // accessor function
-            error get( const std::string& _key, ValueType& _val ) {
-                if ( !has_entry( _key ) ) {
-                    return ERROR( KEY_NOT_FOUND, "key not found" );
-                }
+            _val = _val_itr->second;
 
-                _val = table_[ _key ];
+            return SUCCESS();
+        }
 
-                return SUCCESS();
-            }
-
-            // =-=-=-=-=-=-=-
-            // mutator function
-            error set( const std::string& _key, const ValueType& _val ) {
-                table_[ _key ] = _val;
-                return SUCCESS();
-            } // set
+        // =-=-=-=-=-=-=-
+        // mutator function
+        error set(const key_type& _key, const value_type& _val)
+        {
+            table_.insert_or_assign(_key, _val);
+            return SUCCESS();
+        }
 
     }; // class lookup_table
 
 
-// =-=-=-=-=-=-=-
-// partial specialization created to support templating the get/set
-// functions which need to manage exception handling etc from
-// a boost::any_cast
-    template< typename KeyType, typename HashType >
-    class lookup_table < boost::any, KeyType, HashType > {
-        protected:
-            typedef HASH_TYPE< KeyType, boost::any, HashType > irods_hash_map;
-            irods_hash_map table_;
+    // =-=-=-=-=-=-=-
+    // partial specialization created to support templating the get/set
+    // functions which need to manage exception handling etc from
+    // a boost::any_cast
+    template <typename KeyType, typename HashType>
+    class lookup_table<boost::any, KeyType, HashType>
+    {
+      protected:
+        using irods_hash_map = HASH_TYPE<KeyType, boost::any, HashType>;
 
-        public:
-            typedef typename irods_hash_map::iterator iterator;
-            lookup_table() {};
-            virtual ~lookup_table() {}
-            boost::any& operator[]( KeyType _k ) {
-                return table_[ _k ];
+        irods_hash_map table_;
+
+      public:
+        // clang-format off
+        using key_type                     = typename irods_hash_map::key_type;
+        using value_type                   = typename irods_hash_map::mapped_type;
+        using size_type                    = typename irods_hash_map::size_type;
+        using hasher                       = typename irods_hash_map::hasher;
+        using iterator                     = typename irods_hash_map::iterator;
+        using iterator_value_type          = typename irods_hash_map::value_type;
+        // clang-format on
+
+        lookup_table() = default;
+        virtual ~lookup_table() = default;
+
+        value_type& operator[](key_type _k)
+        {
+            return table_[_k];
+        }
+
+        size_type size() const noexcept
+        {
+            return table_.size();
+        }
+
+        bool has_entry(const key_type& _k) const
+        {
+            return table_.contains(_k);
+        }
+
+        size_type erase(const key_type& _k)
+        {
+            return table_.erase(_k);
+        }
+
+        void clear() noexcept
+        {
+            table_.clear();
+        }
+
+        [[nodiscard]]
+        bool empty() const noexcept
+        {
+            return table_.empty();
+        }
+
+        iterator begin() noexcept
+        {
+            return table_.begin();
+        }
+
+        iterator end() noexcept
+        {
+            return table_.end();
+        }
+
+        iterator find(const key_type& _k)
+        {
+            return table_.find(_k);
+        }
+
+        // =-=-=-=-=-=-=-
+        // get a property from the table if it exists. catch the exception in the case where
+        // the template types may not match and return success/fail
+        template <typename T>
+        error get(const key_type& _key, T& _val)
+        {
+            // check params
+            if (_key.empty()) {
+                return ERROR(KEY_NOT_FOUND, "empty key");
             }
-            int size() {
-                return table_.size();
-            }
-            bool has_entry( KeyType _k ) {
-                return !( table_.end() == table_.find( _k ) );
-            }
-            size_t erase( KeyType _k ) {
-                return table_.erase( _k );
-            }
-            void clear() {
-                table_.clear();
-            }
-            bool empty() {
-                return table_.empty();
-            }
-            iterator begin() {
-                return table_.begin();
-            }
-            iterator end()   {
-                return table_.end();
-            }
-            iterator find( KeyType _k ) {
-                return table_.find( _k );
+
+            auto _val_itr = find(_key);
+
+            if (_val_itr == end()) {
+                return ERROR(KEY_NOT_FOUND, fmt::format("failed to find key [{}] in table", _key));
             }
 
-            // =-=-=-=-=-=-=-
-            // get a property from the table if it exists.  catch the exception in the case where
-            // the template types may not match and return success/fail
-            template< typename T >
-            error get( const std::string& _key, T& _val ) {
-                // =-=-=-=-=-=-=-
-                // check params
-                if ( _key.empty() ) {
-                    return ERROR( KEY_NOT_FOUND, "the key is empty" );
-                }
+            // attempt to any_cast property value to given type
+            try {
+                _val = boost::any_cast<T>(_val_itr->second);
+                return SUCCESS();
+            }
+            catch (const boost::bad_any_cast&) {
+                return ERROR(KEY_TYPE_MISMATCH, fmt::format("type and property key [{}] mismatch", _key));
+            }
 
-                if ( !has_entry( _key ) ) {
-                    std::stringstream msg;
-                    msg << "failed to find key [";
-                    msg << _key;
-                    msg << "] in table.";
-                    return ERROR( KEY_NOT_FOUND, msg.str() );
-                }
+            // invalid location in the code
+            return ERROR(INVALID_LOCATION, "reached unreachable code");
 
-                // =-=-=-=-=-=-=-
-                // attempt to any_cast property value to given type.  catch exception and log
-                // failure then exit
-                try {
-                    _val = boost::any_cast< T >( table_[ _key ] );
-                    return SUCCESS();
-                }
-                catch ( const boost::bad_any_cast& ) {
-                    std::stringstream msg;
-                    msg << "type and property key [";
-                    msg << _key;
-                    msg << "] mismatch";
-                    return ERROR( KEY_TYPE_MISMATCH, msg.str() );
-                }
+        } // get
 
-                // =-=-=-=-=-=-=-
-                // invalid location in the code
-                return ERROR( INVALID_LOCATION, "shouldn't get here." );
+        // =-=-=-=-=-=-=-
+        // set a property in the table
+        template <typename T>
+        error set(const key_type& _key, const T& _val)
+        {
+            // check params
+            if (_key.empty()) {
+                return ERROR(KEY_NOT_FOUND, "empty key");
+            }
 
-            } // get_property
+            // add property to map
+            table_.insert_or_assign(_key, _val);
 
-            // =-=-=-=-=-=-=-
-            // set a property in the table
-            template< typename T >
-            error set( const std::string& _key, const T& _val ) {
-                // =-=-=-=-=-=-=-
-                // check params
-                if ( _key.empty() ) {
-                    return ERROR( KEY_NOT_FOUND, "empty key" );
-                }
-
-                // =-=-=-=-=-=-=-
-                // add property to map
-                table_[ _key ] = _val;
-
-                return SUCCESS() ;
-
-            } // set_property
+            return SUCCESS();
+        }
 
     }; // class lookup_table
-    
-    typedef lookup_table<boost::any> plugin_property_map;
+
+    using plugin_property_map = lookup_table<boost::any>;
 
 }; // namespace irods
 
 #endif // PLUGIN_TABLE_HPP
-
-
-
