5838766c975d7bc4526c537817f5a0c72a24ffbf
Kory Draughn <korydraughn@renci.org>
2024-06-13 20:49:09 -0400

    [#7673,#7678] Add tests for GenQuery2 enhancements.

--- unit_tests/src/test_rc_genquery2.cpp
+++ unit_tests/src/test_rc_genquery2.cpp
@@ -14,15 +14,15 @@
 // NOLINTNEXTLINE(readability-function-cognitive-complexity)
 TEST_CASE("rc_genquery2")
 {
+    load_client_api_plugins();
+
+    rodsEnv env;
+    _getRodsEnv(env);
+
+    irods::experimental::client_connection conn;
+
     SECTION("execute a simple query")
     {
-        load_client_api_plugins();
-
-        rodsEnv env;
-        _getRodsEnv(env);
-
-        irods::experimental::client_connection conn;
-
         auto query_string = fmt::format("select COLL_NAME where COLL_NAME = '{}'", env.rodsHome);
 
         Genquery2Input input{};
@@ -55,5 +55,67 @@ TEST_CASE("rc_genquery2")
 
         CHECK(rc_genquery2(&comm, &input, nullptr) == SYS_INVALID_INPUT_PARAM);
         CHECK_FALSE(output);
+    }
+
+    SECTION("supports nested, multi-argument functions in projection list")
+    {
+        auto query_string = fmt::format(
+            "select concat(COLL_NAME, concat('--', COLL_ACCESS_USER_NAME)) where COLL_NAME = '{}'", env.rodsHome);
+
+        Genquery2Input input{};
+        input.query_string = query_string.data();
+
+        char* output{};
+        REQUIRE(rc_genquery2(static_cast<RcComm*>(conn), &input, &output) == 0);
+        REQUIRE_FALSE(output == nullptr);
+
+        REQUIRE_NOTHROW([&env, &output] {
+            const auto result = nlohmann::json::parse(output);
+            CHECK_FALSE(result.empty());
+            const auto expected = fmt::format("{}--{}", env.rodsHome, env.rodsUserName);
+            CHECK(result.at(0).at(0).get_ref<const std::string&>() == expected);
+        }());
+    }
+
+    SECTION("supports nested, multi-argument functions in where clause")
+    {
+        auto query_string =
+            fmt::format("select COLL_NAME where concat(COLL_NAME, concat('--', COLL_ACCESS_USER_NAME)) = '{}--{}'",
+                        env.rodsHome,
+                        env.rodsUserName);
+
+        Genquery2Input input{};
+        input.query_string = query_string.data();
+
+        char* output{};
+        REQUIRE(rc_genquery2(static_cast<RcComm*>(conn), &input, &output) == 0);
+        REQUIRE_FALSE(output == nullptr);
+
+        REQUIRE_NOTHROW([&env, &output] {
+            const auto result = nlohmann::json::parse(output);
+            CHECK_FALSE(result.empty());
+            CHECK(result.at(0).at(0).get_ref<const std::string&>() == env.rodsHome);
+        }());
+    }
+
+    SECTION("supports nested, multi-argument functions in order-by clause")
+    {
+        auto query_string = fmt::format("select concat(COLL_NAME, concat('--', COLL_ACCESS_USER_NAME)) where COLL_NAME "
+                                        "= '{}' order by concat(COLL_NAME, concat('--', COLL_ACCESS_USER_NAME))",
+                                        env.rodsHome);
+
+        Genquery2Input input{};
+        input.query_string = query_string.data();
+
+        char* output{};
+        REQUIRE(rc_genquery2(static_cast<RcComm*>(conn), &input, &output) == 0);
+        REQUIRE_FALSE(output == nullptr);
+
+        REQUIRE_NOTHROW([&env, &output] {
+            const auto result = nlohmann::json::parse(output);
+            CHECK_FALSE(result.empty());
+            const auto expected = fmt::format("{}--{}", env.rodsHome, env.rodsUserName);
+            CHECK(result.at(0).at(0).get_ref<const std::string&>() == expected);
+        }());
     }
 }
