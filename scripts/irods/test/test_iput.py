582c9a002aab46b0867913050f5f84825b3a65a0
Kory Draughn <korydraughn@renci.org>
2024-06-16 20:41:24 -0400

    [?] Tweak tests for GenQuery1 flex/bison parser.

--- scripts/irods/test/test_iput.py
+++ scripts/irods/test/test_iput.py
@@ -94,7 +94,7 @@ class Test_Iput(session.make_sessions_mixin(rodsadmins, rodsusers), unittest.Tes
             munge_resc = 'munge_resc'
             local_file = os.path.join(self.admin.local_session_dir, 'local_file')
             logical_path = os.path.join(self.admin.session_collection, 'foo')
-            question = '''"select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'"'''.format(
+            question = "select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'".format(
                 os.path.basename(logical_path), os.path.dirname(logical_path))
 
             try:
@@ -199,7 +199,7 @@ class Test_Iput(session.make_sessions_mixin(rodsadmins, rodsusers), unittest.Tes
             munge_resc = 'munge_resc'
             local_file = os.path.join(self.admin.local_session_dir, 'local_file')
             logical_path = os.path.join(self.admin.session_collection, 'foo')
-            question = '''"select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'"'''.format(
+            question = "select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'".format(
                 os.path.basename(logical_path), os.path.dirname(logical_path))
 
             try:
@@ -260,7 +260,7 @@ class Test_Iput(session.make_sessions_mixin(rodsadmins, rodsusers), unittest.Tes
             munge_resc = 'munge_resc'
             local_file = os.path.join(self.admin.local_session_dir, 'local_file')
             logical_path = os.path.join(self.admin.session_collection, 'foo')
-            question = '''"select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'"'''.format(
+            question = "select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'".format(
                 os.path.basename(logical_path), os.path.dirname(logical_path))
 
             try:
@@ -365,7 +365,7 @@ class Test_Iput(session.make_sessions_mixin(rodsadmins, rodsusers), unittest.Tes
             munge_resc = 'munge_resc'
             local_file = os.path.join(self.admin.local_session_dir, 'local_file')
             logical_path = os.path.join(self.admin.session_collection, 'foo')
-            question = '''"select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'"'''.format(
+            question = "select DATA_REPL_NUM, DATA_RESC_NAME, DATA_REPL_STATUS where DATA_NAME = '{0}' and COLL_NAME = '{1}'".format(
                 os.path.basename(logical_path), os.path.dirname(logical_path))
 
             try:
@@ -887,7 +887,7 @@ class test_iput_with_checksums(session.make_sessions_mixin(rodsadmins, rodsusers
         self.run_tests(self.object_name, 5120000)
 
     def get_checksum(self, object_name):
-        iquest_result,_,ec = self.admin.run_icommand(['iquest', '%s', '"select DATA_CHECKSUM where DATA_NAME = \'{}\'"'.format(object_name)])
+        iquest_result,_,ec = self.admin.run_icommand(['iquest', '%s', 'select DATA_CHECKSUM where DATA_NAME = \'{}\''.format(object_name)])
         self.assertEqual(0, ec)
 
         print(iquest_result)
