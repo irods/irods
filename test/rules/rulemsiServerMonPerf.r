bc37155219521d9ff9e28fa88f364caa322fdf09
Alan King <alanking@renci.org>
2024-06-25 12:05:47 -0400

    [irods/irods#7760] Remove rulemsiServerMonPerf.r
    This rule file is no longer needed by the test generator because a
    test specifically for this microservice has replaced it. The specific
    test has been introduced because extra setup is required to run tests
    for this microservice.
    
    This is the name of the test which replaces the one which would be
    generated by the file which is being removed in this commit:
    test_all_rules.Test_AllRules.test_msiServerMonPerf_default_3736

--- test/rules/rulemsiServerMonPerf.r
+++
@@ -1,8 +0,0 @@
-acServerMonPerf {
-#This microservice invokes a command in server/bin/cmd
-#    irodsServerMonPerf     - a perl script to get monitoring information
-    	msiServerMonPerf("default","default");
-    	msiServerMonPerf("verbose","default");
-}
-INPUT null
-OUTPUT ruleExecOut
