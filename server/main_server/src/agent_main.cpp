addf431468a976c6cebd5b56ae3095e8cebbe29a
Kory Draughn <korydraughn@renci.org>
2024-10-28 12:45:12 -0400

    [#7229,#8050] Fix error handling logic for heartbeat operation.

--- server/main_server/src/agent_main.cpp
+++ server/main_server/src/agent_main.cpp
@@ -764,14 +764,19 @@ namespace
 
             while (bytes_to_send) {
                 const int bytes_sent = send(net_obj->socket_handle(), &(heartbeat[heartbeat_length - bytes_to_send]), bytes_to_send, 0);
-                const int errsav = errno;
-                if (errsav != EINTR) {
-                    log_agent::error("{}: Socket error encountered during heartbeat; socket returned {}",
-                                      __func__, strerror(errsav));
-                    break;
-                }
 
-                if (bytes_sent > 0) {
+                // We do not check for 0 because it is not considered an error condition. It only means 0
+                // bytes were sent. As longs as we have more bytes to send and no error has occurred, we
+                // keep attempting to send bytes.
+                if (bytes_sent == -1) {
+                    const int errsav = errno;
+                    if (errsav != EINTR) {
+                        log_agent::error("{}: Socket error encountered during heartbeat; socket returned {}",
+                                          __func__, strerror(errsav));
+                        break;
+                    }
+                }
+                else if (bytes_sent > 0) {
                     bytes_to_send -= bytes_sent;
                 }
             }
