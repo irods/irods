b2fefe8e8075c837e370e3c90fce982ed98eda6a
Kory Draughn <korydraughn@renci.org>
2024-12-11 22:09:13 -0500

    squash w/ [?] Add utility for converting single quotes to their hex encoding.

--- lib/core/include/irods/escape_utilities.hpp
+++ lib/core/include/irods/escape_utilities.hpp
@@ -20,7 +20,7 @@ namespace irods
     /// \since 4.3.3
     // NOLINTNEXTLINE(cppcoreguidelines-macro-usage)
 #define IRODS_SINGLE_QUOTES_TO_HEX_COMPAT(_s) \
-    [&t = (_s)] { \
+    [t = (_s)] { \
         constexpr const auto* var = "IRODS_ENABLE_GENQUERY1_FLEX_BISON_PARSER"; \
         if (const auto* v = std::getenv(var); v && std::strcmp(v, "1") == 0) { \
             return irods::single_quotes_to_hex(t); \
