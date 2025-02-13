82b29b2bfd5f459a7efff4249bbbb7c51c2fcff4
Alan King <alanking@renci.org>
2025-02-13 09:21:43 -0500

    [#4984] Clean up headers in rsDataGet and rsDataPut

--- server/api/include/irods/rsDataPut.hpp
+++ server/api/include/irods/rsDataPut.hpp
@@ -1,11 +1,12 @@
 #ifndef RS_DATA_PUT_HPP
 #define RS_DATA_PUT_HPP
 
-#include "irods/rcConnect.h"
-#include "irods/dataObjInpOut.h"
+struct DataOprInp;
+struct RsComm;
+struct portalOprOut;
+struct rodsServerHost;
 
-int rsDataPut( rsComm_t *rsComm, dataOprInp_t *dataPutInp, portalOprOut_t **portalOprOut );
-int remoteDataPut( rsComm_t *rsComm, dataOprInp_t *dataPutInp, portalOprOut_t **portalOprOut, rodsServerHost_t *rodsServerHost );
-int _rsDataPut( rsComm_t *rsComm, dataOprInp_t *dataPutInp, portalOprOut_t **portalOprOut );
+int rsDataPut(RsComm* rsComm, DataOprInp* dataPutInp, portalOprOut** portalOprOut);
+int remoteDataPut(RsComm* rsComm, DataOprInp* dataPutInp, portalOprOut** portalOprOut, rodsServerHost* rodsServerHost);
 
-#endif
+#endif // RS_DATA_PUT_HPP
