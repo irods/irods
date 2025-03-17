dc888253e920d9070f927fe7d5969b7b171e8b03
Kory Draughn <korydraughn@renci.org>
2025-03-17 10:22:22 -0400

    [#8260] Add python tests for access time enhancements.

--- scripts/irods/lib.py
+++ scripts/irods/lib.py
@@ -699,6 +699,13 @@ def get_replica_mtime(session, logical_path, replica_number):
                 os.path.basename(logical_path),
                 str(replica_number))])[0].strip()
 
+def get_replica_atime(session, logical_path, replica_number):
+    coll_name = os.path.dirname(logical_path)
+    data_name = os.path.basename(logical_path)
+    query = f"select DATA_ACCESS_TIME where COLL_NAME = '{coll_name}' and DATA_NAME = '{data_name}' and DATA_REPL_NUM = '{replica_number}'"
+    out, _, _ = session.run_icommand(['iquery', query])
+    return json.loads(out.strip())[0][0]
+
 def collection_exists(session, logical_path):
     out = session.run_icommand(['iquest', f"select COLL_ID where COLL_NAME = '{logical_path}'"])[0]
 
