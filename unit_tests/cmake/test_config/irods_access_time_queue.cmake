65404f36edab9df3f8084d612e8b0675538b51f9
Kory Draughn <korydraughn@renci.org>
2025-03-18 01:31:04 -0400

    [#8260] Add C++ unit tests for access_time_queue library.

---
+++ unit_tests/cmake/test_config/irods_access_time_queue.cmake
@@ -0,0 +1,9 @@
+set(IRODS_TEST_TARGET irods_access_time_queue)
+
+set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_access_time_queue.cpp)
+
+set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
+
+set(IRODS_TEST_LINK_LIBRARIES irods_common
+                              irods_server)
