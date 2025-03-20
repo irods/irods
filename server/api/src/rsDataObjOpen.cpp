fbbb7c0ace480ff10181a713ce1269f854ee6579
Kory Draughn <korydraughn@renci.org>
2025-03-20 08:29:54 -0400

    squash w/ [8260] Make server track access time for replicas.

--- server/api/src/rsDataObjOpen.cpp
+++ server/api/src/rsDataObjOpen.cpp
@@ -896,7 +896,7 @@ namespace
         return allocAndSetL1descForZoneOpr(remoteL1descInx, &_inp, &_server_host, stat);
     } // remote_open
 
-    auto replica_access_time_needs_update(int _openmode, const std::string_view _atime, const std::string_view _mtime)
+    auto replica_access_time_needs_update(int _openmode, const std::string_view _atime, const std::string_view _mtime, const std::time_t _current_time)
         -> bool
     {
         if (g_atime_invalid_resolution_in_seconds_detected) {
@@ -913,11 +913,7 @@ namespace
 
         try {
             const auto atime = std::stoull(std::string{_atime});
-
-            using clock_type = std::chrono::system_clock;
-            const auto now = clock_type::to_time_t(clock_type::now());
-
-            return std::cmp_greater_equal(now - atime, g_atime_resolution_in_seconds);
+            return std::cmp_greater_equal(_current_time - atime, g_atime_resolution_in_seconds);
         }
         catch (const irods::exception& e) {
             log_api::warn(
@@ -930,12 +926,9 @@ namespace
         return false;
     } // replica_access_time_needs_update
 
-    auto try_enqueue_update_of_replica_access_time(const replica_proxy& _replica) -> void
+    auto try_enqueue_update_of_replica_access_time(const replica_proxy& _replica, const std::time_t _current_time) -> void
     {
         try {
-            using clock_type = std::chrono::system_clock;
-            const auto now = clock_type::to_time_t(clock_type::now());
-
             log_api::debug("{}: Enqueuing access time update for replica: data_id=[{}], replica_number=[{}]",
                            __func__,
                            _replica.data_id(),
@@ -944,7 +937,7 @@ namespace
             irods::access_time_queue::access_time_data data{};
             data.data_id = static_cast<std::size_t>(_replica.data_id());
             data.replica_number = static_cast<std::size_t>(_replica.replica_number());
-            fmt::format("{:011}", now).copy(data.last_accessed, sizeof(data.last_accessed) - 1);
+            fmt::format("{:011}", _current_time).copy(data.last_accessed, sizeof(data.last_accessed) - 1);
 
             irods::access_time_queue::try_enqueue(data);
         }
@@ -1290,8 +1283,11 @@ namespace
             }
         }
 
-        if (replica_access_time_needs_update(dataObjInp->openFlags, replica.atime(), replica.mtime())) {
-            try_enqueue_update_of_replica_access_time(replica);
+        using clock_type = std::chrono::system_clock;
+        const auto now = clock_type::to_time_t(clock_type::now());
+
+        if (replica_access_time_needs_update(dataObjInp->openFlags, replica.atime(), replica.mtime(), now)) {
+            try_enqueue_update_of_replica_access_time(replica, now);
         }
 
         return l1descInx;
