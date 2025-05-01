a326bd6f73f6908b4bef9be811d000948580129b
Alan King <alanking@renci.org>
2025-05-01 16:02:08 -0400

    [#8334] Fix leak in native auth plugin

--- plugins/auth/src/native.cpp
+++ plugins/auth/src/native.cpp
@@ -299,6 +299,7 @@ namespace irods
             return resp;
         } // native_auth_agent_request
 
+        // NOLINTNEXTLINE(readability-function-cognitive-complexity)
         json native_auth_agent_response(rsComm_t& comm, const json& req)
         {
             irods_auth::throw_if_request_message_is_missing_key(
@@ -339,6 +340,16 @@ namespace irods
             authCheckInp.username = const_cast<char*>(username.data());
 
             authCheckOut_t* authCheckOut = nullptr;
+            const auto free_authCheckOut = irods::at_scope_exit{[&authCheckOut] {
+                if (nullptr != authCheckOut) {
+                    if (nullptr != authCheckOut->serverResponse) {
+                        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc,cppcoreguidelines-owning-memory)
+                        std::free(authCheckOut->serverResponse);
+                    }
+                    // NOLINTNEXTLINE(cppcoreguidelines-no-malloc,cppcoreguidelines-owning-memory)
+                    std::free(authCheckOut);
+                }
+            }};
             if (LOCAL_HOST == rodsServerHost->localFlag) {
                 status = rsAuthCheck(&comm, &authCheckInp, &authCheckOut);
             }
@@ -483,13 +494,6 @@ namespace irods
             else {          /* proxyUser and clientUser are the same */
                 comm.proxyUser.authInfo.authFlag =
                     comm.clientUser.authInfo.authFlag = authCheckOut->privLevel;
-            }
-
-            if ( authCheckOut != NULL ) {
-                if ( authCheckOut->serverResponse != NULL ) {
-                    free( authCheckOut->serverResponse );
-                }
-                free( authCheckOut );
             }
 
             return resp;
