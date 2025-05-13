787717bc58515b025249b33f7bee3b54fee76924
Derek D <ddong@renci.org>
2025-05-13 18:44:30 +0000

    [#8451] Bump version to 5.0.0

--- test/c_api_test/test.c
+++ test/c_api_test/test.c
@@ -15,7 +15,7 @@
 #include "irods/dataObjClose.h"
 #include "irods/checksum.h"
 
-#if IRODS_VERSION_INTEGER != 4090000
+#if IRODS_VERSION_INTEGER != 5000000
 #  error "IRODS_VERSION_INTEGER needs attention"
 #endif
 
