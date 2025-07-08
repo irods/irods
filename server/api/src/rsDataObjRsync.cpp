e55288413dee8110ff0b11938c6130b1a0248b46
Derek D <ddong@renci.org>
2025-07-08 05:58:33 +0000

    [#8384] Add additional checksum return checks to rsync
    This will check for CHECK_VERIFICATION_RESULTS from rsDataObjChksum and then
    parse the resultant JSON array. Originally, CHECK_VERIFICATION_RESULTS was
    erroneously treated as an error and irsync would terminate abnormally because
    of it. Now, if any error_code is nonzero, it will still fail and return while
    logging a message (i.e. all 0 error codes indicates everything is fine). For
    the case listed in the ticket, the resultant JSON has a 0 error code: it's
    informing the caller that the replica is skipped due to it being stale.
    Therefore, it will pass and allow the copy to progress.

--- server/api/src/rsDataObjRsync.cpp
+++ server/api/src/rsDataObjRsync.cpp
@@ -11,9 +11,18 @@
 #include "irods/rsDataObjCopy.hpp"
 
 #include "irods/irods_resource_redirect.hpp"
+#include "irods/irods_logger.hpp"
 
 #define IRODS_FILESYSTEM_ENABLE_SERVER_SIDE_API
 #include "irods/filesystem.hpp"
+
+#include <nlohmann/json.hpp>
+
+namespace
+{
+    using json = nlohmann::json;
+    using log_api = irods::experimental::log::api;
+} //namespace
 
 int
 rsDataObjRsync( rsComm_t *rsComm, dataObjInp_t *dataObjInp,
@@ -261,7 +270,41 @@ rsRsyncDataToData( rsComm_t *rsComm, dataObjInp_t *dataObjInp ) {
         /* use rsDataObjChksum because the path could be in a remote zone */
         status = rsDataObjChksum(rsComm, &dataObjCopyInp.destDataObjInp, &destChksumStr);
 
-        if (status < 0 && status != CAT_NO_ACCESS_PERMISSION && status != CAT_NO_ROWS_FOUND) {
+        if (CHECK_VERIFICATION_RESULTS == status) {
+            try {
+                const auto& verification_result = json::parse(destChksumStr);
+                for (const auto& entry : verification_result) {
+                    if (const auto ec = entry.at("error_code").get<int>(); ec != 0) {
+                        // "actual" error happened: log it and fail
+                        log_api::error("{}: error in rsDataObjChksum, message = [{}]; error_code = [{}]",
+                                       __func__,
+                                       entry.at("message").get_ref<const std::string&>(),
+                                       ec);
+                        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
+                        free(srcChksumStr);
+                        // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
+                        free(destChksumStr);
+                        clearKeyVal(&dataObjCopyInp.srcDataObjInp.condInput);
+                        clearKeyVal(&dataObjCopyInp.destDataObjInp.condInput);
+                        return status;
+                    }
+                }
+            }
+            catch (const json::exception& ex) {
+                log_api::error("{}: json exception from rsDataObjChksum return, destChksumStr = \"{}\"; what = [{}]",
+                               __func__,
+                               destChksumStr ? destChksumStr : "nullptr",
+                               ex.what());
+                // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
+                free(srcChksumStr);
+                // NOLINTNEXTLINE(cppcoreguidelines-no-malloc, cppcoreguidelines-owning-memory)
+                free(destChksumStr);
+                clearKeyVal(&dataObjCopyInp.srcDataObjInp.condInput);
+                clearKeyVal(&dataObjCopyInp.destDataObjInp.condInput);
+                return status;
+            }
+        }
+        else if (status < 0 && status != CAT_NO_ACCESS_PERMISSION && status != CAT_NO_ROWS_FOUND) {
             // NOLINTNEXTLINE(cppcoreguidelines-pro-type-vararg, cppcoreguidelines-pro-bounds-array-to-pointer-decay)
             rodsLog(LOG_ERROR,
                     "rsRsyncDataToData: _rsDataObjChksum error for %s, status = %d",
