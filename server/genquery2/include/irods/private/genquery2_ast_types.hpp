14b302498a86489a080d77fa99d2977b2631d06a
Ramsey <NotEdify@proton.me>
2025-08-08 20:16:46 -0400

    [#8093] genquery2: added function support to group by clause

--- server/genquery2/include/irods/private/genquery2_ast_types.hpp
+++ server/genquery2/include/irods/private/genquery2_ast_types.hpp
@@ -252,7 +252,7 @@ namespace irods::experimental::genquery2
 
     struct group_by
     {
-        std::vector<std::string> columns;
+        std::vector<std::variant<function, column>> expressions;
     }; // struct group_by
 
     struct order_by
