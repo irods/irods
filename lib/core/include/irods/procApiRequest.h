f17929b22e0d5ca878584308834f4d1e4b895714
Kory Draughn <korydraughn@renci.org>
2025-09-12 12:33:28 -0400

    [8653] Allow override of packing instructions for API requests
    This commit adapts the compatibility solution from the HTTP API and
    makes it available to all future C clients.
    
    procApiRequest_raw allows developers to use alternative packing
    instructions and packing instruction tables to maintain backward and
    forward compatibility. This tool is most useful for situations where a
    packing instruction has changed in the iRODS server.

--- lib/core/include/irods/procApiRequest.h
+++ lib/core/include/irods/procApiRequest.h
@@ -1,18 +1,87 @@
-#ifndef PROC_API_REQUEST_H__
-#define PROC_API_REQUEST_H__
+#ifndef PROC_API_REQUEST_H
+#define PROC_API_REQUEST_H
 
+/// \file
+
+#include "irods/packStruct.h"
 #include "irods/rods.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
+/// Executes an iRODS API request.
+///
+/// This function uses the global packing instruction table for serialization and deserialization.
+///
+/// \param[in]  conn        The RcComm used for communication.
+/// \param[in]  apiNumber   The integer which identifies the API to execute. See apiNumberData.h.
+/// \param[in]  inputStruct \parblock The data to serialize, typically an input data structure.
+///                         Can be passed \p NULL if no input is necessary. \endparblock
+/// \param[in]  inputBsBBuf The byte buffer to stream to the server.
+///                         Can be passed \p NULL if no input byte stream is necessary.
+///                         \endparblock
+/// \param[out] outStruct   \parblock The output data structure to be filled by the server. The
+///                         output will be heap-allocated and assigned to the pointer. Can be
+///                         passed \p NULL if no output is expected. \endparblock
+/// \param[out] outBsBBuf   \parblock The output byte buffer to be filled by the server.
+///                         Can be passed \p NULL if no output stream is expected. \endparblock
+///
+/// \return An integer indicating success or failure.
+/// \retval >=0 On success.
+/// \retval  <0 On failure.
 int procApiRequest(rcComm_t *conn,
                    int apiNumber,
                    const void *inputStruct,
                    const bytesBuf_t *inputBsBBuf,
                    void **outStruct,
                    bytesBuf_t *outBsBBuf);
+
+/// Executes an iRODS API request.
+///
+/// Grants the caller control over which packing instructions are used during serializing and
+/// deserialization of the data structures. This form of procApiRequest is useful for maintaining
+/// backward compartibility.
+///
+/// This function assumes the caller's only goal is to use different packing instructions. All
+/// other API specifications must remain as they are.
+///
+/// \param[in]  conn        The RcComm used for communication.
+/// \param[in]  apiNumber   The integer which identifies the API to execute. See apiNumberData.h.
+/// \param[in]  packingInstructionTable \parblock The table holding various packing instructions.
+///                         All packing instructions needed for serialization of the
+///                         data structure must be defined in the table. If passed \p NULL, the
+///                         global packing instruction table will be used. \endparblock
+/// \param[in]  inputPackingInstruction \parblock The packing instruction to use for serialization.
+///                         Cannot be \p NULL. \endparblock
+/// \param[in]  inputStruct \parblock The data to serialize, typically an input data structure.
+///                         Can be passed \p NULL if no input is necessary. \endparblock
+/// \param[in]  inputBsBBuf The byte buffer to stream to the server.
+///                         Can be passed \p NULL if no input byte stream is necessary.
+///                         \endparblock
+/// \param[in]  outputPackingInstruction \parblock The packing instruction to use for
+///                         deserialization. Can be set to \p NULL if no output is expected.
+///                         \endparblock
+/// \param[out] outStruct   \parblock The output data structure to be filled by the server. The
+///                         output will be heap-allocated and assigned to the pointer. Can be
+///                         passed \p NULL if no output is expected. \endparblock
+/// \param[out] outBsBBuf   \parblock The output byte buffer to be filled by the server.
+///                         Can be passed \p NULL if no output stream is expected. \endparblock
+///
+/// \return An integer indicating success or failure.
+/// \retval >=0 On success.
+/// \retval  <0 On failure.
+///
+/// \since 4.3.5
+int procApiRequest_raw(rcComm_t* conn,
+                       int apiNumber,
+                       const struct PackingInstruction* packingInstructionTable,
+                       const char* inputPackingInstruction,
+                       const void* inputStruct,
+                       const bytesBuf_t* inputBsBBuf,
+                       const char* outputPackingInstruction,
+                       void** outStruct,
+                       bytesBuf_t* outBsBBuf);
 
 int sendApiRequest(rcComm_t *conn,
                    int apiInx,
@@ -48,7 +117,7 @@ int _cliGetCollOprStat(rcComm_t *conn, collOprStat_t **collOprStat);
 int apiTableLookup(int apiNumber);
 
 #ifdef __cplusplus
-}
+} // extern "C"
 #endif
 
-#endif	// PROC_API_REQUEST_H__
+#endif // PROC_API_REQUEST_H
