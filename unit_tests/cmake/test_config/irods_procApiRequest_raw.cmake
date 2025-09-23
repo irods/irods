9c662e4269e22b14e1d8b29e2ae70381ee3be8ad
Kory Draughn <korydraughn@renci.org>
2025-09-23 00:09:33 -0400

    squash. update cmake for unit test

--- unit_tests/cmake/test_config/irods_procApiRequest_raw.cmake
+++ unit_tests/cmake/test_config/irods_procApiRequest_raw.cmake
@@ -1,6 +1,7 @@
 set(IRODS_TEST_TARGET irods_procApiRequest_raw)
 
-set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/test_procApiRequest_raw.cpp)
+set(IRODS_TEST_SOURCE_FILES ${CMAKE_CURRENT_SOURCE_DIR}/src/main.cpp
+                            ${CMAKE_CURRENT_SOURCE_DIR}/src/test_procApiRequest_raw.cpp)
 
 set(IRODS_TEST_INCLUDE_PATH ${IRODS_EXTERNALS_FULLPATH_BOOST}/include)
  
