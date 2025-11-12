4fe91a2afc77da3b83a02fdffc7d20237a0da678
Markus Kitsinger (SwooshyCueb) <root@swooshalicio.us>
2025-11-12 15:33:49 -0600

    [#8740] Do not cross lib->server boundary unconditionally

--- lib/api/include/irods/chkNVPathPerm.h
+++ lib/api/include/irods/chkNVPathPerm.h
@@ -1,8 +1,12 @@
 #ifndef CHK_NV_PATH_PERM_H__
 #define CHK_NV_PATH_PERM_H__
 
+// Remove in iRODS 6.0
+#if __has_include("irods/rodsConnect.h")
+#  include "irods/rodsConnect.h"
+#endif
+
 #include "irods/fileOpen.h"
-#include "irods/rodsConnect.h"
 #include "irods/rcConnect.h"
 
 #ifdef __cplusplus
