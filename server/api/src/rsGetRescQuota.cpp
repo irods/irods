5bf091cee11a09d6fbe1dc42d56b8e5c9329b816
Derek D <ddong@renci.org>
2025-12-03 16:00:13 +0000

    [8758] Refactor quota query so quotas are properly processed
    Previously, the quota system did not function properly due to several
    misconceptions. The queries for fetching quota rows returned them
    in most-severe order, but the linked list created from the rows
    reversed it, so quota code that only read the head of the list only
    processed the least-severe quota. As a result, after rectifying the
    queries, the two-pass quota check in rsGetRescQuota is no longer
    needed as a single query properly handles both total and resource
    quotas.

--- server/api/src/rsGetRescQuota.cpp
+++ server/api/src/rsGetRescQuota.cpp
@@ -217,10 +217,7 @@ int setRescQuota(
     }
 
     rodsLong_t resc_overrun = 0;
-    ret = irods::get_resource_property<rodsLong_t>(
-                           resc_id,
-                           irods::RESOURCE_QUOTA_OVERRUN,
-                           resc_overrun );
+    ret = irods::get_resource_property<rodsLong_t>(resc_id, irods::RESOURCE_QUOTA_OVERRUN, resc_overrun);
 
     // fetch a global quota for the user if it exists
     getRescQuotaInp_t get_resc_quota_inp;
@@ -235,23 +232,6 @@ int setRescQuota(
         _comm->clientUser.userName,
         _comm->clientUser.rodsZone );
 
-    rescQuota_t *total_quota = NULL;
-    status = rsGetRescQuota(
-                 _comm,
-                 &get_resc_quota_inp,
-                 &total_quota );
-    if ( status >= 0 && total_quota ) {
-        rodsLong_t compare_overrun = total_quota->quotaOverrun;
-        freeAllRescQuota(total_quota);
-        if ( compare_overrun != 0 && // disabled if 0
-             compare_overrun + resc_overrun + _data_size > 0 ) {
-            return SYS_RESC_QUOTA_EXCEEDED;
-        }
-    } else {
-        log_api::debug("{} rsGetRescQuota for total quota - status {}", __FUNCTION__, status);
-    }
-
-    // if no global enforcement was successful try per resource
     rstrcpy(
         get_resc_quota_inp.rescName,
         _resc_name,
@@ -267,17 +247,7 @@ int setRescQuota(
     }
 
     rodsLong_t compare_overrun = resc_quota->quotaOverrun;
-    rescQuota_t* iterator = resc_quota;
 
-    // Traverse the quota result linked list and find the
-    // "most egregious" overrun: i.e., if there are
-    // two groups with quotas on the same resource, find
-    // the one most in violation.
-    while (iterator = iterator->next) {
-        if (compare_overrun < iterator->quotaOverrun) {
-            compare_overrun = iterator->quotaOverrun;
-        }
-    }
     freeAllRescQuota(resc_quota);
     if ( compare_overrun != 0 && // disabled if 0
          compare_overrun + resc_overrun + _data_size > 0 ) {
