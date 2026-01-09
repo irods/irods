b63de3c22ab020778f84ed84f6a6a6dad772d516
Kory Draughn <korydraughn@renci.org>
2026-01-08 19:43:54 -0500

    [irods/irods#8793] iget: Clear input struct to avoid segfault

--- src/iget.cpp
+++ src/iget.cpp
@@ -24,7 +24,6 @@ main( int argc, char **argv ) {
     rErrMsg_t errMsg;
     rcComm_t *conn;
     rodsArguments_t myRodsArgs;
-    rodsPathInp_t rodsPathInp;
     int reconnFlag;
 
 
@@ -35,6 +34,7 @@ main( int argc, char **argv ) {
         return 1;
     }
 
+    rodsPathInp_t rodsPathInp{};
     const auto free_rodsPathInp = irods::at_scope_exit{[&rodsPathInp] { freeRodsPathInpMembers(&rodsPathInp); }};
     int p_err = parse_opts_and_paths(
                     argc,
