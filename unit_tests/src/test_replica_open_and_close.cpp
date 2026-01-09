d5db1e022d1e38b67de64dc3c5a1fb62d014724e
Alan King <alanking@renci.org>
2026-01-09 16:16:23 -0500

    [#7128] Add unit tests for replica_open with no write
    The mtime will be updated whenever the replica is truncated, but it
    will not update whenever the replica is not truncated, regardless of
    the fact that the replica was not written to (just opened and closed).
    Uses replica_open and replica_close.

--- unit_tests/src/test_replica_open_and_close.cpp
+++ unit_tests/src/test_replica_open_and_close.cpp
@@ -1,21 +1,27 @@
 #include <catch2/catch.hpp>
 
-#include "irods/rodsClient.h"
 #include "irods/client_connection.hpp"
 #include "irods/connection_pool.hpp"
-#include "irods/filesystem.hpp"
-#include "irods/replica_open.h"
-#include "irods/replica_close.h"
 #include "irods/dataObjWrite.h"
-#include "irods/irods_query.hpp"
+#include "irods/dstream.hpp"
+#include "irods/filesystem.hpp"
 #include "irods/irods_at_scope_exit.hpp"
+#include "irods/irods_query.hpp"
 #include "irods/key_value_proxy.hpp"
 #include "irods/modDataObjMeta.h"
+#include "irods/replica.hpp"
+#include "irods/replica_close.h"
+#include "irods/replica_open.h"
+#include "irods/replica_proxy.hpp"
+#include "irods/rodsClient.h"
+#include "irods/transport/default_transport.hpp"
 
 #include <fmt/format.h>
 #include <nlohmann/json.hpp>
 
+#include <chrono>
 #include <string_view>
+#include <thread>
 
 TEST_CASE("replica_open and replica_close")
 {
@@ -187,6 +193,247 @@ TEST_CASE("replica_open and replica_close")
 
             for (auto&& row : irods::query{conn_ptr, gql}) {
                 REQUIRE_FALSE(row[0].empty());
+            }
+        }
+    }
+}
+
+TEST_CASE("test mtime changes on open and close replica without writing to it")
+{
+    namespace fs = irods::experimental::filesystem;
+    namespace replica = irods::experimental::replica;
+    using json = nlohmann::json;
+    using namespace std::chrono_literals;
+
+    load_client_api_plugins();
+
+    rodsEnv env;
+    _getRodsEnv(env);
+
+    auto conn_pool = irods::make_connection_pool();
+    auto conn = conn_pool->get_connection();
+    auto* comm = static_cast<RcComm*>(conn);
+    const auto sandbox = fs::path{static_cast<char*>(env.rodsHome)} / "unit_testing_sandbox";
+
+    if (!fs::client::exists(conn, sandbox)) {
+        REQUIRE(fs::client::create_collection(conn, sandbox));
+    }
+
+    const irods::at_scope_exit remove_sandbox{
+        [&conn, &sandbox] { REQUIRE(fs::client::remove_all(conn, sandbox, fs::remove_options::no_trash)); }};
+
+    const auto path = sandbox / "test_data_object";
+
+    // Ensure that the replica is unlocked so that it can be removed. This must happen before remove_sandbox above.
+    const auto unlock_replica = irods::at_scope_exit{[&] {
+        ModDataObjMetaInp mod_meta_inp{};
+
+        DataObjInfo info{};
+        std::strncpy(static_cast<char*>(info.objPath), path.c_str(), MAX_NAME_LEN - 1);
+        info.replNum = 0;
+
+        auto [kvp, lm] = irods::experimental::make_key_value_proxy();
+        kvp[REPL_STATUS_KW] = std::to_string(STALE_REPLICA);
+        kvp[ADMIN_KW] = "";
+
+        mod_meta_inp.regParam = kvp.get();
+        mod_meta_inp.dataObjInfo = &info;
+
+        REQUIRE(0 == rcModDataObjMeta(comm, &mod_meta_inp));
+    }};
+
+    // Create an empty data object.
+    REQUIRE(!fs::client::exists(conn, path));
+    {
+        irods::experimental::io::client::native_transport transport{conn};
+        irods::experimental::io::odstream{transport, path} << "";
+    }
+
+    // Get the original mtime so we have something against which to compare.
+    const auto original_mtime = replica::last_write_time(*comm, path, 0);
+
+    // Sleep for a second so mtime has the possibility to be updated on the next open.
+    std::this_thread::sleep_for(1s);
+
+    dataObjInp_t input{};
+    std::strncpy(static_cast<char*>(input.objPath), path.c_str(), MAX_NAME_LEN - 1);
+
+    char* json_output = nullptr;
+
+    SECTION("O_WRONLY alone does not update mtime")
+    {
+        // Open the replica with only O_WRONLY flag. Specify that we are opening for write only (no truncate).
+        input.openFlags = O_WRONLY;
+        // NOLINTNEXTLINE(readability-identifier-length)
+        const auto fd = rc_replica_open(comm, &input, &json_output);
+        REQUIRE(fd >= 3);
+
+        // Get the mtime just after open so we have something against which to compare.
+        const auto post_open_mtime = replica::last_write_time(*comm, path, 0);
+
+        // mtime should not be updated on open because replica was not truncated.
+        REQUIRE(original_mtime == post_open_mtime);
+
+        // Sleep for a second so mtime has the possibility to be updated on close.
+        std::this_thread::sleep_for(1s);
+
+        SECTION("do update status")
+        {
+            SECTION("do update size")
+            {
+                SECTION("do compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", true}, {"compute_checksum", true}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(original_mtime == replica::last_write_time(*comm, path, 0));
+                }
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", true}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+            SECTION("do not update size")
+            {
+                SECTION("do compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", false}, {"compute_checksum", true}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", false}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+        }
+        SECTION("do not update status")
+        {
+            SECTION("do update size")
+            {
+                // "do compute checksum" is not compatible with "do not update status", so the mtime assertions are
+                // immaterial. So, the test case is omitted here.
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", false}, {"update_size", true}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+            SECTION("do not update size")
+            {
+                // "do compute checksum" is not compatible with "do not update status", so the mtime assertions are
+                // immaterial. So, the test case is omitted here.
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", false}, {"update_size", false}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+        }
+    }
+
+    SECTION("O_WRONLY with O_TRUNC updates mtime")
+    {
+        // Open replica with O_WRONLY and O_TRUNC flags so that the replica gets truncated.
+        input.openFlags = O_WRONLY | O_TRUNC;
+        // NOLINTNEXTLINE(readability-identifier-length)
+        const auto fd = rc_replica_open(comm, &input, &json_output);
+        REQUIRE(fd >= 3);
+
+        // Get the mtime just after open so we have something against which to compare.
+        const auto post_open_mtime = replica::last_write_time(*comm, path, 0);
+
+        // mtime should be updated on open because replica was truncated.
+        REQUIRE(original_mtime < post_open_mtime);
+
+        // Sleep for a second so mtime has the possibility to be updated on close.
+        std::this_thread::sleep_for(1s);
+
+        SECTION("do update status")
+        {
+            SECTION("do update size")
+            {
+                SECTION("do compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", true}, {"compute_checksum", true}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", true}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+            SECTION("do not update size")
+            {
+                SECTION("do compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", false}, {"compute_checksum", true}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", true}, {"update_size", false}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+        }
+        SECTION("do not update status")
+        {
+            SECTION("do update size")
+            {
+                // "do compute checksum" is not compatible with "do not update status", so the mtime assertions are
+                // immaterial. So, the test case is omitted here.
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", false}, {"update_size", true}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
+            }
+            SECTION("do not update size")
+            {
+                // "do compute checksum" is not compatible with "do not update status", so the mtime assertions are
+                // immaterial. So, the test case is omitted here.
+                SECTION("do not compute checksum")
+                {
+                    const auto close_input =
+                        json{{"fd", fd}, {"update_status", false}, {"update_size", false}, {"compute_checksum", false}}
+                            .dump();
+                    REQUIRE(rc_replica_close(comm, close_input.c_str()) == 0);
+                    CHECK(post_open_mtime == replica::last_write_time(*comm, path, 0));
+                }
             }
         }
     }
