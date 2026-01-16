58eb54801c494e6812a8d6f8f8ddf3c0d333f8ee
Alan King <alanking@renci.org>
2026-01-16 15:50:10 -0500

    [#5763] Honor logical locking in reg/unreg APIs
    This commit adds logical locking to UnregDataObj and RegReplica so that
    clients cannot modify locked objects. The server may internally bypass
    logical locking for these operations as they are instrumental in recovering
    from errors and creating new replicas on locked objects as part of other
    operations such as replication.

--- lib/api/include/irods/regDataObj.h
+++ lib/api/include/irods/regDataObj.h
@@ -1,23 +1,26 @@
-#ifndef REG_DATA_OBJ_H__
-#define REG_DATA_OBJ_H__
+#ifndef IRODS_REG_DATA_OBJ_H
+#define IRODS_REG_DATA_OBJ_H
 
 #include "irods/rcConnect.h"
 #include "irods/objInfo.h"
 
-
-/* rcRegDataObj - Register a iRODS dataObject.
- * Input -
- *   rcComm_t *conn - The client connection handle.
- *   dataObjInfo_t *dataObjInfo - the dataObjInfo to register
- *   dataObjInfo_t **outDataObjInfo - the output DataObjInfo which has
- *      the dataId from the registration as output
- *
- * OutPut -
- *   int status - status of the operation.
- */
 #ifdef __cplusplus
-extern "C"
+extern "C" {
 #endif
+
+/// Register a new data object in the iRODS catalog.
+///
+/// \param[in] conn A pointer to \p RcComm.
+/// \param[in] dataObjInfo \p DataObjInfo with the information to use in the new catalog entry.
+/// \param[out] outDataObjInfo \p DataObjInfo with the data ID generated for the registration.
+///
+/// \return An integer representing an iRODS error code.
+/// \retval 0 on success.
+/// \retval <0 on failure.
 int rcRegDataObj( rcComm_t *conn, dataObjInfo_t *dataObjInfo, dataObjInfo_t **outDataObjInfo );
 
+#ifdef __cplusplus
+} // extern "C"
 #endif
+
+#endif // IRODS_REG_DATA_OBJ_H
