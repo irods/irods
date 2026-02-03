d9b52eeb7a0872e1b7427f79aa004f929e2ca2fc
Alan King <alanking@renci.org>
2026-02-03 12:51:50 -0500

    [#8307] data_object_modify_info API: Check for null inputs

--- lib/api/src/rc_data_object_modify_info.cpp
+++ lib/api/src/rc_data_object_modify_info.cpp
@@ -6,6 +6,10 @@
 
 auto rc_data_object_modify_info(RcComm* _comm, ModDataObjMetaInp* _input) -> int
 {
+    if (nullptr == _comm || nullptr == _input) {
+        return INVALID_INPUT_ARGUMENT_NULL_POINTER;
+    }
+
     return _input ? procApiRequest(_comm, DATA_OBJECT_MODIFY_INFO_APN, _input, nullptr, nullptr, nullptr)
                   : SYS_INVALID_INPUT_PARAM;
 }
