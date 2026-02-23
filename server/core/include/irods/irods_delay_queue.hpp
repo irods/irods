c8c7f18791064dd42ebd10edbfd72c0104e4d607
Kory Draughn <korydraughn@renci.org>
2026-02-23 13:12:48 -0500

    [#8868] Format irods_delay_queue.hpp

--- server/core/include/irods/irods_delay_queue.hpp
+++ server/core/include/irods/irods_delay_queue.hpp
@@ -23,7 +23,7 @@ namespace irods
 {
     class delay_queue // NOLINT(cppcoreguidelines-special-member-functions)
     {
-    public:
+      public:
         explicit delay_queue(std::int64_t _pool_size_in_bytes)
         {
             namespace bpmr = boost::container::pmr;
@@ -53,8 +53,9 @@ namespace irods
         bool contains_rule_id(const std::string& _rule_id)
         {
             std::lock_guard rules_lock{rules_mutex_};
-            return std::any_of(queued_rules_->begin(), queued_rules_->end(),
-                       [&](const auto& q_rule) { return q_rule.data() == _rule_id; });
+            return std::any_of(queued_rules_->begin(), queued_rules_->end(), [&](const auto& q_rule) {
+                return q_rule.data() == _rule_id;
+            });
         }
 
         void enqueue_rule(const std::string& rule_id)
@@ -74,7 +75,7 @@ namespace irods
             }
         }
 
-    private:
+      private:
         std::mutex rules_mutex_;
 #if BOOST_VERSION >= 107200
         std::vector<std::byte> buffer_;
@@ -85,4 +86,3 @@ namespace irods
 } // namespace irods
 
 #endif // IRODS_DELAY_QUEUE_HPP
-
